deafb4e74a5dab296a32c1fedd597a863fbc9cf4
Brendan Hay <brendan.g.hay@gmail.com>
2015-09-22 13:07:41 +0200

    Initialising repository

---
+++ share/library.mk
@@ -0,0 +1,11 @@
+SHELL   := /usr/bin/env bash
+NAME    ?= $(notdir $(CURDIR:a/%=%))
+VERSION ?= $(shell sed -n 's/^version: *\(.*\)$$/\1/p' $(NAME).cabal)
+
+default:
+
+sdist:
+	cabal sdist
+
+upload:
+	cabal upload dist/$(NAME)-$(VERSION).tar.gz
