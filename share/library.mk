b52b14b215fb219bfe379c96d1d9a52142f28a66
Brendan Hay <brendan.g.hay@gmail.com>
2015-09-28 20:41:03 +0200

    Candidate upload

--- share/library.mk
+++ share/library.mk
@@ -2,17 +2,20 @@ SHELL   := /usr/bin/env bash
 NAME    ?= $(notdir $(CURDIR:a/%=%))
 VERSION ?= $(shell sed -n 's/^version: *\(.*\)$$/\1/p' $(NAME).cabal)
 
+CANDIDATE = $(realpath dist/$(NAME)-$(VERSION).tar.gz)
+
 default:
 
 sdist:
 	cabal sdist
 
 candidate:
-	curl \
- -i -X POST \
- -H "Content-Type: multipart/form-data"
- "file=@dist/$(NAME)-$(VERSION).tar.gz"
+	curl -X POST -H 'Content-Type: multipart/form-data' \
+ --form package=@$(CANDIDATE) \
+ --form press=submit \
  http://hackage.haskell.org/packages/candidates/
 
 upload:
 	@echo "noop"
+
+
