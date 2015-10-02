bec6ff8a35fc9caf59eb4e3e2d62a5bc78443f31
Brendan Hay <brendan.g.hay@gmail.com>
2015-10-02 10:02:27 +0200

    Adding placeholder top-level export

---
+++ gogol/src/Network/Google.hs
@@ -0,0 +1,11 @@
+-- |
+-- Module      : Network.Google
+-- Copyright   : (c) 2015 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : provisional
+-- Portability : non-portable (GHC extensions)
+--
+module Network.Google where
+
+import           Network.Google.Prelude ()
