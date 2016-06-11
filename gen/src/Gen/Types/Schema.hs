e437f32e578290517fe03339187be268b50a72f5
Brendan Hay <brendan.g.hay@gmail.com>
2016-06-11 08:20:43 +0200

    Adding support for google-datetime literal type

--- gen/src/Gen/Types/Schema.hs
+++ gen/src/Gen/Types/Schema.hs
@@ -210,22 +210,23 @@ instance FromJSON Lit where
         t <- o .: "format" <|> o .: "type"
         case t of
             -- Types
-            "string"    -> pure Text
-            "boolean"   -> pure Bool
+            "string"          -> pure Text
+            "boolean"         -> pure Bool
 
             -- Formats
-            "float"     -> pure Float
-            "double"    -> pure Double
-            "byte"      -> pure Byte
-            "uint32"    -> pure UInt32
-            "uint64"    -> pure UInt64
-            "int32"     -> pure Int32
-            "int64"     -> pure Int64
-            "time"      -> pure Time
-            "date"      -> pure Date
-            "date-time" -> pure DateTime
+            "float"           -> pure Float
+            "double"          -> pure Double
+            "byte"            -> pure Byte
+            "uint32"          -> pure UInt32
+            "uint64"          -> pure UInt64
+            "int32"           -> pure Int32
+            "int64"           -> pure Int64
+            "time"            -> pure Time
+            "date"            -> pure Date
+            "google-datetime" -> pure DateTime
+            "date-time"       -> pure DateTime
 
-            _           -> fail $
+            _                 -> fail $
                 "Unable to parse Literal from: " ++ Text.unpack t
 
 newtype Enm = Enm { _eValues :: [(Text, Help)] }
