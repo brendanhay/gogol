904153abc833d8c5861d2ab612ea4103ea836c0d
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-17 09:37:16 +0100

    script: auto-format cabal files

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
+            haskellPackages.cabal-fmt
             nixpkgs-fmt
             ormolu
             shellcheck
