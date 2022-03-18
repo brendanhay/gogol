4208308291c3e3ec34762a06b1290e6c7f569c08
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 11:23:15 +0100

    gogol-streetviewpublish: regenerating service

---
+++ gogol-streetviewpublish/gen/Network/Google/StreetViewPublish/Photos/BatchDelete.hs
@@ -0,0 +1,120 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Network.Google.StreetViewPublish.Photos.BatchDelete
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Deletes a list of Photos and their metadata. Note that if BatchDeletePhotos fails, either critical fields are missing or there is an authentication error. Even if BatchDeletePhotos succeeds, individual photos in the batch may have failures. These failures are specified in each PhotoResponse.status in BatchDeletePhotosResponse.results. See DeletePhoto for specific failures that can occur per photo.
+--
+-- /See:/ <https://developers.google.com/streetview/publish/ Street View Publish API Reference> for @streetviewpublish.photos.batchDelete@.
+module Network.Google.StreetViewPublish.Photos.BatchDelete
+  ( -- * Resource
+    StreetViewPublishPhotosBatchDeleteResource,
+
+    -- ** Constructing a Request
+    newStreetViewPublishPhotosBatchDelete,
+    StreetViewPublishPhotosBatchDelete,
+  )
+where
+
+import qualified Network.Google.Prelude as Core
+import Network.Google.StreetViewPublish.Types
+
+-- | A resource alias for @streetviewpublish.photos.batchDelete@ method which the
+-- 'StreetViewPublishPhotosBatchDelete' request conforms to.
+type StreetViewPublishPhotosBatchDeleteResource =
+  "v1"
+    Core.:> "photos:batchDelete"
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.ReqBody '[Core.JSON] BatchDeletePhotosRequest
+    Core.:> Core.Post '[Core.JSON] BatchDeletePhotosResponse
+
+-- | Deletes a list of Photos and their metadata. Note that if BatchDeletePhotos fails, either critical fields are missing or there is an authentication error. Even if BatchDeletePhotos succeeds, individual photos in the batch may have failures. These failures are specified in each PhotoResponse.status in BatchDeletePhotosResponse.results. See DeletePhoto for specific failures that can occur per photo.
+--
+-- /See:/ 'newStreetViewPublishPhotosBatchDelete' smart constructor.
+data StreetViewPublishPhotosBatchDelete = StreetViewPublishPhotosBatchDelete
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | Multipart request metadata.
+    payload :: BatchDeletePhotosRequest,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'StreetViewPublishPhotosBatchDelete' with the minimum fields required to make a request.
+newStreetViewPublishPhotosBatchDelete ::
+  -- |  Multipart request metadata. See 'payload'.
+  BatchDeletePhotosRequest ->
+  StreetViewPublishPhotosBatchDelete
+newStreetViewPublishPhotosBatchDelete payload =
+  StreetViewPublishPhotosBatchDelete
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      payload = payload,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    StreetViewPublishPhotosBatchDelete
+  where
+  type
+    Rs StreetViewPublishPhotosBatchDelete =
+      BatchDeletePhotosResponse
+  type
+    Scopes StreetViewPublishPhotosBatchDelete =
+      '["https://www.googleapis.com/auth/streetviewpublish"]
+  requestClient StreetViewPublishPhotosBatchDelete {..} =
+    go
+      xgafv
+      accessToken
+      callback
+      uploadType
+      uploadProtocol
+      (Core.Just Core.AltJSON)
+      payload
+      streetViewPublishService
+    where
+      go =
+        Core.buildClient
+          ( Core.Proxy ::
+              Core.Proxy
+                StreetViewPublishPhotosBatchDeleteResource
+          )
+          Core.mempty
---
+++ gogol-streetviewpublish/gen/Network/Google/StreetViewPublish/Photos/BatchUpdate.hs
@@ -0,0 +1,120 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Network.Google.StreetViewPublish.Photos.BatchUpdate
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Updates the metadata of Photos, such as pose, place association, connections, etc. Changing the pixels of photos is not supported. Note that if BatchUpdatePhotos fails, either critical fields are missing or there is an authentication error. Even if BatchUpdatePhotos succeeds, individual photos in the batch may have failures. These failures are specified in each PhotoResponse.status in BatchUpdatePhotosResponse.results. See UpdatePhoto for specific failures that can occur per photo. Only the fields specified in updateMask field are used. If @updateMask@ is not present, the update applies to all fields. The number of UpdatePhotoRequest messages in a BatchUpdatePhotosRequest must not exceed 20. > Note: To update Pose.altitude, Pose.latLngPair has to be filled as well. Otherwise, the request will fail.
+--
+-- /See:/ <https://developers.google.com/streetview/publish/ Street View Publish API Reference> for @streetviewpublish.photos.batchUpdate@.
+module Network.Google.StreetViewPublish.Photos.BatchUpdate
+  ( -- * Resource
+    StreetViewPublishPhotosBatchUpdateResource,
+
+    -- ** Constructing a Request
+    newStreetViewPublishPhotosBatchUpdate,
+    StreetViewPublishPhotosBatchUpdate,
+  )
+where
+
+import qualified Network.Google.Prelude as Core
+import Network.Google.StreetViewPublish.Types
+
+-- | A resource alias for @streetviewpublish.photos.batchUpdate@ method which the
+-- 'StreetViewPublishPhotosBatchUpdate' request conforms to.
+type StreetViewPublishPhotosBatchUpdateResource =
+  "v1"
+    Core.:> "photos:batchUpdate"
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.ReqBody '[Core.JSON] BatchUpdatePhotosRequest
+    Core.:> Core.Post '[Core.JSON] BatchUpdatePhotosResponse
+
+-- | Updates the metadata of Photos, such as pose, place association, connections, etc. Changing the pixels of photos is not supported. Note that if BatchUpdatePhotos fails, either critical fields are missing or there is an authentication error. Even if BatchUpdatePhotos succeeds, individual photos in the batch may have failures. These failures are specified in each PhotoResponse.status in BatchUpdatePhotosResponse.results. See UpdatePhoto for specific failures that can occur per photo. Only the fields specified in updateMask field are used. If @updateMask@ is not present, the update applies to all fields. The number of UpdatePhotoRequest messages in a BatchUpdatePhotosRequest must not exceed 20. > Note: To update Pose.altitude, Pose.latLngPair has to be filled as well. Otherwise, the request will fail.
+--
+-- /See:/ 'newStreetViewPublishPhotosBatchUpdate' smart constructor.
+data StreetViewPublishPhotosBatchUpdate = StreetViewPublishPhotosBatchUpdate
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | Multipart request metadata.
+    payload :: BatchUpdatePhotosRequest,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'StreetViewPublishPhotosBatchUpdate' with the minimum fields required to make a request.
+newStreetViewPublishPhotosBatchUpdate ::
+  -- |  Multipart request metadata. See 'payload'.
+  BatchUpdatePhotosRequest ->
+  StreetViewPublishPhotosBatchUpdate
+newStreetViewPublishPhotosBatchUpdate payload =
+  StreetViewPublishPhotosBatchUpdate
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      payload = payload,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    StreetViewPublishPhotosBatchUpdate
+  where
+  type
+    Rs StreetViewPublishPhotosBatchUpdate =
+      BatchUpdatePhotosResponse
+  type
+    Scopes StreetViewPublishPhotosBatchUpdate =
+      '["https://www.googleapis.com/auth/streetviewpublish"]
+  requestClient StreetViewPublishPhotosBatchUpdate {..} =
+    go
+      xgafv
+      accessToken
+      callback
+      uploadType
+      uploadProtocol
+      (Core.Just Core.AltJSON)
+      payload
+      streetViewPublishService
+    where
+      go =
+        Core.buildClient
+          ( Core.Proxy ::
+              Core.Proxy
+                StreetViewPublishPhotosBatchUpdateResource
+          )
+          Core.mempty
