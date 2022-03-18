e41d7249d812064913de57db3e65c727ffd0481b
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:05 +0100

    gogol-genomics: regenerating service with updated namespace

--- lib/services/gogol-genomics/gen/Gogol/Genomics/Projects/Operations/Get.hs
+++ lib/services/gogol-genomics/gen/Gogol/Genomics/Projects/Operations/Get.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,89 +36,81 @@
 --
 -- /See:/ <https://cloud.google.com/genomics Genomics API Reference> for @genomics.projects.operations.get@.
 module Gogol.Genomics.Projects.Operations.Get
-  ( -- * Resource
-    GenomicsProjectsOperationsGetResource,
+    (
+    -- * Resource
+      GenomicsProjectsOperationsGetResource
 
     -- ** Constructing a Request
-    newGenomicsProjectsOperationsGet,
-    GenomicsProjectsOperationsGet,
-  )
-where
+    , newGenomicsProjectsOperationsGet
+    , GenomicsProjectsOperationsGet
+    ) where
 
-import Gogol.Genomics.Types
 import qualified Gogol.Prelude as Core
+import Gogol.Genomics.Types
 
 -- | A resource alias for @genomics.projects.operations.get@ method which the
 -- 'GenomicsProjectsOperationsGet' request conforms to.
 type GenomicsProjectsOperationsGetResource =
-  "v2alpha1"
-    Core.:> Core.Capture "name" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] Operation
+     "v2alpha1" Core.:>
+       Core.Capture "name" Core.Text Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "uploadType" Core.Text Core.:>
+                 Core.QueryParam "upload_protocol" Core.Text Core.:>
+                   Core.QueryParam "alt" Core.AltJSON Core.:>
+                     Core.Get '[Core.JSON] Operation
 
 -- | Gets the latest state of a long-running operation. Clients can use this method to poll the operation result at intervals as recommended by the API service. Authorization requires the following <https://cloud.google.com/iam Google IAM> permission: * @genomics.operations.get@
 --
 -- /See:/ 'newGenomicsProjectsOperationsGet' smart constructor.
 data GenomicsProjectsOperationsGet = GenomicsProjectsOperationsGet
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | The name of the operation resource.
-    name :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | The name of the operation resource.
+    , name :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'GenomicsProjectsOperationsGet' with the minimum fields required to make a request.
-newGenomicsProjectsOperationsGet ::
-  -- |  The name of the operation resource. See 'name'.
-  Core.Text ->
-  GenomicsProjectsOperationsGet
+newGenomicsProjectsOperationsGet 
+    ::  Core.Text
+       -- ^  The name of the operation resource. See 'name'.
+    -> GenomicsProjectsOperationsGet
 newGenomicsProjectsOperationsGet name =
   GenomicsProjectsOperationsGet
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      name = name,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , name = name
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    GenomicsProjectsOperationsGet
-  where
-  type Rs GenomicsProjectsOperationsGet = Operation
-  type
-    Scopes GenomicsProjectsOperationsGet =
-      '[ "https://www.googleapis.com/auth/cloud-platform",
-         "https://www.googleapis.com/auth/genomics"
-       ]
-  requestClient GenomicsProjectsOperationsGet {..} =
-    go
-      name
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      genomicsService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy GenomicsProjectsOperationsGetResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           GenomicsProjectsOperationsGet
+         where
+        type Rs GenomicsProjectsOperationsGet = Operation
+        type Scopes GenomicsProjectsOperationsGet =
+             '["https://www.googleapis.com/auth/cloud-platform",
+               "https://www.googleapis.com/auth/genomics"]
+        requestClient GenomicsProjectsOperationsGet{..}
+          = go name xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              genomicsService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy GenomicsProjectsOperationsGetResource)
+                      Core.mempty
+
