0e124f3c9e76b76173f02e71e94a3cd01fcb82e1
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:11 +0100

    gogol-siteverification: regenerating service with updated namespace

--- lib/services/gogol-siteverification/gen/Gogol/SiteVerification/WebResource/Insert.hs
+++ lib/services/gogol-siteverification/gen/Gogol/SiteVerification/WebResource/Insert.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://developers.google.com/site-verification/ Google Site Verification API Reference> for @siteVerification.webResource.insert@.
 module Gogol.SiteVerification.WebResource.Insert
-  ( -- * Resource
-    SiteVerificationWebResourceInsertResource,
+    (
+    -- * Resource
+      SiteVerificationWebResourceInsertResource
 
     -- ** Constructing a Request
-    newSiteVerificationWebResourceInsert,
-    SiteVerificationWebResourceInsert,
-  )
-where
+    , newSiteVerificationWebResourceInsert
+    , SiteVerificationWebResourceInsert
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.SiteVerification.Types
@@ -45,64 +51,57 @@ import Gogol.SiteVerification.Types
 -- | A resource alias for @siteVerification.webResource.insert@ method which the
 -- 'SiteVerificationWebResourceInsert' request conforms to.
 type SiteVerificationWebResourceInsertResource =
-  "siteVerification"
-    Core.:> "v1"
-    Core.:> "webResource"
-    Core.:> Core.QueryParam "verificationMethod" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody
-              '[Core.JSON]
-              SiteVerificationWebResourceResource
-    Core.:> Core.Post
-              '[Core.JSON]
-              SiteVerificationWebResourceResource
+     "siteVerification" Core.:>
+       "v1" Core.:>
+         "webResource" Core.:>
+           Core.QueryParam "verificationMethod" Core.Text
+             Core.:>
+             Core.QueryParam "alt" Core.AltJSON Core.:>
+               Core.ReqBody '[Core.JSON]
+                 SiteVerificationWebResourceResource
+                 Core.:>
+                 Core.Post '[Core.JSON]
+                   SiteVerificationWebResourceResource
 
 -- | Attempt verification of a website or domain.
 --
 -- /See:/ 'newSiteVerificationWebResourceInsert' smart constructor.
 data SiteVerificationWebResourceInsert = SiteVerificationWebResourceInsert
-  { -- | Multipart request metadata.
-    payload :: SiteVerificationWebResourceResource,
-    -- | The method to use for verifying a site or domain.
-    verificationMethod :: Core.Text
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | Multipart request metadata.
+      payload :: SiteVerificationWebResourceResource
+      -- | The method to use for verifying a site or domain.
+    , verificationMethod :: Core.Text
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'SiteVerificationWebResourceInsert' with the minimum fields required to make a request.
-newSiteVerificationWebResourceInsert ::
-  -- |  Multipart request metadata. See 'payload'.
-  SiteVerificationWebResourceResource ->
-  -- |  The method to use for verifying a site or domain. See 'verificationMethod'.
-  Core.Text ->
-  SiteVerificationWebResourceInsert
+newSiteVerificationWebResourceInsert 
+    ::  SiteVerificationWebResourceResource
+       -- ^  Multipart request metadata. See 'payload'.
+    -> Core.Text
+       -- ^  The method to use for verifying a site or domain. See 'verificationMethod'.
+    -> SiteVerificationWebResourceInsert
 newSiteVerificationWebResourceInsert payload verificationMethod =
   SiteVerificationWebResourceInsert
-    { payload = payload,
-      verificationMethod = verificationMethod
-    }
+    {payload = payload, verificationMethod = verificationMethod}
 
-instance
-  Core.GoogleRequest
-    SiteVerificationWebResourceInsert
-  where
-  type
-    Rs SiteVerificationWebResourceInsert =
-      SiteVerificationWebResourceResource
-  type
-    Scopes SiteVerificationWebResourceInsert =
-      '[ "https://www.googleapis.com/auth/siteverification",
-         "https://www.googleapis.com/auth/siteverification.verify_only"
-       ]
-  requestClient SiteVerificationWebResourceInsert {..} =
-    go
-      (Core.Just verificationMethod)
-      (Core.Just Core.AltJSON)
-      payload
-      siteVerificationService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy SiteVerificationWebResourceInsertResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           SiteVerificationWebResourceInsert
+         where
+        type Rs SiteVerificationWebResourceInsert =
+             SiteVerificationWebResourceResource
+        type Scopes SiteVerificationWebResourceInsert =
+             '["https://www.googleapis.com/auth/siteverification",
+               "https://www.googleapis.com/auth/siteverification.verify_only"]
+        requestClient SiteVerificationWebResourceInsert{..}
+          = go (Core.Just verificationMethod)
+              (Core.Just Core.AltJSON)
+              payload
+              siteVerificationService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy SiteVerificationWebResourceInsertResource)
+                      Core.mempty
+
