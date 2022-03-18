7ee83d8d5cce4c89705c965bc6e001547fde7f8d
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 11:23:04 +0100

    gogol-cloudscheduler: regenerating service

--- gogol-cloudscheduler/gen/Network/Google/CloudScheduler.hs
+++ gogol-cloudscheduler/gen/Network/Google/CloudScheduler.hs
@@ -1,15 +1,28 @@
-{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE TypeOperators     #-}
-
-{-# OPTIONS_GHC -fno-warn-unused-imports    #-}
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 -- |
 -- Module      : Network.Google.CloudScheduler
--- Copyright   : (c) 2015-2016 Brendan Hay
+-- Copyright   : (c) 2015-2022 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 --
@@ -17,250 +30,184 @@
 --
 -- /See:/ <https://cloud.google.com/scheduler/ Cloud Scheduler API Reference>
 module Network.Google.CloudScheduler
-    (
-    -- * Service Configuration
-      cloudSchedulerService
+  ( -- * Configuration
+    cloudSchedulerService,
 
     -- * OAuth Scopes
-    , cloudPlatformScope
-
-    -- * API Declaration
-    , CloudSchedulerAPI
+    cloudPlatformScope,
 
     -- * Resources
 
     -- ** cloudscheduler.projects.locations.get
-    , module Network.Google.Resource.CloudScheduler.Projects.Locations.Get
+    CloudSchedulerProjectsLocationsGetResource,
+    newCloudSchedulerProjectsLocationsGet,
+    CloudSchedulerProjectsLocationsGet,
 
     -- ** cloudscheduler.projects.locations.jobs.create
-    , module Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Create
+    CloudSchedulerProjectsLocationsJobsCreateResource,
+    newCloudSchedulerProjectsLocationsJobsCreate,
+    CloudSchedulerProjectsLocationsJobsCreate,
 
     -- ** cloudscheduler.projects.locations.jobs.delete
-    , module Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Delete
+    CloudSchedulerProjectsLocationsJobsDeleteResource,
+    newCloudSchedulerProjectsLocationsJobsDelete,
+    CloudSchedulerProjectsLocationsJobsDelete,
 
     -- ** cloudscheduler.projects.locations.jobs.get
-    , module Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Get
+    CloudSchedulerProjectsLocationsJobsGetResource,
+    newCloudSchedulerProjectsLocationsJobsGet,
+    CloudSchedulerProjectsLocationsJobsGet,
 
     -- ** cloudscheduler.projects.locations.jobs.list
-    , module Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.List
+    CloudSchedulerProjectsLocationsJobsListResource,
+    newCloudSchedulerProjectsLocationsJobsList,
+    CloudSchedulerProjectsLocationsJobsList,
 
     -- ** cloudscheduler.projects.locations.jobs.patch
-    , module Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Patch
+    CloudSchedulerProjectsLocationsJobsPatchResource,
+    newCloudSchedulerProjectsLocationsJobsPatch,
+    CloudSchedulerProjectsLocationsJobsPatch,
 
     -- ** cloudscheduler.projects.locations.jobs.pause
-    , module Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Pause
+    CloudSchedulerProjectsLocationsJobsPauseResource,
+    newCloudSchedulerProjectsLocationsJobsPause,
+    CloudSchedulerProjectsLocationsJobsPause,
 
     -- ** cloudscheduler.projects.locations.jobs.resume
-    , module Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Resume
+    CloudSchedulerProjectsLocationsJobsResumeResource,
+    newCloudSchedulerProjectsLocationsJobsResume,
+    CloudSchedulerProjectsLocationsJobsResume,
 
     -- ** cloudscheduler.projects.locations.jobs.run
-    , module Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Run
+    CloudSchedulerProjectsLocationsJobsRunResource,
+    newCloudSchedulerProjectsLocationsJobsRun,
+    CloudSchedulerProjectsLocationsJobsRun,
 
     -- ** cloudscheduler.projects.locations.list
-    , module Network.Google.Resource.CloudScheduler.Projects.Locations.List
+    CloudSchedulerProjectsLocationsListResource,
+    newCloudSchedulerProjectsLocationsList,
+    CloudSchedulerProjectsLocationsList,
 
     -- * Types
 
-    -- ** OAuthToken
-    , OAuthToken
-    , oAuthToken
-    , oatScope
-    , oatServiceAccountEmail
-
-    -- ** Status
-    , Status
-    , status
-    , sDetails
-    , sCode
-    , sMessage
-
-    -- ** PauseJobRequest
-    , PauseJobRequest
-    , pauseJobRequest
-
-    -- ** ListLocationsResponse
-    , ListLocationsResponse
-    , listLocationsResponse
-    , llrNextPageToken
-    , llrLocations
-
-    -- ** ResumeJobRequest
-    , ResumeJobRequest
-    , resumeJobRequest
-
-    -- ** OidcToken
-    , OidcToken
-    , oidcToken
-    , otAudience
-    , otServiceAccountEmail
-
-    -- ** AppEngineHTTPTargetHeaders
-    , AppEngineHTTPTargetHeaders
-    , appEngineHTTPTargetHeaders
-    , aehttpthAddtional
-
-    -- ** RetryConfig
-    , RetryConfig
-    , retryConfig
-    , rcMaxDoublings
-    , rcMaxRetryDuration
-    , rcMinBackoffDuration
-    , rcRetryCount
-    , rcMaxBackoffDuration
-
-    -- ** Location
-    , Location
-    , location
-    , lName
-    , lMetadata
-    , lDisplayName
-    , lLabels
-    , lLocationId
-
-    -- ** Empty
-    , Empty
-    , empty
-
-    -- ** PubsubTargetAttributes
-    , PubsubTargetAttributes
-    , pubsubTargetAttributes
-    , ptaAddtional
-
-    -- ** HTTPTargetHeaders
-    , HTTPTargetHeaders
-    , hTTPTargetHeaders
-    , httpthAddtional
-
-    -- ** PubsubMessage
-    , PubsubMessage
-    , pubsubMessage
-    , pmData
-    , pmPublishTime
-    , pmAttributes
-    , pmMessageId
-    , pmOrderingKey
-
-    -- ** JobState
-    , JobState (..)
-
-    -- ** StatusDetailsItem
-    , StatusDetailsItem
-    , statusDetailsItem
-    , sdiAddtional
-
-    -- ** AppEngineHTTPTarget
-    , AppEngineHTTPTarget
-    , appEngineHTTPTarget
-    , aehttptHTTPMethod
-    , aehttptRelativeURI
-    , aehttptBody
-    , aehttptHeaders
-    , aehttptAppEngineRouting
-
-    -- ** HTTPTarget
-    , HTTPTarget
-    , hTTPTarget
-    , httptOAuthToken
-    , httptHTTPMethod
-    , httptOidcToken
-    , httptBody
-    , httptURI
-    , httptHeaders
-
-    -- ** RunJobRequest
-    , RunJobRequest
-    , runJobRequest
-
-    -- ** Job
-    , Job
-    , job
-    , jStatus
-    , jAttemptDeadline
-    , jState
-    , jLastAttemptTime
-    , jRetryConfig
-    , jSchedule
-    , jScheduleTime
-    , jAppEngineHTTPTarget
-    , jHTTPTarget
-    , jName
-    , jPubsubTarget
-    , jUserUpdateTime
-    , jTimeZone
-    , jDescription
-
-    -- ** PubsubMessageAttributes
-    , PubsubMessageAttributes
-    , pubsubMessageAttributes
-    , pmaAddtional
-
-    -- ** PubsubTarget
-    , PubsubTarget
-    , pubsubTarget
-    , ptData
-    , ptTopicName
-    , ptAttributes
-
     -- ** Xgafv
-    , Xgafv (..)
+    Xgafv (..),
 
-    -- ** ListJobsResponse
-    , ListJobsResponse
-    , listJobsResponse
-    , ljrNextPageToken
-    , ljrJobs
+    -- ** AppEngineHttpTarget
+    AppEngineHttpTarget (..),
+    newAppEngineHttpTarget,
 
-    -- ** LocationLabels
-    , LocationLabels
-    , locationLabels
-    , llAddtional
+    -- ** AppEngineHttpTarget_Headers
+    AppEngineHttpTarget_Headers (..),
+    newAppEngineHttpTarget_Headers,
 
-    -- ** LocationMetadata
-    , LocationMetadata
-    , locationMetadata
-    , lmAddtional
+    -- ** AppEngineHttpTarget_HttpMethod
+    AppEngineHttpTarget_HttpMethod (..),
 
     -- ** AppEngineRouting
-    , AppEngineRouting
-    , appEngineRouting
-    , aerService
-    , aerVersion
-    , aerHost
-    , aerInstance
+    AppEngineRouting (..),
+    newAppEngineRouting,
 
-    -- ** HTTPTargetHTTPMethod
-    , HTTPTargetHTTPMethod (..)
+    -- ** Empty
+    Empty (..),
+    newEmpty,
 
-    -- ** AppEngineHTTPTargetHTTPMethod
-    , AppEngineHTTPTargetHTTPMethod (..)
-    ) where
+    -- ** HttpTarget
+    HttpTarget (..),
+    newHttpTarget,
 
-import Network.Google.Prelude
+    -- ** HttpTarget_Headers
+    HttpTarget_Headers (..),
+    newHttpTarget_Headers,
+
+    -- ** HttpTarget_HttpMethod
+    HttpTarget_HttpMethod (..),
+
+    -- ** Job
+    Job (..),
+    newJob,
+
+    -- ** Job_State
+    Job_State (..),
+
+    -- ** ListJobsResponse
+    ListJobsResponse (..),
+    newListJobsResponse,
+
+    -- ** ListLocationsResponse
+    ListLocationsResponse (..),
+    newListLocationsResponse,
+
+    -- ** Location
+    Location (..),
+    newLocation,
+
+    -- ** Location_Labels
+    Location_Labels (..),
+    newLocation_Labels,
+
+    -- ** Location_Metadata
+    Location_Metadata (..),
+    newLocation_Metadata,
+
+    -- ** OAuthToken
+    OAuthToken (..),
+    newOAuthToken,
+
+    -- ** OidcToken
+    OidcToken (..),
+    newOidcToken,
+
+    -- ** PauseJobRequest
+    PauseJobRequest (..),
+    newPauseJobRequest,
+
+    -- ** PubsubMessage
+    PubsubMessage (..),
+    newPubsubMessage,
+
+    -- ** PubsubMessage_Attributes
+    PubsubMessage_Attributes (..),
+    newPubsubMessage_Attributes,
+
+    -- ** PubsubTarget
+    PubsubTarget (..),
+    newPubsubTarget,
+
+    -- ** PubsubTarget_Attributes
+    PubsubTarget_Attributes (..),
+    newPubsubTarget_Attributes,
+
+    -- ** ResumeJobRequest
+    ResumeJobRequest (..),
+    newResumeJobRequest,
+
+    -- ** RetryConfig
+    RetryConfig (..),
+    newRetryConfig,
+
+    -- ** RunJobRequest
+    RunJobRequest (..),
+    newRunJobRequest,
+
+    -- ** Status
+    Status (..),
+    newStatus,
+
+    -- ** Status_DetailsItem
+    Status_DetailsItem (..),
+    newStatus_DetailsItem,
+  )
+where
+
+import Network.Google.CloudScheduler.Projects.Locations.Get
+import Network.Google.CloudScheduler.Projects.Locations.Jobs.Create
+import Network.Google.CloudScheduler.Projects.Locations.Jobs.Delete
+import Network.Google.CloudScheduler.Projects.Locations.Jobs.Get
+import Network.Google.CloudScheduler.Projects.Locations.Jobs.List
+import Network.Google.CloudScheduler.Projects.Locations.Jobs.Patch
+import Network.Google.CloudScheduler.Projects.Locations.Jobs.Pause
+import Network.Google.CloudScheduler.Projects.Locations.Jobs.Resume
+import Network.Google.CloudScheduler.Projects.Locations.Jobs.Run
+import Network.Google.CloudScheduler.Projects.Locations.List
 import Network.Google.CloudScheduler.Types
-import Network.Google.Resource.CloudScheduler.Projects.Locations.Get
-import Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Create
-import Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Delete
-import Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Get
-import Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.List
-import Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Patch
-import Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Pause
-import Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Resume
-import Network.Google.Resource.CloudScheduler.Projects.Locations.Jobs.Run
-import Network.Google.Resource.CloudScheduler.Projects.Locations.List
-
-{- $resources
-TODO
--}
-
--- | Represents the entirety of the methods and resources available for the Cloud Scheduler API service.
-type CloudSchedulerAPI =
-     ProjectsLocationsJobsListResource :<|>
-       ProjectsLocationsJobsPatchResource
-       :<|> ProjectsLocationsJobsGetResource
-       :<|> ProjectsLocationsJobsRunResource
-       :<|> ProjectsLocationsJobsCreateResource
-       :<|> ProjectsLocationsJobsPauseResource
-       :<|> ProjectsLocationsJobsDeleteResource
-       :<|> ProjectsLocationsJobsResumeResource
-       :<|> ProjectsLocationsListResource
-       :<|> ProjectsLocationsGetResource
---
+++ gogol-cloudscheduler/gen/Network/Google/CloudScheduler/Projects/Locations/Jobs/Pause.hs
@@ -0,0 +1,127 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Network.Google.CloudScheduler.Projects.Locations.Jobs.Pause
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Pauses a job. If a job is paused then the system will stop executing the job until it is re-enabled via ResumeJob. The state of the job is stored in state; if paused it will be set to Job.State.PAUSED. A job must be in Job.State.ENABLED to be paused.
+--
+-- /See:/ <https://cloud.google.com/scheduler/ Cloud Scheduler API Reference> for @cloudscheduler.projects.locations.jobs.pause@.
+module Network.Google.CloudScheduler.Projects.Locations.Jobs.Pause
+  ( -- * Resource
+    CloudSchedulerProjectsLocationsJobsPauseResource,
+
+    -- ** Constructing a Request
+    newCloudSchedulerProjectsLocationsJobsPause,
+    CloudSchedulerProjectsLocationsJobsPause,
+  )
+where
+
+import Network.Google.CloudScheduler.Types
+import qualified Network.Google.Prelude as Core
+
+-- | A resource alias for @cloudscheduler.projects.locations.jobs.pause@ method which the
+-- 'CloudSchedulerProjectsLocationsJobsPause' request conforms to.
+type CloudSchedulerProjectsLocationsJobsPauseResource =
+  "v1"
+    Core.:> Core.CaptureMode "name" "pause" Core.Text
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.ReqBody '[Core.JSON] PauseJobRequest
+    Core.:> Core.Post '[Core.JSON] Job
+
+-- | Pauses a job. If a job is paused then the system will stop executing the job until it is re-enabled via ResumeJob. The state of the job is stored in state; if paused it will be set to Job.State.PAUSED. A job must be in Job.State.ENABLED to be paused.
+--
+-- /See:/ 'newCloudSchedulerProjectsLocationsJobsPause' smart constructor.
+data CloudSchedulerProjectsLocationsJobsPause = CloudSchedulerProjectsLocationsJobsPause
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | Required. The job name. For example: @projects\/PROJECT_ID\/locations\/LOCATION_ID\/jobs\/JOB_ID@.
+    name :: Core.Text,
+    -- | Multipart request metadata.
+    payload :: PauseJobRequest,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'CloudSchedulerProjectsLocationsJobsPause' with the minimum fields required to make a request.
+newCloudSchedulerProjectsLocationsJobsPause ::
+  -- |  Required. The job name. For example: @projects\/PROJECT_ID\/locations\/LOCATION_ID\/jobs\/JOB_ID@. See 'name'.
+  Core.Text ->
+  -- |  Multipart request metadata. See 'payload'.
+  PauseJobRequest ->
+  CloudSchedulerProjectsLocationsJobsPause
+newCloudSchedulerProjectsLocationsJobsPause name payload =
+  CloudSchedulerProjectsLocationsJobsPause
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      name = name,
+      payload = payload,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    CloudSchedulerProjectsLocationsJobsPause
+  where
+  type
+    Rs CloudSchedulerProjectsLocationsJobsPause =
+      Job
+  type
+    Scopes CloudSchedulerProjectsLocationsJobsPause =
+      '["https://www.googleapis.com/auth/cloud-platform"]
+  requestClient
+    CloudSchedulerProjectsLocationsJobsPause {..} =
+      go
+        name
+        xgafv
+        accessToken
+        callback
+        uploadType
+        uploadProtocol
+        (Core.Just Core.AltJSON)
+        payload
+        cloudSchedulerService
+      where
+        go =
+          Core.buildClient
+            ( Core.Proxy ::
+                Core.Proxy
+                  CloudSchedulerProjectsLocationsJobsPauseResource
+            )
+            Core.mempty
