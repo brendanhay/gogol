0318f9b6266a7b9f70e113d6dc198a8c1afbea20
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 11:23:07 +0100

    gogol-firebasehosting: regenerating service

--- gogol-firebasehosting/gen/Network/Google/FirebaseHosting/Types.hs
+++ gogol-firebasehosting/gen/Network/Google/FirebaseHosting/Types.hs
@@ -1,79 +1,80 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DataKinds          #-}
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE NoImplicitPrelude  #-}
-{-# LANGUAGE OverloadedStrings  #-}
-
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 -- |
 -- Module      : Network.Google.FirebaseHosting.Types
--- Copyright   : (c) 2015-2016 Brendan Hay
+-- Copyright   : (c) 2015-2022 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
---
 module Network.Google.FirebaseHosting.Types
-    (
-    -- * Service Configuration
-      firebaseHostingService
+  ( -- * Configuration
+    firebaseHostingService,
 
-    -- * Status
-    , Status
-    , status
-    , sDetails
-    , sCode
-    , sMessage
+    -- * Types
 
-    -- * ListOperationsResponse
-    , ListOperationsResponse
-    , listOperationsResponse
-    , lorNextPageToken
-    , lorOperations
+    -- ** Xgafv
+    Xgafv (..),
 
-    -- * CancelOperationRequest
-    , CancelOperationRequest
-    , cancelOperationRequest
+    -- ** CancelOperationRequest
+    CancelOperationRequest (..),
+    newCancelOperationRequest,
 
-    -- * Operation
-    , Operation
-    , operation
-    , oDone
-    , oError
-    , oResponse
-    , oName
-    , oMetadata
+    -- ** Empty
+    Empty (..),
+    newEmpty,
 
-    -- * Empty
-    , Empty
-    , empty
+    -- ** ListOperationsResponse
+    ListOperationsResponse (..),
+    newListOperationsResponse,
 
-    -- * StatusDetailsItem
-    , StatusDetailsItem
-    , statusDetailsItem
-    , sdiAddtional
+    -- ** Operation
+    Operation (..),
+    newOperation,
 
-    -- * Xgafv
-    , Xgafv (..)
+    -- ** Operation_Metadata
+    Operation_Metadata (..),
+    newOperation_Metadata,
 
-    -- * OperationMetadata
-    , OperationMetadata
-    , operationMetadata
-    , omAddtional
+    -- ** Operation_Response
+    Operation_Response (..),
+    newOperation_Response,
 
-    -- * OperationResponse
-    , OperationResponse
-    , operationResponse
-    , orAddtional
-    ) where
+    -- ** Status
+    Status (..),
+    newStatus,
 
-import Network.Google.FirebaseHosting.Types.Product
-import Network.Google.FirebaseHosting.Types.Sum
-import Network.Google.Prelude
+    -- ** Status_DetailsItem
+    Status_DetailsItem (..),
+    newStatus_DetailsItem,
+  )
+where
 
--- | Default request referring to version 'v1' of the Firebase Hosting API. This contains the host and root path used as a starting point for constructing service requests.
-firebaseHostingService :: ServiceConfig
-firebaseHostingService
-  = defaultService (ServiceId "firebasehosting:v1")
-      "firebasehosting.googleapis.com"
+import Network.Google.FirebaseHosting.Internal.Product
+import Network.Google.FirebaseHosting.Internal.Sum
+import qualified Network.Google.Prelude as Core
+
+-- | Default request referring to version @v1@ of the Firebase Hosting API. This contains the host and root path used as a starting point for constructing service requests.
+firebaseHostingService :: Core.ServiceConfig
+firebaseHostingService =
+  Core.defaultService
+    (Core.ServiceId "firebasehosting:v1")
+    "firebasehosting.googleapis.com"
