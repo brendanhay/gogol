8eebe97d61fa9f2fd771f7e39ed480292748d543
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 12:30:25 +0100

    gogol-partners: regenerating service with updated namespace

---
+++ gogol-partners/gen/Gogol/Partners.hs
@@ -0,0 +1,421 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.Partners
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Searches certified companies and creates contact leads with them, and also audits the usage of clients.
+--
+-- /See:/ <https://developers.google.com/partners/ Google Partners API Reference>
+module Gogol.Partners
+  ( -- * Configuration
+    partnersService,
+
+    -- * Resources
+
+    -- ** partners.analytics.list
+    PartnersAnalyticsListResource,
+    newPartnersAnalyticsList,
+    PartnersAnalyticsList,
+
+    -- ** partners.clientMessages.log
+    PartnersClientMessagesLogResource,
+    newPartnersClientMessagesLog,
+    PartnersClientMessagesLog,
+
+    -- ** partners.companies.get
+    PartnersCompaniesGetResource,
+    newPartnersCompaniesGet,
+    PartnersCompaniesGet,
+
+    -- ** partners.companies.leads.create
+    PartnersCompaniesLeadsCreateResource,
+    newPartnersCompaniesLeadsCreate,
+    PartnersCompaniesLeadsCreate,
+
+    -- ** partners.companies.list
+    PartnersCompaniesListResource,
+    newPartnersCompaniesList,
+    PartnersCompaniesList,
+
+    -- ** partners.getPartnersstatus
+    PartnersGetPartnersstatusResource,
+    newPartnersGetPartnersstatus,
+    PartnersGetPartnersstatus,
+
+    -- ** partners.leads.list
+    PartnersLeadsListResource,
+    newPartnersLeadsList,
+    PartnersLeadsList,
+
+    -- ** partners.offers.history.list
+    PartnersOffersHistoryListResource,
+    newPartnersOffersHistoryList,
+    PartnersOffersHistoryList,
+
+    -- ** partners.offers.list
+    PartnersOffersListResource,
+    newPartnersOffersList,
+    PartnersOffersList,
+
+    -- ** partners.updateCompanies
+    PartnersUpdateCompaniesResource,
+    newPartnersUpdateCompanies,
+    PartnersUpdateCompanies,
+
+    -- ** partners.updateLeads
+    PartnersUpdateLeadsResource,
+    newPartnersUpdateLeads,
+    PartnersUpdateLeads,
+
+    -- ** partners.userEvents.log
+    PartnersUserEventsLogResource,
+    newPartnersUserEventsLog,
+    PartnersUserEventsLog,
+
+    -- ** partners.userStates.list
+    PartnersUserStatesListResource,
+    newPartnersUserStatesList,
+    PartnersUserStatesList,
+
+    -- ** partners.users.createCompanyRelation
+    PartnersUsersCreateCompanyRelationResource,
+    newPartnersUsersCreateCompanyRelation,
+    PartnersUsersCreateCompanyRelation,
+
+    -- ** partners.users.deleteCompanyRelation
+    PartnersUsersDeleteCompanyRelationResource,
+    newPartnersUsersDeleteCompanyRelation,
+    PartnersUsersDeleteCompanyRelation,
+
+    -- ** partners.users.get
+    PartnersUsersGetResource,
+    newPartnersUsersGet,
+    PartnersUsersGet,
+
+    -- ** partners.users.updateProfile
+    PartnersUsersUpdateProfileResource,
+    newPartnersUsersUpdateProfile,
+    PartnersUsersUpdateProfile,
+
+    -- * Types
+
+    -- ** Xgafv
+    Xgafv (..),
+
+    -- ** AdWordsManagerAccountInfo
+    AdWordsManagerAccountInfo (..),
+    newAdWordsManagerAccountInfo,
+
+    -- ** Analytics
+    Analytics (..),
+    newAnalytics,
+
+    -- ** AnalyticsDataPoint
+    AnalyticsDataPoint (..),
+    newAnalyticsDataPoint,
+
+    -- ** AnalyticsSummary
+    AnalyticsSummary (..),
+    newAnalyticsSummary,
+
+    -- ** AvailableOffer
+    AvailableOffer (..),
+    newAvailableOffer,
+
+    -- ** AvailableOffer_OfferLevel
+    AvailableOffer_OfferLevel (..),
+
+    -- ** AvailableOffer_OfferType
+    AvailableOffer_OfferType (..),
+
+    -- ** Certification
+    Certification (..),
+    newCertification,
+
+    -- ** Certification_CertificationType
+    Certification_CertificationType (..),
+
+    -- ** CertificationExamStatus
+    CertificationExamStatus (..),
+    newCertificationExamStatus,
+
+    -- ** CertificationExamStatus_Type
+    CertificationExamStatus_Type (..),
+
+    -- ** CertificationStatus
+    CertificationStatus (..),
+    newCertificationStatus,
+
+    -- ** CertificationStatus_Type
+    CertificationStatus_Type (..),
+
+    -- ** Company
+    Company (..),
+    newCompany,
+
+    -- ** Company_BadgeTier
+    Company_BadgeTier (..),
+
+    -- ** Company_ProfileStatus
+    Company_ProfileStatus (..),
+
+    -- ** CompanyRelation
+    CompanyRelation (..),
+    newCompanyRelation,
+
+    -- ** CompanyRelation_BadgeTier
+    CompanyRelation_BadgeTier (..),
+
+    -- ** CompanyRelation_State
+    CompanyRelation_State (..),
+
+    -- ** CountryOfferInfo
+    CountryOfferInfo (..),
+    newCountryOfferInfo,
+
+    -- ** CountryOfferInfo_OfferType
+    CountryOfferInfo_OfferType (..),
+
+    -- ** CreateLeadRequest
+    CreateLeadRequest (..),
+    newCreateLeadRequest,
+
+    -- ** CreateLeadResponse
+    CreateLeadResponse (..),
+    newCreateLeadResponse,
+
+    -- ** CreateLeadResponse_RecaptchaStatus
+    CreateLeadResponse_RecaptchaStatus (..),
+
+    -- ** Date
+    Date (..),
+    newDate,
+
+    -- ** DebugInfo
+    DebugInfo (..),
+    newDebugInfo,
+
+    -- ** Empty
+    Empty (..),
+    newEmpty,
+
+    -- ** EventData
+    EventData (..),
+    newEventData,
+
+    -- ** EventData_Key
+    EventData_Key (..),
+
+    -- ** ExamStatus
+    ExamStatus (..),
+    newExamStatus,
+
+    -- ** ExamStatus_ExamType
+    ExamStatus_ExamType (..),
+
+    -- ** GetCompanyResponse
+    GetCompanyResponse (..),
+    newGetCompanyResponse,
+
+    -- ** GetPartnersStatusResponse
+    GetPartnersStatusResponse (..),
+    newGetPartnersStatusResponse,
+
+    -- ** HistoricalOffer
+    HistoricalOffer (..),
+    newHistoricalOffer,
+
+    -- ** HistoricalOffer_OfferType
+    HistoricalOffer_OfferType (..),
+
+    -- ** HistoricalOffer_Status
+    HistoricalOffer_Status (..),
+
+    -- ** LatLng
+    LatLng (..),
+    newLatLng,
+
+    -- ** Lead
+    Lead (..),
+    newLead,
+
+    -- ** Lead_State
+    Lead_State (..),
+
+    -- ** Lead_Type
+    Lead_Type (..),
+
+    -- ** ListAnalyticsResponse
+    ListAnalyticsResponse (..),
+    newListAnalyticsResponse,
+
+    -- ** ListCompaniesResponse
+    ListCompaniesResponse (..),
+    newListCompaniesResponse,
+
+    -- ** ListLeadsResponse
+    ListLeadsResponse (..),
+    newListLeadsResponse,
+
+    -- ** ListOffersHistoryResponse
+    ListOffersHistoryResponse (..),
+    newListOffersHistoryResponse,
+
+    -- ** ListOffersResponse
+    ListOffersResponse (..),
+    newListOffersResponse,
+
+    -- ** ListOffersResponse_NoOfferReason
+    ListOffersResponse_NoOfferReason (..),
+
+    -- ** ListUserStatesResponse
+    ListUserStatesResponse (..),
+    newListUserStatesResponse,
+
+    -- ** LocalizedCompanyInfo
+    LocalizedCompanyInfo (..),
+    newLocalizedCompanyInfo,
+
+    -- ** Location
+    Location (..),
+    newLocation,
+
+    -- ** LogMessageRequest
+    LogMessageRequest (..),
+    newLogMessageRequest,
+
+    -- ** LogMessageRequest_ClientInfo
+    LogMessageRequest_ClientInfo (..),
+    newLogMessageRequest_ClientInfo,
+
+    -- ** LogMessageRequest_Level
+    LogMessageRequest_Level (..),
+
+    -- ** LogMessageResponse
+    LogMessageResponse (..),
+    newLogMessageResponse,
+
+    -- ** LogUserEventRequest
+    LogUserEventRequest (..),
+    newLogUserEventRequest,
+
+    -- ** LogUserEventRequest_EventAction
+    LogUserEventRequest_EventAction (..),
+
+    -- ** LogUserEventRequest_EventCategory
+    LogUserEventRequest_EventCategory (..),
+
+    -- ** LogUserEventRequest_EventScope
+    LogUserEventRequest_EventScope (..),
+
+    -- ** LogUserEventResponse
+    LogUserEventResponse (..),
+    newLogUserEventResponse,
+
+    -- ** Money
+    Money (..),
+    newMoney,
+
+    -- ** OfferCustomer
+    OfferCustomer (..),
+    newOfferCustomer,
+
+    -- ** OfferCustomer_OfferType
+    OfferCustomer_OfferType (..),
+
+    -- ** OptIns
+    OptIns (..),
+    newOptIns,
+
+    -- ** PublicProfile
+    PublicProfile (..),
+    newPublicProfile,
+
+    -- ** Rank
+    Rank (..),
+    newRank,
+
+    -- ** Rank_Type
+    Rank_Type (..),
+
+    -- ** RecaptchaChallenge
+    RecaptchaChallenge (..),
+    newRecaptchaChallenge,
+
+    -- ** RequestMetadata
+    RequestMetadata (..),
+    newRequestMetadata,
+
+    -- ** ResponseMetadata
+    ResponseMetadata (..),
+    newResponseMetadata,
+
+    -- ** SpecializationStatus
+    SpecializationStatus (..),
+    newSpecializationStatus,
+
+    -- ** SpecializationStatus_BadgeSpecialization
+    SpecializationStatus_BadgeSpecialization (..),
+
+    -- ** SpecializationStatus_BadgeSpecializationState
+    SpecializationStatus_BadgeSpecializationState (..),
+
+    -- ** TrafficSource
+    TrafficSource (..),
+    newTrafficSource,
+
+    -- ** User
+    User (..),
+    newUser,
+
+    -- ** UserOverrides
+    UserOverrides (..),
+    newUserOverrides,
+
+    -- ** UserProfile
+    UserProfile (..),
+    newUserProfile,
+  )
+where
+
+import Gogol.Partners.Analytics.List
+import Gogol.Partners.ClientMessages.Log
+import Gogol.Partners.Companies.Get
+import Gogol.Partners.Companies.Leads.Create
+import Gogol.Partners.Companies.List
+import Gogol.Partners.GetPartnersstatus
+import Gogol.Partners.Leads.List
+import Gogol.Partners.Offers.History.List
+import Gogol.Partners.Offers.List
+import Gogol.Partners.Types
+import Gogol.Partners.UpdateCompanies
+import Gogol.Partners.UpdateLeads
+import Gogol.Partners.UserEvents.Log
+import Gogol.Partners.UserStates.List
+import Gogol.Partners.Users.CreateCompanyRelation
+import Gogol.Partners.Users.DeleteCompanyRelation
+import Gogol.Partners.Users.Get
+import Gogol.Partners.Users.UpdateProfile
---
+++ gogol-partners/gen/Gogol/Partners/Leads/List.hs
@@ -0,0 +1,165 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.Partners.Leads.List
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Lists advertiser leads for a user\'s associated company. Should only be called within the context of an authorized logged in user.
+--
+-- /See:/ <https://developers.google.com/partners/ Google Partners API Reference> for @partners.leads.list@.
+module Gogol.Partners.Leads.List
+  ( -- * Resource
+    PartnersLeadsListResource,
+
+    -- ** Constructing a Request
+    newPartnersLeadsList,
+    PartnersLeadsList,
+  )
+where
+
+import Gogol.Partners.Types
+import qualified Gogol.Prelude as Core
+
+-- | A resource alias for @partners.leads.list@ method which the
+-- 'PartnersLeadsList' request conforms to.
+type PartnersLeadsListResource =
+  "v2"
+    Core.:> "leads"
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "orderBy" Core.Text
+    Core.:> Core.QueryParam "pageSize" Core.Int32
+    Core.:> Core.QueryParam "pageToken" Core.Text
+    Core.:> Core.QueryParams
+              "requestMetadata.experimentIds"
+              Core.Text
+    Core.:> Core.QueryParam "requestMetadata.locale" Core.Text
+    Core.:> Core.QueryParam
+              "requestMetadata.partnersSessionId"
+              Core.Text
+    Core.:> Core.QueryParam
+              "requestMetadata.trafficSource.trafficSourceId"
+              Core.Text
+    Core.:> Core.QueryParam
+              "requestMetadata.trafficSource.trafficSubId"
+              Core.Text
+    Core.:> Core.QueryParam
+              "requestMetadata.userOverrides.ipAddress"
+              Core.Text
+    Core.:> Core.QueryParam
+              "requestMetadata.userOverrides.userId"
+              Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Get '[Core.JSON] ListLeadsResponse
+
+-- | Lists advertiser leads for a user\'s associated company. Should only be called within the context of an authorized logged in user.
+--
+-- /See:/ 'newPartnersLeadsList' smart constructor.
+data PartnersLeadsList = PartnersLeadsList
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | How to order Leads. Currently, only @create_time@ and @create_time desc@ are supported
+    orderBy :: (Core.Maybe Core.Text),
+    -- | Requested page size. Server may return fewer leads than requested. If unspecified, server picks an appropriate default.
+    pageSize :: (Core.Maybe Core.Int32),
+    -- | A token identifying a page of results that the server returns. Typically, this is the value of @ListLeadsResponse.next_page_token@ returned from the previous call to ListLeads.
+    pageToken :: (Core.Maybe Core.Text),
+    -- | Experiment IDs the current request belongs to.
+    requestMetadataExperimentIds :: (Core.Maybe [Core.Text]),
+    -- | Locale to use for the current request.
+    requestMetadataLocale :: (Core.Maybe Core.Text),
+    -- | Google Partners session ID.
+    requestMetadataPartnersSessionId :: (Core.Maybe Core.Text),
+    -- | Identifier to indicate where the traffic comes from. An identifier has multiple letters created by a team which redirected the traffic to us.
+    requestMetadataTrafficSourceTrafficSourceId :: (Core.Maybe Core.Text),
+    -- | Second level identifier to indicate where the traffic comes from. An identifier has multiple letters created by a team which redirected the traffic to us.
+    requestMetadataTrafficSourceTrafficSubId :: (Core.Maybe Core.Text),
+    -- | IP address to use instead of the user\'s geo-located IP address.
+    requestMetadataUserOverridesIpAddress :: (Core.Maybe Core.Text),
+    -- | Logged-in user ID to impersonate instead of the user\'s ID.
+    requestMetadataUserOverridesUserId :: (Core.Maybe Core.Text),
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'PartnersLeadsList' with the minimum fields required to make a request.
+newPartnersLeadsList ::
+  PartnersLeadsList
+newPartnersLeadsList =
+  PartnersLeadsList
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      orderBy = Core.Nothing,
+      pageSize = Core.Nothing,
+      pageToken = Core.Nothing,
+      requestMetadataExperimentIds = Core.Nothing,
+      requestMetadataLocale = Core.Nothing,
+      requestMetadataPartnersSessionId = Core.Nothing,
+      requestMetadataTrafficSourceTrafficSourceId = Core.Nothing,
+      requestMetadataTrafficSourceTrafficSubId = Core.Nothing,
+      requestMetadataUserOverridesIpAddress = Core.Nothing,
+      requestMetadataUserOverridesUserId = Core.Nothing,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance Core.GoogleRequest PartnersLeadsList where
+  type Rs PartnersLeadsList = ListLeadsResponse
+  type Scopes PartnersLeadsList = '[]
+  requestClient PartnersLeadsList {..} =
+    go
+      xgafv
+      accessToken
+      callback
+      orderBy
+      pageSize
+      pageToken
+      (requestMetadataExperimentIds Core.^. Core._Default)
+      requestMetadataLocale
+      requestMetadataPartnersSessionId
+      requestMetadataTrafficSourceTrafficSourceId
+      requestMetadataTrafficSourceTrafficSubId
+      requestMetadataUserOverridesIpAddress
+      requestMetadataUserOverridesUserId
+      uploadType
+      uploadProtocol
+      (Core.Just Core.AltJSON)
+      partnersService
+    where
+      go =
+        Core.buildClient
+          (Core.Proxy :: Core.Proxy PartnersLeadsListResource)
+          Core.mempty
