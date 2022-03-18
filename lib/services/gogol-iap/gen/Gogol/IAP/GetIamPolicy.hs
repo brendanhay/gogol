1699e0312b2abf15889fcf5f682760b7065f5b0b
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:06 +0100

    gogol-iap: regenerating service with updated namespace

--- lib/services/gogol-iap/gen/Gogol/IAP/GetIamPolicy.hs
+++ lib/services/gogol-iap/gen/Gogol/IAP/GetIamPolicy.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,89 +36,85 @@
 --
 -- /See:/ <https://cloud.google.com/iap Cloud Identity-Aware Proxy API Reference> for @iap.getIamPolicy@.
 module Gogol.IAP.GetIamPolicy
-  ( -- * Resource
-    IAPGetIamPolicyResource,
+    (
+    -- * Resource
+      IAPGetIamPolicyResource
 
     -- ** Constructing a Request
-    newIAPGetIamPolicy,
-    IAPGetIamPolicy,
-  )
-where
+    , newIAPGetIamPolicy
+    , IAPGetIamPolicy
+    ) where
 
-import Gogol.IAP.Types
 import qualified Gogol.Prelude as Core
+import Gogol.IAP.Types
 
 -- | A resource alias for @iap.getIamPolicy@ method which the
 -- 'IAPGetIamPolicy' request conforms to.
 type IAPGetIamPolicyResource =
-  "v1"
-    Core.:> Core.CaptureMode "resource" "getIamPolicy" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] GetIamPolicyRequest
-    Core.:> Core.Post '[Core.JSON] Policy
+     "v1" Core.:>
+       Core.CaptureMode "resource" "getIamPolicy" Core.Text
+         Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "uploadType" Core.Text Core.:>
+                 Core.QueryParam "upload_protocol" Core.Text Core.:>
+                   Core.QueryParam "alt" Core.AltJSON Core.:>
+                     Core.ReqBody '[Core.JSON] GetIamPolicyRequest Core.:>
+                       Core.Post '[Core.JSON] Policy
 
 -- | Gets the access control policy for an Identity-Aware Proxy protected resource. More information about managing access via IAP can be found at: https:\/\/cloud.google.com\/iap\/docs\/managing-access#managing/access/via/the/api
 --
 -- /See:/ 'newIAPGetIamPolicy' smart constructor.
 data IAPGetIamPolicy = IAPGetIamPolicy
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Multipart request metadata.
-    payload :: GetIamPolicyRequest,
-    -- | REQUIRED: The resource for which the policy is being requested. See the operation documentation for the appropriate value for this field.
-    resource :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Multipart request metadata.
+    , payload :: GetIamPolicyRequest
+      -- | REQUIRED: The resource for which the policy is being requested. See the operation documentation for the appropriate value for this field.
+    , resource :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'IAPGetIamPolicy' with the minimum fields required to make a request.
-newIAPGetIamPolicy ::
-  -- |  Multipart request metadata. See 'payload'.
-  GetIamPolicyRequest ->
-  -- |  REQUIRED: The resource for which the policy is being requested. See the operation documentation for the appropriate value for this field. See 'resource'.
-  Core.Text ->
-  IAPGetIamPolicy
+newIAPGetIamPolicy 
+    ::  GetIamPolicyRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> Core.Text
+       -- ^  REQUIRED: The resource for which the policy is being requested. See the operation documentation for the appropriate value for this field. See 'resource'.
+    -> IAPGetIamPolicy
 newIAPGetIamPolicy payload resource =
   IAPGetIamPolicy
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      payload = payload,
-      resource = resource,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , payload = payload
+    , resource = resource
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
 instance Core.GoogleRequest IAPGetIamPolicy where
-  type Rs IAPGetIamPolicy = Policy
-  type
-    Scopes IAPGetIamPolicy =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient IAPGetIamPolicy {..} =
-    go
-      resource
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      payload
-      iAPService
-    where
-      go =
-        Core.buildClient
-          (Core.Proxy :: Core.Proxy IAPGetIamPolicyResource)
-          Core.mempty
+        type Rs IAPGetIamPolicy = Policy
+        type Scopes IAPGetIamPolicy =
+             '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient IAPGetIamPolicy{..}
+          = go resource xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              iAPService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy :: Core.Proxy IAPGetIamPolicyResource)
+                      Core.mempty
+
--- lib/services/gogol-iap/gen/Gogol/IAP/Projects/Brands/IdentityAwareProxyClients/List.hs
+++ lib/services/gogol-iap/gen/Gogol/IAP/Projects/Brands/IdentityAwareProxyClients/List.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,106 +36,98 @@
 --
 -- /See:/ <https://cloud.google.com/iap Cloud Identity-Aware Proxy API Reference> for @iap.projects.brands.identityAwareProxyClients.list@.
 module Gogol.IAP.Projects.Brands.IdentityAwareProxyClients.List
-  ( -- * Resource
-    IAPProjectsBrandsIdentityAwareProxyClientsListResource,
+    (
+    -- * Resource
+      IAPProjectsBrandsIdentityAwareProxyClientsListResource
 
     -- ** Constructing a Request
-    newIAPProjectsBrandsIdentityAwareProxyClientsList,
-    IAPProjectsBrandsIdentityAwareProxyClientsList,
-  )
-where
+    , newIAPProjectsBrandsIdentityAwareProxyClientsList
+    , IAPProjectsBrandsIdentityAwareProxyClientsList
+    ) where
 
-import Gogol.IAP.Types
 import qualified Gogol.Prelude as Core
+import Gogol.IAP.Types
 
 -- | A resource alias for @iap.projects.brands.identityAwareProxyClients.list@ method which the
 -- 'IAPProjectsBrandsIdentityAwareProxyClientsList' request conforms to.
-type IAPProjectsBrandsIdentityAwareProxyClientsListResource =
-  "v1"
-    Core.:> Core.Capture "parent" Core.Text
-    Core.:> "identityAwareProxyClients"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "pageSize" Core.Int32
-    Core.:> Core.QueryParam "pageToken" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get
-              '[Core.JSON]
-              ListIdentityAwareProxyClientsResponse
+type IAPProjectsBrandsIdentityAwareProxyClientsListResource
+     =
+     "v1" Core.:>
+       Core.Capture "parent" Core.Text Core.:>
+         "identityAwareProxyClients" Core.:>
+           Core.QueryParam "$.xgafv" Xgafv Core.:>
+             Core.QueryParam "access_token" Core.Text Core.:>
+               Core.QueryParam "callback" Core.Text Core.:>
+                 Core.QueryParam "pageSize" Core.Int32 Core.:>
+                   Core.QueryParam "pageToken" Core.Text Core.:>
+                     Core.QueryParam "uploadType" Core.Text Core.:>
+                       Core.QueryParam "upload_protocol" Core.Text Core.:>
+                         Core.QueryParam "alt" Core.AltJSON Core.:>
+                           Core.Get '[Core.JSON]
+                             ListIdentityAwareProxyClientsResponse
 
 -- | Lists the existing clients for the brand.
 --
 -- /See:/ 'newIAPProjectsBrandsIdentityAwareProxyClientsList' smart constructor.
 data IAPProjectsBrandsIdentityAwareProxyClientsList = IAPProjectsBrandsIdentityAwareProxyClientsList
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | The maximum number of clients to return. The service may return fewer than this value. If unspecified, at most 100 clients will be returned. The maximum value is 1000; values above 1000 will be coerced to 1000.
-    pageSize :: (Core.Maybe Core.Int32),
-    -- | A page token, received from a previous @ListIdentityAwareProxyClients@ call. Provide this to retrieve the subsequent page. When paginating, all other parameters provided to @ListIdentityAwareProxyClients@ must match the call that provided the page token.
-    pageToken :: (Core.Maybe Core.Text),
-    -- | Required. Full brand path. In the following format: projects\/{project_number\/id}\/brands\/{brand}.
-    parent :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | The maximum number of clients to return. The service may return fewer than this value. If unspecified, at most 100 clients will be returned. The maximum value is 1000; values above 1000 will be coerced to 1000.
+    , pageSize :: (Core.Maybe Core.Int32)
+      -- | A page token, received from a previous @ListIdentityAwareProxyClients@ call. Provide this to retrieve the subsequent page. When paginating, all other parameters provided to @ListIdentityAwareProxyClients@ must match the call that provided the page token.
+    , pageToken :: (Core.Maybe Core.Text)
+      -- | Required. Full brand path. In the following format: projects\/{project_number\/id}\/brands\/{brand}.
+    , parent :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'IAPProjectsBrandsIdentityAwareProxyClientsList' with the minimum fields required to make a request.
-newIAPProjectsBrandsIdentityAwareProxyClientsList ::
-  -- |  Required. Full brand path. In the following format: projects\/{project_number\/id}\/brands\/{brand}. See 'parent'.
-  Core.Text ->
-  IAPProjectsBrandsIdentityAwareProxyClientsList
+newIAPProjectsBrandsIdentityAwareProxyClientsList 
+    ::  Core.Text
+       -- ^  Required. Full brand path. In the following format: projects\/{project_number\/id}\/brands\/{brand}. See 'parent'.
+    -> IAPProjectsBrandsIdentityAwareProxyClientsList
 newIAPProjectsBrandsIdentityAwareProxyClientsList parent =
   IAPProjectsBrandsIdentityAwareProxyClientsList
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      pageSize = Core.Nothing,
-      pageToken = Core.Nothing,
-      parent = parent,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , pageSize = Core.Nothing
+    , pageToken = Core.Nothing
+    , parent = parent
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    IAPProjectsBrandsIdentityAwareProxyClientsList
-  where
-  type
-    Rs
-      IAPProjectsBrandsIdentityAwareProxyClientsList =
-      ListIdentityAwareProxyClientsResponse
-  type
-    Scopes
-      IAPProjectsBrandsIdentityAwareProxyClientsList =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient
-    IAPProjectsBrandsIdentityAwareProxyClientsList {..} =
-      go
-        parent
-        xgafv
-        accessToken
-        callback
-        pageSize
-        pageToken
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        iAPService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  IAPProjectsBrandsIdentityAwareProxyClientsListResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           IAPProjectsBrandsIdentityAwareProxyClientsList
+         where
+        type Rs
+               IAPProjectsBrandsIdentityAwareProxyClientsList
+             = ListIdentityAwareProxyClientsResponse
+        type Scopes
+               IAPProjectsBrandsIdentityAwareProxyClientsList
+             = '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient
+          IAPProjectsBrandsIdentityAwareProxyClientsList{..}
+          = go parent xgafv accessToken callback pageSize
+              pageToken
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              iAPService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           IAPProjectsBrandsIdentityAwareProxyClientsListResource)
+                      Core.mempty
+
