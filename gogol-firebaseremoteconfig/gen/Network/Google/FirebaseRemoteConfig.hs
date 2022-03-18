523a9def7bde46f98f4d31fc07f7b16a265dbd82
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 11:23:08 +0100

    gogol-firebaseremoteconfig: regenerating service

--- gogol-firebaseremoteconfig/gen/Network/Google/FirebaseRemoteConfig.hs
+++ gogol-firebaseremoteconfig/gen/Network/Google/FirebaseRemoteConfig.hs
@@ -1,94 +1,84 @@
-{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE TypeOperators     #-}
-
-{-# OPTIONS_GHC -fno-warn-unused-imports    #-}
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 -- |
 -- Module      : Network.Google.FirebaseRemoteConfig
--- Copyright   : (c) 2015-2016 Brendan Hay
+-- Copyright   : (c) 2015-2022 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 --
--- Firebase Remote Config API allows the 3P clients to manage Remote Config
--- conditions and parameters for Firebase applications.
+-- Firebase Remote Config API allows the 3P clients to manage Remote Config conditions and parameters for Firebase applications.
 --
 -- /See:/ <https://firebase.google.com/docs/remote-config/ Firebase Remote Config API Reference>
 module Network.Google.FirebaseRemoteConfig
-    (
-    -- * Service Configuration
-      firebaseRemoteConfigService
-
-    -- * API Declaration
-    , FirebaseRemoteConfigAPI
+  ( -- * Configuration
+    firebaseRemoteConfigService,
 
     -- * Resources
 
     -- ** firebaseremoteconfig.projects.getRemoteConfig
-    , module Network.Google.Resource.FirebaseRemoteConfig.Projects.GetRemoteConfig
+    FirebaseRemoteConfigProjectsGetRemoteConfigResource,
+    newFirebaseRemoteConfigProjectsGetRemoteConfig,
+    FirebaseRemoteConfigProjectsGetRemoteConfig,
 
     -- ** firebaseremoteconfig.projects.updateRemoteConfig
-    , module Network.Google.Resource.FirebaseRemoteConfig.Projects.UpdateRemoteConfig
+    FirebaseRemoteConfigProjectsUpdateRemoteConfigResource,
+    newFirebaseRemoteConfigProjectsUpdateRemoteConfig,
+    FirebaseRemoteConfigProjectsUpdateRemoteConfig,
 
     -- * Types
 
-    -- ** RemoteConfigParameterValue
-    , RemoteConfigParameterValue
-    , remoteConfigParameterValue
-    , rcpvValue
-    , rcpvUseInAppDefault
-
-    -- ** RemoteConfigParameterConditionalValues
-    , RemoteConfigParameterConditionalValues
-    , remoteConfigParameterConditionalValues
-    , rcpcvAddtional
-
-    -- ** RemoteConfigParameters
-    , RemoteConfigParameters
-    , remoteConfigParameters
-    , rcpAddtional
-
-    -- ** RemoteConfigParameter
-    , RemoteConfigParameter
-    , remoteConfigParameter
-    , rcpDefaultValue
-    , rcpDescription
-    , rcpConditionalValues
-
     -- ** Xgafv
-    , Xgafv (..)
-
-    -- ** RemoteConfigConditionTagColor
-    , RemoteConfigConditionTagColor (..)
-
-    -- ** RemoteConfigCondition
-    , RemoteConfigCondition
-    , remoteConfigCondition
-    , rccTagColor
-    , rccName
-    , rccExpression
-    , rccDescription
+    Xgafv (..),
 
     -- ** RemoteConfig
-    , RemoteConfig
-    , remoteConfig
-    , rcParameters
-    , rcConditions
-    ) where
+    RemoteConfig (..),
+    newRemoteConfig,
 
-import Network.Google.Prelude
+    -- ** RemoteConfig_Parameters
+    RemoteConfig_Parameters (..),
+    newRemoteConfig_Parameters,
+
+    -- ** RemoteConfigCondition
+    RemoteConfigCondition (..),
+    newRemoteConfigCondition,
+
+    -- ** RemoteConfigCondition_TagColor
+    RemoteConfigCondition_TagColor (..),
+
+    -- ** RemoteConfigParameter
+    RemoteConfigParameter (..),
+    newRemoteConfigParameter,
+
+    -- ** RemoteConfigParameter_ConditionalValues
+    RemoteConfigParameter_ConditionalValues (..),
+    newRemoteConfigParameter_ConditionalValues,
+
+    -- ** RemoteConfigParameterValue
+    RemoteConfigParameterValue (..),
+    newRemoteConfigParameterValue,
+  )
+where
+
+import Network.Google.FirebaseRemoteConfig.Projects.GetRemoteConfig
+import Network.Google.FirebaseRemoteConfig.Projects.UpdateRemoteConfig
 import Network.Google.FirebaseRemoteConfig.Types
-import Network.Google.Resource.FirebaseRemoteConfig.Projects.GetRemoteConfig
-import Network.Google.Resource.FirebaseRemoteConfig.Projects.UpdateRemoteConfig
-
-{- $resources
-TODO
--}
-
--- | Represents the entirety of the methods and resources available for the Firebase Remote Config API service.
-type FirebaseRemoteConfigAPI =
-     ProjectsUpdateRemoteConfigResource :<|>
-       ProjectsGetRemoteConfigResource
---
+++ gogol-firebaseremoteconfig/gen/Network/Google/FirebaseRemoteConfig/Projects/GetRemoteConfig.hs
@@ -0,0 +1,132 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Network.Google.FirebaseRemoteConfig.Projects.GetRemoteConfig
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Get the latest version Remote Configuration for a project. Returns the RemoteConfig as the payload, and also the eTag as a response header.
+--
+-- /See:/ <https://firebase.google.com/docs/remote-config/ Firebase Remote Config API Reference> for @firebaseremoteconfig.projects.getRemoteConfig@.
+module Network.Google.FirebaseRemoteConfig.Projects.GetRemoteConfig
+  ( -- * Resource
+    FirebaseRemoteConfigProjectsGetRemoteConfigResource,
+
+    -- ** Constructing a Request
+    newFirebaseRemoteConfigProjectsGetRemoteConfig,
+    FirebaseRemoteConfigProjectsGetRemoteConfig,
+  )
+where
+
+import Network.Google.FirebaseRemoteConfig.Types
+import qualified Network.Google.Prelude as Core
+
+-- | A resource alias for @firebaseremoteconfig.projects.getRemoteConfig@ method which the
+-- 'FirebaseRemoteConfigProjectsGetRemoteConfig' request conforms to.
+type FirebaseRemoteConfigProjectsGetRemoteConfigResource =
+  "v1"
+    Core.:> Core.Capture "project" Core.Text
+    Core.:> "remoteConfig"
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "bearer_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "pp" Core.Bool
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Get '[Core.JSON] RemoteConfig
+
+-- | Get the latest version Remote Configuration for a project. Returns the RemoteConfig as the payload, and also the eTag as a response header.
+--
+-- /See:/ 'newFirebaseRemoteConfigProjectsGetRemoteConfig' smart constructor.
+data FirebaseRemoteConfigProjectsGetRemoteConfig = FirebaseRemoteConfigProjectsGetRemoteConfig
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | OAuth bearer token.
+    bearerToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | Pretty-print response.
+    pp :: Core.Bool,
+    -- | The GMP project identifier. Required. See note at the beginning of this file regarding project ids.
+    project :: Core.Text,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'FirebaseRemoteConfigProjectsGetRemoteConfig' with the minimum fields required to make a request.
+newFirebaseRemoteConfigProjectsGetRemoteConfig ::
+  -- |  The GMP project identifier. Required. See note at the beginning of this file regarding project ids. See 'project'.
+  Core.Text ->
+  FirebaseRemoteConfigProjectsGetRemoteConfig
+newFirebaseRemoteConfigProjectsGetRemoteConfig project =
+  FirebaseRemoteConfigProjectsGetRemoteConfig
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      bearerToken = Core.Nothing,
+      callback = Core.Nothing,
+      pp = Core.True,
+      project = project,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    FirebaseRemoteConfigProjectsGetRemoteConfig
+  where
+  type
+    Rs FirebaseRemoteConfigProjectsGetRemoteConfig =
+      RemoteConfig
+  type
+    Scopes
+      FirebaseRemoteConfigProjectsGetRemoteConfig =
+      '[]
+  requestClient
+    FirebaseRemoteConfigProjectsGetRemoteConfig {..} =
+      go
+        project
+        xgafv
+        accessToken
+        bearerToken
+        callback
+        (Core.Just pp)
+        uploadType
+        uploadProtocol
+        (Core.Just Core.AltJSON)
+        firebaseRemoteConfigService
+      where
+        go =
+          Core.buildClient
+            ( Core.Proxy ::
+                Core.Proxy
+                  FirebaseRemoteConfigProjectsGetRemoteConfigResource
+            )
+            Core.mempty
