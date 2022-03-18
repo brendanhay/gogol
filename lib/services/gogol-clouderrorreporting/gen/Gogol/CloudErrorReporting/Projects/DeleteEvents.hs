8b6e02888f88dd5c8419d63867dfac97ecb8234e
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:41:59 +0100

    gogol-clouderrorreporting: regenerating service with updated namespace

--- lib/services/gogol-clouderrorreporting/gen/Gogol/CloudErrorReporting/Projects/DeleteEvents.hs
+++ lib/services/gogol-clouderrorreporting/gen/Gogol/CloudErrorReporting/Projects/DeleteEvents.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,92 +36,86 @@
 --
 -- /See:/ <https://cloud.google.com/error-reporting/ Error Reporting API Reference> for @clouderrorreporting.projects.deleteEvents@.
 module Gogol.CloudErrorReporting.Projects.DeleteEvents
-  ( -- * Resource
-    CloudErrorReportingProjectsDeleteEventsResource,
+    (
+    -- * Resource
+      CloudErrorReportingProjectsDeleteEventsResource
 
     -- ** Constructing a Request
-    newCloudErrorReportingProjectsDeleteEvents,
-    CloudErrorReportingProjectsDeleteEvents,
-  )
-where
+    , newCloudErrorReportingProjectsDeleteEvents
+    , CloudErrorReportingProjectsDeleteEvents
+    ) where
 
-import Gogol.CloudErrorReporting.Types
 import qualified Gogol.Prelude as Core
+import Gogol.CloudErrorReporting.Types
 
 -- | A resource alias for @clouderrorreporting.projects.deleteEvents@ method which the
 -- 'CloudErrorReportingProjectsDeleteEvents' request conforms to.
-type CloudErrorReportingProjectsDeleteEventsResource =
-  "v1beta1"
-    Core.:> Core.Capture "projectName" Core.Text
-    Core.:> "events"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Delete '[Core.JSON] DeleteEventsResponse
+type CloudErrorReportingProjectsDeleteEventsResource
+     =
+     "v1beta1" Core.:>
+       Core.Capture "projectName" Core.Text Core.:>
+         "events" Core.:>
+           Core.QueryParam "$.xgafv" Xgafv Core.:>
+             Core.QueryParam "access_token" Core.Text Core.:>
+               Core.QueryParam "callback" Core.Text Core.:>
+                 Core.QueryParam "uploadType" Core.Text Core.:>
+                   Core.QueryParam "upload_protocol" Core.Text Core.:>
+                     Core.QueryParam "alt" Core.AltJSON Core.:>
+                       Core.Delete '[Core.JSON] DeleteEventsResponse
 
 -- | Deletes all error events of a given project.
 --
 -- /See:/ 'newCloudErrorReportingProjectsDeleteEvents' smart constructor.
 data CloudErrorReportingProjectsDeleteEvents = CloudErrorReportingProjectsDeleteEvents
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Required. The resource name of the Google Cloud Platform project. Written as @projects\/{projectID}@, where @{projectID}@ is the <https://support.google.com/cloud/answer/6158840 Google Cloud Platform project ID>. Example: @projects\/my-project-123@.
-    projectName :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Required. The resource name of the Google Cloud Platform project. Written as @projects\/{projectID}@, where @{projectID}@ is the <https://support.google.com/cloud/answer/6158840 Google Cloud Platform project ID>. Example: @projects\/my-project-123@.
+    , projectName :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'CloudErrorReportingProjectsDeleteEvents' with the minimum fields required to make a request.
-newCloudErrorReportingProjectsDeleteEvents ::
-  -- |  Required. The resource name of the Google Cloud Platform project. Written as @projects\/{projectID}@, where @{projectID}@ is the <https://support.google.com/cloud/answer/6158840 Google Cloud Platform project ID>. Example: @projects\/my-project-123@. See 'projectName'.
-  Core.Text ->
-  CloudErrorReportingProjectsDeleteEvents
+newCloudErrorReportingProjectsDeleteEvents 
+    ::  Core.Text
+       -- ^  Required. The resource name of the Google Cloud Platform project. Written as @projects\/{projectID}@, where @{projectID}@ is the <https://support.google.com/cloud/answer/6158840 Google Cloud Platform project ID>. Example: @projects\/my-project-123@. See 'projectName'.
+    -> CloudErrorReportingProjectsDeleteEvents
 newCloudErrorReportingProjectsDeleteEvents projectName =
   CloudErrorReportingProjectsDeleteEvents
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      projectName = projectName,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , projectName = projectName
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    CloudErrorReportingProjectsDeleteEvents
-  where
-  type
-    Rs CloudErrorReportingProjectsDeleteEvents =
-      DeleteEventsResponse
-  type
-    Scopes CloudErrorReportingProjectsDeleteEvents =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient
-    CloudErrorReportingProjectsDeleteEvents {..} =
-      go
-        projectName
-        xgafv
-        accessToken
-        callback
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        cloudErrorReportingService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  CloudErrorReportingProjectsDeleteEventsResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           CloudErrorReportingProjectsDeleteEvents
+         where
+        type Rs CloudErrorReportingProjectsDeleteEvents =
+             DeleteEventsResponse
+        type Scopes CloudErrorReportingProjectsDeleteEvents =
+             '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient
+          CloudErrorReportingProjectsDeleteEvents{..}
+          = go projectName xgafv accessToken callback
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              cloudErrorReportingService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           CloudErrorReportingProjectsDeleteEventsResource)
+                      Core.mempty
+
--- lib/services/gogol-clouderrorreporting/gen/Gogol/CloudErrorReporting/Projects/Groups/Get.hs
+++ lib/services/gogol-clouderrorreporting/gen/Gogol/CloudErrorReporting/Projects/Groups/Get.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,91 +36,83 @@
 --
 -- /See:/ <https://cloud.google.com/error-reporting/ Error Reporting API Reference> for @clouderrorreporting.projects.groups.get@.
 module Gogol.CloudErrorReporting.Projects.Groups.Get
-  ( -- * Resource
-    CloudErrorReportingProjectsGroupsGetResource,
+    (
+    -- * Resource
+      CloudErrorReportingProjectsGroupsGetResource
 
     -- ** Constructing a Request
-    newCloudErrorReportingProjectsGroupsGet,
-    CloudErrorReportingProjectsGroupsGet,
-  )
-where
+    , newCloudErrorReportingProjectsGroupsGet
+    , CloudErrorReportingProjectsGroupsGet
+    ) where
 
-import Gogol.CloudErrorReporting.Types
 import qualified Gogol.Prelude as Core
+import Gogol.CloudErrorReporting.Types
 
 -- | A resource alias for @clouderrorreporting.projects.groups.get@ method which the
 -- 'CloudErrorReportingProjectsGroupsGet' request conforms to.
 type CloudErrorReportingProjectsGroupsGetResource =
-  "v1beta1"
-    Core.:> Core.Capture "groupName" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] ErrorGroup
+     "v1beta1" Core.:>
+       Core.Capture "groupName" Core.Text Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "uploadType" Core.Text Core.:>
+                 Core.QueryParam "upload_protocol" Core.Text Core.:>
+                   Core.QueryParam "alt" Core.AltJSON Core.:>
+                     Core.Get '[Core.JSON] ErrorGroup
 
 -- | Get the specified group.
 --
 -- /See:/ 'newCloudErrorReportingProjectsGroupsGet' smart constructor.
 data CloudErrorReportingProjectsGroupsGet = CloudErrorReportingProjectsGroupsGet
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Required. The group resource name. Written as @projects\/{projectID}\/groups\/{group_name}@. Call <https://cloud.google.com/error-reporting/reference/rest/v1beta1/projects.groupStats/list groupStats.list> to return a list of groups belonging to this project. Example: @projects\/my-project-123\/groups\/my-group@
-    groupName :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Required. The group resource name. Written as @projects\/{projectID}\/groups\/{group_name}@. Call <https://cloud.google.com/error-reporting/reference/rest/v1beta1/projects.groupStats/list groupStats.list> to return a list of groups belonging to this project. Example: @projects\/my-project-123\/groups\/my-group@
+    , groupName :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'CloudErrorReportingProjectsGroupsGet' with the minimum fields required to make a request.
-newCloudErrorReportingProjectsGroupsGet ::
-  -- |  Required. The group resource name. Written as @projects\/{projectID}\/groups\/{group_name}@. Call <https://cloud.google.com/error-reporting/reference/rest/v1beta1/projects.groupStats/list groupStats.list> to return a list of groups belonging to this project. Example: @projects\/my-project-123\/groups\/my-group@ See 'groupName'.
-  Core.Text ->
-  CloudErrorReportingProjectsGroupsGet
+newCloudErrorReportingProjectsGroupsGet 
+    ::  Core.Text
+       -- ^  Required. The group resource name. Written as @projects\/{projectID}\/groups\/{group_name}@. Call <https://cloud.google.com/error-reporting/reference/rest/v1beta1/projects.groupStats/list groupStats.list> to return a list of groups belonging to this project. Example: @projects\/my-project-123\/groups\/my-group@ See 'groupName'.
+    -> CloudErrorReportingProjectsGroupsGet
 newCloudErrorReportingProjectsGroupsGet groupName =
   CloudErrorReportingProjectsGroupsGet
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      groupName = groupName,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , groupName = groupName
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    CloudErrorReportingProjectsGroupsGet
-  where
-  type
-    Rs CloudErrorReportingProjectsGroupsGet =
-      ErrorGroup
-  type
-    Scopes CloudErrorReportingProjectsGroupsGet =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient
-    CloudErrorReportingProjectsGroupsGet {..} =
-      go
-        groupName
-        xgafv
-        accessToken
-        callback
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        cloudErrorReportingService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  CloudErrorReportingProjectsGroupsGetResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           CloudErrorReportingProjectsGroupsGet
+         where
+        type Rs CloudErrorReportingProjectsGroupsGet =
+             ErrorGroup
+        type Scopes CloudErrorReportingProjectsGroupsGet =
+             '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient
+          CloudErrorReportingProjectsGroupsGet{..}
+          = go groupName xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              cloudErrorReportingService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           CloudErrorReportingProjectsGroupsGetResource)
+                      Core.mempty
+
