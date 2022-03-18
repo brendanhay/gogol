1a493b75dbf82ea98c5b423f41322f0c200a4a70
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:01 +0100

    gogol-consumersurveys: regenerating service with updated namespace

--- lib/services/gogol-consumersurveys/gen/Gogol/ConsumerSurveys/Results/Get.hs
+++ lib/services/gogol-consumersurveys/gen/Gogol/ConsumerSurveys/Results/Get.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,102 +36,92 @@
 --
 -- /See:/ <https://developers.google.com/surveys/ Consumer Surveys API Reference> for @consumersurveys.results.get@.
 module Gogol.ConsumerSurveys.Results.Get
-  ( -- * Resource
-    ConsumerSurveysResultsGetResource,
+    (
+    -- * Resource
+      ConsumerSurveysResultsGetResource
 
     -- ** Constructing a Request
-    newConsumerSurveysResultsGet,
-    ConsumerSurveysResultsGet,
-  )
-where
+    , newConsumerSurveysResultsGet
+    , ConsumerSurveysResultsGet
+    ) where
 
-import Gogol.ConsumerSurveys.Types
 import qualified Gogol.Prelude as Core
+import Gogol.ConsumerSurveys.Types
 
 -- | A resource alias for @consumersurveys.results.get@ method which the
 -- 'ConsumerSurveysResultsGet' request conforms to.
 type ConsumerSurveysResultsGetResource =
-  "consumersurveys"
-    Core.:> "v2"
-    Core.:> "surveys"
-    Core.:> Core.Capture "surveyUrlId" Core.Text
-    Core.:> "results"
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] ResultsGetRequest
-    Core.:> Core.Get '[Core.JSON] SurveyResults
-    Core.:<|> "consumersurveys"
-      Core.:> "v2"
-      Core.:> "surveys"
-      Core.:> Core.Capture "surveyUrlId" Core.Text
-      Core.:> "results"
-      Core.:> Core.QueryParam "alt" Core.AltMedia
-      Core.:> Core.Get '[Core.OctetStream] Core.Stream
+     "consumersurveys" Core.:>
+       "v2" Core.:>
+         "surveys" Core.:>
+           Core.Capture "surveyUrlId" Core.Text Core.:>
+             "results" Core.:>
+               Core.QueryParam "alt" Core.AltJSON Core.:>
+                 Core.ReqBody '[Core.JSON] ResultsGetRequest Core.:>
+                   Core.Get '[Core.JSON] SurveyResults
+       Core.:<|>
+       "consumersurveys" Core.:>
+         "v2" Core.:>
+           "surveys" Core.:>
+             Core.Capture "surveyUrlId" Core.Text Core.:>
+               "results" Core.:>
+                 Core.QueryParam "alt" Core.AltMedia Core.:>
+                   Core.Get '[Core.OctetStream] Core.Stream
 
 -- | Retrieves any survey results that have been produced so far. Results are formatted as an Excel file. You must add \"?alt=media\" to the URL as an argument to get results.
 --
 -- /See:/ 'newConsumerSurveysResultsGet' smart constructor.
 data ConsumerSurveysResultsGet = ConsumerSurveysResultsGet
-  { -- | Multipart request metadata.
-    payload :: ResultsGetRequest,
-    -- | External URL ID for the survey.
-    surveyUrlId :: Core.Text
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | Multipart request metadata.
+      payload :: ResultsGetRequest
+      -- | External URL ID for the survey.
+    , surveyUrlId :: Core.Text
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'ConsumerSurveysResultsGet' with the minimum fields required to make a request.
-newConsumerSurveysResultsGet ::
-  -- |  Multipart request metadata. See 'payload'.
-  ResultsGetRequest ->
-  -- |  External URL ID for the survey. See 'surveyUrlId'.
-  Core.Text ->
-  ConsumerSurveysResultsGet
+newConsumerSurveysResultsGet 
+    ::  ResultsGetRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> Core.Text
+       -- ^  External URL ID for the survey. See 'surveyUrlId'.
+    -> ConsumerSurveysResultsGet
 newConsumerSurveysResultsGet payload surveyUrlId =
   ConsumerSurveysResultsGet {payload = payload, surveyUrlId = surveyUrlId}
 
-instance Core.GoogleRequest ConsumerSurveysResultsGet where
-  type Rs ConsumerSurveysResultsGet = SurveyResults
-  type
-    Scopes ConsumerSurveysResultsGet =
-      '[ "https://www.googleapis.com/auth/consumersurveys",
-         "https://www.googleapis.com/auth/consumersurveys.readonly",
-         "https://www.googleapis.com/auth/userinfo.email"
-       ]
-  requestClient ConsumerSurveysResultsGet {..} =
-    go
-      surveyUrlId
-      (Core.Just Core.AltJSON)
-      payload
-      consumerSurveysService
-    where
-      go Core.:<|> _ =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy ConsumerSurveysResultsGetResource
-          )
-          Core.mempty
+instance Core.GoogleRequest ConsumerSurveysResultsGet
+         where
+        type Rs ConsumerSurveysResultsGet = SurveyResults
+        type Scopes ConsumerSurveysResultsGet =
+             '["https://www.googleapis.com/auth/consumersurveys",
+               "https://www.googleapis.com/auth/consumersurveys.readonly",
+               "https://www.googleapis.com/auth/userinfo.email"]
+        requestClient ConsumerSurveysResultsGet{..}
+          = go surveyUrlId (Core.Just Core.AltJSON) payload
+              consumerSurveysService
+          where go Core.:<|> _
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy ConsumerSurveysResultsGetResource)
+                      Core.mempty
 
-instance
-  Core.GoogleRequest
-    (Core.MediaDownload ConsumerSurveysResultsGet)
-  where
-  type
-    Rs
-      (Core.MediaDownload ConsumerSurveysResultsGet) =
-      Core.Stream
-  type
-    Scopes
-      (Core.MediaDownload ConsumerSurveysResultsGet) =
-      Core.Scopes ConsumerSurveysResultsGet
-  requestClient
-    (Core.MediaDownload ConsumerSurveysResultsGet {..}) =
-      go
-        surveyUrlId
-        (Core.Just Core.AltMedia)
-        consumerSurveysService
-      where
-        _ Core.:<|> go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy ConsumerSurveysResultsGetResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           (Core.MediaDownload ConsumerSurveysResultsGet)
+         where
+        type Rs
+               (Core.MediaDownload ConsumerSurveysResultsGet)
+             = Core.Stream
+        type Scopes
+               (Core.MediaDownload ConsumerSurveysResultsGet)
+             = Core.Scopes ConsumerSurveysResultsGet
+        requestClient
+          (Core.MediaDownload ConsumerSurveysResultsGet{..})
+          = go surveyUrlId (Core.Just Core.AltMedia)
+              consumerSurveysService
+          where _ Core.:<|> go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy ConsumerSurveysResultsGetResource)
+                      Core.mempty
+
--- lib/services/gogol-consumersurveys/gen/Gogol/ConsumerSurveys/Surveys/Insert.hs
+++ lib/services/gogol-consumersurveys/gen/Gogol/ConsumerSurveys/Surveys/Insert.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,64 +36,59 @@
 --
 -- /See:/ <https://developers.google.com/surveys/ Consumer Surveys API Reference> for @consumersurveys.surveys.insert@.
 module Gogol.ConsumerSurveys.Surveys.Insert
-  ( -- * Resource
-    ConsumerSurveysSurveysInsertResource,
+    (
+    -- * Resource
+      ConsumerSurveysSurveysInsertResource
 
     -- ** Constructing a Request
-    newConsumerSurveysSurveysInsert,
-    ConsumerSurveysSurveysInsert,
-  )
-where
+    , newConsumerSurveysSurveysInsert
+    , ConsumerSurveysSurveysInsert
+    ) where
 
-import Gogol.ConsumerSurveys.Types
 import qualified Gogol.Prelude as Core
+import Gogol.ConsumerSurveys.Types
 
 -- | A resource alias for @consumersurveys.surveys.insert@ method which the
 -- 'ConsumerSurveysSurveysInsert' request conforms to.
 type ConsumerSurveysSurveysInsertResource =
-  "consumersurveys"
-    Core.:> "v2"
-    Core.:> "surveys"
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] Survey
-    Core.:> Core.Post '[Core.JSON] Survey
+     "consumersurveys" Core.:>
+       "v2" Core.:>
+         "surveys" Core.:>
+           Core.QueryParam "alt" Core.AltJSON Core.:>
+             Core.ReqBody '[Core.JSON] Survey Core.:>
+               Core.Post '[Core.JSON] Survey
 
 -- | Creates a survey.
 --
 -- /See:/ 'newConsumerSurveysSurveysInsert' smart constructor.
 newtype ConsumerSurveysSurveysInsert = ConsumerSurveysSurveysInsert
-  { -- | Multipart request metadata.
-    payload :: Survey
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | Multipart request metadata.
+      payload :: Survey
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'ConsumerSurveysSurveysInsert' with the minimum fields required to make a request.
-newConsumerSurveysSurveysInsert ::
-  -- |  Multipart request metadata. See 'payload'.
-  Survey ->
-  ConsumerSurveysSurveysInsert
+newConsumerSurveysSurveysInsert 
+    ::  Survey
+       -- ^  Multipart request metadata. See 'payload'.
+    -> ConsumerSurveysSurveysInsert
 newConsumerSurveysSurveysInsert payload =
   ConsumerSurveysSurveysInsert {payload = payload}
 
-instance
-  Core.GoogleRequest
-    ConsumerSurveysSurveysInsert
-  where
-  type Rs ConsumerSurveysSurveysInsert = Survey
-  type
-    Scopes ConsumerSurveysSurveysInsert =
-      '[ "https://www.googleapis.com/auth/consumersurveys",
-         "https://www.googleapis.com/auth/userinfo.email"
-       ]
-  requestClient ConsumerSurveysSurveysInsert {..} =
-    go
-      (Core.Just Core.AltJSON)
-      payload
-      consumerSurveysService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy ConsumerSurveysSurveysInsertResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           ConsumerSurveysSurveysInsert
+         where
+        type Rs ConsumerSurveysSurveysInsert = Survey
+        type Scopes ConsumerSurveysSurveysInsert =
+             '["https://www.googleapis.com/auth/consumersurveys",
+               "https://www.googleapis.com/auth/userinfo.email"]
+        requestClient ConsumerSurveysSurveysInsert{..}
+          = go (Core.Just Core.AltJSON) payload
+              consumerSurveysService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy ConsumerSurveysSurveysInsertResource)
+                      Core.mempty
+
