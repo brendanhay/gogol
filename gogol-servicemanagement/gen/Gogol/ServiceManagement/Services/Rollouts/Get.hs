7f6126f14f196ac403179ecaaf1e049ac930308c
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 12:30:27 +0100

    gogol-servicemanagement: regenerating service with updated namespace

---
+++ gogol-servicemanagement/gen/Gogol/ServiceManagement/Services/Rollouts/Get.hs
@@ -0,0 +1,133 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.ServiceManagement.Services.Rollouts.Get
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Gets a service configuration rollout.
+--
+-- /See:/ <https://cloud.google.com/service-management/ Service Management API Reference> for @servicemanagement.services.rollouts.get@.
+module Gogol.ServiceManagement.Services.Rollouts.Get
+  ( -- * Resource
+    ServiceManagementServicesRolloutsGetResource,
+
+    -- ** Constructing a Request
+    newServiceManagementServicesRolloutsGet,
+    ServiceManagementServicesRolloutsGet,
+  )
+where
+
+import qualified Gogol.Prelude as Core
+import Gogol.ServiceManagement.Types
+
+-- | A resource alias for @servicemanagement.services.rollouts.get@ method which the
+-- 'ServiceManagementServicesRolloutsGet' request conforms to.
+type ServiceManagementServicesRolloutsGetResource =
+  "v1"
+    Core.:> "services"
+    Core.:> Core.Capture "serviceName" Core.Text
+    Core.:> "rollouts"
+    Core.:> Core.Capture "rolloutId" Core.Text
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Get '[Core.JSON] Rollout
+
+-- | Gets a service configuration rollout.
+--
+-- /See:/ 'newServiceManagementServicesRolloutsGet' smart constructor.
+data ServiceManagementServicesRolloutsGet = ServiceManagementServicesRolloutsGet
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | Required. The id of the rollout resource.
+    rolloutId :: Core.Text,
+    -- | Required. The name of the service. See the </service-management/overview overview> for naming requirements. For example: @example.googleapis.com@.
+    serviceName :: Core.Text,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'ServiceManagementServicesRolloutsGet' with the minimum fields required to make a request.
+newServiceManagementServicesRolloutsGet ::
+  -- |  Required. The id of the rollout resource. See 'rolloutId'.
+  Core.Text ->
+  -- |  Required. The name of the service. See the </service-management/overview overview> for naming requirements. For example: @example.googleapis.com@. See 'serviceName'.
+  Core.Text ->
+  ServiceManagementServicesRolloutsGet
+newServiceManagementServicesRolloutsGet rolloutId serviceName =
+  ServiceManagementServicesRolloutsGet
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      rolloutId = rolloutId,
+      serviceName = serviceName,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    ServiceManagementServicesRolloutsGet
+  where
+  type
+    Rs ServiceManagementServicesRolloutsGet =
+      Rollout
+  type
+    Scopes ServiceManagementServicesRolloutsGet =
+      '[ "https://www.googleapis.com/auth/cloud-platform",
+         "https://www.googleapis.com/auth/cloud-platform.read-only",
+         "https://www.googleapis.com/auth/service.management",
+         "https://www.googleapis.com/auth/service.management.readonly"
+       ]
+  requestClient
+    ServiceManagementServicesRolloutsGet {..} =
+      go
+        serviceName
+        rolloutId
+        xgafv
+        accessToken
+        callback
+        uploadType
+        uploadProtocol
+        (Core.Just Core.AltJSON)
+        serviceManagementService
+      where
+        go =
+          Core.buildClient
+            ( Core.Proxy ::
+                Core.Proxy
+                  ServiceManagementServicesRolloutsGetResource
+            )
+            Core.mempty
