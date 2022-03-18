dea77c9e6b2bec3732995f5e4929da09e77cd61c
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:41:57 +0100

    gogol-apps-reseller: regenerating service with updated namespace

--- lib/services/gogol-apps-reseller/gen/Gogol/AppsReseller/Reseller/Customers/Get.hs
+++ lib/services/gogol-apps-reseller/gen/Gogol/AppsReseller/Reseller/Customers/Get.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,89 +36,83 @@
 --
 -- /See:/ <https://developers.google.com/google-apps/reseller/ Google Workspace Reseller API Reference> for @reseller.customers.get@.
 module Gogol.AppsReseller.Reseller.Customers.Get
-  ( -- * Resource
-    ResellerCustomersGetResource,
+    (
+    -- * Resource
+      ResellerCustomersGetResource
 
     -- ** Constructing a Request
-    newResellerCustomersGet,
-    ResellerCustomersGet,
-  )
-where
+    , newResellerCustomersGet
+    , ResellerCustomersGet
+    ) where
 
-import Gogol.AppsReseller.Types
 import qualified Gogol.Prelude as Core
+import Gogol.AppsReseller.Types
 
 -- | A resource alias for @reseller.customers.get@ method which the
 -- 'ResellerCustomersGet' request conforms to.
 type ResellerCustomersGetResource =
-  "apps"
-    Core.:> "reseller"
-    Core.:> "v1"
-    Core.:> "customers"
-    Core.:> Core.Capture "customerId" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] Customer
+     "apps" Core.:>
+       "reseller" Core.:>
+         "v1" Core.:>
+           "customers" Core.:>
+             Core.Capture "customerId" Core.Text Core.:>
+               Core.QueryParam "$.xgafv" Xgafv Core.:>
+                 Core.QueryParam "access_token" Core.Text Core.:>
+                   Core.QueryParam "callback" Core.Text Core.:>
+                     Core.QueryParam "uploadType" Core.Text Core.:>
+                       Core.QueryParam "upload_protocol" Core.Text Core.:>
+                         Core.QueryParam "alt" Core.AltJSON Core.:>
+                           Core.Get '[Core.JSON] Customer
 
 -- | Gets a customer account. Use this operation to see a customer account already in your reseller management, or to see the minimal account information for an existing customer that you do not manage. For more information about the API response for existing customers, see </admin-sdk/reseller/v1/how-tos/manage_customers#get_customer retrieving a customer account>.
 --
 -- /See:/ 'newResellerCustomersGet' smart constructor.
 data ResellerCustomersGet = ResellerCustomersGet
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | This can be either the customer\'s primary domain name or the customer\'s unique identifier. If the domain name for a customer changes, the old domain name cannot be used to access the customer, but the customer\'s unique identifier (as returned by the API) can always be used. We recommend storing the unique identifier in your systems where applicable.
-    customerId :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | This can be either the customer\'s primary domain name or the customer\'s unique identifier. If the domain name for a customer changes, the old domain name cannot be used to access the customer, but the customer\'s unique identifier (as returned by the API) can always be used. We recommend storing the unique identifier in your systems where applicable.
+    , customerId :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'ResellerCustomersGet' with the minimum fields required to make a request.
-newResellerCustomersGet ::
-  -- |  This can be either the customer\'s primary domain name or the customer\'s unique identifier. If the domain name for a customer changes, the old domain name cannot be used to access the customer, but the customer\'s unique identifier (as returned by the API) can always be used. We recommend storing the unique identifier in your systems where applicable. See 'customerId'.
-  Core.Text ->
-  ResellerCustomersGet
+newResellerCustomersGet 
+    ::  Core.Text
+       -- ^  This can be either the customer\'s primary domain name or the customer\'s unique identifier. If the domain name for a customer changes, the old domain name cannot be used to access the customer, but the customer\'s unique identifier (as returned by the API) can always be used. We recommend storing the unique identifier in your systems where applicable. See 'customerId'.
+    -> ResellerCustomersGet
 newResellerCustomersGet customerId =
   ResellerCustomersGet
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      customerId = customerId,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , customerId = customerId
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance Core.GoogleRequest ResellerCustomersGet where
-  type Rs ResellerCustomersGet = Customer
-  type
-    Scopes ResellerCustomersGet =
-      '[ "https://www.googleapis.com/auth/apps.order",
-         "https://www.googleapis.com/auth/apps.order.readonly"
-       ]
-  requestClient ResellerCustomersGet {..} =
-    go
-      customerId
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      appsResellerService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy ResellerCustomersGetResource
-          )
-          Core.mempty
+instance Core.GoogleRequest ResellerCustomersGet
+         where
+        type Rs ResellerCustomersGet = Customer
+        type Scopes ResellerCustomersGet =
+             '["https://www.googleapis.com/auth/apps.order",
+               "https://www.googleapis.com/auth/apps.order.readonly"]
+        requestClient ResellerCustomersGet{..}
+          = go customerId xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              appsResellerService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy ResellerCustomersGetResource)
+                      Core.mempty
+
--- lib/services/gogol-apps-reseller/gen/Gogol/AppsReseller/Reseller/Subscriptions/ChangePlan.hs
+++ lib/services/gogol-apps-reseller/gen/Gogol/AppsReseller/Reseller/Subscriptions/ChangePlan.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,108 +36,101 @@
 --
 -- /See:/ <https://developers.google.com/google-apps/reseller/ Google Workspace Reseller API Reference> for @reseller.subscriptions.changePlan@.
 module Gogol.AppsReseller.Reseller.Subscriptions.ChangePlan
-  ( -- * Resource
-    ResellerSubscriptionsChangePlanResource,
+    (
+    -- * Resource
+      ResellerSubscriptionsChangePlanResource
 
     -- ** Constructing a Request
-    newResellerSubscriptionsChangePlan,
-    ResellerSubscriptionsChangePlan,
-  )
-where
+    , newResellerSubscriptionsChangePlan
+    , ResellerSubscriptionsChangePlan
+    ) where
 
-import Gogol.AppsReseller.Types
 import qualified Gogol.Prelude as Core
+import Gogol.AppsReseller.Types
 
 -- | A resource alias for @reseller.subscriptions.changePlan@ method which the
 -- 'ResellerSubscriptionsChangePlan' request conforms to.
 type ResellerSubscriptionsChangePlanResource =
-  "apps"
-    Core.:> "reseller"
-    Core.:> "v1"
-    Core.:> "customers"
-    Core.:> Core.Capture "customerId" Core.Text
-    Core.:> "subscriptions"
-    Core.:> Core.Capture "subscriptionId" Core.Text
-    Core.:> "changePlan"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] ChangePlanRequest
-    Core.:> Core.Post '[Core.JSON] Subscription
+     "apps" Core.:>
+       "reseller" Core.:>
+         "v1" Core.:>
+           "customers" Core.:>
+             Core.Capture "customerId" Core.Text Core.:>
+               "subscriptions" Core.:>
+                 Core.Capture "subscriptionId" Core.Text Core.:>
+                   "changePlan" Core.:>
+                     Core.QueryParam "$.xgafv" Xgafv Core.:>
+                       Core.QueryParam "access_token" Core.Text Core.:>
+                         Core.QueryParam "callback" Core.Text Core.:>
+                           Core.QueryParam "uploadType" Core.Text Core.:>
+                             Core.QueryParam "upload_protocol" Core.Text Core.:>
+                               Core.QueryParam "alt" Core.AltJSON Core.:>
+                                 Core.ReqBody '[Core.JSON] ChangePlanRequest
+                                   Core.:> Core.Post '[Core.JSON] Subscription
 
 -- | Updates a subscription plan. Use this method to update a plan for a 30-day trial or a flexible plan subscription to an annual commitment plan with monthly or yearly payments. How a plan is updated differs depending on the plan and the products. For more information, see the description in </admin-sdk/reseller/v1/how-tos/manage_subscriptions#update_subscription_plan manage subscriptions>.
 --
 -- /See:/ 'newResellerSubscriptionsChangePlan' smart constructor.
 data ResellerSubscriptionsChangePlan = ResellerSubscriptionsChangePlan
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | This can be either the customer\'s primary domain name or the customer\'s unique identifier. If the domain name for a customer changes, the old domain name cannot be used to access the customer, but the customer\'s unique identifier (as returned by the API) can always be used. We recommend storing the unique identifier in your systems where applicable.
-    customerId :: Core.Text,
-    -- | Multipart request metadata.
-    payload :: ChangePlanRequest,
-    -- | This is a required property. The @subscriptionId@ is the subscription identifier and is unique for each customer. Since a @subscriptionId@ changes when a subscription is updated, we recommend to not use this ID as a key for persistent data. And the @subscriptionId@ can be found using the retrieve all reseller subscriptions method.
-    subscriptionId :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | This can be either the customer\'s primary domain name or the customer\'s unique identifier. If the domain name for a customer changes, the old domain name cannot be used to access the customer, but the customer\'s unique identifier (as returned by the API) can always be used. We recommend storing the unique identifier in your systems where applicable.
+    , customerId :: Core.Text
+      -- | Multipart request metadata.
+    , payload :: ChangePlanRequest
+      -- | This is a required property. The @subscriptionId@ is the subscription identifier and is unique for each customer. Since a @subscriptionId@ changes when a subscription is updated, we recommend to not use this ID as a key for persistent data. And the @subscriptionId@ can be found using the retrieve all reseller subscriptions method.
+    , subscriptionId :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'ResellerSubscriptionsChangePlan' with the minimum fields required to make a request.
-newResellerSubscriptionsChangePlan ::
-  -- |  This can be either the customer\'s primary domain name or the customer\'s unique identifier. If the domain name for a customer changes, the old domain name cannot be used to access the customer, but the customer\'s unique identifier (as returned by the API) can always be used. We recommend storing the unique identifier in your systems where applicable. See 'customerId'.
-  Core.Text ->
-  -- |  Multipart request metadata. See 'payload'.
-  ChangePlanRequest ->
-  -- |  This is a required property. The @subscriptionId@ is the subscription identifier and is unique for each customer. Since a @subscriptionId@ changes when a subscription is updated, we recommend to not use this ID as a key for persistent data. And the @subscriptionId@ can be found using the retrieve all reseller subscriptions method. See 'subscriptionId'.
-  Core.Text ->
-  ResellerSubscriptionsChangePlan
+newResellerSubscriptionsChangePlan 
+    ::  Core.Text
+       -- ^  This can be either the customer\'s primary domain name or the customer\'s unique identifier. If the domain name for a customer changes, the old domain name cannot be used to access the customer, but the customer\'s unique identifier (as returned by the API) can always be used. We recommend storing the unique identifier in your systems where applicable. See 'customerId'.
+    -> ChangePlanRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> Core.Text
+       -- ^  This is a required property. The @subscriptionId@ is the subscription identifier and is unique for each customer. Since a @subscriptionId@ changes when a subscription is updated, we recommend to not use this ID as a key for persistent data. And the @subscriptionId@ can be found using the retrieve all reseller subscriptions method. See 'subscriptionId'.
+    -> ResellerSubscriptionsChangePlan
 newResellerSubscriptionsChangePlan customerId payload subscriptionId =
   ResellerSubscriptionsChangePlan
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      customerId = customerId,
-      payload = payload,
-      subscriptionId = subscriptionId,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , customerId = customerId
+    , payload = payload
+    , subscriptionId = subscriptionId
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    ResellerSubscriptionsChangePlan
-  where
-  type
-    Rs ResellerSubscriptionsChangePlan =
-      Subscription
-  type
-    Scopes ResellerSubscriptionsChangePlan =
-      '["https://www.googleapis.com/auth/apps.order"]
-  requestClient ResellerSubscriptionsChangePlan {..} =
-    go
-      customerId
-      subscriptionId
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      payload
-      appsResellerService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy ResellerSubscriptionsChangePlanResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           ResellerSubscriptionsChangePlan
+         where
+        type Rs ResellerSubscriptionsChangePlan =
+             Subscription
+        type Scopes ResellerSubscriptionsChangePlan =
+             '["https://www.googleapis.com/auth/apps.order"]
+        requestClient ResellerSubscriptionsChangePlan{..}
+          = go customerId subscriptionId xgafv accessToken
+              callback
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              appsResellerService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy ResellerSubscriptionsChangePlanResource)
+                      Core.mempty
+
