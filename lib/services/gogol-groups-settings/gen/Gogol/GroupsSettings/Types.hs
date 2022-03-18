90b61acb48b4a26733d51dcdf4924781a913066e
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:05 +0100

    gogol-groups-settings: regenerating service with updated namespace

--- lib/services/gogol-groups-settings/gen/Gogol/GroupsSettings/Types.hs
+++ lib/services/gogol-groups-settings/gen/Gogol/GroupsSettings/Types.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -25,31 +31,32 @@
 -- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
+--
 module Gogol.GroupsSettings.Types
-  ( -- * Configuration
-    groupsSettingsService,
+    (
+    -- * Configuration
+      groupsSettingsService
 
     -- * OAuth Scopes
-    appsGroupsSettingsScope,
+    , appsGroupsSettingsScope
 
     -- * Types
 
     -- ** Groups
-    Groups (..),
-    newGroups,
-  )
-where
+    , Groups (..)
+    , newGroups
+    ) where
 
+import qualified Gogol.Prelude as Core
 import Gogol.GroupsSettings.Internal.Product
 import Gogol.GroupsSettings.Internal.Sum
-import qualified Gogol.Prelude as Core
 
 -- | Default request referring to version @v1@ of the Groups Settings API. This contains the host and root path used as a starting point for constructing service requests.
 groupsSettingsService :: Core.ServiceConfig
-groupsSettingsService =
-  Core.defaultService
-    (Core.ServiceId "groupssettings:v1")
-    "www.googleapis.com"
+groupsSettingsService
+  = Core.defaultService
+      (Core.ServiceId "groupssettings:v1")
+      "www.googleapis.com"
 
 -- | View and manage the settings of a G Suite group
 appsGroupsSettingsScope :: Core.Proxy '["https://www.googleapis.com/auth/apps.groups.settings"]
