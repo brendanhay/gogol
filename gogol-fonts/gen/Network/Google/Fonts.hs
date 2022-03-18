1c411f181b3bb58b8d1fa1230832bf30ccebcf9b
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 11:23:08 +0100

    gogol-fonts: regenerating service

--- gogol-fonts/gen/Network/Google/Fonts.hs
+++ gogol-fonts/gen/Network/Google/Fonts.hs
@@ -1,74 +1,66 @@
-{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE TypeOperators     #-}
-
-{-# OPTIONS_GHC -fno-warn-unused-imports    #-}
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 -- |
 -- Module      : Network.Google.Fonts
--- Copyright   : (c) 2015-2016 Brendan Hay
+-- Copyright   : (c) 2015-2022 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 --
--- The Google Web Fonts Developer API lets you retrieve information about
--- web fonts served by Google.
+-- The Google Web Fonts Developer API lets you retrieve information about web fonts served by Google.
 --
 -- /See:/ <https://developers.google.com/fonts/docs/developer_api Web Fonts Developer API Reference>
 module Network.Google.Fonts
-    (
-    -- * Service Configuration
-      fontsService
-
-    -- * API Declaration
-    , FontsAPI
+  ( -- * Configuration
+    fontsService,
 
     -- * Resources
 
     -- ** webfonts.webfonts.list
-    , module Network.Google.Resource.Webfonts.Webfonts.List
+    WebfontsWebfontsListResource,
+    newWebfontsWebfontsList,
+    WebfontsWebfontsList,
 
     -- * Types
 
+    -- ** Xgafv
+    Xgafv (..),
+
     -- ** Webfont
-    , Webfont
-    , webfont
-    , wVariants
-    , wKind
-    , wCategory
-    , wFamily
-    , wVersion
-    , wFiles
-    , wSubSets
-    , wLastModified
+    Webfont (..),
+    newWebfont,
+
+    -- ** Webfont_Files
+    Webfont_Files (..),
+    newWebfont_Files,
 
     -- ** WebfontList
-    , WebfontList
-    , webfontList
-    , wlKind
-    , wlItems
-
-    -- ** Xgafv
-    , Xgafv (..)
+    WebfontList (..),
+    newWebfontList,
 
     -- ** WebfontsListSort
-    , WebfontsListSort (..)
+    WebfontsListSort (..),
+  )
+where
 
-    -- ** WebfontFiles
-    , WebfontFiles
-    , webfontFiles
-    , wfAddtional
-    ) where
-
-import Network.Google.Prelude
 import Network.Google.Fonts.Types
-import Network.Google.Resource.Webfonts.Webfonts.List
-
-{- $resources
-TODO
--}
-
--- | Represents the entirety of the methods and resources available for the Web Fonts Developer API service.
-type FontsAPI = WebfontsListResource
+import Network.Google.Fonts.Webfonts.List
