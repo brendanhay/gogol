18fb94d587f273513081ac9a67bc22f775b3463a
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:41:57 +0100

    gogol-apps-licensing: regenerating service with updated namespace

--- lib/services/gogol-apps-licensing/gen/Gogol/AppsLicensing/Internal/Product.hs
+++ lib/services/gogol-apps-licensing/gen/Gogol/AppsLicensing/Internal/Product.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -25,8 +31,11 @@
 -- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
+--
 module Gogol.AppsLicensing.Internal.Product
-  ( -- * Empty
+  (
+
+    -- * Empty
     Empty (..),
     newEmpty,
 
@@ -41,175 +50,170 @@ module Gogol.AppsLicensing.Internal.Product
     -- * LicenseAssignmentList
     LicenseAssignmentList (..),
     newLicenseAssignmentList,
-  )
-where
+  ) where
 
-import Gogol.AppsLicensing.Internal.Sum
 import qualified Gogol.Prelude as Core
+import Gogol.AppsLicensing.Internal.Sum
 
 -- | A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs. A typical example is to use it as the request or the response type of an API method. For instance: service Foo { rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty); } The JSON representation for @Empty@ is empty JSON object @{}@.
 --
 -- /See:/ 'newEmpty' smart constructor.
 data Empty = Empty
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'Empty' with the minimum fields required to make a request.
-newEmpty ::
-  Empty
+newEmpty 
+    ::  Empty
 newEmpty = Empty
 
 instance Core.FromJSON Empty where
-  parseJSON =
-    Core.withObject "Empty" (\o -> Core.pure Empty)
+        parseJSON
+          = Core.withObject "Empty" (\ o -> Core.pure Empty)
 
 instance Core.ToJSON Empty where
-  toJSON = Core.const Core.emptyObject
+        toJSON = Core.const Core.emptyObject
+
 
 -- | Representation of a license assignment.
 --
 -- /See:/ 'newLicenseAssignment' smart constructor.
 data LicenseAssignment = LicenseAssignment
-  { -- | ETag of the resource.
-    etags :: (Core.Maybe Core.Text),
-    -- | Identifies the resource as a LicenseAssignment, which is @licensing#licenseAssignment@.
-    kind :: Core.Text,
-    -- | A product\'s unique identifier. For more information about products in this version of the API, see Product and SKU IDs.
-    productId :: (Core.Maybe Core.Text),
-    -- | Display Name of the product.
-    productName :: (Core.Maybe Core.Text),
-    -- | Link to this page.
-    selfLink :: (Core.Maybe Core.Text),
-    -- | A product SKU\'s unique identifier. For more information about available SKUs in this version of the API, see Products and SKUs.
-    skuId :: (Core.Maybe Core.Text),
-    -- | Display Name of the sku of the product.
-    skuName :: (Core.Maybe Core.Text),
-    -- | The user\'s current primary email address. If the user\'s email address changes, use the new email address in your API requests. Since a @userId@ is subject to change, do not use a @userId@ value as a key for persistent data. This key could break if the current user\'s email address changes. If the @userId@ is suspended, the license status changes.
-    userId :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | ETag of the resource.
+      etags :: (Core.Maybe Core.Text)
+      -- | Identifies the resource as a LicenseAssignment, which is @licensing#licenseAssignment@.
+    , kind :: Core.Text
+      -- | A product\'s unique identifier. For more information about products in this version of the API, see Product and SKU IDs.
+    , productId :: (Core.Maybe Core.Text)
+      -- | Display Name of the product.
+    , productName :: (Core.Maybe Core.Text)
+      -- | Link to this page.
+    , selfLink :: (Core.Maybe Core.Text)
+      -- | A product SKU\'s unique identifier. For more information about available SKUs in this version of the API, see Products and SKUs.
+    , skuId :: (Core.Maybe Core.Text)
+      -- | Display Name of the sku of the product.
+    , skuName :: (Core.Maybe Core.Text)
+      -- | The user\'s current primary email address. If the user\'s email address changes, use the new email address in your API requests. Since a @userId@ is subject to change, do not use a @userId@ value as a key for persistent data. This key could break if the current user\'s email address changes. If the @userId@ is suspended, the license status changes.
+    , userId :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'LicenseAssignment' with the minimum fields required to make a request.
-newLicenseAssignment ::
-  LicenseAssignment
+newLicenseAssignment 
+    ::  LicenseAssignment
 newLicenseAssignment =
   LicenseAssignment
-    { etags = Core.Nothing,
-      kind = "licensing#licenseAssignment",
-      productId = Core.Nothing,
-      productName = Core.Nothing,
-      selfLink = Core.Nothing,
-      skuId = Core.Nothing,
-      skuName = Core.Nothing,
-      userId = Core.Nothing
+    { etags = Core.Nothing
+    , kind = "licensing#licenseAssignment"
+    , productId = Core.Nothing
+    , productName = Core.Nothing
+    , selfLink = Core.Nothing
+    , skuId = Core.Nothing
+    , skuName = Core.Nothing
+    , userId = Core.Nothing
     }
 
 instance Core.FromJSON LicenseAssignment where
-  parseJSON =
-    Core.withObject
-      "LicenseAssignment"
-      ( \o ->
-          LicenseAssignment
-            Core.<$> (o Core..:? "etags")
-            Core.<*> ( o Core..:? "kind"
-                         Core..!= "licensing#licenseAssignment"
-                     )
-            Core.<*> (o Core..:? "productId")
-            Core.<*> (o Core..:? "productName")
-            Core.<*> (o Core..:? "selfLink")
-            Core.<*> (o Core..:? "skuId")
-            Core.<*> (o Core..:? "skuName")
-            Core.<*> (o Core..:? "userId")
-      )
+        parseJSON
+          = Core.withObject "LicenseAssignment"
+              (\ o ->
+                 LicenseAssignment Core.<$>
+                   (o Core..:? "etags") Core.<*>
+                     (o Core..:? "kind" Core..!=
+                        "licensing#licenseAssignment")
+                     Core.<*> (o Core..:? "productId")
+                     Core.<*> (o Core..:? "productName")
+                     Core.<*> (o Core..:? "selfLink")
+                     Core.<*> (o Core..:? "skuId")
+                     Core.<*> (o Core..:? "skuName")
+                     Core.<*> (o Core..:? "userId"))
 
 instance Core.ToJSON LicenseAssignment where
-  toJSON LicenseAssignment {..} =
-    Core.object
-      ( Core.catMaybes
-          [ ("etags" Core..=) Core.<$> etags,
-            Core.Just ("kind" Core..= kind),
-            ("productId" Core..=) Core.<$> productId,
-            ("productName" Core..=) Core.<$> productName,
-            ("selfLink" Core..=) Core.<$> selfLink,
-            ("skuId" Core..=) Core.<$> skuId,
-            ("skuName" Core..=) Core.<$> skuName,
-            ("userId" Core..=) Core.<$> userId
-          ]
-      )
+        toJSON LicenseAssignment{..}
+          = Core.object
+              (Core.catMaybes
+                 [("etags" Core..=) Core.<$> etags,
+                  Core.Just ("kind" Core..= kind),
+                  ("productId" Core..=) Core.<$> productId,
+                  ("productName" Core..=) Core.<$> productName,
+                  ("selfLink" Core..=) Core.<$> selfLink,
+                  ("skuId" Core..=) Core.<$> skuId,
+                  ("skuName" Core..=) Core.<$> skuName,
+                  ("userId" Core..=) Core.<$> userId])
+
 
 -- | Representation of a license assignment.
 --
 -- /See:/ 'newLicenseAssignmentInsert' smart constructor.
 newtype LicenseAssignmentInsert = LicenseAssignmentInsert
-  { -- | Email id of the user
-    userId :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | Email id of the user
+      userId :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'LicenseAssignmentInsert' with the minimum fields required to make a request.
-newLicenseAssignmentInsert ::
-  LicenseAssignmentInsert
+newLicenseAssignmentInsert 
+    ::  LicenseAssignmentInsert
 newLicenseAssignmentInsert = LicenseAssignmentInsert {userId = Core.Nothing}
 
 instance Core.FromJSON LicenseAssignmentInsert where
-  parseJSON =
-    Core.withObject
-      "LicenseAssignmentInsert"
-      ( \o ->
-          LicenseAssignmentInsert
-            Core.<$> (o Core..:? "userId")
-      )
+        parseJSON
+          = Core.withObject "LicenseAssignmentInsert"
+              (\ o ->
+                 LicenseAssignmentInsert Core.<$>
+                   (o Core..:? "userId"))
 
 instance Core.ToJSON LicenseAssignmentInsert where
-  toJSON LicenseAssignmentInsert {..} =
-    Core.object
-      (Core.catMaybes [("userId" Core..=) Core.<$> userId])
+        toJSON LicenseAssignmentInsert{..}
+          = Core.object
+              (Core.catMaybes [("userId" Core..=) Core.<$> userId])
+
 
 --
 -- /See:/ 'newLicenseAssignmentList' smart constructor.
 data LicenseAssignmentList = LicenseAssignmentList
-  { -- | ETag of the resource.
-    etag :: (Core.Maybe Core.Text),
-    -- | The LicenseAssignments in this page of results.
-    items :: (Core.Maybe [LicenseAssignment]),
-    -- | Identifies the resource as a collection of LicenseAssignments.
-    kind :: Core.Text,
-    -- | The token that you must submit in a subsequent request to retrieve additional license results matching your query parameters. The @maxResults@ query string is related to the @nextPageToken@ since @maxResults@ determines how many entries are returned on each next page.
-    nextPageToken :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | ETag of the resource.
+      etag :: (Core.Maybe Core.Text)
+      -- | The LicenseAssignments in this page of results.
+    , items :: (Core.Maybe [LicenseAssignment])
+      -- | Identifies the resource as a collection of LicenseAssignments.
+    , kind :: Core.Text
+      -- | The token that you must submit in a subsequent request to retrieve additional license results matching your query parameters. The @maxResults@ query string is related to the @nextPageToken@ since @maxResults@ determines how many entries are returned on each next page.
+    , nextPageToken :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'LicenseAssignmentList' with the minimum fields required to make a request.
-newLicenseAssignmentList ::
-  LicenseAssignmentList
+newLicenseAssignmentList 
+    ::  LicenseAssignmentList
 newLicenseAssignmentList =
   LicenseAssignmentList
-    { etag = Core.Nothing,
-      items = Core.Nothing,
-      kind = "licensing#licenseAssignmentList",
-      nextPageToken = Core.Nothing
+    { etag = Core.Nothing
+    , items = Core.Nothing
+    , kind = "licensing#licenseAssignmentList"
+    , nextPageToken = Core.Nothing
     }
 
 instance Core.FromJSON LicenseAssignmentList where
-  parseJSON =
-    Core.withObject
-      "LicenseAssignmentList"
-      ( \o ->
-          LicenseAssignmentList
-            Core.<$> (o Core..:? "etag")
-            Core.<*> (o Core..:? "items" Core..!= Core.mempty)
-            Core.<*> ( o Core..:? "kind"
-                         Core..!= "licensing#licenseAssignmentList"
-                     )
-            Core.<*> (o Core..:? "nextPageToken")
-      )
+        parseJSON
+          = Core.withObject "LicenseAssignmentList"
+              (\ o ->
+                 LicenseAssignmentList Core.<$>
+                   (o Core..:? "etag") Core.<*>
+                     (o Core..:? "items" Core..!= Core.mempty)
+                     Core.<*>
+                     (o Core..:? "kind" Core..!=
+                        "licensing#licenseAssignmentList")
+                     Core.<*> (o Core..:? "nextPageToken"))
 
 instance Core.ToJSON LicenseAssignmentList where
-  toJSON LicenseAssignmentList {..} =
-    Core.object
-      ( Core.catMaybes
-          [ ("etag" Core..=) Core.<$> etag,
-            ("items" Core..=) Core.<$> items,
-            Core.Just ("kind" Core..= kind),
-            ("nextPageToken" Core..=) Core.<$> nextPageToken
-          ]
-      )
+        toJSON LicenseAssignmentList{..}
+          = Core.object
+              (Core.catMaybes
+                 [("etag" Core..=) Core.<$> etag,
+                  ("items" Core..=) Core.<$> items,
+                  Core.Just ("kind" Core..= kind),
+                  ("nextPageToken" Core..=) Core.<$> nextPageToken])
+
--- lib/services/gogol-apps-licensing/gen/Gogol/AppsLicensing/Licensing/LicenseAssignments/Update.hs
+++ lib/services/gogol-apps-licensing/gen/Gogol/AppsLicensing/Licensing/LicenseAssignments/Update.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,115 +36,109 @@
 --
 -- /See:/ <https://developers.google.com/admin-sdk/licensing/ Enterprise License Manager API Reference> for @licensing.licenseAssignments.update@.
 module Gogol.AppsLicensing.Licensing.LicenseAssignments.Update
-  ( -- * Resource
-    LicensingLicenseAssignmentsUpdateResource,
+    (
+    -- * Resource
+      LicensingLicenseAssignmentsUpdateResource
 
     -- ** Constructing a Request
-    newLicensingLicenseAssignmentsUpdate,
-    LicensingLicenseAssignmentsUpdate,
-  )
-where
+    , newLicensingLicenseAssignmentsUpdate
+    , LicensingLicenseAssignmentsUpdate
+    ) where
 
-import Gogol.AppsLicensing.Types
 import qualified Gogol.Prelude as Core
+import Gogol.AppsLicensing.Types
 
 -- | A resource alias for @licensing.licenseAssignments.update@ method which the
 -- 'LicensingLicenseAssignmentsUpdate' request conforms to.
 type LicensingLicenseAssignmentsUpdateResource =
-  "apps"
-    Core.:> "licensing"
-    Core.:> "v1"
-    Core.:> "product"
-    Core.:> Core.Capture "productId" Core.Text
-    Core.:> "sku"
-    Core.:> Core.Capture "skuId" Core.Text
-    Core.:> "user"
-    Core.:> Core.Capture "userId" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] LicenseAssignment
-    Core.:> Core.Put '[Core.JSON] LicenseAssignment
+     "apps" Core.:>
+       "licensing" Core.:>
+         "v1" Core.:>
+           "product" Core.:>
+             Core.Capture "productId" Core.Text Core.:>
+               "sku" Core.:>
+                 Core.Capture "skuId" Core.Text Core.:>
+                   "user" Core.:>
+                     Core.Capture "userId" Core.Text Core.:>
+                       Core.QueryParam "$.xgafv" Xgafv Core.:>
+                         Core.QueryParam "access_token" Core.Text Core.:>
+                           Core.QueryParam "callback" Core.Text Core.:>
+                             Core.QueryParam "uploadType" Core.Text Core.:>
+                               Core.QueryParam "upload_protocol" Core.Text
+                                 Core.:>
+                                 Core.QueryParam "alt" Core.AltJSON Core.:>
+                                   Core.ReqBody '[Core.JSON] LicenseAssignment
+                                     Core.:>
+                                     Core.Put '[Core.JSON] LicenseAssignment
 
 -- | Reassign a user\'s product SKU with a different SKU in the same product.
 --
 -- /See:/ 'newLicensingLicenseAssignmentsUpdate' smart constructor.
 data LicensingLicenseAssignmentsUpdate = LicensingLicenseAssignmentsUpdate
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Multipart request metadata.
-    payload :: LicenseAssignment,
-    -- | A product\'s unique identifier. For more information about products in this version of the API, see Products and SKUs.
-    productId :: Core.Text,
-    -- | A product SKU\'s unique identifier. For more information about available SKUs in this version of the API, see Products and SKUs.
-    skuId :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text),
-    -- | The user\'s current primary email address. If the user\'s email address changes, use the new email address in your API requests. Since a @userId@ is subject to change, do not use a @userId@ value as a key for persistent data. This key could break if the current user\'s email address changes. If the @userId@ is suspended, the license status changes.
-    userId :: Core.Text
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Multipart request metadata.
+    , payload :: LicenseAssignment
+      -- | A product\'s unique identifier. For more information about products in this version of the API, see Products and SKUs.
+    , productId :: Core.Text
+      -- | A product SKU\'s unique identifier. For more information about available SKUs in this version of the API, see Products and SKUs.
+    , skuId :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+      -- | The user\'s current primary email address. If the user\'s email address changes, use the new email address in your API requests. Since a @userId@ is subject to change, do not use a @userId@ value as a key for persistent data. This key could break if the current user\'s email address changes. If the @userId@ is suspended, the license status changes.
+    , userId :: Core.Text
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'LicensingLicenseAssignmentsUpdate' with the minimum fields required to make a request.
-newLicensingLicenseAssignmentsUpdate ::
-  -- |  Multipart request metadata. See 'payload'.
-  LicenseAssignment ->
-  -- |  A product\'s unique identifier. For more information about products in this version of the API, see Products and SKUs. See 'productId'.
-  Core.Text ->
-  -- |  A product SKU\'s unique identifier. For more information about available SKUs in this version of the API, see Products and SKUs. See 'skuId'.
-  Core.Text ->
-  -- |  The user\'s current primary email address. If the user\'s email address changes, use the new email address in your API requests. Since a @userId@ is subject to change, do not use a @userId@ value as a key for persistent data. This key could break if the current user\'s email address changes. If the @userId@ is suspended, the license status changes. See 'userId'.
-  Core.Text ->
-  LicensingLicenseAssignmentsUpdate
+newLicensingLicenseAssignmentsUpdate 
+    ::  LicenseAssignment
+       -- ^  Multipart request metadata. See 'payload'.
+    -> Core.Text
+       -- ^  A product\'s unique identifier. For more information about products in this version of the API, see Products and SKUs. See 'productId'.
+    -> Core.Text
+       -- ^  A product SKU\'s unique identifier. For more information about available SKUs in this version of the API, see Products and SKUs. See 'skuId'.
+    -> Core.Text
+       -- ^  The user\'s current primary email address. If the user\'s email address changes, use the new email address in your API requests. Since a @userId@ is subject to change, do not use a @userId@ value as a key for persistent data. This key could break if the current user\'s email address changes. If the @userId@ is suspended, the license status changes. See 'userId'.
+    -> LicensingLicenseAssignmentsUpdate
 newLicensingLicenseAssignmentsUpdate payload productId skuId userId =
   LicensingLicenseAssignmentsUpdate
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      payload = payload,
-      productId = productId,
-      skuId = skuId,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing,
-      userId = userId
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , payload = payload
+    , productId = productId
+    , skuId = skuId
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
+    , userId = userId
     }
 
-instance
-  Core.GoogleRequest
-    LicensingLicenseAssignmentsUpdate
-  where
-  type
-    Rs LicensingLicenseAssignmentsUpdate =
-      LicenseAssignment
-  type
-    Scopes LicensingLicenseAssignmentsUpdate =
-      '["https://www.googleapis.com/auth/apps.licensing"]
-  requestClient LicensingLicenseAssignmentsUpdate {..} =
-    go
-      productId
-      skuId
-      userId
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      payload
-      appsLicensingService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy LicensingLicenseAssignmentsUpdateResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           LicensingLicenseAssignmentsUpdate
+         where
+        type Rs LicensingLicenseAssignmentsUpdate =
+             LicenseAssignment
+        type Scopes LicensingLicenseAssignmentsUpdate =
+             '["https://www.googleapis.com/auth/apps.licensing"]
+        requestClient LicensingLicenseAssignmentsUpdate{..}
+          = go productId skuId userId xgafv accessToken
+              callback
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              appsLicensingService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy LicensingLicenseAssignmentsUpdateResource)
+                      Core.mempty
+
