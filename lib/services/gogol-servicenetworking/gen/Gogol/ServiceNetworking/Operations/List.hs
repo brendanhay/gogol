f42f0f992c020cedf1270f55ed58ee65530e032b
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:10 +0100

    gogol-servicenetworking: regenerating service with updated namespace

--- lib/services/gogol-servicenetworking/gen/Gogol/ServiceNetworking/Operations/List.hs
+++ lib/services/gogol-servicenetworking/gen/Gogol/ServiceNetworking/Operations/List.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://cloud.google.com/service-infrastructure/docs/service-networking/getting-started Service Networking API Reference> for @servicenetworking.operations.list@.
 module Gogol.ServiceNetworking.Operations.List
-  ( -- * Resource
-    ServiceNetworkingOperationsListResource,
+    (
+    -- * Resource
+      ServiceNetworkingOperationsListResource
 
     -- ** Constructing a Request
-    newServiceNetworkingOperationsList,
-    ServiceNetworkingOperationsList,
-  )
-where
+    , newServiceNetworkingOperationsList
+    , ServiceNetworkingOperationsList
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.ServiceNetworking.Types
@@ -45,91 +51,81 @@ import Gogol.ServiceNetworking.Types
 -- | A resource alias for @servicenetworking.operations.list@ method which the
 -- 'ServiceNetworkingOperationsList' request conforms to.
 type ServiceNetworkingOperationsListResource =
-  "v1"
-    Core.:> Core.Capture "name" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "filter" Core.Text
-    Core.:> Core.QueryParam "pageSize" Core.Int32
-    Core.:> Core.QueryParam "pageToken" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] ListOperationsResponse
+     "v1" Core.:>
+       Core.Capture "name" Core.Text Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "filter" Core.Text Core.:>
+                 Core.QueryParam "pageSize" Core.Int32 Core.:>
+                   Core.QueryParam "pageToken" Core.Text Core.:>
+                     Core.QueryParam "uploadType" Core.Text Core.:>
+                       Core.QueryParam "upload_protocol" Core.Text Core.:>
+                         Core.QueryParam "alt" Core.AltJSON Core.:>
+                           Core.Get '[Core.JSON] ListOperationsResponse
 
 -- | Lists operations that match the specified filter in the request. If the server doesn\'t support this method, it returns @UNIMPLEMENTED@. NOTE: the @name@ binding allows API services to override the binding to use different resource name schemes, such as @users\/*\/operations@. To override the binding, API services can add a binding such as @\"\/v1\/{name=users\/*}\/operations\"@ to their service configuration. For backwards compatibility, the default name includes the operations collection id, however overriding users must ensure the name binding is the parent resource, without the operations collection id.
 --
 -- /See:/ 'newServiceNetworkingOperationsList' smart constructor.
 data ServiceNetworkingOperationsList = ServiceNetworkingOperationsList
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | The standard list filter.
-    filter :: (Core.Maybe Core.Text),
-    -- | The name of the operation\'s parent resource.
-    name :: Core.Text,
-    -- | The standard list page size.
-    pageSize :: (Core.Maybe Core.Int32),
-    -- | The standard list page token.
-    pageToken :: (Core.Maybe Core.Text),
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | The standard list filter.
+    , filter :: (Core.Maybe Core.Text)
+      -- | The name of the operation\'s parent resource.
+    , name :: Core.Text
+      -- | The standard list page size.
+    , pageSize :: (Core.Maybe Core.Int32)
+      -- | The standard list page token.
+    , pageToken :: (Core.Maybe Core.Text)
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'ServiceNetworkingOperationsList' with the minimum fields required to make a request.
-newServiceNetworkingOperationsList ::
-  -- |  The name of the operation\'s parent resource. See 'name'.
-  Core.Text ->
-  ServiceNetworkingOperationsList
+newServiceNetworkingOperationsList 
+    ::  Core.Text
+       -- ^  The name of the operation\'s parent resource. See 'name'.
+    -> ServiceNetworkingOperationsList
 newServiceNetworkingOperationsList name =
   ServiceNetworkingOperationsList
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      filter = Core.Nothing,
-      name = name,
-      pageSize = Core.Nothing,
-      pageToken = Core.Nothing,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , filter = Core.Nothing
+    , name = name
+    , pageSize = Core.Nothing
+    , pageToken = Core.Nothing
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    ServiceNetworkingOperationsList
-  where
-  type
-    Rs ServiceNetworkingOperationsList =
-      ListOperationsResponse
-  type
-    Scopes ServiceNetworkingOperationsList =
-      '[ "https://www.googleapis.com/auth/cloud-platform",
-         "https://www.googleapis.com/auth/service.management"
-       ]
-  requestClient ServiceNetworkingOperationsList {..} =
-    go
-      name
-      xgafv
-      accessToken
-      callback
-      filter
-      pageSize
-      pageToken
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      serviceNetworkingService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy ServiceNetworkingOperationsListResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           ServiceNetworkingOperationsList
+         where
+        type Rs ServiceNetworkingOperationsList =
+             ListOperationsResponse
+        type Scopes ServiceNetworkingOperationsList =
+             '["https://www.googleapis.com/auth/cloud-platform",
+               "https://www.googleapis.com/auth/service.management"]
+        requestClient ServiceNetworkingOperationsList{..}
+          = go name xgafv accessToken callback filter pageSize
+              pageToken
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              serviceNetworkingService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy ServiceNetworkingOperationsListResource)
+                      Core.mempty
+
