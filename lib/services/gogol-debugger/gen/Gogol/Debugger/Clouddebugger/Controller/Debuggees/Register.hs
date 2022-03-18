acfdc93259678e034ec19b8745e681b6d7e68651
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:02 +0100

    gogol-debugger: regenerating service with updated namespace

--- lib/services/gogol-debugger/gen/Gogol/Debugger/Clouddebugger/Controller/Debuggees/Register.hs
+++ lib/services/gogol-debugger/gen/Gogol/Debugger/Clouddebugger/Controller/Debuggees/Register.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,96 +36,91 @@
 --
 -- /See:/ <https://cloud.google.com/debugger Cloud Debugger API Reference> for @clouddebugger.controller.debuggees.register@.
 module Gogol.Debugger.Clouddebugger.Controller.Debuggees.Register
-  ( -- * Resource
-    ClouddebuggerControllerDebuggeesRegisterResource,
+    (
+    -- * Resource
+      ClouddebuggerControllerDebuggeesRegisterResource
 
     -- ** Constructing a Request
-    newClouddebuggerControllerDebuggeesRegister,
-    ClouddebuggerControllerDebuggeesRegister,
-  )
-where
+    , newClouddebuggerControllerDebuggeesRegister
+    , ClouddebuggerControllerDebuggeesRegister
+    ) where
 
-import Gogol.Debugger.Types
 import qualified Gogol.Prelude as Core
+import Gogol.Debugger.Types
 
 -- | A resource alias for @clouddebugger.controller.debuggees.register@ method which the
 -- 'ClouddebuggerControllerDebuggeesRegister' request conforms to.
-type ClouddebuggerControllerDebuggeesRegisterResource =
-  "v2"
-    Core.:> "controller"
-    Core.:> "debuggees"
-    Core.:> "register"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] RegisterDebuggeeRequest
-    Core.:> Core.Post '[Core.JSON] RegisterDebuggeeResponse
+type ClouddebuggerControllerDebuggeesRegisterResource
+     =
+     "v2" Core.:>
+       "controller" Core.:>
+         "debuggees" Core.:>
+           "register" Core.:>
+             Core.QueryParam "$.xgafv" Xgafv Core.:>
+               Core.QueryParam "access_token" Core.Text Core.:>
+                 Core.QueryParam "callback" Core.Text Core.:>
+                   Core.QueryParam "uploadType" Core.Text Core.:>
+                     Core.QueryParam "upload_protocol" Core.Text Core.:>
+                       Core.QueryParam "alt" Core.AltJSON Core.:>
+                         Core.ReqBody '[Core.JSON] RegisterDebuggeeRequest
+                           Core.:>
+                           Core.Post '[Core.JSON] RegisterDebuggeeResponse
 
 -- | Registers the debuggee with the controller service. All agents attached to the same application must call this method with exactly the same request content to get back the same stable @debuggee_id@. Agents should call this method again whenever @google.rpc.Code.NOT_FOUND@ is returned from any controller method. This protocol allows the controller service to disable debuggees, recover from data loss, or change the @debuggee_id@ format. Agents must handle @debuggee_id@ value changing upon re-registration.
 --
 -- /See:/ 'newClouddebuggerControllerDebuggeesRegister' smart constructor.
 data ClouddebuggerControllerDebuggeesRegister = ClouddebuggerControllerDebuggeesRegister
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Multipart request metadata.
-    payload :: RegisterDebuggeeRequest,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Multipart request metadata.
+    , payload :: RegisterDebuggeeRequest
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'ClouddebuggerControllerDebuggeesRegister' with the minimum fields required to make a request.
-newClouddebuggerControllerDebuggeesRegister ::
-  -- |  Multipart request metadata. See 'payload'.
-  RegisterDebuggeeRequest ->
-  ClouddebuggerControllerDebuggeesRegister
+newClouddebuggerControllerDebuggeesRegister 
+    ::  RegisterDebuggeeRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> ClouddebuggerControllerDebuggeesRegister
 newClouddebuggerControllerDebuggeesRegister payload =
   ClouddebuggerControllerDebuggeesRegister
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      payload = payload,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , payload = payload
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    ClouddebuggerControllerDebuggeesRegister
-  where
-  type
-    Rs ClouddebuggerControllerDebuggeesRegister =
-      RegisterDebuggeeResponse
-  type
-    Scopes ClouddebuggerControllerDebuggeesRegister =
-      '[ "https://www.googleapis.com/auth/cloud-platform",
-         "https://www.googleapis.com/auth/cloud_debugger"
-       ]
-  requestClient
-    ClouddebuggerControllerDebuggeesRegister {..} =
-      go
-        xgafv
-        accessToken
-        callback
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        payload
-        debuggerService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  ClouddebuggerControllerDebuggeesRegisterResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           ClouddebuggerControllerDebuggeesRegister
+         where
+        type Rs ClouddebuggerControllerDebuggeesRegister =
+             RegisterDebuggeeResponse
+        type Scopes ClouddebuggerControllerDebuggeesRegister
+             =
+             '["https://www.googleapis.com/auth/cloud-platform",
+               "https://www.googleapis.com/auth/cloud_debugger"]
+        requestClient
+          ClouddebuggerControllerDebuggeesRegister{..}
+          = go xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              debuggerService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           ClouddebuggerControllerDebuggeesRegisterResource)
+                      Core.mempty
+
--- lib/services/gogol-debugger/gen/Gogol/Debugger/Clouddebugger/Debugger/Debuggees/Breakpoints/Delete.hs
+++ lib/services/gogol-debugger/gen/Gogol/Debugger/Clouddebugger/Debugger/Debuggees/Breakpoints/Delete.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,110 +36,104 @@
 --
 -- /See:/ <https://cloud.google.com/debugger Cloud Debugger API Reference> for @clouddebugger.debugger.debuggees.breakpoints.delete@.
 module Gogol.Debugger.Clouddebugger.Debugger.Debuggees.Breakpoints.Delete
-  ( -- * Resource
-    ClouddebuggerDebuggerDebuggeesBreakpointsDeleteResource,
+    (
+    -- * Resource
+      ClouddebuggerDebuggerDebuggeesBreakpointsDeleteResource
 
     -- ** Constructing a Request
-    newClouddebuggerDebuggerDebuggeesBreakpointsDelete,
-    ClouddebuggerDebuggerDebuggeesBreakpointsDelete,
-  )
-where
+    , newClouddebuggerDebuggerDebuggeesBreakpointsDelete
+    , ClouddebuggerDebuggerDebuggeesBreakpointsDelete
+    ) where
 
-import Gogol.Debugger.Types
 import qualified Gogol.Prelude as Core
+import Gogol.Debugger.Types
 
 -- | A resource alias for @clouddebugger.debugger.debuggees.breakpoints.delete@ method which the
 -- 'ClouddebuggerDebuggerDebuggeesBreakpointsDelete' request conforms to.
-type ClouddebuggerDebuggerDebuggeesBreakpointsDeleteResource =
-  "v2"
-    Core.:> "debugger"
-    Core.:> "debuggees"
-    Core.:> Core.Capture "debuggeeId" Core.Text
-    Core.:> "breakpoints"
-    Core.:> Core.Capture "breakpointId" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "clientVersion" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Delete '[Core.JSON] Empty
+type ClouddebuggerDebuggerDebuggeesBreakpointsDeleteResource
+     =
+     "v2" Core.:>
+       "debugger" Core.:>
+         "debuggees" Core.:>
+           Core.Capture "debuggeeId" Core.Text Core.:>
+             "breakpoints" Core.:>
+               Core.Capture "breakpointId" Core.Text Core.:>
+                 Core.QueryParam "$.xgafv" Xgafv Core.:>
+                   Core.QueryParam "access_token" Core.Text Core.:>
+                     Core.QueryParam "callback" Core.Text Core.:>
+                       Core.QueryParam "clientVersion" Core.Text Core.:>
+                         Core.QueryParam "uploadType" Core.Text Core.:>
+                           Core.QueryParam "upload_protocol" Core.Text Core.:>
+                             Core.QueryParam "alt" Core.AltJSON Core.:>
+                               Core.Delete '[Core.JSON] Empty
 
 -- | Deletes the breakpoint from the debuggee.
 --
 -- /See:/ 'newClouddebuggerDebuggerDebuggeesBreakpointsDelete' smart constructor.
 data ClouddebuggerDebuggerDebuggeesBreakpointsDelete = ClouddebuggerDebuggerDebuggeesBreakpointsDelete
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | Required. ID of the breakpoint to delete.
-    breakpointId :: Core.Text,
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Required. The client version making the call. Schema: @domain\/type\/version@ (e.g., @google.com\/intellij\/v1@).
-    clientVersion :: (Core.Maybe Core.Text),
-    -- | Required. ID of the debuggee whose breakpoint to delete.
-    debuggeeId :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | Required. ID of the breakpoint to delete.
+    , breakpointId :: Core.Text
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Required. The client version making the call. Schema: @domain\/type\/version@ (e.g., @google.com\/intellij\/v1@).
+    , clientVersion :: (Core.Maybe Core.Text)
+      -- | Required. ID of the debuggee whose breakpoint to delete.
+    , debuggeeId :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'ClouddebuggerDebuggerDebuggeesBreakpointsDelete' with the minimum fields required to make a request.
-newClouddebuggerDebuggerDebuggeesBreakpointsDelete ::
-  -- |  Required. ID of the breakpoint to delete. See 'breakpointId'.
-  Core.Text ->
-  -- |  Required. ID of the debuggee whose breakpoint to delete. See 'debuggeeId'.
-  Core.Text ->
-  ClouddebuggerDebuggerDebuggeesBreakpointsDelete
+newClouddebuggerDebuggerDebuggeesBreakpointsDelete 
+    ::  Core.Text
+       -- ^  Required. ID of the breakpoint to delete. See 'breakpointId'.
+    -> Core.Text
+       -- ^  Required. ID of the debuggee whose breakpoint to delete. See 'debuggeeId'.
+    -> ClouddebuggerDebuggerDebuggeesBreakpointsDelete
 newClouddebuggerDebuggerDebuggeesBreakpointsDelete breakpointId debuggeeId =
   ClouddebuggerDebuggerDebuggeesBreakpointsDelete
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      breakpointId = breakpointId,
-      callback = Core.Nothing,
-      clientVersion = Core.Nothing,
-      debuggeeId = debuggeeId,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , breakpointId = breakpointId
+    , callback = Core.Nothing
+    , clientVersion = Core.Nothing
+    , debuggeeId = debuggeeId
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    ClouddebuggerDebuggerDebuggeesBreakpointsDelete
-  where
-  type
-    Rs
-      ClouddebuggerDebuggerDebuggeesBreakpointsDelete =
-      Empty
-  type
-    Scopes
-      ClouddebuggerDebuggerDebuggeesBreakpointsDelete =
-      '[ "https://www.googleapis.com/auth/cloud-platform",
-         "https://www.googleapis.com/auth/cloud_debugger"
-       ]
-  requestClient
-    ClouddebuggerDebuggerDebuggeesBreakpointsDelete {..} =
-      go
-        debuggeeId
-        breakpointId
-        xgafv
-        accessToken
-        callback
-        clientVersion
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        debuggerService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  ClouddebuggerDebuggerDebuggeesBreakpointsDeleteResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           ClouddebuggerDebuggerDebuggeesBreakpointsDelete
+         where
+        type Rs
+               ClouddebuggerDebuggerDebuggeesBreakpointsDelete
+             = Empty
+        type Scopes
+               ClouddebuggerDebuggerDebuggeesBreakpointsDelete
+             =
+             '["https://www.googleapis.com/auth/cloud-platform",
+               "https://www.googleapis.com/auth/cloud_debugger"]
+        requestClient
+          ClouddebuggerDebuggerDebuggeesBreakpointsDelete{..}
+          = go debuggeeId breakpointId xgafv accessToken
+              callback
+              clientVersion
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              debuggerService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           ClouddebuggerDebuggerDebuggeesBreakpointsDeleteResource)
+                      Core.mempty
+
