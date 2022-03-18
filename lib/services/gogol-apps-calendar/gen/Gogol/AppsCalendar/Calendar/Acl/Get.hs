280bf2803ad0816b158fc2371b1c5394ad5eab51
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:41:57 +0100

    gogol-apps-calendar: regenerating service with updated namespace

--- lib/services/gogol-apps-calendar/gen/Gogol/AppsCalendar/Calendar/Acl/Get.hs
+++ lib/services/gogol-apps-calendar/gen/Gogol/AppsCalendar/Calendar/Acl/Get.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,66 +36,62 @@
 --
 -- /See:/ <https://developers.google.com/google-apps/calendar/firstapp Calendar API Reference> for @calendar.acl.get@.
 module Gogol.AppsCalendar.Calendar.Acl.Get
-  ( -- * Resource
-    CalendarAclGetResource,
+    (
+    -- * Resource
+      CalendarAclGetResource
 
     -- ** Constructing a Request
-    newCalendarAclGet,
-    CalendarAclGet,
-  )
-where
+    , newCalendarAclGet
+    , CalendarAclGet
+    ) where
 
-import Gogol.AppsCalendar.Types
 import qualified Gogol.Prelude as Core
+import Gogol.AppsCalendar.Types
 
 -- | A resource alias for @calendar.acl.get@ method which the
 -- 'CalendarAclGet' request conforms to.
 type CalendarAclGetResource =
-  "calendar"
-    Core.:> "v3"
-    Core.:> "calendars"
-    Core.:> Core.Capture "calendarId" Core.Text
-    Core.:> "acl"
-    Core.:> Core.Capture "ruleId" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] AclRule
+     "calendar" Core.:>
+       "v3" Core.:>
+         "calendars" Core.:>
+           Core.Capture "calendarId" Core.Text Core.:>
+             "acl" Core.:>
+               Core.Capture "ruleId" Core.Text Core.:>
+                 Core.QueryParam "alt" Core.AltJSON Core.:>
+                   Core.Get '[Core.JSON] AclRule
 
 -- | Returns an access control rule.
 --
 -- /See:/ 'newCalendarAclGet' smart constructor.
 data CalendarAclGet = CalendarAclGet
-  { -- | Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the \"primary\" keyword.
-    calendarId :: Core.Text,
-    -- | ACL rule identifier.
-    ruleId :: Core.Text
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the \"primary\" keyword.
+      calendarId :: Core.Text
+      -- | ACL rule identifier.
+    , ruleId :: Core.Text
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'CalendarAclGet' with the minimum fields required to make a request.
-newCalendarAclGet ::
-  -- |  Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the \"primary\" keyword. See 'calendarId'.
-  Core.Text ->
-  -- |  ACL rule identifier. See 'ruleId'.
-  Core.Text ->
-  CalendarAclGet
+newCalendarAclGet 
+    ::  Core.Text
+       -- ^  Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the \"primary\" keyword. See 'calendarId'.
+    -> Core.Text
+       -- ^  ACL rule identifier. See 'ruleId'.
+    -> CalendarAclGet
 newCalendarAclGet calendarId ruleId =
   CalendarAclGet {calendarId = calendarId, ruleId = ruleId}
 
 instance Core.GoogleRequest CalendarAclGet where
-  type Rs CalendarAclGet = AclRule
-  type
-    Scopes CalendarAclGet =
-      '[ "https://www.googleapis.com/auth/calendar",
-         "https://www.googleapis.com/auth/calendar.readonly"
-       ]
-  requestClient CalendarAclGet {..} =
-    go
-      calendarId
-      ruleId
-      (Core.Just Core.AltJSON)
-      appsCalendarService
-    where
-      go =
-        Core.buildClient
-          (Core.Proxy :: Core.Proxy CalendarAclGetResource)
-          Core.mempty
+        type Rs CalendarAclGet = AclRule
+        type Scopes CalendarAclGet =
+             '["https://www.googleapis.com/auth/calendar",
+               "https://www.googleapis.com/auth/calendar.readonly"]
+        requestClient CalendarAclGet{..}
+          = go calendarId ruleId (Core.Just Core.AltJSON)
+              appsCalendarService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy :: Core.Proxy CalendarAclGetResource)
+                      Core.mempty
+
--- lib/services/gogol-apps-calendar/gen/Gogol/AppsCalendar/Calendar/Calendars/Patch.hs
+++ lib/services/gogol-apps-calendar/gen/Gogol/AppsCalendar/Calendar/Calendars/Patch.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,65 +36,62 @@
 --
 -- /See:/ <https://developers.google.com/google-apps/calendar/firstapp Calendar API Reference> for @calendar.calendars.patch@.
 module Gogol.AppsCalendar.Calendar.Calendars.Patch
-  ( -- * Resource
-    CalendarCalendarsPatchResource,
+    (
+    -- * Resource
+      CalendarCalendarsPatchResource
 
     -- ** Constructing a Request
-    newCalendarCalendarsPatch,
-    CalendarCalendarsPatch,
-  )
-where
+    , newCalendarCalendarsPatch
+    , CalendarCalendarsPatch
+    ) where
 
-import Gogol.AppsCalendar.Types
 import qualified Gogol.Prelude as Core
+import Gogol.AppsCalendar.Types
 
 -- | A resource alias for @calendar.calendars.patch@ method which the
 -- 'CalendarCalendarsPatch' request conforms to.
 type CalendarCalendarsPatchResource =
-  "calendar"
-    Core.:> "v3"
-    Core.:> "calendars"
-    Core.:> Core.Capture "calendarId" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] Calendar
-    Core.:> Core.Patch '[Core.JSON] Calendar
+     "calendar" Core.:>
+       "v3" Core.:>
+         "calendars" Core.:>
+           Core.Capture "calendarId" Core.Text Core.:>
+             Core.QueryParam "alt" Core.AltJSON Core.:>
+               Core.ReqBody '[Core.JSON] Calendar Core.:>
+                 Core.Patch '[Core.JSON] Calendar
 
 -- | Updates metadata for a calendar. This method supports patch semantics.
 --
 -- /See:/ 'newCalendarCalendarsPatch' smart constructor.
 data CalendarCalendarsPatch = CalendarCalendarsPatch
-  { -- | Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the \"primary\" keyword.
-    calendarId :: Core.Text,
-    -- | Multipart request metadata.
-    payload :: Calendar
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the \"primary\" keyword.
+      calendarId :: Core.Text
+      -- | Multipart request metadata.
+    , payload :: Calendar
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'CalendarCalendarsPatch' with the minimum fields required to make a request.
-newCalendarCalendarsPatch ::
-  -- |  Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the \"primary\" keyword. See 'calendarId'.
-  Core.Text ->
-  -- |  Multipart request metadata. See 'payload'.
-  Calendar ->
-  CalendarCalendarsPatch
+newCalendarCalendarsPatch 
+    ::  Core.Text
+       -- ^  Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the \"primary\" keyword. See 'calendarId'.
+    -> Calendar
+       -- ^  Multipart request metadata. See 'payload'.
+    -> CalendarCalendarsPatch
 newCalendarCalendarsPatch calendarId payload =
   CalendarCalendarsPatch {calendarId = calendarId, payload = payload}
 
-instance Core.GoogleRequest CalendarCalendarsPatch where
-  type Rs CalendarCalendarsPatch = Calendar
-  type
-    Scopes CalendarCalendarsPatch =
-      '["https://www.googleapis.com/auth/calendar"]
-  requestClient CalendarCalendarsPatch {..} =
-    go
-      calendarId
-      (Core.Just Core.AltJSON)
-      payload
-      appsCalendarService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy CalendarCalendarsPatchResource
-          )
-          Core.mempty
+instance Core.GoogleRequest CalendarCalendarsPatch
+         where
+        type Rs CalendarCalendarsPatch = Calendar
+        type Scopes CalendarCalendarsPatch =
+             '["https://www.googleapis.com/auth/calendar"]
+        requestClient CalendarCalendarsPatch{..}
+          = go calendarId (Core.Just Core.AltJSON) payload
+              appsCalendarService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy CalendarCalendarsPatchResource)
+                      Core.mempty
+
