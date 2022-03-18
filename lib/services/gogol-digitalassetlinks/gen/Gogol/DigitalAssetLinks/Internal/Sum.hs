14731a4d935cfaef483889348631eac0602f9bd8
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:03 +0100

    gogol-digitalassetlinks: regenerating service with updated namespace

--- lib/services/gogol-digitalassetlinks/gen/Gogol/DigitalAssetLinks/Internal/Sum.hs
+++ lib/services/gogol-digitalassetlinks/gen/Gogol/DigitalAssetLinks/Internal/Sum.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -25,17 +31,22 @@
 -- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
+--
 module Gogol.DigitalAssetLinks.Internal.Sum
-  ( -- * Xgafv
+  (
+
+    -- * Xgafv
     Xgafv
-      ( Xgafv_1,
+      (
+        Xgafv_1,
         Xgafv_2,
         ..
       ),
 
     -- * BulkCheckResponse_BulkErrorCode
     BulkCheckResponse_BulkErrorCode
-      ( BulkCheckResponse_BulkErrorCode_ERRORCODEUNSPECIFIED,
+      (
+        BulkCheckResponse_BulkErrorCode_ERRORCODEUNSPECIFIED,
         BulkCheckResponse_BulkErrorCode_ERRORCODEINVALIDQUERY,
         BulkCheckResponse_BulkErrorCode_ERRORCODEFETCHERROR,
         BulkCheckResponse_BulkErrorCode_ERRORCODEFAILEDSSLVALIDATION,
@@ -51,7 +62,8 @@ module Gogol.DigitalAssetLinks.Internal.Sum
 
     -- * CheckResponse_ErrorCodeItem
     CheckResponse_ErrorCodeItem
-      ( CheckResponse_ErrorCodeItem_ERRORCODEUNSPECIFIED,
+      (
+        CheckResponse_ErrorCodeItem_ERRORCODEUNSPECIFIED,
         CheckResponse_ErrorCodeItem_ERRORCODEINVALIDQUERY,
         CheckResponse_ErrorCodeItem_ERRORCODEFETCHERROR,
         CheckResponse_ErrorCodeItem_ERRORCODEFAILEDSSLVALIDATION,
@@ -67,7 +79,8 @@ module Gogol.DigitalAssetLinks.Internal.Sum
 
     -- * ListResponse_ErrorCodeItem
     ListResponse_ErrorCodeItem
-      ( ListResponse_ErrorCodeItem_ERRORCODEUNSPECIFIED,
+      (
+        ListResponse_ErrorCodeItem_ERRORCODEUNSPECIFIED,
         ListResponse_ErrorCodeItem_ERRORCODEINVALIDQUERY,
         ListResponse_ErrorCodeItem_ERRORCODEFETCHERROR,
         ListResponse_ErrorCodeItem_ERRORCODEFAILEDSSLVALIDATION,
@@ -80,23 +93,22 @@ module Gogol.DigitalAssetLinks.Internal.Sum
         ListResponse_ErrorCodeItem_ERRORCODEFETCHBUDGETEXHAUSTED,
         ..
       ),
-  )
-where
+  ) where
 
 import qualified Gogol.Prelude as Core
 
 -- | V1 error format.
-newtype Xgafv = Xgafv {fromXgafv :: Core.Text}
-  deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
-  deriving newtype
-    ( Core.Hashable,
-      Core.ToHttpApiData,
-      Core.FromHttpApiData,
-      Core.ToJSON,
-      Core.ToJSONKey,
-      Core.FromJSON,
-      Core.FromJSONKey
-    )
+newtype Xgafv = Xgafv { fromXgafv :: Core.Text }
+    deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
+    deriving newtype
+      ( Core.Hashable
+      , Core.ToHttpApiData
+      , Core.FromHttpApiData
+      , Core.ToJSON
+      , Core.ToJSONKey
+      , Core.FromJSON
+      , Core.FromJSONKey
+      )
 
 -- | v1 error format
 pattern Xgafv_1 :: Xgafv
@@ -109,23 +121,22 @@ pattern Xgafv_2 = Xgafv "2"
 {-# COMPLETE
   Xgafv_1,
   Xgafv_2,
-  Xgafv
-  #-}
+  Xgafv #-}
 
 -- | Error code for the entire request. Present only if the entire request failed. Individual check errors will not trigger the presence of this field.
-newtype BulkCheckResponse_BulkErrorCode = BulkCheckResponse_BulkErrorCode {fromBulkCheckResponse_BulkErrorCode :: Core.Text}
-  deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
-  deriving newtype
-    ( Core.Hashable,
-      Core.ToHttpApiData,
-      Core.FromHttpApiData,
-      Core.ToJSON,
-      Core.ToJSONKey,
-      Core.FromJSON,
-      Core.FromJSONKey
-    )
+newtype BulkCheckResponse_BulkErrorCode = BulkCheckResponse_BulkErrorCode { fromBulkCheckResponse_BulkErrorCode :: Core.Text }
+    deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
+    deriving newtype
+      ( Core.Hashable
+      , Core.ToHttpApiData
+      , Core.FromHttpApiData
+      , Core.ToJSON
+      , Core.ToJSONKey
+      , Core.FromJSON
+      , Core.FromJSONKey
+      )
 
--- |
+-- | 
 pattern BulkCheckResponse_BulkErrorCode_ERRORCODEUNSPECIFIED :: BulkCheckResponse_BulkErrorCode
 pattern BulkCheckResponse_BulkErrorCode_ERRORCODEUNSPECIFIED = BulkCheckResponse_BulkErrorCode "ERROR_CODE_UNSPECIFIED"
 
@@ -181,22 +192,21 @@ pattern BulkCheckResponse_BulkErrorCode_ERRORCODEFETCHBUDGETEXHAUSTED = BulkChec
   BulkCheckResponse_BulkErrorCode_ERRORCODEMALFORMEDCONTENT,
   BulkCheckResponse_BulkErrorCode_ERRORCODESECUREASSETINCLUDESINSECURE,
   BulkCheckResponse_BulkErrorCode_ERRORCODEFETCHBUDGETEXHAUSTED,
-  BulkCheckResponse_BulkErrorCode
-  #-}
+  BulkCheckResponse_BulkErrorCode #-}
 
-newtype CheckResponse_ErrorCodeItem = CheckResponse_ErrorCodeItem {fromCheckResponse_ErrorCodeItem :: Core.Text}
-  deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
-  deriving newtype
-    ( Core.Hashable,
-      Core.ToHttpApiData,
-      Core.FromHttpApiData,
-      Core.ToJSON,
-      Core.ToJSONKey,
-      Core.FromJSON,
-      Core.FromJSONKey
-    )
+newtype CheckResponse_ErrorCodeItem = CheckResponse_ErrorCodeItem { fromCheckResponse_ErrorCodeItem :: Core.Text }
+    deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
+    deriving newtype
+      ( Core.Hashable
+      , Core.ToHttpApiData
+      , Core.FromHttpApiData
+      , Core.ToJSON
+      , Core.ToJSONKey
+      , Core.FromJSON
+      , Core.FromJSONKey
+      )
 
--- |
+-- | 
 pattern CheckResponse_ErrorCodeItem_ERRORCODEUNSPECIFIED :: CheckResponse_ErrorCodeItem
 pattern CheckResponse_ErrorCodeItem_ERRORCODEUNSPECIFIED = CheckResponse_ErrorCodeItem "ERROR_CODE_UNSPECIFIED"
 
@@ -252,22 +262,21 @@ pattern CheckResponse_ErrorCodeItem_ERRORCODEFETCHBUDGETEXHAUSTED = CheckRespons
   CheckResponse_ErrorCodeItem_ERRORCODEMALFORMEDCONTENT,
   CheckResponse_ErrorCodeItem_ERRORCODESECUREASSETINCLUDESINSECURE,
   CheckResponse_ErrorCodeItem_ERRORCODEFETCHBUDGETEXHAUSTED,
-  CheckResponse_ErrorCodeItem
-  #-}
+  CheckResponse_ErrorCodeItem #-}
 
-newtype ListResponse_ErrorCodeItem = ListResponse_ErrorCodeItem {fromListResponse_ErrorCodeItem :: Core.Text}
-  deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
-  deriving newtype
-    ( Core.Hashable,
-      Core.ToHttpApiData,
-      Core.FromHttpApiData,
-      Core.ToJSON,
-      Core.ToJSONKey,
-      Core.FromJSON,
-      Core.FromJSONKey
-    )
+newtype ListResponse_ErrorCodeItem = ListResponse_ErrorCodeItem { fromListResponse_ErrorCodeItem :: Core.Text }
+    deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
+    deriving newtype
+      ( Core.Hashable
+      , Core.ToHttpApiData
+      , Core.FromHttpApiData
+      , Core.ToJSON
+      , Core.ToJSONKey
+      , Core.FromJSON
+      , Core.FromJSONKey
+      )
 
--- |
+-- | 
 pattern ListResponse_ErrorCodeItem_ERRORCODEUNSPECIFIED :: ListResponse_ErrorCodeItem
 pattern ListResponse_ErrorCodeItem_ERRORCODEUNSPECIFIED = ListResponse_ErrorCodeItem "ERROR_CODE_UNSPECIFIED"
 
@@ -323,5 +332,4 @@ pattern ListResponse_ErrorCodeItem_ERRORCODEFETCHBUDGETEXHAUSTED = ListResponse_
   ListResponse_ErrorCodeItem_ERRORCODEMALFORMEDCONTENT,
   ListResponse_ErrorCodeItem_ERRORCODESECUREASSETINCLUDESINSECURE,
   ListResponse_ErrorCodeItem_ERRORCODEFETCHBUDGETEXHAUSTED,
-  ListResponse_ErrorCodeItem
-  #-}
+  ListResponse_ErrorCodeItem #-}
