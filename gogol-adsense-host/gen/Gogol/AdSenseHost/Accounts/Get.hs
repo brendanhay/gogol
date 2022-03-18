15708adae60cfe0483549ee9da4bcf5a6a09ce72
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 12:30:13 +0100

    gogol-adsense-host: regenerating service with updated namespace

---
+++ gogol-adsense-host/gen/Gogol/AdSenseHost/Accounts/Get.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.AdSenseHost.Accounts.Get
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Get information about the selected associated AdSense account.
+--
+-- /See:/ <https://developers.google.com/adsense/host/ AdSense Host API Reference> for @adsensehost.accounts.get@.
+module Gogol.AdSenseHost.Accounts.Get
+  ( -- * Resource
+    AdSenseHostAccountsGetResource,
+
+    -- ** Constructing a Request
+    newAdSenseHostAccountsGet,
+    AdSenseHostAccountsGet,
+  )
+where
+
+import Gogol.AdSenseHost.Types
+import qualified Gogol.Prelude as Core
+
+-- | A resource alias for @adsensehost.accounts.get@ method which the
+-- 'AdSenseHostAccountsGet' request conforms to.
+type AdSenseHostAccountsGetResource =
+  "adsensehost"
+    Core.:> "v4.1"
+    Core.:> "accounts"
+    Core.:> Core.Capture "accountId" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Get '[Core.JSON] Account
+
+-- | Get information about the selected associated AdSense account.
+--
+-- /See:/ 'newAdSenseHostAccountsGet' smart constructor.
+newtype AdSenseHostAccountsGet = AdSenseHostAccountsGet
+  { -- | Account to get information about.
+    accountId :: Core.Text
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'AdSenseHostAccountsGet' with the minimum fields required to make a request.
+newAdSenseHostAccountsGet ::
+  -- |  Account to get information about. See 'accountId'.
+  Core.Text ->
+  AdSenseHostAccountsGet
+newAdSenseHostAccountsGet accountId =
+  AdSenseHostAccountsGet {accountId = accountId}
+
+instance Core.GoogleRequest AdSenseHostAccountsGet where
+  type Rs AdSenseHostAccountsGet = Account
+  type
+    Scopes AdSenseHostAccountsGet =
+      '["https://www.googleapis.com/auth/adsensehost"]
+  requestClient AdSenseHostAccountsGet {..} =
+    go
+      accountId
+      (Core.Just Core.AltJSON)
+      adSenseHostService
+    where
+      go =
+        Core.buildClient
+          ( Core.Proxy ::
+              Core.Proxy AdSenseHostAccountsGetResource
+          )
+          Core.mempty
---
+++ gogol-adsense-host/gen/Gogol/AdSenseHost/Customchannels/Patch.hs
@@ -0,0 +1,110 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.AdSenseHost.Customchannels.Patch
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Update a custom channel in the host AdSense account. This method supports patch semantics.
+--
+-- /See:/ <https://developers.google.com/adsense/host/ AdSense Host API Reference> for @adsensehost.customchannels.patch@.
+module Gogol.AdSenseHost.Customchannels.Patch
+  ( -- * Resource
+    AdSenseHostCustomchannelsPatchResource,
+
+    -- ** Constructing a Request
+    newAdSenseHostCustomchannelsPatch,
+    AdSenseHostCustomchannelsPatch,
+  )
+where
+
+import Gogol.AdSenseHost.Types
+import qualified Gogol.Prelude as Core
+
+-- | A resource alias for @adsensehost.customchannels.patch@ method which the
+-- 'AdSenseHostCustomchannelsPatch' request conforms to.
+type AdSenseHostCustomchannelsPatchResource =
+  "adsensehost"
+    Core.:> "v4.1"
+    Core.:> "adclients"
+    Core.:> Core.Capture "adClientId" Core.Text
+    Core.:> "customchannels"
+    Core.:> Core.QueryParam "customChannelId" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.ReqBody '[Core.JSON] CustomChannel
+    Core.:> Core.Patch '[Core.JSON] CustomChannel
+
+-- | Update a custom channel in the host AdSense account. This method supports patch semantics.
+--
+-- /See:/ 'newAdSenseHostCustomchannelsPatch' smart constructor.
+data AdSenseHostCustomchannelsPatch = AdSenseHostCustomchannelsPatch
+  { -- | Ad client in which the custom channel will be updated.
+    adClientId :: Core.Text,
+    -- | Custom channel to get.
+    customChannelId :: Core.Text,
+    -- | Multipart request metadata.
+    payload :: CustomChannel
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'AdSenseHostCustomchannelsPatch' with the minimum fields required to make a request.
+newAdSenseHostCustomchannelsPatch ::
+  -- |  Ad client in which the custom channel will be updated. See 'adClientId'.
+  Core.Text ->
+  -- |  Custom channel to get. See 'customChannelId'.
+  Core.Text ->
+  -- |  Multipart request metadata. See 'payload'.
+  CustomChannel ->
+  AdSenseHostCustomchannelsPatch
+newAdSenseHostCustomchannelsPatch adClientId customChannelId payload =
+  AdSenseHostCustomchannelsPatch
+    { adClientId = adClientId,
+      customChannelId = customChannelId,
+      payload = payload
+    }
+
+instance
+  Core.GoogleRequest
+    AdSenseHostCustomchannelsPatch
+  where
+  type
+    Rs AdSenseHostCustomchannelsPatch =
+      CustomChannel
+  type
+    Scopes AdSenseHostCustomchannelsPatch =
+      '["https://www.googleapis.com/auth/adsensehost"]
+  requestClient AdSenseHostCustomchannelsPatch {..} =
+    go
+      adClientId
+      (Core.Just customChannelId)
+      (Core.Just Core.AltJSON)
+      payload
+      adSenseHostService
+    where
+      go =
+        Core.buildClient
+          ( Core.Proxy ::
+              Core.Proxy AdSenseHostCustomchannelsPatchResource
+          )
+          Core.mempty
