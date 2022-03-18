53872f902384a35ae65c2f367c4f57d7bf1bcfc0
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:13 +0100

    gogol-tpu: regenerating service with updated namespace

--- lib/services/gogol-tpu/gen/Gogol/TPU/Projects/Locations/Nodes/Delete.hs
+++ lib/services/gogol-tpu/gen/Gogol/TPU/Projects/Locations/Nodes/Delete.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://cloud.google.com/tpu/ Cloud TPU API Reference> for @tpu.projects.locations.nodes.delete@.
 module Gogol.TPU.Projects.Locations.Nodes.Delete
-  ( -- * Resource
-    TPUProjectsLocationsNodesDeleteResource,
+    (
+    -- * Resource
+      TPUProjectsLocationsNodesDeleteResource
 
     -- ** Constructing a Request
-    newTPUProjectsLocationsNodesDelete,
-    TPUProjectsLocationsNodesDelete,
-  )
-where
+    , newTPUProjectsLocationsNodesDelete
+    , TPUProjectsLocationsNodesDelete
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.TPU.Types
@@ -45,77 +51,70 @@ import Gogol.TPU.Types
 -- | A resource alias for @tpu.projects.locations.nodes.delete@ method which the
 -- 'TPUProjectsLocationsNodesDelete' request conforms to.
 type TPUProjectsLocationsNodesDeleteResource =
-  "v2alpha1"
-    Core.:> Core.Capture "name" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "requestId" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Delete '[Core.JSON] Operation
+     "v2alpha1" Core.:>
+       Core.Capture "name" Core.Text Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "requestId" Core.Text Core.:>
+                 Core.QueryParam "uploadType" Core.Text Core.:>
+                   Core.QueryParam "upload_protocol" Core.Text Core.:>
+                     Core.QueryParam "alt" Core.AltJSON Core.:>
+                       Core.Delete '[Core.JSON] Operation
 
 -- | Deletes a node.
 --
 -- /See:/ 'newTPUProjectsLocationsNodesDelete' smart constructor.
 data TPUProjectsLocationsNodesDelete = TPUProjectsLocationsNodesDelete
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Required. The resource name.
-    name :: Core.Text,
-    -- | Idempotent request UUID.
-    requestId :: (Core.Maybe Core.Text),
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Required. The resource name.
+    , name :: Core.Text
+      -- | Idempotent request UUID.
+    , requestId :: (Core.Maybe Core.Text)
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'TPUProjectsLocationsNodesDelete' with the minimum fields required to make a request.
-newTPUProjectsLocationsNodesDelete ::
-  -- |  Required. The resource name. See 'name'.
-  Core.Text ->
-  TPUProjectsLocationsNodesDelete
+newTPUProjectsLocationsNodesDelete 
+    ::  Core.Text
+       -- ^  Required. The resource name. See 'name'.
+    -> TPUProjectsLocationsNodesDelete
 newTPUProjectsLocationsNodesDelete name =
   TPUProjectsLocationsNodesDelete
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      name = name,
-      requestId = Core.Nothing,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , name = name
+    , requestId = Core.Nothing
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    TPUProjectsLocationsNodesDelete
-  where
-  type Rs TPUProjectsLocationsNodesDelete = Operation
-  type
-    Scopes TPUProjectsLocationsNodesDelete =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient TPUProjectsLocationsNodesDelete {..} =
-    go
-      name
-      xgafv
-      accessToken
-      callback
-      requestId
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      tPUService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy TPUProjectsLocationsNodesDeleteResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           TPUProjectsLocationsNodesDelete
+         where
+        type Rs TPUProjectsLocationsNodesDelete = Operation
+        type Scopes TPUProjectsLocationsNodesDelete =
+             '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient TPUProjectsLocationsNodesDelete{..}
+          = go name xgafv accessToken callback requestId
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              tPUService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy TPUProjectsLocationsNodesDeleteResource)
+                      Core.mempty
+
--- lib/services/gogol-tpu/gen/Gogol/TPU/Projects/Locations/RuntimeVersions/Get.hs
+++ lib/services/gogol-tpu/gen/Gogol/TPU/Projects/Locations/RuntimeVersions/Get.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://cloud.google.com/tpu/ Cloud TPU API Reference> for @tpu.projects.locations.runtimeVersions.get@.
 module Gogol.TPU.Projects.Locations.RuntimeVersions.Get
-  ( -- * Resource
-    TPUProjectsLocationsRuntimeVersionsGetResource,
+    (
+    -- * Resource
+      TPUProjectsLocationsRuntimeVersionsGetResource
 
     -- ** Constructing a Request
-    newTPUProjectsLocationsRuntimeVersionsGet,
-    TPUProjectsLocationsRuntimeVersionsGet,
-  )
-where
+    , newTPUProjectsLocationsRuntimeVersionsGet
+    , TPUProjectsLocationsRuntimeVersionsGet
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.TPU.Types
@@ -45,76 +51,68 @@ import Gogol.TPU.Types
 -- | A resource alias for @tpu.projects.locations.runtimeVersions.get@ method which the
 -- 'TPUProjectsLocationsRuntimeVersionsGet' request conforms to.
 type TPUProjectsLocationsRuntimeVersionsGetResource =
-  "v2alpha1"
-    Core.:> Core.Capture "name" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] RuntimeVersion
+     "v2alpha1" Core.:>
+       Core.Capture "name" Core.Text Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "uploadType" Core.Text Core.:>
+                 Core.QueryParam "upload_protocol" Core.Text Core.:>
+                   Core.QueryParam "alt" Core.AltJSON Core.:>
+                     Core.Get '[Core.JSON] RuntimeVersion
 
 -- | Gets a runtime version.
 --
 -- /See:/ 'newTPUProjectsLocationsRuntimeVersionsGet' smart constructor.
 data TPUProjectsLocationsRuntimeVersionsGet = TPUProjectsLocationsRuntimeVersionsGet
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Required. The resource name.
-    name :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Required. The resource name.
+    , name :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'TPUProjectsLocationsRuntimeVersionsGet' with the minimum fields required to make a request.
-newTPUProjectsLocationsRuntimeVersionsGet ::
-  -- |  Required. The resource name. See 'name'.
-  Core.Text ->
-  TPUProjectsLocationsRuntimeVersionsGet
+newTPUProjectsLocationsRuntimeVersionsGet 
+    ::  Core.Text
+       -- ^  Required. The resource name. See 'name'.
+    -> TPUProjectsLocationsRuntimeVersionsGet
 newTPUProjectsLocationsRuntimeVersionsGet name =
   TPUProjectsLocationsRuntimeVersionsGet
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      name = name,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , name = name
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    TPUProjectsLocationsRuntimeVersionsGet
-  where
-  type
-    Rs TPUProjectsLocationsRuntimeVersionsGet =
-      RuntimeVersion
-  type
-    Scopes TPUProjectsLocationsRuntimeVersionsGet =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient
-    TPUProjectsLocationsRuntimeVersionsGet {..} =
-      go
-        name
-        xgafv
-        accessToken
-        callback
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        tPUService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  TPUProjectsLocationsRuntimeVersionsGetResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           TPUProjectsLocationsRuntimeVersionsGet
+         where
+        type Rs TPUProjectsLocationsRuntimeVersionsGet =
+             RuntimeVersion
+        type Scopes TPUProjectsLocationsRuntimeVersionsGet =
+             '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient
+          TPUProjectsLocationsRuntimeVersionsGet{..}
+          = go name xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              tPUService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           TPUProjectsLocationsRuntimeVersionsGetResource)
+                      Core.mempty
+
