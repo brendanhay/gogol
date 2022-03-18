835c018c760f0a51fd839bfd3c5e7c4a9acd67bc
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:14 +0100

    gogol-youtube-analytics: regenerating service with updated namespace

--- lib/services/gogol-youtube-analytics/gen/Gogol/YouTubeAnalytics/GroupItems/Insert.hs
+++ lib/services/gogol-youtube-analytics/gen/Gogol/YouTubeAnalytics/GroupItems/Insert.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://developers.google.com/youtube/analytics YouTube Analytics API Reference> for @youtubeAnalytics.groupItems.insert@.
 module Gogol.YouTubeAnalytics.GroupItems.Insert
-  ( -- * Resource
-    YouTubeAnalyticsGroupItemsInsertResource,
+    (
+    -- * Resource
+      YouTubeAnalyticsGroupItemsInsertResource
 
     -- ** Constructing a Request
-    newYouTubeAnalyticsGroupItemsInsert,
-    YouTubeAnalyticsGroupItemsInsert,
-  )
-where
+    , newYouTubeAnalyticsGroupItemsInsert
+    , YouTubeAnalyticsGroupItemsInsert
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.YouTubeAnalytics.Types
@@ -45,83 +51,78 @@ import Gogol.YouTubeAnalytics.Types
 -- | A resource alias for @youtubeAnalytics.groupItems.insert@ method which the
 -- 'YouTubeAnalyticsGroupItemsInsert' request conforms to.
 type YouTubeAnalyticsGroupItemsInsertResource =
-  "v2"
-    Core.:> "groupItems"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "onBehalfOfContentOwner" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] GroupItem
-    Core.:> Core.Post '[Core.JSON] GroupItem
+     "v2" Core.:>
+       "groupItems" Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "onBehalfOfContentOwner" Core.Text
+                 Core.:>
+                 Core.QueryParam "uploadType" Core.Text Core.:>
+                   Core.QueryParam "upload_protocol" Core.Text Core.:>
+                     Core.QueryParam "alt" Core.AltJSON Core.:>
+                       Core.ReqBody '[Core.JSON] GroupItem Core.:>
+                         Core.Post '[Core.JSON] GroupItem
 
 -- | Creates a group item.
 --
 -- /See:/ 'newYouTubeAnalyticsGroupItemsInsert' smart constructor.
 data YouTubeAnalyticsGroupItemsInsert = YouTubeAnalyticsGroupItemsInsert
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | This parameter can only be used in a properly authorized request. __Note:__ This parameter is intended exclusively for YouTube content partners that own and manage many different YouTube channels. The @onBehalfOfContentOwner@ parameter indicates that the request\'s authorization credentials identify a YouTube user who is acting on behalf of the content owner specified in the parameter value. It allows content owners to authenticate once and get access to all their video and channel data, without having to provide authentication credentials for each individual channel. The account that the user authenticates with must be linked to the specified YouTube content owner.
-    onBehalfOfContentOwner :: (Core.Maybe Core.Text),
-    -- | Multipart request metadata.
-    payload :: GroupItem,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | This parameter can only be used in a properly authorized request. __Note:__ This parameter is intended exclusively for YouTube content partners that own and manage many different YouTube channels. The @onBehalfOfContentOwner@ parameter indicates that the request\'s authorization credentials identify a YouTube user who is acting on behalf of the content owner specified in the parameter value. It allows content owners to authenticate once and get access to all their video and channel data, without having to provide authentication credentials for each individual channel. The account that the user authenticates with must be linked to the specified YouTube content owner.
+    , onBehalfOfContentOwner :: (Core.Maybe Core.Text)
+      -- | Multipart request metadata.
+    , payload :: GroupItem
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'YouTubeAnalyticsGroupItemsInsert' with the minimum fields required to make a request.
-newYouTubeAnalyticsGroupItemsInsert ::
-  -- |  Multipart request metadata. See 'payload'.
-  GroupItem ->
-  YouTubeAnalyticsGroupItemsInsert
+newYouTubeAnalyticsGroupItemsInsert 
+    ::  GroupItem
+       -- ^  Multipart request metadata. See 'payload'.
+    -> YouTubeAnalyticsGroupItemsInsert
 newYouTubeAnalyticsGroupItemsInsert payload =
   YouTubeAnalyticsGroupItemsInsert
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      onBehalfOfContentOwner = Core.Nothing,
-      payload = payload,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , onBehalfOfContentOwner = Core.Nothing
+    , payload = payload
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    YouTubeAnalyticsGroupItemsInsert
-  where
-  type Rs YouTubeAnalyticsGroupItemsInsert = GroupItem
-  type
-    Scopes YouTubeAnalyticsGroupItemsInsert =
-      '[ "https://www.googleapis.com/auth/youtube",
-         "https://www.googleapis.com/auth/youtube.readonly",
-         "https://www.googleapis.com/auth/youtubepartner",
-         "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
-         "https://www.googleapis.com/auth/yt-analytics.readonly"
-       ]
-  requestClient YouTubeAnalyticsGroupItemsInsert {..} =
-    go
-      xgafv
-      accessToken
-      callback
-      onBehalfOfContentOwner
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      payload
-      youTubeAnalyticsService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy YouTubeAnalyticsGroupItemsInsertResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           YouTubeAnalyticsGroupItemsInsert
+         where
+        type Rs YouTubeAnalyticsGroupItemsInsert = GroupItem
+        type Scopes YouTubeAnalyticsGroupItemsInsert =
+             '["https://www.googleapis.com/auth/youtube",
+               "https://www.googleapis.com/auth/youtube.readonly",
+               "https://www.googleapis.com/auth/youtubepartner",
+               "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
+               "https://www.googleapis.com/auth/yt-analytics.readonly"]
+        requestClient YouTubeAnalyticsGroupItemsInsert{..}
+          = go xgafv accessToken callback
+              onBehalfOfContentOwner
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              youTubeAnalyticsService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy YouTubeAnalyticsGroupItemsInsertResource)
+                      Core.mempty
+
