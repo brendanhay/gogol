1cfe1527adf43635c1064a7a792f8935eb6b912c
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:41:57 +0100

    gogol-appstate: regenerating service with updated namespace

--- lib/services/gogol-appstate/gen/Gogol/AppState/Types.hs
+++ lib/services/gogol-appstate/gen/Gogol/AppState/Types.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -25,43 +31,43 @@
 -- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
+--
 module Gogol.AppState.Types
-  ( -- * Configuration
-    appStateService,
+    (
+    -- * Configuration
+      appStateService
 
     -- * OAuth Scopes
-    appStateScope,
+    , appStateScope
 
     -- * Types
 
     -- ** GetResponse
-    GetResponse (..),
-    newGetResponse,
+    , GetResponse (..)
+    , newGetResponse
 
     -- ** ListResponse
-    ListResponse (..),
-    newListResponse,
+    , ListResponse (..)
+    , newListResponse
 
     -- ** UpdateRequest
-    UpdateRequest (..),
-    newUpdateRequest,
+    , UpdateRequest (..)
+    , newUpdateRequest
 
     -- ** WriteResult
-    WriteResult (..),
-    newWriteResult,
-  )
-where
+    , WriteResult (..)
+    , newWriteResult
+    ) where
 
+import qualified Gogol.Prelude as Core
 import Gogol.AppState.Internal.Product
 import Gogol.AppState.Internal.Sum
-import qualified Gogol.Prelude as Core
 
 -- | Default request referring to version @v1@ of the Google App State API. This contains the host and root path used as a starting point for constructing service requests.
 appStateService :: Core.ServiceConfig
-appStateService =
-  Core.defaultService
-    (Core.ServiceId "appstate:v1")
-    "www.googleapis.com"
+appStateService
+  = Core.defaultService (Core.ServiceId "appstate:v1")
+      "www.googleapis.com"
 
 -- | View and manage your data for this application
 appStateScope :: Core.Proxy '["https://www.googleapis.com/auth/appstate"]
