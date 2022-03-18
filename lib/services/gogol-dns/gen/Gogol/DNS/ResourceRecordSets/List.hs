8325c50b9538d63e83fbd8e25fe69ff6228deced
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:03 +0100

    gogol-dns: regenerating service with updated namespace

--- lib/services/gogol-dns/gen/Gogol/DNS/ResourceRecordSets/List.hs
+++ lib/services/gogol-dns/gen/Gogol/DNS/ResourceRecordSets/List.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,131 +36,125 @@
 --
 -- /See:/ <https://cloud.google.com/dns/docs Cloud DNS API Reference> for @dns.resourceRecordSets.list@.
 module Gogol.DNS.ResourceRecordSets.List
-  ( -- * Resource
-    DNSResourceRecordSetsListResource,
+    (
+    -- * Resource
+      DNSResourceRecordSetsListResource
 
     -- ** Constructing a Request
-    newDNSResourceRecordSetsList,
-    DNSResourceRecordSetsList,
-  )
-where
+    , newDNSResourceRecordSetsList
+    , DNSResourceRecordSetsList
+    ) where
 
-import Gogol.DNS.Types
 import qualified Gogol.Prelude as Core
+import Gogol.DNS.Types
 
 -- | A resource alias for @dns.resourceRecordSets.list@ method which the
 -- 'DNSResourceRecordSetsList' request conforms to.
 type DNSResourceRecordSetsListResource =
-  "dns"
-    Core.:> "v2"
-    Core.:> "projects"
-    Core.:> Core.Capture "project" Core.Text
-    Core.:> "locations"
-    Core.:> Core.Capture "location" Core.Text
-    Core.:> "managedZones"
-    Core.:> Core.Capture "managedZone" Core.Text
-    Core.:> "rrsets"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "maxResults" Core.Int32
-    Core.:> Core.QueryParam "name" Core.Text
-    Core.:> Core.QueryParam "pageToken" Core.Text
-    Core.:> Core.QueryParam "type" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam
-              "upload_protocol"
-              Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get
-              '[Core.JSON]
-              ResourceRecordSetsListResponse
+     "dns" Core.:>
+       "v2" Core.:>
+         "projects" Core.:>
+           Core.Capture "project" Core.Text Core.:>
+             "locations" Core.:>
+               Core.Capture "location" Core.Text Core.:>
+                 "managedZones" Core.:>
+                   Core.Capture "managedZone" Core.Text Core.:>
+                     "rrsets" Core.:>
+                       Core.QueryParam "$.xgafv" Xgafv Core.:>
+                         Core.QueryParam "access_token" Core.Text Core.:>
+                           Core.QueryParam "callback" Core.Text Core.:>
+                             Core.QueryParam "maxResults" Core.Int32 Core.:>
+                               Core.QueryParam "name" Core.Text Core.:>
+                                 Core.QueryParam "pageToken" Core.Text Core.:>
+                                   Core.QueryParam "type" Core.Text Core.:>
+                                     Core.QueryParam "uploadType" Core.Text
+                                       Core.:>
+                                       Core.QueryParam "upload_protocol"
+                                         Core.Text
+                                         Core.:>
+                                         Core.QueryParam "alt" Core.AltJSON
+                                           Core.:>
+                                           Core.Get '[Core.JSON]
+                                             ResourceRecordSetsListResponse
 
 -- | Enumerates ResourceRecordSets that you have created but not yet deleted.
 --
 -- /See:/ 'newDNSResourceRecordSetsList' smart constructor.
 data DNSResourceRecordSetsList = DNSResourceRecordSetsList
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Specifies the location of the resource. This information will be used for routing and will be part of the resource name.
-    location :: Core.Text,
-    -- | Identifies the managed zone addressed by this request. Can be the managed zone name or ID.
-    managedZone :: Core.Text,
-    -- | Optional. Maximum number of results to be returned. If unspecified, the server decides how many results to return.
-    maxResults :: (Core.Maybe Core.Int32),
-    -- | Restricts the list to return only records with this fully qualified domain name.
-    name :: (Core.Maybe Core.Text),
-    -- | Optional. A tag returned by a previous list request that was truncated. Use this parameter to continue a previous list request.
-    pageToken :: (Core.Maybe Core.Text),
-    -- | Identifies the project addressed by this request.
-    project :: Core.Text,
-    -- | Restricts the list to return only records of this type. If present, the \"name\" parameter must also be present.
-    type' :: (Core.Maybe Core.Text),
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Specifies the location of the resource. This information will be used for routing and will be part of the resource name.
+    , location :: Core.Text
+      -- | Identifies the managed zone addressed by this request. Can be the managed zone name or ID.
+    , managedZone :: Core.Text
+      -- | Optional. Maximum number of results to be returned. If unspecified, the server decides how many results to return.
+    , maxResults :: (Core.Maybe Core.Int32)
+      -- | Restricts the list to return only records with this fully qualified domain name.
+    , name :: (Core.Maybe Core.Text)
+      -- | Optional. A tag returned by a previous list request that was truncated. Use this parameter to continue a previous list request.
+    , pageToken :: (Core.Maybe Core.Text)
+      -- | Identifies the project addressed by this request.
+    , project :: Core.Text
+      -- | Restricts the list to return only records of this type. If present, the \"name\" parameter must also be present.
+    , type' :: (Core.Maybe Core.Text)
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'DNSResourceRecordSetsList' with the minimum fields required to make a request.
-newDNSResourceRecordSetsList ::
-  -- |  Identifies the managed zone addressed by this request. Can be the managed zone name or ID. See 'managedZone'.
-  Core.Text ->
-  -- |  Identifies the project addressed by this request. See 'project'.
-  Core.Text ->
-  DNSResourceRecordSetsList
+newDNSResourceRecordSetsList 
+    ::  Core.Text
+       -- ^  Identifies the managed zone addressed by this request. Can be the managed zone name or ID. See 'managedZone'.
+    -> Core.Text
+       -- ^  Identifies the project addressed by this request. See 'project'.
+    -> DNSResourceRecordSetsList
 newDNSResourceRecordSetsList managedZone project =
   DNSResourceRecordSetsList
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      location = "global",
-      managedZone = managedZone,
-      maxResults = Core.Nothing,
-      name = Core.Nothing,
-      pageToken = Core.Nothing,
-      project = project,
-      type' = Core.Nothing,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , location = "global"
+    , managedZone = managedZone
+    , maxResults = Core.Nothing
+    , name = Core.Nothing
+    , pageToken = Core.Nothing
+    , project = project
+    , type' = Core.Nothing
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance Core.GoogleRequest DNSResourceRecordSetsList where
-  type
-    Rs DNSResourceRecordSetsList =
-      ResourceRecordSetsListResponse
-  type
-    Scopes DNSResourceRecordSetsList =
-      '[ "https://www.googleapis.com/auth/cloud-platform",
-         "https://www.googleapis.com/auth/cloud-platform.read-only",
-         "https://www.googleapis.com/auth/ndev.clouddns.readonly",
-         "https://www.googleapis.com/auth/ndev.clouddns.readwrite"
-       ]
-  requestClient DNSResourceRecordSetsList {..} =
-    go
-      project
-      location
-      managedZone
-      xgafv
-      accessToken
-      callback
-      maxResults
-      name
-      pageToken
-      type'
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      dNSService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy DNSResourceRecordSetsListResource
-          )
-          Core.mempty
+instance Core.GoogleRequest DNSResourceRecordSetsList
+         where
+        type Rs DNSResourceRecordSetsList =
+             ResourceRecordSetsListResponse
+        type Scopes DNSResourceRecordSetsList =
+             '["https://www.googleapis.com/auth/cloud-platform",
+               "https://www.googleapis.com/auth/cloud-platform.read-only",
+               "https://www.googleapis.com/auth/ndev.clouddns.readonly",
+               "https://www.googleapis.com/auth/ndev.clouddns.readwrite"]
+        requestClient DNSResourceRecordSetsList{..}
+          = go project location managedZone xgafv accessToken
+              callback
+              maxResults
+              name
+              pageToken
+              type'
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              dNSService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy DNSResourceRecordSetsListResource)
+                      Core.mempty
+
--- lib/services/gogol-dns/gen/Gogol/DNS/ResponsePolicies/Update.hs
+++ lib/services/gogol-dns/gen/Gogol/DNS/ResponsePolicies/Update.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,118 +36,113 @@
 --
 -- /See:/ <https://cloud.google.com/dns/docs Cloud DNS API Reference> for @dns.responsePolicies.update@.
 module Gogol.DNS.ResponsePolicies.Update
-  ( -- * Resource
-    DNSResponsePoliciesUpdateResource,
+    (
+    -- * Resource
+      DNSResponsePoliciesUpdateResource
 
     -- ** Constructing a Request
-    newDNSResponsePoliciesUpdate,
-    DNSResponsePoliciesUpdate,
-  )
-where
+    , newDNSResponsePoliciesUpdate
+    , DNSResponsePoliciesUpdate
+    ) where
 
-import Gogol.DNS.Types
 import qualified Gogol.Prelude as Core
+import Gogol.DNS.Types
 
 -- | A resource alias for @dns.responsePolicies.update@ method which the
 -- 'DNSResponsePoliciesUpdate' request conforms to.
 type DNSResponsePoliciesUpdateResource =
-  "dns"
-    Core.:> "v2"
-    Core.:> "projects"
-    Core.:> Core.Capture "project" Core.Text
-    Core.:> "locations"
-    Core.:> Core.Capture "location" Core.Text
-    Core.:> "responsePolicies"
-    Core.:> Core.Capture "responsePolicy" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "clientOperationId" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] ResponsePolicy
-    Core.:> Core.Put
-              '[Core.JSON]
-              ResponsePoliciesUpdateResponse
+     "dns" Core.:>
+       "v2" Core.:>
+         "projects" Core.:>
+           Core.Capture "project" Core.Text Core.:>
+             "locations" Core.:>
+               Core.Capture "location" Core.Text Core.:>
+                 "responsePolicies" Core.:>
+                   Core.Capture "responsePolicy" Core.Text Core.:>
+                     Core.QueryParam "$.xgafv" Xgafv Core.:>
+                       Core.QueryParam "access_token" Core.Text Core.:>
+                         Core.QueryParam "callback" Core.Text Core.:>
+                           Core.QueryParam "clientOperationId" Core.Text Core.:>
+                             Core.QueryParam "uploadType" Core.Text Core.:>
+                               Core.QueryParam "upload_protocol" Core.Text
+                                 Core.:>
+                                 Core.QueryParam "alt" Core.AltJSON Core.:>
+                                   Core.ReqBody '[Core.JSON] ResponsePolicy
+                                     Core.:>
+                                     Core.Put '[Core.JSON]
+                                       ResponsePoliciesUpdateResponse
 
 -- | Updates an existing Response Policy.
 --
 -- /See:/ 'newDNSResponsePoliciesUpdate' smart constructor.
 data DNSResponsePoliciesUpdate = DNSResponsePoliciesUpdate
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | For mutating operation requests only. An optional identifier specified by the client. Must be unique for operation resources in the Operations collection.
-    clientOperationId :: (Core.Maybe Core.Text),
-    -- | Specifies the location of the resource. This information will be used for routing and will be part of the resource name.
-    location :: Core.Text,
-    -- | Multipart request metadata.
-    payload :: ResponsePolicy,
-    -- | Identifies the project addressed by this request.
-    project :: Core.Text,
-    -- | User assigned name of the Response Policy addressed by this request.
-    responsePolicy :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | For mutating operation requests only. An optional identifier specified by the client. Must be unique for operation resources in the Operations collection.
+    , clientOperationId :: (Core.Maybe Core.Text)
+      -- | Specifies the location of the resource. This information will be used for routing and will be part of the resource name.
+    , location :: Core.Text
+      -- | Multipart request metadata.
+    , payload :: ResponsePolicy
+      -- | Identifies the project addressed by this request.
+    , project :: Core.Text
+      -- | User assigned name of the Response Policy addressed by this request.
+    , responsePolicy :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'DNSResponsePoliciesUpdate' with the minimum fields required to make a request.
-newDNSResponsePoliciesUpdate ::
-  -- |  Multipart request metadata. See 'payload'.
-  ResponsePolicy ->
-  -- |  Identifies the project addressed by this request. See 'project'.
-  Core.Text ->
-  -- |  User assigned name of the Response Policy addressed by this request. See 'responsePolicy'.
-  Core.Text ->
-  DNSResponsePoliciesUpdate
+newDNSResponsePoliciesUpdate 
+    ::  ResponsePolicy
+       -- ^  Multipart request metadata. See 'payload'.
+    -> Core.Text
+       -- ^  Identifies the project addressed by this request. See 'project'.
+    -> Core.Text
+       -- ^  User assigned name of the Response Policy addressed by this request. See 'responsePolicy'.
+    -> DNSResponsePoliciesUpdate
 newDNSResponsePoliciesUpdate payload project responsePolicy =
   DNSResponsePoliciesUpdate
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      clientOperationId = Core.Nothing,
-      location = "global",
-      payload = payload,
-      project = project,
-      responsePolicy = responsePolicy,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , clientOperationId = Core.Nothing
+    , location = "global"
+    , payload = payload
+    , project = project
+    , responsePolicy = responsePolicy
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance Core.GoogleRequest DNSResponsePoliciesUpdate where
-  type
-    Rs DNSResponsePoliciesUpdate =
-      ResponsePoliciesUpdateResponse
-  type
-    Scopes DNSResponsePoliciesUpdate =
-      '[ "https://www.googleapis.com/auth/cloud-platform",
-         "https://www.googleapis.com/auth/ndev.clouddns.readwrite"
-       ]
-  requestClient DNSResponsePoliciesUpdate {..} =
-    go
-      project
-      location
-      responsePolicy
-      xgafv
-      accessToken
-      callback
-      clientOperationId
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      payload
-      dNSService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy DNSResponsePoliciesUpdateResource
-          )
-          Core.mempty
+instance Core.GoogleRequest DNSResponsePoliciesUpdate
+         where
+        type Rs DNSResponsePoliciesUpdate =
+             ResponsePoliciesUpdateResponse
+        type Scopes DNSResponsePoliciesUpdate =
+             '["https://www.googleapis.com/auth/cloud-platform",
+               "https://www.googleapis.com/auth/ndev.clouddns.readwrite"]
+        requestClient DNSResponsePoliciesUpdate{..}
+          = go project location responsePolicy xgafv
+              accessToken
+              callback
+              clientOperationId
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              dNSService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy DNSResponsePoliciesUpdateResource)
+                      Core.mempty
+
