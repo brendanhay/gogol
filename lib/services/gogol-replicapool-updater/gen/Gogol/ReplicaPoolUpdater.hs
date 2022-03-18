d3f8f181c43bcd78a9f4324bedb07e38098e194f
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:09 +0100

    gogol-replicapool-updater: regenerating service with updated namespace

--- lib/services/gogol-replicapool-updater/gen/Gogol/ReplicaPoolUpdater.hs
+++ lib/services/gogol-replicapool-updater/gen/Gogol/ReplicaPoolUpdater.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,130 +36,130 @@
 --
 -- /See:/ <https://cloud.google.com/compute/docs/instance-groups/manager/#applying_rolling_updates_using_the_updater_service Google Compute Engine Instance Group Updater API Reference>
 module Gogol.ReplicaPoolUpdater
-  ( -- * Configuration
-    replicaPoolUpdaterService,
+    (
+    -- * Configuration
+      replicaPoolUpdaterService
 
     -- * OAuth Scopes
-    cloudPlatformScope,
-    cloudPlatformReadOnlyScope,
-    replicapoolScope,
-    replicapoolReadOnlyScope,
+    , cloudPlatformScope
+    , cloudPlatformReadOnlyScope
+    , replicapoolScope
+    , replicapoolReadOnlyScope
 
     -- * Resources
 
     -- ** replicapoolupdater.rollingUpdates.cancel
-    ReplicaPoolUpdaterRollingUpdatesCancelResource,
-    newReplicaPoolUpdaterRollingUpdatesCancel,
-    ReplicaPoolUpdaterRollingUpdatesCancel,
+    , ReplicaPoolUpdaterRollingUpdatesCancelResource
+    , newReplicaPoolUpdaterRollingUpdatesCancel
+    , ReplicaPoolUpdaterRollingUpdatesCancel
 
     -- ** replicapoolupdater.rollingUpdates.get
-    ReplicaPoolUpdaterRollingUpdatesGetResource,
-    newReplicaPoolUpdaterRollingUpdatesGet,
-    ReplicaPoolUpdaterRollingUpdatesGet,
+    , ReplicaPoolUpdaterRollingUpdatesGetResource
+    , newReplicaPoolUpdaterRollingUpdatesGet
+    , ReplicaPoolUpdaterRollingUpdatesGet
 
     -- ** replicapoolupdater.rollingUpdates.insert
-    ReplicaPoolUpdaterRollingUpdatesInsertResource,
-    newReplicaPoolUpdaterRollingUpdatesInsert,
-    ReplicaPoolUpdaterRollingUpdatesInsert,
+    , ReplicaPoolUpdaterRollingUpdatesInsertResource
+    , newReplicaPoolUpdaterRollingUpdatesInsert
+    , ReplicaPoolUpdaterRollingUpdatesInsert
 
     -- ** replicapoolupdater.rollingUpdates.list
-    ReplicaPoolUpdaterRollingUpdatesListResource,
-    newReplicaPoolUpdaterRollingUpdatesList,
-    ReplicaPoolUpdaterRollingUpdatesList,
+    , ReplicaPoolUpdaterRollingUpdatesListResource
+    , newReplicaPoolUpdaterRollingUpdatesList
+    , ReplicaPoolUpdaterRollingUpdatesList
 
     -- ** replicapoolupdater.rollingUpdates.listInstanceUpdates
-    ReplicaPoolUpdaterRollingUpdatesListInstanceUpdatesResource,
-    newReplicaPoolUpdaterRollingUpdatesListInstanceUpdates,
-    ReplicaPoolUpdaterRollingUpdatesListInstanceUpdates,
+    , ReplicaPoolUpdaterRollingUpdatesListInstanceUpdatesResource
+    , newReplicaPoolUpdaterRollingUpdatesListInstanceUpdates
+    , ReplicaPoolUpdaterRollingUpdatesListInstanceUpdates
 
     -- ** replicapoolupdater.rollingUpdates.pause
-    ReplicaPoolUpdaterRollingUpdatesPauseResource,
-    newReplicaPoolUpdaterRollingUpdatesPause,
-    ReplicaPoolUpdaterRollingUpdatesPause,
+    , ReplicaPoolUpdaterRollingUpdatesPauseResource
+    , newReplicaPoolUpdaterRollingUpdatesPause
+    , ReplicaPoolUpdaterRollingUpdatesPause
 
     -- ** replicapoolupdater.rollingUpdates.resume
-    ReplicaPoolUpdaterRollingUpdatesResumeResource,
-    newReplicaPoolUpdaterRollingUpdatesResume,
-    ReplicaPoolUpdaterRollingUpdatesResume,
+    , ReplicaPoolUpdaterRollingUpdatesResumeResource
+    , newReplicaPoolUpdaterRollingUpdatesResume
+    , ReplicaPoolUpdaterRollingUpdatesResume
 
     -- ** replicapoolupdater.rollingUpdates.rollback
-    ReplicaPoolUpdaterRollingUpdatesRollbackResource,
-    newReplicaPoolUpdaterRollingUpdatesRollback,
-    ReplicaPoolUpdaterRollingUpdatesRollback,
+    , ReplicaPoolUpdaterRollingUpdatesRollbackResource
+    , newReplicaPoolUpdaterRollingUpdatesRollback
+    , ReplicaPoolUpdaterRollingUpdatesRollback
 
     -- ** replicapoolupdater.zoneOperations.get
-    ReplicaPoolUpdaterZoneOperationsGetResource,
-    newReplicaPoolUpdaterZoneOperationsGet,
-    ReplicaPoolUpdaterZoneOperationsGet,
+    , ReplicaPoolUpdaterZoneOperationsGetResource
+    , newReplicaPoolUpdaterZoneOperationsGet
+    , ReplicaPoolUpdaterZoneOperationsGet
 
     -- ** replicapoolupdater.zoneOperations.list
-    ReplicaPoolUpdaterZoneOperationsListResource,
-    newReplicaPoolUpdaterZoneOperationsList,
-    ReplicaPoolUpdaterZoneOperationsList,
+    , ReplicaPoolUpdaterZoneOperationsListResource
+    , newReplicaPoolUpdaterZoneOperationsList
+    , ReplicaPoolUpdaterZoneOperationsList
 
     -- * Types
 
     -- ** InstanceUpdate
-    InstanceUpdate (..),
-    newInstanceUpdate,
+    , InstanceUpdate (..)
+    , newInstanceUpdate
 
     -- ** InstanceUpdate_Error
-    InstanceUpdate_Error (..),
-    newInstanceUpdate_Error,
+    , InstanceUpdate_Error (..)
+    , newInstanceUpdate_Error
 
     -- ** InstanceUpdate_Error_ErrorsItem
-    InstanceUpdate_Error_ErrorsItem (..),
-    newInstanceUpdate_Error_ErrorsItem,
+    , InstanceUpdate_Error_ErrorsItem (..)
+    , newInstanceUpdate_Error_ErrorsItem
 
     -- ** InstanceUpdateList
-    InstanceUpdateList (..),
-    newInstanceUpdateList,
+    , InstanceUpdateList (..)
+    , newInstanceUpdateList
 
     -- ** Operation
-    Operation (..),
-    newOperation,
+    , Operation (..)
+    , newOperation
 
     -- ** Operation_Error
-    Operation_Error (..),
-    newOperation_Error,
+    , Operation_Error (..)
+    , newOperation_Error
 
     -- ** Operation_Error_ErrorsItem
-    Operation_Error_ErrorsItem (..),
-    newOperation_Error_ErrorsItem,
+    , Operation_Error_ErrorsItem (..)
+    , newOperation_Error_ErrorsItem
 
     -- ** Operation_WarningsItem
-    Operation_WarningsItem (..),
-    newOperation_WarningsItem,
+    , Operation_WarningsItem (..)
+    , newOperation_WarningsItem
 
     -- ** Operation_WarningsItem_DataItem
-    Operation_WarningsItem_DataItem (..),
-    newOperation_WarningsItem_DataItem,
+    , Operation_WarningsItem_DataItem (..)
+    , newOperation_WarningsItem_DataItem
 
     -- ** OperationList
-    OperationList (..),
-    newOperationList,
+    , OperationList (..)
+    , newOperationList
 
     -- ** RollingUpdate
-    RollingUpdate (..),
-    newRollingUpdate,
+    , RollingUpdate (..)
+    , newRollingUpdate
 
     -- ** RollingUpdate_Error
-    RollingUpdate_Error (..),
-    newRollingUpdate_Error,
+    , RollingUpdate_Error (..)
+    , newRollingUpdate_Error
 
     -- ** RollingUpdate_Error_ErrorsItem
-    RollingUpdate_Error_ErrorsItem (..),
-    newRollingUpdate_Error_ErrorsItem,
+    , RollingUpdate_Error_ErrorsItem (..)
+    , newRollingUpdate_Error_ErrorsItem
 
     -- ** RollingUpdate_Policy
-    RollingUpdate_Policy (..),
-    newRollingUpdate_Policy,
+    , RollingUpdate_Policy (..)
+    , newRollingUpdate_Policy
 
     -- ** RollingUpdateList
-    RollingUpdateList (..),
-    newRollingUpdateList,
-  )
-where
+    , RollingUpdateList (..)
+    , newRollingUpdateList
+    ) where
 
 import Gogol.ReplicaPoolUpdater.RollingUpdates.Cancel
 import Gogol.ReplicaPoolUpdater.RollingUpdates.Get
--- lib/services/gogol-replicapool-updater/gen/Gogol/ReplicaPoolUpdater/RollingUpdates/Get.hs
+++ lib/services/gogol-replicapool-updater/gen/Gogol/ReplicaPoolUpdater/RollingUpdates/Get.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://cloud.google.com/compute/docs/instance-groups/manager/#applying_rolling_updates_using_the_updater_service Google Compute Engine Instance Group Updater API Reference> for @replicapoolupdater.rollingUpdates.get@.
 module Gogol.ReplicaPoolUpdater.RollingUpdates.Get
-  ( -- * Resource
-    ReplicaPoolUpdaterRollingUpdatesGetResource,
+    (
+    -- * Resource
+      ReplicaPoolUpdaterRollingUpdatesGetResource
 
     -- ** Constructing a Request
-    newReplicaPoolUpdaterRollingUpdatesGet,
-    ReplicaPoolUpdaterRollingUpdatesGet,
-  )
-where
+    , newReplicaPoolUpdaterRollingUpdatesGet
+    , ReplicaPoolUpdaterRollingUpdatesGet
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.ReplicaPoolUpdater.Types
@@ -45,72 +51,62 @@ import Gogol.ReplicaPoolUpdater.Types
 -- | A resource alias for @replicapoolupdater.rollingUpdates.get@ method which the
 -- 'ReplicaPoolUpdaterRollingUpdatesGet' request conforms to.
 type ReplicaPoolUpdaterRollingUpdatesGetResource =
-  "replicapoolupdater"
-    Core.:> "v1beta1"
-    Core.:> "projects"
-    Core.:> Core.Capture "project" Core.Text
-    Core.:> "zones"
-    Core.:> Core.Capture "zone" Core.Text
-    Core.:> "rollingUpdates"
-    Core.:> Core.Capture "rollingUpdate" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] RollingUpdate
+     "replicapoolupdater" Core.:>
+       "v1beta1" Core.:>
+         "projects" Core.:>
+           Core.Capture "project" Core.Text Core.:>
+             "zones" Core.:>
+               Core.Capture "zone" Core.Text Core.:>
+                 "rollingUpdates" Core.:>
+                   Core.Capture "rollingUpdate" Core.Text Core.:>
+                     Core.QueryParam "alt" Core.AltJSON Core.:>
+                       Core.Get '[Core.JSON] RollingUpdate
 
 -- | Returns information about an update.
 --
 -- /See:/ 'newReplicaPoolUpdaterRollingUpdatesGet' smart constructor.
 data ReplicaPoolUpdaterRollingUpdatesGet = ReplicaPoolUpdaterRollingUpdatesGet
-  { -- | The Google Developers Console project name.
-    project :: Core.Text,
-    -- | The name of the update.
-    rollingUpdate :: Core.Text,
-    -- | The name of the zone in which the update\'s target resides.
-    zone :: Core.Text
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | The Google Developers Console project name.
+      project :: Core.Text
+      -- | The name of the update.
+    , rollingUpdate :: Core.Text
+      -- | The name of the zone in which the update\'s target resides.
+    , zone :: Core.Text
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'ReplicaPoolUpdaterRollingUpdatesGet' with the minimum fields required to make a request.
-newReplicaPoolUpdaterRollingUpdatesGet ::
-  -- |  The Google Developers Console project name. See 'project'.
-  Core.Text ->
-  -- |  The name of the update. See 'rollingUpdate'.
-  Core.Text ->
-  -- |  The name of the zone in which the update\'s target resides. See 'zone'.
-  Core.Text ->
-  ReplicaPoolUpdaterRollingUpdatesGet
+newReplicaPoolUpdaterRollingUpdatesGet 
+    ::  Core.Text
+       -- ^  The Google Developers Console project name. See 'project'.
+    -> Core.Text
+       -- ^  The name of the update. See 'rollingUpdate'.
+    -> Core.Text
+       -- ^  The name of the zone in which the update\'s target resides. See 'zone'.
+    -> ReplicaPoolUpdaterRollingUpdatesGet
 newReplicaPoolUpdaterRollingUpdatesGet project rollingUpdate zone =
   ReplicaPoolUpdaterRollingUpdatesGet
-    { project = project,
-      rollingUpdate = rollingUpdate,
-      zone = zone
-    }
+    {project = project, rollingUpdate = rollingUpdate, zone = zone}
 
-instance
-  Core.GoogleRequest
-    ReplicaPoolUpdaterRollingUpdatesGet
-  where
-  type
-    Rs ReplicaPoolUpdaterRollingUpdatesGet =
-      RollingUpdate
-  type
-    Scopes ReplicaPoolUpdaterRollingUpdatesGet =
-      '[ "https://www.googleapis.com/auth/cloud-platform",
-         "https://www.googleapis.com/auth/cloud-platform.read-only",
-         "https://www.googleapis.com/auth/replicapool",
-         "https://www.googleapis.com/auth/replicapool.readonly"
-       ]
-  requestClient ReplicaPoolUpdaterRollingUpdatesGet {..} =
-    go
-      project
-      zone
-      rollingUpdate
-      (Core.Just Core.AltJSON)
-      replicaPoolUpdaterService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy
-                ReplicaPoolUpdaterRollingUpdatesGetResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           ReplicaPoolUpdaterRollingUpdatesGet
+         where
+        type Rs ReplicaPoolUpdaterRollingUpdatesGet =
+             RollingUpdate
+        type Scopes ReplicaPoolUpdaterRollingUpdatesGet =
+             '["https://www.googleapis.com/auth/cloud-platform",
+               "https://www.googleapis.com/auth/cloud-platform.read-only",
+               "https://www.googleapis.com/auth/replicapool",
+               "https://www.googleapis.com/auth/replicapool.readonly"]
+        requestClient ReplicaPoolUpdaterRollingUpdatesGet{..}
+          = go project zone rollingUpdate
+              (Core.Just Core.AltJSON)
+              replicaPoolUpdaterService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           ReplicaPoolUpdaterRollingUpdatesGetResource)
+                      Core.mempty
+
