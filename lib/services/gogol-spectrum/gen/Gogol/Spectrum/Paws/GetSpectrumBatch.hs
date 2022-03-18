12240c267f46cdfd793ef97ae6f6fbab84f26cb8
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:11 +0100

    gogol-spectrum: regenerating service with updated namespace

--- lib/services/gogol-spectrum/gen/Gogol/Spectrum/Paws/GetSpectrumBatch.hs
+++ lib/services/gogol-spectrum/gen/Gogol/Spectrum/Paws/GetSpectrumBatch.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <http://developers.google.com/spectrum Google Spectrum Database API Reference> for @spectrum.paws.getSpectrumBatch@.
 module Gogol.Spectrum.Paws.GetSpectrumBatch
-  ( -- * Resource
-    SpectrumPawsGetSpectrumBatchResource,
+    (
+    -- * Resource
+      SpectrumPawsGetSpectrumBatchResource
 
     -- ** Constructing a Request
-    newSpectrumPawsGetSpectrumBatch,
-    SpectrumPawsGetSpectrumBatch,
-  )
-where
+    , newSpectrumPawsGetSpectrumBatch
+    , SpectrumPawsGetSpectrumBatch
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.Spectrum.Types
@@ -45,45 +51,44 @@ import Gogol.Spectrum.Types
 -- | A resource alias for @spectrum.paws.getSpectrumBatch@ method which the
 -- 'SpectrumPawsGetSpectrumBatch' request conforms to.
 type SpectrumPawsGetSpectrumBatchResource =
-  "spectrum"
-    Core.:> "v1explorer"
-    Core.:> "paws"
-    Core.:> "getSpectrumBatch"
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] PawsGetSpectrumBatchRequest
-    Core.:> Core.Post '[Core.JSON] PawsGetSpectrumBatchResponse
+     "spectrum" Core.:>
+       "v1explorer" Core.:>
+         "paws" Core.:>
+           "getSpectrumBatch" Core.:>
+             Core.QueryParam "alt" Core.AltJSON Core.:>
+               Core.ReqBody '[Core.JSON] PawsGetSpectrumBatchRequest
+                 Core.:>
+                 Core.Post '[Core.JSON] PawsGetSpectrumBatchResponse
 
 -- | The Google Spectrum Database does not support batch requests, so this method always yields an UNIMPLEMENTED error.
 --
 -- /See:/ 'newSpectrumPawsGetSpectrumBatch' smart constructor.
 newtype SpectrumPawsGetSpectrumBatch = SpectrumPawsGetSpectrumBatch
-  { -- | Multipart request metadata.
-    payload :: PawsGetSpectrumBatchRequest
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | Multipart request metadata.
+      payload :: PawsGetSpectrumBatchRequest
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'SpectrumPawsGetSpectrumBatch' with the minimum fields required to make a request.
-newSpectrumPawsGetSpectrumBatch ::
-  -- |  Multipart request metadata. See 'payload'.
-  PawsGetSpectrumBatchRequest ->
-  SpectrumPawsGetSpectrumBatch
+newSpectrumPawsGetSpectrumBatch 
+    ::  PawsGetSpectrumBatchRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> SpectrumPawsGetSpectrumBatch
 newSpectrumPawsGetSpectrumBatch payload =
   SpectrumPawsGetSpectrumBatch {payload = payload}
 
-instance
-  Core.GoogleRequest
-    SpectrumPawsGetSpectrumBatch
-  where
-  type
-    Rs SpectrumPawsGetSpectrumBatch =
-      PawsGetSpectrumBatchResponse
-  type Scopes SpectrumPawsGetSpectrumBatch = '[]
-  requestClient SpectrumPawsGetSpectrumBatch {..} =
-    go (Core.Just Core.AltJSON) payload spectrumService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy SpectrumPawsGetSpectrumBatchResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           SpectrumPawsGetSpectrumBatch
+         where
+        type Rs SpectrumPawsGetSpectrumBatch =
+             PawsGetSpectrumBatchResponse
+        type Scopes SpectrumPawsGetSpectrumBatch = '[]
+        requestClient SpectrumPawsGetSpectrumBatch{..}
+          = go (Core.Just Core.AltJSON) payload spectrumService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy SpectrumPawsGetSpectrumBatchResource)
+                      Core.mempty
+
