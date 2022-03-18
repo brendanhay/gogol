49981d675b3abd2f1319ecb42bb05140f9fa7ab7
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:03 +0100

    gogol-discovery: regenerating service with updated namespace

--- lib/services/gogol-discovery/gen/Gogol/Discovery/Apis/GetRest.hs
+++ lib/services/gogol-discovery/gen/Gogol/Discovery/Apis/GetRest.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,64 +36,62 @@
 --
 -- /See:/ <https://developers.google.com/discovery/ API Discovery Service Reference> for @discovery.apis.getRest@.
 module Gogol.Discovery.Apis.GetRest
-  ( -- * Resource
-    DiscoveryApisGetRestResource,
+    (
+    -- * Resource
+      DiscoveryApisGetRestResource
 
     -- ** Constructing a Request
-    newDiscoveryApisGetRest,
-    DiscoveryApisGetRest,
-  )
-where
+    , newDiscoveryApisGetRest
+    , DiscoveryApisGetRest
+    ) where
 
-import Gogol.Discovery.Types
 import qualified Gogol.Prelude as Core
+import Gogol.Discovery.Types
 
 -- | A resource alias for @discovery.apis.getRest@ method which the
 -- 'DiscoveryApisGetRest' request conforms to.
 type DiscoveryApisGetRestResource =
-  "discovery"
-    Core.:> "v1"
-    Core.:> "apis"
-    Core.:> Core.Capture "api" Core.Text
-    Core.:> Core.Capture "version" Core.Text
-    Core.:> "rest"
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] RestDescription
+     "discovery" Core.:>
+       "v1" Core.:>
+         "apis" Core.:>
+           Core.Capture "api" Core.Text Core.:>
+             Core.Capture "version" Core.Text Core.:>
+               "rest" Core.:>
+                 Core.QueryParam "alt" Core.AltJSON Core.:>
+                   Core.Get '[Core.JSON] RestDescription
 
 -- | Retrieve the description of a particular version of an api.
 --
 -- /See:/ 'newDiscoveryApisGetRest' smart constructor.
 data DiscoveryApisGetRest = DiscoveryApisGetRest
-  { -- | The name of the API.
-    api :: Core.Text,
-    -- | The version of the API.
-    version :: Core.Text
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | The name of the API.
+      api :: Core.Text
+      -- | The version of the API.
+    , version :: Core.Text
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'DiscoveryApisGetRest' with the minimum fields required to make a request.
-newDiscoveryApisGetRest ::
-  -- |  The name of the API. See 'api'.
-  Core.Text ->
-  -- |  The version of the API. See 'version'.
-  Core.Text ->
-  DiscoveryApisGetRest
+newDiscoveryApisGetRest 
+    ::  Core.Text
+       -- ^  The name of the API. See 'api'.
+    -> Core.Text
+       -- ^  The version of the API. See 'version'.
+    -> DiscoveryApisGetRest
 newDiscoveryApisGetRest api version =
   DiscoveryApisGetRest {api = api, version = version}
 
-instance Core.GoogleRequest DiscoveryApisGetRest where
-  type Rs DiscoveryApisGetRest = RestDescription
-  type Scopes DiscoveryApisGetRest = '[]
-  requestClient DiscoveryApisGetRest {..} =
-    go
-      api
-      version
-      (Core.Just Core.AltJSON)
-      discoveryService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy DiscoveryApisGetRestResource
-          )
-          Core.mempty
+instance Core.GoogleRequest DiscoveryApisGetRest
+         where
+        type Rs DiscoveryApisGetRest = RestDescription
+        type Scopes DiscoveryApisGetRest = '[]
+        requestClient DiscoveryApisGetRest{..}
+          = go api version (Core.Just Core.AltJSON)
+              discoveryService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy DiscoveryApisGetRestResource)
+                      Core.mempty
+
--- lib/services/gogol-discovery/gen/Gogol/Discovery/Apis/List.hs
+++ lib/services/gogol-discovery/gen/Gogol/Discovery/Apis/List.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,57 +36,56 @@
 --
 -- /See:/ <https://developers.google.com/discovery/ API Discovery Service Reference> for @discovery.apis.list@.
 module Gogol.Discovery.Apis.List
-  ( -- * Resource
-    DiscoveryApisListResource,
+    (
+    -- * Resource
+      DiscoveryApisListResource
 
     -- ** Constructing a Request
-    newDiscoveryApisList,
-    DiscoveryApisList,
-  )
-where
+    , newDiscoveryApisList
+    , DiscoveryApisList
+    ) where
 
-import Gogol.Discovery.Types
 import qualified Gogol.Prelude as Core
+import Gogol.Discovery.Types
 
 -- | A resource alias for @discovery.apis.list@ method which the
 -- 'DiscoveryApisList' request conforms to.
 type DiscoveryApisListResource =
-  "discovery"
-    Core.:> "v1"
-    Core.:> "apis"
-    Core.:> Core.QueryParam "name" Core.Text
-    Core.:> Core.QueryParam "preferred" Core.Bool
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] DirectoryList
+     "discovery" Core.:>
+       "v1" Core.:>
+         "apis" Core.:>
+           Core.QueryParam "name" Core.Text Core.:>
+             Core.QueryParam "preferred" Core.Bool Core.:>
+               Core.QueryParam "alt" Core.AltJSON Core.:>
+                 Core.Get '[Core.JSON] DirectoryList
 
 -- | Retrieve the list of APIs supported at this endpoint.
 --
 -- /See:/ 'newDiscoveryApisList' smart constructor.
 data DiscoveryApisList = DiscoveryApisList
-  { -- | Only include APIs with the given name.
-    name :: (Core.Maybe Core.Text),
-    -- | Return only the preferred version of an API.
-    preferred :: Core.Bool
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | Only include APIs with the given name.
+      name :: (Core.Maybe Core.Text)
+      -- | Return only the preferred version of an API.
+    , preferred :: Core.Bool
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'DiscoveryApisList' with the minimum fields required to make a request.
-newDiscoveryApisList ::
-  DiscoveryApisList
+newDiscoveryApisList 
+    ::  DiscoveryApisList
 newDiscoveryApisList =
   DiscoveryApisList {name = Core.Nothing, preferred = Core.False}
 
 instance Core.GoogleRequest DiscoveryApisList where
-  type Rs DiscoveryApisList = DirectoryList
-  type Scopes DiscoveryApisList = '[]
-  requestClient DiscoveryApisList {..} =
-    go
-      name
-      (Core.Just preferred)
-      (Core.Just Core.AltJSON)
-      discoveryService
-    where
-      go =
-        Core.buildClient
-          (Core.Proxy :: Core.Proxy DiscoveryApisListResource)
-          Core.mempty
+        type Rs DiscoveryApisList = DirectoryList
+        type Scopes DiscoveryApisList = '[]
+        requestClient DiscoveryApisList{..}
+          = go name (Core.Just preferred)
+              (Core.Just Core.AltJSON)
+              discoveryService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy :: Core.Proxy DiscoveryApisListResource)
+                      Core.mempty
+
