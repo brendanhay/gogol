73c3a4c1e8d81014f172178467981fd1bd59795f
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:09 +0100

    gogol-searchconsole: regenerating service with updated namespace

--- lib/services/gogol-searchconsole/gen/Gogol/SearchConsole/Webmasters/Searchanalytics/Query.hs
+++ lib/services/gogol-searchconsole/gen/Gogol/SearchConsole/Webmasters/Searchanalytics/Query.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://developers.google.com/webmaster-tools/search-console-api/ Google Search Console API Reference> for @webmasters.searchanalytics.query@.
 module Gogol.SearchConsole.Webmasters.Searchanalytics.Query
-  ( -- * Resource
-    WebmastersSearchanalyticsQueryResource,
+    (
+    -- * Resource
+      WebmastersSearchanalyticsQueryResource
 
     -- ** Constructing a Request
-    newWebmastersSearchanalyticsQuery,
-    WebmastersSearchanalyticsQuery,
-  )
-where
+    , newWebmastersSearchanalyticsQuery
+    , WebmastersSearchanalyticsQuery
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.SearchConsole.Types
@@ -45,91 +51,81 @@ import Gogol.SearchConsole.Types
 -- | A resource alias for @webmasters.searchanalytics.query@ method which the
 -- 'WebmastersSearchanalyticsQuery' request conforms to.
 type WebmastersSearchanalyticsQueryResource =
-  "webmasters"
-    Core.:> "v3"
-    Core.:> "sites"
-    Core.:> Core.Capture "siteUrl" Core.Text
-    Core.:> "searchAnalytics"
-    Core.:> "query"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody
-              '[Core.JSON]
-              SearchAnalyticsQueryRequest
-    Core.:> Core.Post
-              '[Core.JSON]
-              SearchAnalyticsQueryResponse
+     "webmasters" Core.:>
+       "v3" Core.:>
+         "sites" Core.:>
+           Core.Capture "siteUrl" Core.Text Core.:>
+             "searchAnalytics" Core.:>
+               "query" Core.:>
+                 Core.QueryParam "$.xgafv" Xgafv Core.:>
+                   Core.QueryParam "access_token" Core.Text Core.:>
+                     Core.QueryParam "callback" Core.Text Core.:>
+                       Core.QueryParam "uploadType" Core.Text Core.:>
+                         Core.QueryParam "upload_protocol" Core.Text Core.:>
+                           Core.QueryParam "alt" Core.AltJSON Core.:>
+                             Core.ReqBody '[Core.JSON]
+                               SearchAnalyticsQueryRequest
+                               Core.:>
+                               Core.Post '[Core.JSON]
+                                 SearchAnalyticsQueryResponse
 
 -- | Query your data with filters and parameters that you define. Returns zero or more rows grouped by the row keys that you define. You must define a date range of one or more days. When date is one of the group by values, any days without data are omitted from the result list. If you need to know which days have data, issue a broad date range query grouped by date for any metric, and see which day rows are returned.
 --
 -- /See:/ 'newWebmastersSearchanalyticsQuery' smart constructor.
 data WebmastersSearchanalyticsQuery = WebmastersSearchanalyticsQuery
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Multipart request metadata.
-    payload :: SearchAnalyticsQueryRequest,
-    -- | The site\'s URL, including protocol. For example: @http:\/\/www.example.com\/@.
-    siteUrl :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Multipart request metadata.
+    , payload :: SearchAnalyticsQueryRequest
+      -- | The site\'s URL, including protocol. For example: @http:\/\/www.example.com\/@.
+    , siteUrl :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'WebmastersSearchanalyticsQuery' with the minimum fields required to make a request.
-newWebmastersSearchanalyticsQuery ::
-  -- |  Multipart request metadata. See 'payload'.
-  SearchAnalyticsQueryRequest ->
-  -- |  The site\'s URL, including protocol. For example: @http:\/\/www.example.com\/@. See 'siteUrl'.
-  Core.Text ->
-  WebmastersSearchanalyticsQuery
+newWebmastersSearchanalyticsQuery 
+    ::  SearchAnalyticsQueryRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> Core.Text
+       -- ^  The site\'s URL, including protocol. For example: @http:\/\/www.example.com\/@. See 'siteUrl'.
+    -> WebmastersSearchanalyticsQuery
 newWebmastersSearchanalyticsQuery payload siteUrl =
   WebmastersSearchanalyticsQuery
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      payload = payload,
-      siteUrl = siteUrl,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , payload = payload
+    , siteUrl = siteUrl
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    WebmastersSearchanalyticsQuery
-  where
-  type
-    Rs WebmastersSearchanalyticsQuery =
-      SearchAnalyticsQueryResponse
-  type
-    Scopes WebmastersSearchanalyticsQuery =
-      '[ "https://www.googleapis.com/auth/webmasters",
-         "https://www.googleapis.com/auth/webmasters.readonly"
-       ]
-  requestClient WebmastersSearchanalyticsQuery {..} =
-    go
-      siteUrl
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      payload
-      searchConsoleService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy WebmastersSearchanalyticsQueryResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           WebmastersSearchanalyticsQuery
+         where
+        type Rs WebmastersSearchanalyticsQuery =
+             SearchAnalyticsQueryResponse
+        type Scopes WebmastersSearchanalyticsQuery =
+             '["https://www.googleapis.com/auth/webmasters",
+               "https://www.googleapis.com/auth/webmasters.readonly"]
+        requestClient WebmastersSearchanalyticsQuery{..}
+          = go siteUrl xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              searchConsoleService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy WebmastersSearchanalyticsQueryResource)
+                      Core.mempty
+
--- lib/services/gogol-searchconsole/gen/Gogol/SearchConsole/Webmasters/Sites/Delete.hs
+++ lib/services/gogol-searchconsole/gen/Gogol/SearchConsole/Webmasters/Sites/Delete.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://developers.google.com/webmaster-tools/search-console-api/ Google Search Console API Reference> for @webmasters.sites.delete@.
 module Gogol.SearchConsole.Webmasters.Sites.Delete
-  ( -- * Resource
-    WebmastersSitesDeleteResource,
+    (
+    -- * Resource
+      WebmastersSitesDeleteResource
 
     -- ** Constructing a Request
-    newWebmastersSitesDelete,
-    WebmastersSitesDelete,
-  )
-where
+    , newWebmastersSitesDelete
+    , WebmastersSitesDelete
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.SearchConsole.Types
@@ -45,71 +51,66 @@ import Gogol.SearchConsole.Types
 -- | A resource alias for @webmasters.sites.delete@ method which the
 -- 'WebmastersSitesDelete' request conforms to.
 type WebmastersSitesDeleteResource =
-  "webmasters"
-    Core.:> "v3"
-    Core.:> "sites"
-    Core.:> Core.Capture "siteUrl" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Delete '[Core.JSON] ()
+     "webmasters" Core.:>
+       "v3" Core.:>
+         "sites" Core.:>
+           Core.Capture "siteUrl" Core.Text Core.:>
+             Core.QueryParam "$.xgafv" Xgafv Core.:>
+               Core.QueryParam "access_token" Core.Text Core.:>
+                 Core.QueryParam "callback" Core.Text Core.:>
+                   Core.QueryParam "uploadType" Core.Text Core.:>
+                     Core.QueryParam "upload_protocol" Core.Text Core.:>
+                       Core.QueryParam "alt" Core.AltJSON Core.:>
+                         Core.Delete '[Core.JSON] ()
 
 -- | Removes a site from the set of the user\'s Search Console sites.
 --
 -- /See:/ 'newWebmastersSitesDelete' smart constructor.
 data WebmastersSitesDelete = WebmastersSitesDelete
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | The URI of the property as defined in Search Console. __Examples:__ @http:\/\/www.example.com\/@ or @sc-domain:example.com@.
-    siteUrl :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | The URI of the property as defined in Search Console. __Examples:__ @http:\/\/www.example.com\/@ or @sc-domain:example.com@.
+    , siteUrl :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'WebmastersSitesDelete' with the minimum fields required to make a request.
-newWebmastersSitesDelete ::
-  -- |  The URI of the property as defined in Search Console. __Examples:__ @http:\/\/www.example.com\/@ or @sc-domain:example.com@. See 'siteUrl'.
-  Core.Text ->
-  WebmastersSitesDelete
+newWebmastersSitesDelete 
+    ::  Core.Text
+       -- ^  The URI of the property as defined in Search Console. __Examples:__ @http:\/\/www.example.com\/@ or @sc-domain:example.com@. See 'siteUrl'.
+    -> WebmastersSitesDelete
 newWebmastersSitesDelete siteUrl =
   WebmastersSitesDelete
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      siteUrl = siteUrl,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , siteUrl = siteUrl
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance Core.GoogleRequest WebmastersSitesDelete where
-  type Rs WebmastersSitesDelete = ()
-  type
-    Scopes WebmastersSitesDelete =
-      '["https://www.googleapis.com/auth/webmasters"]
-  requestClient WebmastersSitesDelete {..} =
-    go
-      siteUrl
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      searchConsoleService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy WebmastersSitesDeleteResource
-          )
-          Core.mempty
+instance Core.GoogleRequest WebmastersSitesDelete
+         where
+        type Rs WebmastersSitesDelete = ()
+        type Scopes WebmastersSitesDelete =
+             '["https://www.googleapis.com/auth/webmasters"]
+        requestClient WebmastersSitesDelete{..}
+          = go siteUrl xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              searchConsoleService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy WebmastersSitesDeleteResource)
+                      Core.mempty
+
