f7f0c7993911ac83e785bd3dbb934645a8834a0c
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:11 +0100

    gogol-slides: regenerating service with updated namespace

--- lib/services/gogol-slides/gen/Gogol/Slides/Presentations/Pages/Get.hs
+++ lib/services/gogol-slides/gen/Gogol/Slides/Presentations/Pages/Get.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://developers.google.com/slides/ Google Slides API Reference> for @slides.presentations.pages.get@.
 module Gogol.Slides.Presentations.Pages.Get
-  ( -- * Resource
-    SlidesPresentationsPagesGetResource,
+    (
+    -- * Resource
+      SlidesPresentationsPagesGetResource
 
     -- ** Constructing a Request
-    newSlidesPresentationsPagesGet,
-    SlidesPresentationsPagesGet,
-  )
-where
+    , newSlidesPresentationsPagesGet
+    , SlidesPresentationsPagesGet
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.Slides.Types
@@ -45,86 +51,79 @@ import Gogol.Slides.Types
 -- | A resource alias for @slides.presentations.pages.get@ method which the
 -- 'SlidesPresentationsPagesGet' request conforms to.
 type SlidesPresentationsPagesGetResource =
-  "v1"
-    Core.:> "presentations"
-    Core.:> Core.Capture "presentationId" Core.Text
-    Core.:> "pages"
-    Core.:> Core.Capture "pageObjectId" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] Page
+     "v1" Core.:>
+       "presentations" Core.:>
+         Core.Capture "presentationId" Core.Text Core.:>
+           "pages" Core.:>
+             Core.Capture "pageObjectId" Core.Text Core.:>
+               Core.QueryParam "$.xgafv" Xgafv Core.:>
+                 Core.QueryParam "access_token" Core.Text Core.:>
+                   Core.QueryParam "callback" Core.Text Core.:>
+                     Core.QueryParam "uploadType" Core.Text Core.:>
+                       Core.QueryParam "upload_protocol" Core.Text Core.:>
+                         Core.QueryParam "alt" Core.AltJSON Core.:>
+                           Core.Get '[Core.JSON] Page
 
 -- | Gets the latest version of the specified page in the presentation.
 --
 -- /See:/ 'newSlidesPresentationsPagesGet' smart constructor.
 data SlidesPresentationsPagesGet = SlidesPresentationsPagesGet
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | The object ID of the page to retrieve.
-    pageObjectId :: Core.Text,
-    -- | The ID of the presentation to retrieve.
-    presentationId :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | The object ID of the page to retrieve.
+    , pageObjectId :: Core.Text
+      -- | The ID of the presentation to retrieve.
+    , presentationId :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'SlidesPresentationsPagesGet' with the minimum fields required to make a request.
-newSlidesPresentationsPagesGet ::
-  -- |  The object ID of the page to retrieve. See 'pageObjectId'.
-  Core.Text ->
-  -- |  The ID of the presentation to retrieve. See 'presentationId'.
-  Core.Text ->
-  SlidesPresentationsPagesGet
+newSlidesPresentationsPagesGet 
+    ::  Core.Text
+       -- ^  The object ID of the page to retrieve. See 'pageObjectId'.
+    -> Core.Text
+       -- ^  The ID of the presentation to retrieve. See 'presentationId'.
+    -> SlidesPresentationsPagesGet
 newSlidesPresentationsPagesGet pageObjectId presentationId =
   SlidesPresentationsPagesGet
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      pageObjectId = pageObjectId,
-      presentationId = presentationId,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , pageObjectId = pageObjectId
+    , presentationId = presentationId
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    SlidesPresentationsPagesGet
-  where
-  type Rs SlidesPresentationsPagesGet = Page
-  type
-    Scopes SlidesPresentationsPagesGet =
-      '[ "https://www.googleapis.com/auth/drive",
-         "https://www.googleapis.com/auth/drive.file",
-         "https://www.googleapis.com/auth/drive.readonly",
-         "https://www.googleapis.com/auth/presentations",
-         "https://www.googleapis.com/auth/presentations.readonly"
-       ]
-  requestClient SlidesPresentationsPagesGet {..} =
-    go
-      presentationId
-      pageObjectId
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      slidesService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy SlidesPresentationsPagesGetResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           SlidesPresentationsPagesGet
+         where
+        type Rs SlidesPresentationsPagesGet = Page
+        type Scopes SlidesPresentationsPagesGet =
+             '["https://www.googleapis.com/auth/drive",
+               "https://www.googleapis.com/auth/drive.file",
+               "https://www.googleapis.com/auth/drive.readonly",
+               "https://www.googleapis.com/auth/presentations",
+               "https://www.googleapis.com/auth/presentations.readonly"]
+        requestClient SlidesPresentationsPagesGet{..}
+          = go presentationId pageObjectId xgafv accessToken
+              callback
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              slidesService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy SlidesPresentationsPagesGetResource)
+                      Core.mempty
+
