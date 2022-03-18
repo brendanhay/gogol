9ddb61120b268f39fb81fe883c2bd98c3f32b0df
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:03 +0100

    gogol-driveactivity: regenerating service with updated namespace

--- lib/services/gogol-driveactivity/gen/Gogol/DriveActivity/Activity/Query.hs
+++ lib/services/gogol-driveactivity/gen/Gogol/DriveActivity/Activity/Query.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,92 +36,85 @@
 --
 -- /See:/ <https://developers.google.com/drive/activity/ Drive Activity API Reference> for @driveactivity.activity.query@.
 module Gogol.DriveActivity.Activity.Query
-  ( -- * Resource
-    DriveActivityActivityQueryResource,
+    (
+    -- * Resource
+      DriveActivityActivityQueryResource
 
     -- ** Constructing a Request
-    newDriveActivityActivityQuery,
-    DriveActivityActivityQuery,
-  )
-where
+    , newDriveActivityActivityQuery
+    , DriveActivityActivityQuery
+    ) where
 
-import Gogol.DriveActivity.Types
 import qualified Gogol.Prelude as Core
+import Gogol.DriveActivity.Types
 
 -- | A resource alias for @driveactivity.activity.query@ method which the
 -- 'DriveActivityActivityQuery' request conforms to.
 type DriveActivityActivityQueryResource =
-  "v2"
-    Core.:> "activity:query"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] QueryDriveActivityRequest
-    Core.:> Core.Post '[Core.JSON] QueryDriveActivityResponse
+     "v2" Core.:>
+       "activity:query" Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "uploadType" Core.Text Core.:>
+                 Core.QueryParam "upload_protocol" Core.Text Core.:>
+                   Core.QueryParam "alt" Core.AltJSON Core.:>
+                     Core.ReqBody '[Core.JSON] QueryDriveActivityRequest
+                       Core.:>
+                       Core.Post '[Core.JSON] QueryDriveActivityResponse
 
 -- | Query past activity in Google Drive.
 --
 -- /See:/ 'newDriveActivityActivityQuery' smart constructor.
 data DriveActivityActivityQuery = DriveActivityActivityQuery
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Multipart request metadata.
-    payload :: QueryDriveActivityRequest,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Multipart request metadata.
+    , payload :: QueryDriveActivityRequest
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'DriveActivityActivityQuery' with the minimum fields required to make a request.
-newDriveActivityActivityQuery ::
-  -- |  Multipart request metadata. See 'payload'.
-  QueryDriveActivityRequest ->
-  DriveActivityActivityQuery
+newDriveActivityActivityQuery 
+    ::  QueryDriveActivityRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> DriveActivityActivityQuery
 newDriveActivityActivityQuery payload =
   DriveActivityActivityQuery
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      payload = payload,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , payload = payload
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    DriveActivityActivityQuery
-  where
-  type
-    Rs DriveActivityActivityQuery =
-      QueryDriveActivityResponse
-  type
-    Scopes DriveActivityActivityQuery =
-      '[ "https://www.googleapis.com/auth/drive.activity",
-         "https://www.googleapis.com/auth/drive.activity.readonly"
-       ]
-  requestClient DriveActivityActivityQuery {..} =
-    go
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      payload
-      driveActivityService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy DriveActivityActivityQueryResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           DriveActivityActivityQuery
+         where
+        type Rs DriveActivityActivityQuery =
+             QueryDriveActivityResponse
+        type Scopes DriveActivityActivityQuery =
+             '["https://www.googleapis.com/auth/drive.activity",
+               "https://www.googleapis.com/auth/drive.activity.readonly"]
+        requestClient DriveActivityActivityQuery{..}
+          = go xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              driveActivityService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy DriveActivityActivityQueryResource)
+                      Core.mempty
+
