3182a608431fa060c7456a449bb36b81af791714
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:41:54 +0100

    gogol-accessapproval: regenerating service with updated namespace

--- lib/services/gogol-accessapproval/gen/Gogol/AccessApproval/Organizations/ApprovalRequests/Get.hs
+++ lib/services/gogol-accessapproval/gen/Gogol/AccessApproval/Organizations/ApprovalRequests/Get.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,93 +36,86 @@
 --
 -- /See:/ <https://cloud.google.com/cloud-provider-access-management/access-approval/docs Access Approval API Reference> for @accessapproval.organizations.approvalRequests.get@.
 module Gogol.AccessApproval.Organizations.ApprovalRequests.Get
-  ( -- * Resource
-    AccessApprovalOrganizationsApprovalRequestsGetResource,
+    (
+    -- * Resource
+      AccessApprovalOrganizationsApprovalRequestsGetResource
 
     -- ** Constructing a Request
-    newAccessApprovalOrganizationsApprovalRequestsGet,
-    AccessApprovalOrganizationsApprovalRequestsGet,
-  )
-where
+    , newAccessApprovalOrganizationsApprovalRequestsGet
+    , AccessApprovalOrganizationsApprovalRequestsGet
+    ) where
 
-import Gogol.AccessApproval.Types
 import qualified Gogol.Prelude as Core
+import Gogol.AccessApproval.Types
 
 -- | A resource alias for @accessapproval.organizations.approvalRequests.get@ method which the
 -- 'AccessApprovalOrganizationsApprovalRequestsGet' request conforms to.
-type AccessApprovalOrganizationsApprovalRequestsGetResource =
-  "v1"
-    Core.:> Core.Capture "name" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] ApprovalRequest
+type AccessApprovalOrganizationsApprovalRequestsGetResource
+     =
+     "v1" Core.:>
+       Core.Capture "name" Core.Text Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "uploadType" Core.Text Core.:>
+                 Core.QueryParam "upload_protocol" Core.Text Core.:>
+                   Core.QueryParam "alt" Core.AltJSON Core.:>
+                     Core.Get '[Core.JSON] ApprovalRequest
 
 -- | Gets an approval request. Returns NOT_FOUND if the request does not exist.
 --
 -- /See:/ 'newAccessApprovalOrganizationsApprovalRequestsGet' smart constructor.
 data AccessApprovalOrganizationsApprovalRequestsGet = AccessApprovalOrganizationsApprovalRequestsGet
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | The name of the approval request to retrieve. Format: \"{projects|folders|organizations}\/{id}\/approvalRequests\/{approval_request}\"
-    name :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | The name of the approval request to retrieve. Format: \"{projects|folders|organizations}\/{id}\/approvalRequests\/{approval_request}\"
+    , name :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'AccessApprovalOrganizationsApprovalRequestsGet' with the minimum fields required to make a request.
-newAccessApprovalOrganizationsApprovalRequestsGet ::
-  -- |  The name of the approval request to retrieve. Format: \"{projects|folders|organizations}\/{id}\/approvalRequests\/{approval_request}\" See 'name'.
-  Core.Text ->
-  AccessApprovalOrganizationsApprovalRequestsGet
+newAccessApprovalOrganizationsApprovalRequestsGet 
+    ::  Core.Text
+       -- ^  The name of the approval request to retrieve. Format: \"{projects|folders|organizations}\/{id}\/approvalRequests\/{approval_request}\" See 'name'.
+    -> AccessApprovalOrganizationsApprovalRequestsGet
 newAccessApprovalOrganizationsApprovalRequestsGet name =
   AccessApprovalOrganizationsApprovalRequestsGet
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      name = name,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , name = name
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    AccessApprovalOrganizationsApprovalRequestsGet
-  where
-  type
-    Rs
-      AccessApprovalOrganizationsApprovalRequestsGet =
-      ApprovalRequest
-  type
-    Scopes
-      AccessApprovalOrganizationsApprovalRequestsGet =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient
-    AccessApprovalOrganizationsApprovalRequestsGet {..} =
-      go
-        name
-        xgafv
-        accessToken
-        callback
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        accessApprovalService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  AccessApprovalOrganizationsApprovalRequestsGetResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           AccessApprovalOrganizationsApprovalRequestsGet
+         where
+        type Rs
+               AccessApprovalOrganizationsApprovalRequestsGet
+             = ApprovalRequest
+        type Scopes
+               AccessApprovalOrganizationsApprovalRequestsGet
+             = '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient
+          AccessApprovalOrganizationsApprovalRequestsGet{..}
+          = go name xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              accessApprovalService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           AccessApprovalOrganizationsApprovalRequestsGetResource)
+                      Core.mempty
+
--- lib/services/gogol-accessapproval/gen/Gogol/AccessApproval/Projects/DeleteAccessApprovalSettings.hs
+++ lib/services/gogol-accessapproval/gen/Gogol/AccessApproval/Projects/DeleteAccessApprovalSettings.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,93 +36,86 @@
 --
 -- /See:/ <https://cloud.google.com/cloud-provider-access-management/access-approval/docs Access Approval API Reference> for @accessapproval.projects.deleteAccessApprovalSettings@.
 module Gogol.AccessApproval.Projects.DeleteAccessApprovalSettings
-  ( -- * Resource
-    AccessApprovalProjectsDeleteAccessApprovalSettingsResource,
+    (
+    -- * Resource
+      AccessApprovalProjectsDeleteAccessApprovalSettingsResource
 
     -- ** Constructing a Request
-    newAccessApprovalProjectsDeleteAccessApprovalSettings,
-    AccessApprovalProjectsDeleteAccessApprovalSettings,
-  )
-where
+    , newAccessApprovalProjectsDeleteAccessApprovalSettings
+    , AccessApprovalProjectsDeleteAccessApprovalSettings
+    ) where
 
-import Gogol.AccessApproval.Types
 import qualified Gogol.Prelude as Core
+import Gogol.AccessApproval.Types
 
 -- | A resource alias for @accessapproval.projects.deleteAccessApprovalSettings@ method which the
 -- 'AccessApprovalProjectsDeleteAccessApprovalSettings' request conforms to.
-type AccessApprovalProjectsDeleteAccessApprovalSettingsResource =
-  "v1"
-    Core.:> Core.Capture "name" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Delete '[Core.JSON] Empty
+type AccessApprovalProjectsDeleteAccessApprovalSettingsResource
+     =
+     "v1" Core.:>
+       Core.Capture "name" Core.Text Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "uploadType" Core.Text Core.:>
+                 Core.QueryParam "upload_protocol" Core.Text Core.:>
+                   Core.QueryParam "alt" Core.AltJSON Core.:>
+                     Core.Delete '[Core.JSON] Empty
 
 -- | Deletes the settings associated with a project, folder, or organization. This will have the effect of disabling Access Approval for the project, folder, or organization, but only if all ancestors also have Access Approval disabled. If Access Approval is enabled at a higher level of the hierarchy, then Access Approval will still be enabled at this level as the settings are inherited.
 --
 -- /See:/ 'newAccessApprovalProjectsDeleteAccessApprovalSettings' smart constructor.
 data AccessApprovalProjectsDeleteAccessApprovalSettings = AccessApprovalProjectsDeleteAccessApprovalSettings
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Name of the AccessApprovalSettings to delete.
-    name :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Name of the AccessApprovalSettings to delete.
+    , name :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'AccessApprovalProjectsDeleteAccessApprovalSettings' with the minimum fields required to make a request.
-newAccessApprovalProjectsDeleteAccessApprovalSettings ::
-  -- |  Name of the AccessApprovalSettings to delete. See 'name'.
-  Core.Text ->
-  AccessApprovalProjectsDeleteAccessApprovalSettings
+newAccessApprovalProjectsDeleteAccessApprovalSettings 
+    ::  Core.Text
+       -- ^  Name of the AccessApprovalSettings to delete. See 'name'.
+    -> AccessApprovalProjectsDeleteAccessApprovalSettings
 newAccessApprovalProjectsDeleteAccessApprovalSettings name =
   AccessApprovalProjectsDeleteAccessApprovalSettings
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      name = name,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , name = name
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    AccessApprovalProjectsDeleteAccessApprovalSettings
-  where
-  type
-    Rs
-      AccessApprovalProjectsDeleteAccessApprovalSettings =
-      Empty
-  type
-    Scopes
-      AccessApprovalProjectsDeleteAccessApprovalSettings =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient
-    AccessApprovalProjectsDeleteAccessApprovalSettings {..} =
-      go
-        name
-        xgafv
-        accessToken
-        callback
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        accessApprovalService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  AccessApprovalProjectsDeleteAccessApprovalSettingsResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           AccessApprovalProjectsDeleteAccessApprovalSettings
+         where
+        type Rs
+               AccessApprovalProjectsDeleteAccessApprovalSettings
+             = Empty
+        type Scopes
+               AccessApprovalProjectsDeleteAccessApprovalSettings
+             = '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient
+          AccessApprovalProjectsDeleteAccessApprovalSettings{..}
+          = go name xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              accessApprovalService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           AccessApprovalProjectsDeleteAccessApprovalSettingsResource)
+                      Core.mempty
+
