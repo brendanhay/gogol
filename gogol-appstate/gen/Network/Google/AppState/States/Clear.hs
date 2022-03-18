0cd97cc272ceadf943475265b1ee6025b2ef1db2
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 11:23:02 +0100

    gogol-appstate: regenerating service

---
+++ gogol-appstate/gen/Network/Google/AppState/States/Clear.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Network.Google.AppState.States.Clear
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Clears (sets to empty) the data for the passed key if and only if the passed version matches the currently stored version. This method results in a conflict error on version mismatch.
+--
+-- /See:/ <https://developers.google.com/games/services/web/api/states Google App State API Reference> for @appstate.states.clear@.
+module Network.Google.AppState.States.Clear
+  ( -- * Resource
+    AppStateStatesClearResource,
+
+    -- ** Constructing a Request
+    newAppStateStatesClear,
+    AppStateStatesClear,
+  )
+where
+
+import Network.Google.AppState.Types
+import qualified Network.Google.Prelude as Core
+
+-- | A resource alias for @appstate.states.clear@ method which the
+-- 'AppStateStatesClear' request conforms to.
+type AppStateStatesClearResource =
+  "appstate"
+    Core.:> "v1"
+    Core.:> "states"
+    Core.:> Core.Capture "stateKey" Core.Int32
+    Core.:> "clear"
+    Core.:> Core.QueryParam "currentDataVersion" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Post '[Core.JSON] WriteResult
+
+-- | Clears (sets to empty) the data for the passed key if and only if the passed version matches the currently stored version. This method results in a conflict error on version mismatch.
+--
+-- /See:/ 'newAppStateStatesClear' smart constructor.
+data AppStateStatesClear = AppStateStatesClear
+  { -- | The version of the data to be cleared. Version strings are returned by the server.
+    currentDataVersion :: (Core.Maybe Core.Text),
+    -- | The key for the data to be retrieved.
+    stateKey :: Core.Int32
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'AppStateStatesClear' with the minimum fields required to make a request.
+newAppStateStatesClear ::
+  -- |  The key for the data to be retrieved. See 'stateKey'.
+  Core.Int32 ->
+  AppStateStatesClear
+newAppStateStatesClear stateKey =
+  AppStateStatesClear {currentDataVersion = Core.Nothing, stateKey = stateKey}
+
+instance Core.GoogleRequest AppStateStatesClear where
+  type Rs AppStateStatesClear = WriteResult
+  type
+    Scopes AppStateStatesClear =
+      '["https://www.googleapis.com/auth/appstate"]
+  requestClient AppStateStatesClear {..} =
+    go
+      stateKey
+      currentDataVersion
+      (Core.Just Core.AltJSON)
+      appStateService
+    where
+      go =
+        Core.buildClient
+          ( Core.Proxy ::
+              Core.Proxy AppStateStatesClearResource
+          )
+          Core.mempty
--- gogol-appstate/gen/Network/Google/AppState/Types.hs
+++ gogol-appstate/gen/Network/Google/AppState/Types.hs
@@ -1,66 +1,68 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DataKinds          #-}
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE NoImplicitPrelude  #-}
-{-# LANGUAGE OverloadedStrings  #-}
-
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 -- |
 -- Module      : Network.Google.AppState.Types
--- Copyright   : (c) 2015-2016 Brendan Hay
+-- Copyright   : (c) 2015-2022 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
---
 module Network.Google.AppState.Types
-    (
-    -- * Service Configuration
-      appStateService
+  ( -- * Configuration
+    appStateService,
 
     -- * OAuth Scopes
-    , appStateScope
+    appStateScope,
 
-    -- * WriteResult
-    , WriteResult
-    , writeResult
-    , wrCurrentStateVersion
-    , wrKind
-    , wrStateKey
+    -- * Types
 
-    -- * ListResponse
-    , ListResponse
-    , listResponse
-    , lrMaximumKeyCount
-    , lrKind
-    , lrItems
+    -- ** GetResponse
+    GetResponse (..),
+    newGetResponse,
 
-    -- * GetResponse
-    , GetResponse
-    , getResponse
-    , grCurrentStateVersion
-    , grKind
-    , grData
-    , grStateKey
+    -- ** ListResponse
+    ListResponse (..),
+    newListResponse,
 
-    -- * UpdateRequest
-    , UpdateRequest
-    , updateRequest
-    , urKind
-    , urData
-    ) where
+    -- ** UpdateRequest
+    UpdateRequest (..),
+    newUpdateRequest,
 
-import Network.Google.AppState.Types.Product
-import Network.Google.AppState.Types.Sum
-import Network.Google.Prelude
+    -- ** WriteResult
+    WriteResult (..),
+    newWriteResult,
+  )
+where
 
--- | Default request referring to version 'v1' of the Google App State API. This contains the host and root path used as a starting point for constructing service requests.
-appStateService :: ServiceConfig
-appStateService
-  = defaultService (ServiceId "appstate:v1")
-      "www.googleapis.com"
+import Network.Google.AppState.Internal.Product
+import Network.Google.AppState.Internal.Sum
+import qualified Network.Google.Prelude as Core
+
+-- | Default request referring to version @v1@ of the Google App State API. This contains the host and root path used as a starting point for constructing service requests.
+appStateService :: Core.ServiceConfig
+appStateService =
+  Core.defaultService
+    (Core.ServiceId "appstate:v1")
+    "www.googleapis.com"
 
 -- | View and manage your data for this application
-appStateScope :: Proxy '["https://www.googleapis.com/auth/appstate"]
-appStateScope = Proxy
+appStateScope :: Core.Proxy '["https://www.googleapis.com/auth/appstate"]
+appStateScope = Core.Proxy
