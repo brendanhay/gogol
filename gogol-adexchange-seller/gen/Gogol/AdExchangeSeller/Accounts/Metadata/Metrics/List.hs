6b4e0a87dc500aaeafd2718fa3b5b476e5449912
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 12:30:12 +0100

    gogol-adexchange-seller: regenerating service with updated namespace

---
+++ gogol-adexchange-seller/gen/Gogol/AdExchangeSeller/Accounts/Metadata/Metrics/List.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.AdExchangeSeller.Accounts.Metadata.Metrics.List
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- List the metadata for the metrics available to this AdExchange account.
+--
+-- /See:/ <https://developers.google.com/ad-exchange/seller-rest/ Ad Exchange Seller API Reference> for @adexchangeseller.accounts.metadata.metrics.list@.
+module Gogol.AdExchangeSeller.Accounts.Metadata.Metrics.List
+  ( -- * Resource
+    AdExchangeSellerAccountsMetadataMetricsListResource,
+
+    -- ** Constructing a Request
+    newAdExchangeSellerAccountsMetadataMetricsList,
+    AdExchangeSellerAccountsMetadataMetricsList,
+  )
+where
+
+import Gogol.AdExchangeSeller.Types
+import qualified Gogol.Prelude as Core
+
+-- | A resource alias for @adexchangeseller.accounts.metadata.metrics.list@ method which the
+-- 'AdExchangeSellerAccountsMetadataMetricsList' request conforms to.
+type AdExchangeSellerAccountsMetadataMetricsListResource =
+  "adexchangeseller"
+    Core.:> "v2.0"
+    Core.:> "accounts"
+    Core.:> Core.Capture "accountId" Core.Text
+    Core.:> "metadata"
+    Core.:> "metrics"
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Get '[Core.JSON] Metadata
+
+-- | List the metadata for the metrics available to this AdExchange account.
+--
+-- /See:/ 'newAdExchangeSellerAccountsMetadataMetricsList' smart constructor.
+newtype AdExchangeSellerAccountsMetadataMetricsList = AdExchangeSellerAccountsMetadataMetricsList
+  { -- | Account with visibility to the metrics.
+    accountId :: Core.Text
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'AdExchangeSellerAccountsMetadataMetricsList' with the minimum fields required to make a request.
+newAdExchangeSellerAccountsMetadataMetricsList ::
+  -- |  Account with visibility to the metrics. See 'accountId'.
+  Core.Text ->
+  AdExchangeSellerAccountsMetadataMetricsList
+newAdExchangeSellerAccountsMetadataMetricsList accountId =
+  AdExchangeSellerAccountsMetadataMetricsList {accountId = accountId}
+
+instance
+  Core.GoogleRequest
+    AdExchangeSellerAccountsMetadataMetricsList
+  where
+  type
+    Rs AdExchangeSellerAccountsMetadataMetricsList =
+      Metadata
+  type
+    Scopes
+      AdExchangeSellerAccountsMetadataMetricsList =
+      '[ "https://www.googleapis.com/auth/adexchange.seller",
+         "https://www.googleapis.com/auth/adexchange.seller.readonly"
+       ]
+  requestClient
+    AdExchangeSellerAccountsMetadataMetricsList {..} =
+      go
+        accountId
+        (Core.Just Core.AltJSON)
+        adExchangeSellerService
+      where
+        go =
+          Core.buildClient
+            ( Core.Proxy ::
+                Core.Proxy
+                  AdExchangeSellerAccountsMetadataMetricsListResource
+            )
+            Core.mempty
---
+++ gogol-adexchange-seller/gen/Gogol/AdExchangeSeller/Accounts/Urlchannels/List.hs
@@ -0,0 +1,118 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.AdExchangeSeller.Accounts.Urlchannels.List
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- List all URL channels in the specified ad client for this Ad Exchange account.
+--
+-- /See:/ <https://developers.google.com/ad-exchange/seller-rest/ Ad Exchange Seller API Reference> for @adexchangeseller.accounts.urlchannels.list@.
+module Gogol.AdExchangeSeller.Accounts.Urlchannels.List
+  ( -- * Resource
+    AdExchangeSellerAccountsUrlchannelsListResource,
+
+    -- ** Constructing a Request
+    newAdExchangeSellerAccountsUrlchannelsList,
+    AdExchangeSellerAccountsUrlchannelsList,
+  )
+where
+
+import Gogol.AdExchangeSeller.Types
+import qualified Gogol.Prelude as Core
+
+-- | A resource alias for @adexchangeseller.accounts.urlchannels.list@ method which the
+-- 'AdExchangeSellerAccountsUrlchannelsList' request conforms to.
+type AdExchangeSellerAccountsUrlchannelsListResource =
+  "adexchangeseller"
+    Core.:> "v2.0"
+    Core.:> "accounts"
+    Core.:> Core.Capture "accountId" Core.Text
+    Core.:> "adclients"
+    Core.:> Core.Capture "adClientId" Core.Text
+    Core.:> "urlchannels"
+    Core.:> Core.QueryParam "maxResults" Core.Word32
+    Core.:> Core.QueryParam "pageToken" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Get '[Core.JSON] UrlChannels
+
+-- | List all URL channels in the specified ad client for this Ad Exchange account.
+--
+-- /See:/ 'newAdExchangeSellerAccountsUrlchannelsList' smart constructor.
+data AdExchangeSellerAccountsUrlchannelsList = AdExchangeSellerAccountsUrlchannelsList
+  { -- | Account to which the ad client belongs.
+    accountId :: Core.Text,
+    -- | Ad client for which to list URL channels.
+    adClientId :: Core.Text,
+    -- | The maximum number of URL channels to include in the response, used for paging.
+    maxResults :: (Core.Maybe Core.Word32),
+    -- | A continuation token, used to page through URL channels. To retrieve the next page, set this parameter to the value of \"nextPageToken\" from the previous response.
+    pageToken :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'AdExchangeSellerAccountsUrlchannelsList' with the minimum fields required to make a request.
+newAdExchangeSellerAccountsUrlchannelsList ::
+  -- |  Account to which the ad client belongs. See 'accountId'.
+  Core.Text ->
+  -- |  Ad client for which to list URL channels. See 'adClientId'.
+  Core.Text ->
+  AdExchangeSellerAccountsUrlchannelsList
+newAdExchangeSellerAccountsUrlchannelsList accountId adClientId =
+  AdExchangeSellerAccountsUrlchannelsList
+    { accountId = accountId,
+      adClientId = adClientId,
+      maxResults = Core.Nothing,
+      pageToken = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    AdExchangeSellerAccountsUrlchannelsList
+  where
+  type
+    Rs AdExchangeSellerAccountsUrlchannelsList =
+      UrlChannels
+  type
+    Scopes AdExchangeSellerAccountsUrlchannelsList =
+      '[ "https://www.googleapis.com/auth/adexchange.seller",
+         "https://www.googleapis.com/auth/adexchange.seller.readonly"
+       ]
+  requestClient
+    AdExchangeSellerAccountsUrlchannelsList {..} =
+      go
+        accountId
+        adClientId
+        maxResults
+        pageToken
+        (Core.Just Core.AltJSON)
+        adExchangeSellerService
+      where
+        go =
+          Core.buildClient
+            ( Core.Proxy ::
+                Core.Proxy
+                  AdExchangeSellerAccountsUrlchannelsListResource
+            )
+            Core.mempty
