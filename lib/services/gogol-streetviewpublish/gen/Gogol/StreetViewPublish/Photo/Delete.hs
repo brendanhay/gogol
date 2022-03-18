9e5100be95484b5ec6cfd0f05a0ab0c11472c313
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:12 +0100

    gogol-streetviewpublish: regenerating service with updated namespace

--- lib/services/gogol-streetviewpublish/gen/Gogol/StreetViewPublish/Photo/Delete.hs
+++ lib/services/gogol-streetviewpublish/gen/Gogol/StreetViewPublish/Photo/Delete.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://developers.google.com/streetview/publish/ Street View Publish API Reference> for @streetviewpublish.photo.delete@.
 module Gogol.StreetViewPublish.Photo.Delete
-  ( -- * Resource
-    StreetViewPublishPhotoDeleteResource,
+    (
+    -- * Resource
+      StreetViewPublishPhotoDeleteResource
 
     -- ** Constructing a Request
-    newStreetViewPublishPhotoDelete,
-    StreetViewPublishPhotoDelete,
-  )
-where
+    , newStreetViewPublishPhotoDelete
+    , StreetViewPublishPhotoDelete
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.StreetViewPublish.Types
@@ -45,73 +51,66 @@ import Gogol.StreetViewPublish.Types
 -- | A resource alias for @streetviewpublish.photo.delete@ method which the
 -- 'StreetViewPublishPhotoDelete' request conforms to.
 type StreetViewPublishPhotoDeleteResource =
-  "v1"
-    Core.:> "photo"
-    Core.:> Core.Capture "photoId" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Delete '[Core.JSON] Empty
+     "v1" Core.:>
+       "photo" Core.:>
+         Core.Capture "photoId" Core.Text Core.:>
+           Core.QueryParam "$.xgafv" Xgafv Core.:>
+             Core.QueryParam "access_token" Core.Text Core.:>
+               Core.QueryParam "callback" Core.Text Core.:>
+                 Core.QueryParam "uploadType" Core.Text Core.:>
+                   Core.QueryParam "upload_protocol" Core.Text Core.:>
+                     Core.QueryParam "alt" Core.AltJSON Core.:>
+                       Core.Delete '[Core.JSON] Empty
 
 -- | Deletes a Photo and its metadata. This method returns the following error codes: * google.rpc.Code.PERMISSION/DENIED if the requesting user did not create the requested photo. * google.rpc.Code.NOT/FOUND if the photo ID does not exist.
 --
 -- /See:/ 'newStreetViewPublishPhotoDelete' smart constructor.
 data StreetViewPublishPhotoDelete = StreetViewPublishPhotoDelete
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Required. ID of the Photo.
-    photoId :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Required. ID of the Photo.
+    , photoId :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'StreetViewPublishPhotoDelete' with the minimum fields required to make a request.
-newStreetViewPublishPhotoDelete ::
-  -- |  Required. ID of the Photo. See 'photoId'.
-  Core.Text ->
-  StreetViewPublishPhotoDelete
+newStreetViewPublishPhotoDelete 
+    ::  Core.Text
+       -- ^  Required. ID of the Photo. See 'photoId'.
+    -> StreetViewPublishPhotoDelete
 newStreetViewPublishPhotoDelete photoId =
   StreetViewPublishPhotoDelete
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      photoId = photoId,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , photoId = photoId
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    StreetViewPublishPhotoDelete
-  where
-  type Rs StreetViewPublishPhotoDelete = Empty
-  type
-    Scopes StreetViewPublishPhotoDelete =
-      '["https://www.googleapis.com/auth/streetviewpublish"]
-  requestClient StreetViewPublishPhotoDelete {..} =
-    go
-      photoId
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      streetViewPublishService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy StreetViewPublishPhotoDeleteResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           StreetViewPublishPhotoDelete
+         where
+        type Rs StreetViewPublishPhotoDelete = Empty
+        type Scopes StreetViewPublishPhotoDelete =
+             '["https://www.googleapis.com/auth/streetviewpublish"]
+        requestClient StreetViewPublishPhotoDelete{..}
+          = go photoId xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              streetViewPublishService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy StreetViewPublishPhotoDeleteResource)
+                      Core.mempty
+
--- lib/services/gogol-streetviewpublish/gen/Gogol/StreetViewPublish/Photo/StartUpload.hs
+++ lib/services/gogol-streetviewpublish/gen/Gogol/StreetViewPublish/Photo/StartUpload.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://developers.google.com/streetview/publish/ Street View Publish API Reference> for @streetviewpublish.photo.startUpload@.
 module Gogol.StreetViewPublish.Photo.StartUpload
-  ( -- * Resource
-    StreetViewPublishPhotoStartUploadResource,
+    (
+    -- * Resource
+      StreetViewPublishPhotoStartUploadResource
 
     -- ** Constructing a Request
-    newStreetViewPublishPhotoStartUpload,
-    StreetViewPublishPhotoStartUpload,
-  )
-where
+    , newStreetViewPublishPhotoStartUpload
+    , StreetViewPublishPhotoStartUpload
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.StreetViewPublish.Types
@@ -45,73 +51,67 @@ import Gogol.StreetViewPublish.Types
 -- | A resource alias for @streetviewpublish.photo.startUpload@ method which the
 -- 'StreetViewPublishPhotoStartUpload' request conforms to.
 type StreetViewPublishPhotoStartUploadResource =
-  "v1"
-    Core.:> "photo:startUpload"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] Empty
-    Core.:> Core.Post '[Core.JSON] UploadRef
+     "v1" Core.:>
+       "photo:startUpload" Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "uploadType" Core.Text Core.:>
+                 Core.QueryParam "upload_protocol" Core.Text Core.:>
+                   Core.QueryParam "alt" Core.AltJSON Core.:>
+                     Core.ReqBody '[Core.JSON] Empty Core.:>
+                       Core.Post '[Core.JSON] UploadRef
 
 -- | Creates an upload session to start uploading photo bytes. The method uses the upload URL of the returned UploadRef to upload the bytes for the Photo. In addition to the photo requirements shown in https:\/\/support.google.com\/maps\/answer\/7012050?ref/topic=6275604, the photo must meet the following requirements: * Photo Sphere XMP metadata must be included in the photo metadata. See https:\/\/developers.google.com\/streetview\/spherical-metadata for the required fields. * The pixel size of the photo must meet the size requirements listed in https:\/\/support.google.com\/maps\/answer\/7012050?ref/topic=6275604, and the photo must be a full 360 horizontally. After the upload completes, the method uses UploadRef with CreatePhoto to create the Photo object entry.
 --
 -- /See:/ 'newStreetViewPublishPhotoStartUpload' smart constructor.
 data StreetViewPublishPhotoStartUpload = StreetViewPublishPhotoStartUpload
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Multipart request metadata.
-    payload :: Empty,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Multipart request metadata.
+    , payload :: Empty
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'StreetViewPublishPhotoStartUpload' with the minimum fields required to make a request.
-newStreetViewPublishPhotoStartUpload ::
-  -- |  Multipart request metadata. See 'payload'.
-  Empty ->
-  StreetViewPublishPhotoStartUpload
+newStreetViewPublishPhotoStartUpload 
+    ::  Empty
+       -- ^  Multipart request metadata. See 'payload'.
+    -> StreetViewPublishPhotoStartUpload
 newStreetViewPublishPhotoStartUpload payload =
   StreetViewPublishPhotoStartUpload
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      payload = payload,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , payload = payload
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    StreetViewPublishPhotoStartUpload
-  where
-  type Rs StreetViewPublishPhotoStartUpload = UploadRef
-  type
-    Scopes StreetViewPublishPhotoStartUpload =
-      '["https://www.googleapis.com/auth/streetviewpublish"]
-  requestClient StreetViewPublishPhotoStartUpload {..} =
-    go
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      payload
-      streetViewPublishService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy StreetViewPublishPhotoStartUploadResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           StreetViewPublishPhotoStartUpload
+         where
+        type Rs StreetViewPublishPhotoStartUpload = UploadRef
+        type Scopes StreetViewPublishPhotoStartUpload =
+             '["https://www.googleapis.com/auth/streetviewpublish"]
+        requestClient StreetViewPublishPhotoStartUpload{..}
+          = go xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              streetViewPublishService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy StreetViewPublishPhotoStartUploadResource)
+                      Core.mempty
+
