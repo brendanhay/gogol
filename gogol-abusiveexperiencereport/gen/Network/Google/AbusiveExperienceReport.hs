b4f98202d9c08c7f56bf21bdfbb4316686c44314
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 11:22:59 +0100

    gogol-abusiveexperiencereport: regenerating service

--- gogol-abusiveexperiencereport/gen/Network/Google/AbusiveExperienceReport.hs
+++ gogol-abusiveexperiencereport/gen/Network/Google/AbusiveExperienceReport.hs
@@ -1,75 +1,71 @@
-{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE TypeOperators     #-}
-
-{-# OPTIONS_GHC -fno-warn-unused-imports    #-}
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 -- |
 -- Module      : Network.Google.AbusiveExperienceReport
--- Copyright   : (c) 2015-2016 Brendan Hay
+-- Copyright   : (c) 2015-2022 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 --
--- Views Abusive Experience Report data, and gets a list of sites that have
--- a significant number of abusive experiences.
+-- Views Abusive Experience Report data, and gets a list of sites that have a significant number of abusive experiences.
 --
 -- /See:/ <https://developers.google.com/abusive-experience-report/ Abusive Experience Report API Reference>
 module Network.Google.AbusiveExperienceReport
-    (
-    -- * Service Configuration
-      abusiveExperienceReportService
-
-    -- * API Declaration
-    , AbusiveExperienceReportAPI
+  ( -- * Configuration
+    abusiveExperienceReportService,
 
     -- * Resources
 
     -- ** abusiveexperiencereport.sites.get
-    , module Network.Google.Resource.AbusiveExperienceReport.Sites.Get
+    AbusiveExperienceReportSitesGetResource,
+    newAbusiveExperienceReportSitesGet,
+    AbusiveExperienceReportSitesGet,
 
     -- ** abusiveexperiencereport.violatingSites.list
-    , module Network.Google.Resource.AbusiveExperienceReport.ViolatingSites.List
+    AbusiveExperienceReportViolatingSitesListResource,
+    newAbusiveExperienceReportViolatingSitesList,
+    AbusiveExperienceReportViolatingSitesList,
 
     -- * Types
 
-    -- ** SiteSummaryResponse
-    , SiteSummaryResponse
-    , siteSummaryResponse
-    , ssrAbusiveStatus
-    , ssrEnforcementTime
-    , ssrLastChangeTime
-    , ssrFilterStatus
-    , ssrUnderReview
-    , ssrReportURL
-    , ssrReviewedSite
+    -- ** Xgafv
+    Xgafv (..),
 
-    -- ** SiteSummaryResponseAbusiveStatus
-    , SiteSummaryResponseAbusiveStatus (..)
+    -- ** SiteSummaryResponse
+    SiteSummaryResponse (..),
+    newSiteSummaryResponse,
+
+    -- ** SiteSummaryResponse_AbusiveStatus
+    SiteSummaryResponse_AbusiveStatus (..),
+
+    -- ** SiteSummaryResponse_FilterStatus
+    SiteSummaryResponse_FilterStatus (..),
 
     -- ** ViolatingSitesResponse
-    , ViolatingSitesResponse
-    , violatingSitesResponse
-    , vsrViolatingSites
+    ViolatingSitesResponse (..),
+    newViolatingSitesResponse,
+  )
+where
 
-    -- ** Xgafv
-    , Xgafv (..)
-
-    -- ** SiteSummaryResponseFilterStatus
-    , SiteSummaryResponseFilterStatus (..)
-    ) where
-
-import Network.Google.Prelude
+import Network.Google.AbusiveExperienceReport.Sites.Get
 import Network.Google.AbusiveExperienceReport.Types
-import Network.Google.Resource.AbusiveExperienceReport.Sites.Get
-import Network.Google.Resource.AbusiveExperienceReport.ViolatingSites.List
-
-{- $resources
-TODO
--}
-
--- | Represents the entirety of the methods and resources available for the Abusive Experience Report API service.
-type AbusiveExperienceReportAPI =
-     ViolatingSitesListResource :<|> SitesGetResource
+import Network.Google.AbusiveExperienceReport.ViolatingSites.List
---
+++ gogol-abusiveexperiencereport/gen/Network/Google/AbusiveExperienceReport/Internal/Sum.hs
@@ -0,0 +1,157 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Network.Google.AbusiveExperienceReport.Internal.Sum
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+module Network.Google.AbusiveExperienceReport.Internal.Sum
+  ( -- * Xgafv
+    Xgafv
+      ( Xgafv_1,
+        Xgafv_2,
+        ..
+      ),
+
+    -- * SiteSummaryResponse_AbusiveStatus
+    SiteSummaryResponse_AbusiveStatus
+      ( SiteSummaryResponse_AbusiveStatus_Unknown,
+        SiteSummaryResponse_AbusiveStatus_Passing,
+        SiteSummaryResponse_AbusiveStatus_Failing,
+        ..
+      ),
+
+    -- * SiteSummaryResponse_FilterStatus
+    SiteSummaryResponse_FilterStatus
+      ( SiteSummaryResponse_FilterStatus_Unknown,
+        SiteSummaryResponse_FilterStatus_ON,
+        SiteSummaryResponse_FilterStatus_Off,
+        SiteSummaryResponse_FilterStatus_Paused,
+        SiteSummaryResponse_FilterStatus_Pending,
+        ..
+      ),
+  )
+where
+
+import qualified Network.Google.Prelude as Core
+
+-- | V1 error format.
+newtype Xgafv = Xgafv {fromXgafv :: Core.Text}
+  deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
+  deriving newtype
+    ( Core.Hashable,
+      Core.ToHttpApiData,
+      Core.FromHttpApiData,
+      Core.ToJSON,
+      Core.ToJSONKey,
+      Core.FromJSON,
+      Core.FromJSONKey
+    )
+
+-- | v1 error format
+pattern Xgafv_1 :: Xgafv
+pattern Xgafv_1 = Xgafv "1"
+
+-- | v2 error format
+pattern Xgafv_2 :: Xgafv
+pattern Xgafv_2 = Xgafv "2"
+
+{-# COMPLETE
+  Xgafv_1,
+  Xgafv_2,
+  Xgafv
+  #-}
+
+-- | The site\'s Abusive Experience Report status.
+newtype SiteSummaryResponse_AbusiveStatus = SiteSummaryResponse_AbusiveStatus {fromSiteSummaryResponse_AbusiveStatus :: Core.Text}
+  deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
+  deriving newtype
+    ( Core.Hashable,
+      Core.ToHttpApiData,
+      Core.FromHttpApiData,
+      Core.ToJSON,
+      Core.ToJSONKey,
+      Core.FromJSON,
+      Core.FromJSONKey
+    )
+
+-- | Not reviewed.
+pattern SiteSummaryResponse_AbusiveStatus_Unknown :: SiteSummaryResponse_AbusiveStatus
+pattern SiteSummaryResponse_AbusiveStatus_Unknown = SiteSummaryResponse_AbusiveStatus "UNKNOWN"
+
+-- | Passing.
+pattern SiteSummaryResponse_AbusiveStatus_Passing :: SiteSummaryResponse_AbusiveStatus
+pattern SiteSummaryResponse_AbusiveStatus_Passing = SiteSummaryResponse_AbusiveStatus "PASSING"
+
+-- | Failing.
+pattern SiteSummaryResponse_AbusiveStatus_Failing :: SiteSummaryResponse_AbusiveStatus
+pattern SiteSummaryResponse_AbusiveStatus_Failing = SiteSummaryResponse_AbusiveStatus "FAILING"
+
+{-# COMPLETE
+  SiteSummaryResponse_AbusiveStatus_Unknown,
+  SiteSummaryResponse_AbusiveStatus_Passing,
+  SiteSummaryResponse_AbusiveStatus_Failing,
+  SiteSummaryResponse_AbusiveStatus
+  #-}
+
+-- | The site\'s <https://support.google.com/webtools/answer/7538608 enforcement status>.
+newtype SiteSummaryResponse_FilterStatus = SiteSummaryResponse_FilterStatus {fromSiteSummaryResponse_FilterStatus :: Core.Text}
+  deriving stock (Core.Show, Core.Read, Core.Eq, Core.Ord, Core.Generic)
+  deriving newtype
+    ( Core.Hashable,
+      Core.ToHttpApiData,
+      Core.FromHttpApiData,
+      Core.ToJSON,
+      Core.ToJSONKey,
+      Core.FromJSON,
+      Core.FromJSONKey
+    )
+
+-- | N\/A.
+pattern SiteSummaryResponse_FilterStatus_Unknown :: SiteSummaryResponse_FilterStatus
+pattern SiteSummaryResponse_FilterStatus_Unknown = SiteSummaryResponse_FilterStatus "UNKNOWN"
+
+-- | Enforcement is on.
+pattern SiteSummaryResponse_FilterStatus_ON :: SiteSummaryResponse_FilterStatus
+pattern SiteSummaryResponse_FilterStatus_ON = SiteSummaryResponse_FilterStatus "ON"
+
+-- | Enforcement is off.
+pattern SiteSummaryResponse_FilterStatus_Off :: SiteSummaryResponse_FilterStatus
+pattern SiteSummaryResponse_FilterStatus_Off = SiteSummaryResponse_FilterStatus "OFF"
+
+-- | Enforcement is paused.
+pattern SiteSummaryResponse_FilterStatus_Paused :: SiteSummaryResponse_FilterStatus
+pattern SiteSummaryResponse_FilterStatus_Paused = SiteSummaryResponse_FilterStatus "PAUSED"
+
+-- | Enforcement is pending.
+pattern SiteSummaryResponse_FilterStatus_Pending :: SiteSummaryResponse_FilterStatus
+pattern SiteSummaryResponse_FilterStatus_Pending = SiteSummaryResponse_FilterStatus "PENDING"
+
+{-# COMPLETE
+  SiteSummaryResponse_FilterStatus_Unknown,
+  SiteSummaryResponse_FilterStatus_ON,
+  SiteSummaryResponse_FilterStatus_Off,
+  SiteSummaryResponse_FilterStatus_Paused,
+  SiteSummaryResponse_FilterStatus_Pending,
+  SiteSummaryResponse_FilterStatus
+  #-}
