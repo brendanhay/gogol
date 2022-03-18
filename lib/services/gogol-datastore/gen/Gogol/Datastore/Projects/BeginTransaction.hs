d02fd9480ff64545dd86019bcc3a8830f42ade44
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:02 +0100

    gogol-datastore: regenerating service with updated namespace

--- lib/services/gogol-datastore/gen/Gogol/Datastore/Projects/BeginTransaction.hs
+++ lib/services/gogol-datastore/gen/Gogol/Datastore/Projects/BeginTransaction.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,102 +36,93 @@
 --
 -- /See:/ <https://cloud.google.com/datastore/ Cloud Datastore API Reference> for @datastore.projects.beginTransaction@.
 module Gogol.Datastore.Projects.BeginTransaction
-  ( -- * Resource
-    DatastoreProjectsBeginTransactionResource,
+    (
+    -- * Resource
+      DatastoreProjectsBeginTransactionResource
 
     -- ** Constructing a Request
-    newDatastoreProjectsBeginTransaction,
-    DatastoreProjectsBeginTransaction,
-  )
-where
+    , newDatastoreProjectsBeginTransaction
+    , DatastoreProjectsBeginTransaction
+    ) where
 
-import Gogol.Datastore.Types
 import qualified Gogol.Prelude as Core
+import Gogol.Datastore.Types
 
 -- | A resource alias for @datastore.projects.beginTransaction@ method which the
 -- 'DatastoreProjectsBeginTransaction' request conforms to.
 type DatastoreProjectsBeginTransactionResource =
-  "v1"
-    Core.:> "projects"
-    Core.:> Core.CaptureMode
-              "projectId"
-              "beginTransaction"
-              Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] BeginTransactionRequest
-    Core.:> Core.Post '[Core.JSON] BeginTransactionResponse
+     "v1" Core.:>
+       "projects" Core.:>
+         Core.CaptureMode "projectId" "beginTransaction"
+           Core.Text
+           Core.:>
+           Core.QueryParam "$.xgafv" Xgafv Core.:>
+             Core.QueryParam "access_token" Core.Text Core.:>
+               Core.QueryParam "callback" Core.Text Core.:>
+                 Core.QueryParam "uploadType" Core.Text Core.:>
+                   Core.QueryParam "upload_protocol" Core.Text Core.:>
+                     Core.QueryParam "alt" Core.AltJSON Core.:>
+                       Core.ReqBody '[Core.JSON] BeginTransactionRequest
+                         Core.:>
+                         Core.Post '[Core.JSON] BeginTransactionResponse
 
 -- | Begins a new transaction.
 --
 -- /See:/ 'newDatastoreProjectsBeginTransaction' smart constructor.
 data DatastoreProjectsBeginTransaction = DatastoreProjectsBeginTransaction
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Multipart request metadata.
-    payload :: BeginTransactionRequest,
-    -- | Required. The ID of the project against which to make the request.
-    projectId :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Multipart request metadata.
+    , payload :: BeginTransactionRequest
+      -- | Required. The ID of the project against which to make the request.
+    , projectId :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'DatastoreProjectsBeginTransaction' with the minimum fields required to make a request.
-newDatastoreProjectsBeginTransaction ::
-  -- |  Multipart request metadata. See 'payload'.
-  BeginTransactionRequest ->
-  -- |  Required. The ID of the project against which to make the request. See 'projectId'.
-  Core.Text ->
-  DatastoreProjectsBeginTransaction
+newDatastoreProjectsBeginTransaction 
+    ::  BeginTransactionRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> Core.Text
+       -- ^  Required. The ID of the project against which to make the request. See 'projectId'.
+    -> DatastoreProjectsBeginTransaction
 newDatastoreProjectsBeginTransaction payload projectId =
   DatastoreProjectsBeginTransaction
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      payload = payload,
-      projectId = projectId,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , payload = payload
+    , projectId = projectId
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    DatastoreProjectsBeginTransaction
-  where
-  type
-    Rs DatastoreProjectsBeginTransaction =
-      BeginTransactionResponse
-  type
-    Scopes DatastoreProjectsBeginTransaction =
-      '[ "https://www.googleapis.com/auth/cloud-platform",
-         "https://www.googleapis.com/auth/datastore"
-       ]
-  requestClient DatastoreProjectsBeginTransaction {..} =
-    go
-      projectId
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      payload
-      datastoreService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy DatastoreProjectsBeginTransactionResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           DatastoreProjectsBeginTransaction
+         where
+        type Rs DatastoreProjectsBeginTransaction =
+             BeginTransactionResponse
+        type Scopes DatastoreProjectsBeginTransaction =
+             '["https://www.googleapis.com/auth/cloud-platform",
+               "https://www.googleapis.com/auth/datastore"]
+        requestClient DatastoreProjectsBeginTransaction{..}
+          = go projectId xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              datastoreService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy DatastoreProjectsBeginTransactionResource)
+                      Core.mempty
+
