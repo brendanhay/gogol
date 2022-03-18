b583aeca34def7c77b4d04efc45dfdff7e9a4e5e
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 11:23:13 +0100

    gogol-serviceusage: regenerating service

---
+++ gogol-serviceusage/gen/Network/Google/ServiceUsage/Operations/List.hs
@@ -0,0 +1,134 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Network.Google.ServiceUsage.Operations.List
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Lists operations that match the specified filter in the request. If the server doesn\'t support this method, it returns @UNIMPLEMENTED@. NOTE: the @name@ binding allows API services to override the binding to use different resource name schemes, such as @users\/*\/operations@. To override the binding, API services can add a binding such as @\"\/v1\/{name=users\/*}\/operations\"@ to their service configuration. For backwards compatibility, the default name includes the operations collection id, however overriding users must ensure the name binding is the parent resource, without the operations collection id.
+--
+-- /See:/ <https://cloud.google.com/service-usage/ Service Usage API Reference> for @serviceusage.operations.list@.
+module Network.Google.ServiceUsage.Operations.List
+  ( -- * Resource
+    ServiceUsageOperationsListResource,
+
+    -- ** Constructing a Request
+    newServiceUsageOperationsList,
+    ServiceUsageOperationsList,
+  )
+where
+
+import qualified Network.Google.Prelude as Core
+import Network.Google.ServiceUsage.Types
+
+-- | A resource alias for @serviceusage.operations.list@ method which the
+-- 'ServiceUsageOperationsList' request conforms to.
+type ServiceUsageOperationsListResource =
+  "v1"
+    Core.:> "operations"
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "filter" Core.Text
+    Core.:> Core.QueryParam "name" Core.Text
+    Core.:> Core.QueryParam "pageSize" Core.Int32
+    Core.:> Core.QueryParam "pageToken" Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Get '[Core.JSON] ListOperationsResponse
+
+-- | Lists operations that match the specified filter in the request. If the server doesn\'t support this method, it returns @UNIMPLEMENTED@. NOTE: the @name@ binding allows API services to override the binding to use different resource name schemes, such as @users\/*\/operations@. To override the binding, API services can add a binding such as @\"\/v1\/{name=users\/*}\/operations\"@ to their service configuration. For backwards compatibility, the default name includes the operations collection id, however overriding users must ensure the name binding is the parent resource, without the operations collection id.
+--
+-- /See:/ 'newServiceUsageOperationsList' smart constructor.
+data ServiceUsageOperationsList = ServiceUsageOperationsList
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | The standard list filter.
+    filter :: (Core.Maybe Core.Text),
+    -- | The name of the operation\'s parent resource.
+    name :: (Core.Maybe Core.Text),
+    -- | The standard list page size.
+    pageSize :: (Core.Maybe Core.Int32),
+    -- | The standard list page token.
+    pageToken :: (Core.Maybe Core.Text),
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'ServiceUsageOperationsList' with the minimum fields required to make a request.
+newServiceUsageOperationsList ::
+  ServiceUsageOperationsList
+newServiceUsageOperationsList =
+  ServiceUsageOperationsList
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      filter = Core.Nothing,
+      name = Core.Nothing,
+      pageSize = Core.Nothing,
+      pageToken = Core.Nothing,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    ServiceUsageOperationsList
+  where
+  type
+    Rs ServiceUsageOperationsList =
+      ListOperationsResponse
+  type
+    Scopes ServiceUsageOperationsList =
+      '[ "https://www.googleapis.com/auth/cloud-platform",
+         "https://www.googleapis.com/auth/service.management"
+       ]
+  requestClient ServiceUsageOperationsList {..} =
+    go
+      xgafv
+      accessToken
+      callback
+      filter
+      name
+      pageSize
+      pageToken
+      uploadType
+      uploadProtocol
+      (Core.Just Core.AltJSON)
+      serviceUsageService
+    where
+      go =
+        Core.buildClient
+          ( Core.Proxy ::
+              Core.Proxy ServiceUsageOperationsListResource
+          )
+          Core.mempty
---
+++ gogol-serviceusage/gen/Network/Google/ServiceUsage/Services/BatchEnable.hs
@@ -0,0 +1,126 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Network.Google.ServiceUsage.Services.BatchEnable
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Enable multiple services on a project. The operation is atomic: if enabling any service fails, then the entire batch fails, and no state changes occur. To enable a single service, use the @EnableService@ method instead.
+--
+-- /See:/ <https://cloud.google.com/service-usage/ Service Usage API Reference> for @serviceusage.services.batchEnable@.
+module Network.Google.ServiceUsage.Services.BatchEnable
+  ( -- * Resource
+    ServiceUsageServicesBatchEnableResource,
+
+    -- ** Constructing a Request
+    newServiceUsageServicesBatchEnable,
+    ServiceUsageServicesBatchEnable,
+  )
+where
+
+import qualified Network.Google.Prelude as Core
+import Network.Google.ServiceUsage.Types
+
+-- | A resource alias for @serviceusage.services.batchEnable@ method which the
+-- 'ServiceUsageServicesBatchEnable' request conforms to.
+type ServiceUsageServicesBatchEnableResource =
+  "v1"
+    Core.:> Core.Capture "parent" Core.Text
+    Core.:> "services:batchEnable"
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.ReqBody '[Core.JSON] BatchEnableServicesRequest
+    Core.:> Core.Post '[Core.JSON] Operation
+
+-- | Enable multiple services on a project. The operation is atomic: if enabling any service fails, then the entire batch fails, and no state changes occur. To enable a single service, use the @EnableService@ method instead.
+--
+-- /See:/ 'newServiceUsageServicesBatchEnable' smart constructor.
+data ServiceUsageServicesBatchEnable = ServiceUsageServicesBatchEnable
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | Parent to enable services on. An example name would be: @projects\/123@ where @123@ is the project number. The @BatchEnableServices@ method currently only supports projects.
+    parent :: Core.Text,
+    -- | Multipart request metadata.
+    payload :: BatchEnableServicesRequest,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'ServiceUsageServicesBatchEnable' with the minimum fields required to make a request.
+newServiceUsageServicesBatchEnable ::
+  -- |  Parent to enable services on. An example name would be: @projects\/123@ where @123@ is the project number. The @BatchEnableServices@ method currently only supports projects. See 'parent'.
+  Core.Text ->
+  -- |  Multipart request metadata. See 'payload'.
+  BatchEnableServicesRequest ->
+  ServiceUsageServicesBatchEnable
+newServiceUsageServicesBatchEnable parent payload =
+  ServiceUsageServicesBatchEnable
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      parent = parent,
+      payload = payload,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    ServiceUsageServicesBatchEnable
+  where
+  type Rs ServiceUsageServicesBatchEnable = Operation
+  type
+    Scopes ServiceUsageServicesBatchEnable =
+      '[ "https://www.googleapis.com/auth/cloud-platform",
+         "https://www.googleapis.com/auth/service.management"
+       ]
+  requestClient ServiceUsageServicesBatchEnable {..} =
+    go
+      parent
+      xgafv
+      accessToken
+      callback
+      uploadType
+      uploadProtocol
+      (Core.Just Core.AltJSON)
+      payload
+      serviceUsageService
+    where
+      go =
+        Core.buildClient
+          ( Core.Proxy ::
+              Core.Proxy ServiceUsageServicesBatchEnableResource
+          )
+          Core.mempty
