b712808c70b05d2ef8159d7b0df803691b289e38
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:09 +0100

    gogol-remotebuildexecution: regenerating service with updated namespace

--- lib/services/gogol-remotebuildexecution/gen/Gogol/RemoteBuildExecution/ActionResults/Update.hs
+++ lib/services/gogol-remotebuildexecution/gen/Gogol/RemoteBuildExecution/ActionResults/Update.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,124 +36,116 @@
 --
 -- /See:/ <https://cloud.google.com/remote-build-execution/docs/ Remote Build Execution API Reference> for @remotebuildexecution.actionResults.update@.
 module Gogol.RemoteBuildExecution.ActionResults.Update
-  ( -- * Resource
-    RemoteBuildExecutionActionResultsUpdateResource,
+    (
+    -- * Resource
+      RemoteBuildExecutionActionResultsUpdateResource
 
     -- ** Constructing a Request
-    newRemoteBuildExecutionActionResultsUpdate,
-    RemoteBuildExecutionActionResultsUpdate,
-  )
-where
+    , newRemoteBuildExecutionActionResultsUpdate
+    , RemoteBuildExecutionActionResultsUpdate
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.RemoteBuildExecution.Types
 
 -- | A resource alias for @remotebuildexecution.actionResults.update@ method which the
 -- 'RemoteBuildExecutionActionResultsUpdate' request conforms to.
-type RemoteBuildExecutionActionResultsUpdateResource =
-  "v2"
-    Core.:> Core.Capture "instanceName" Core.Text
-    Core.:> "actionResults"
-    Core.:> Core.Capture "hash" Core.Text
-    Core.:> Core.Capture "sizeBytes" Core.Int64
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam
-              "resultsCachePolicy.priority"
-              Core.Int32
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody
-              '[Core.JSON]
-              BuildBazelRemoteExecutionV2ActionResult
-    Core.:> Core.Put
-              '[Core.JSON]
-              BuildBazelRemoteExecutionV2ActionResult
+type RemoteBuildExecutionActionResultsUpdateResource
+     =
+     "v2" Core.:>
+       Core.Capture "instanceName" Core.Text Core.:>
+         "actionResults" Core.:>
+           Core.Capture "hash" Core.Text Core.:>
+             Core.Capture "sizeBytes" Core.Int64 Core.:>
+               Core.QueryParam "$.xgafv" Xgafv Core.:>
+                 Core.QueryParam "access_token" Core.Text Core.:>
+                   Core.QueryParam "callback" Core.Text Core.:>
+                     Core.QueryParam "resultsCachePolicy.priority"
+                       Core.Int32
+                       Core.:>
+                       Core.QueryParam "uploadType" Core.Text Core.:>
+                         Core.QueryParam "upload_protocol" Core.Text Core.:>
+                           Core.QueryParam "alt" Core.AltJSON Core.:>
+                             Core.ReqBody '[Core.JSON]
+                               BuildBazelRemoteExecutionV2ActionResult
+                               Core.:>
+                               Core.Put '[Core.JSON]
+                                 BuildBazelRemoteExecutionV2ActionResult
 
 -- | Upload a new execution result. In order to allow the server to perform access control based on the type of action, and to assist with client debugging, the client MUST first upload the Action that produced the result, along with its Command, into the @ContentAddressableStorage@. Server implementations MAY modify the @UpdateActionResultRequest.action_result@ and return an equivalent value. Errors: * @INVALID_ARGUMENT@: One or more arguments are invalid. * @FAILED_PRECONDITION@: One or more errors occurred in updating the action result, such as a missing command or action. * @RESOURCE_EXHAUSTED@: There is insufficient storage space to add the entry to the cache.
 --
 -- /See:/ 'newRemoteBuildExecutionActionResultsUpdate' smart constructor.
 data RemoteBuildExecutionActionResultsUpdate = RemoteBuildExecutionActionResultsUpdate
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | The hash. In the case of SHA-256, it will always be a lowercase hex string exactly 64 characters long.
-    hash :: Core.Text,
-    -- | The instance of the execution system to operate against. A server may support multiple instances of the execution system (with their own workers, storage, caches, etc.). The server MAY require use of this field to select between them in an implementation-defined fashion, otherwise it can be omitted.
-    instanceName :: Core.Text,
-    -- | Multipart request metadata.
-    payload :: BuildBazelRemoteExecutionV2ActionResult,
-    -- | The priority (relative importance) of this content in the overall cache. Generally, a lower value means a longer retention time or other advantage, but the interpretation of a given value is server-dependent. A priority of 0 means a /default/ value, decided by the server. The particular semantics of this field is up to the server. In particular, every server will have their own supported range of priorities, and will decide how these map into retention\/eviction policy.
-    resultsCachePolicyPriority :: (Core.Maybe Core.Int32),
-    -- | The size of the blob, in bytes.
-    sizeBytes :: Core.Int64,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | The hash. In the case of SHA-256, it will always be a lowercase hex string exactly 64 characters long.
+    , hash :: Core.Text
+      -- | The instance of the execution system to operate against. A server may support multiple instances of the execution system (with their own workers, storage, caches, etc.). The server MAY require use of this field to select between them in an implementation-defined fashion, otherwise it can be omitted.
+    , instanceName :: Core.Text
+      -- | Multipart request metadata.
+    , payload :: BuildBazelRemoteExecutionV2ActionResult
+      -- | The priority (relative importance) of this content in the overall cache. Generally, a lower value means a longer retention time or other advantage, but the interpretation of a given value is server-dependent. A priority of 0 means a /default/ value, decided by the server. The particular semantics of this field is up to the server. In particular, every server will have their own supported range of priorities, and will decide how these map into retention\/eviction policy.
+    , resultsCachePolicyPriority :: (Core.Maybe Core.Int32)
+      -- | The size of the blob, in bytes.
+    , sizeBytes :: Core.Int64
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'RemoteBuildExecutionActionResultsUpdate' with the minimum fields required to make a request.
-newRemoteBuildExecutionActionResultsUpdate ::
-  -- |  The hash. In the case of SHA-256, it will always be a lowercase hex string exactly 64 characters long. See 'hash'.
-  Core.Text ->
-  -- |  The instance of the execution system to operate against. A server may support multiple instances of the execution system (with their own workers, storage, caches, etc.). The server MAY require use of this field to select between them in an implementation-defined fashion, otherwise it can be omitted. See 'instanceName'.
-  Core.Text ->
-  -- |  Multipart request metadata. See 'payload'.
-  BuildBazelRemoteExecutionV2ActionResult ->
-  -- |  The size of the blob, in bytes. See 'sizeBytes'.
-  Core.Int64 ->
-  RemoteBuildExecutionActionResultsUpdate
+newRemoteBuildExecutionActionResultsUpdate 
+    ::  Core.Text
+       -- ^  The hash. In the case of SHA-256, it will always be a lowercase hex string exactly 64 characters long. See 'hash'.
+    -> Core.Text
+       -- ^  The instance of the execution system to operate against. A server may support multiple instances of the execution system (with their own workers, storage, caches, etc.). The server MAY require use of this field to select between them in an implementation-defined fashion, otherwise it can be omitted. See 'instanceName'.
+    -> BuildBazelRemoteExecutionV2ActionResult
+       -- ^  Multipart request metadata. See 'payload'.
+    -> Core.Int64
+       -- ^  The size of the blob, in bytes. See 'sizeBytes'.
+    -> RemoteBuildExecutionActionResultsUpdate
 newRemoteBuildExecutionActionResultsUpdate hash instanceName payload sizeBytes =
   RemoteBuildExecutionActionResultsUpdate
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      hash = hash,
-      instanceName = instanceName,
-      payload = payload,
-      resultsCachePolicyPriority = Core.Nothing,
-      sizeBytes = sizeBytes,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , hash = hash
+    , instanceName = instanceName
+    , payload = payload
+    , resultsCachePolicyPriority = Core.Nothing
+    , sizeBytes = sizeBytes
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    RemoteBuildExecutionActionResultsUpdate
-  where
-  type
-    Rs RemoteBuildExecutionActionResultsUpdate =
-      BuildBazelRemoteExecutionV2ActionResult
-  type
-    Scopes RemoteBuildExecutionActionResultsUpdate =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient
-    RemoteBuildExecutionActionResultsUpdate {..} =
-      go
-        instanceName
-        hash
-        sizeBytes
-        xgafv
-        accessToken
-        callback
-        resultsCachePolicyPriority
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        payload
-        remoteBuildExecutionService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  RemoteBuildExecutionActionResultsUpdateResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           RemoteBuildExecutionActionResultsUpdate
+         where
+        type Rs RemoteBuildExecutionActionResultsUpdate =
+             BuildBazelRemoteExecutionV2ActionResult
+        type Scopes RemoteBuildExecutionActionResultsUpdate =
+             '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient
+          RemoteBuildExecutionActionResultsUpdate{..}
+          = go instanceName hash sizeBytes xgafv accessToken
+              callback
+              resultsCachePolicyPriority
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              remoteBuildExecutionService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           RemoteBuildExecutionActionResultsUpdateResource)
+                      Core.mempty
+
--- lib/services/gogol-remotebuildexecution/gen/Gogol/RemoteBuildExecution/Blobs/FindMissing.hs
+++ lib/services/gogol-remotebuildexecution/gen/Gogol/RemoteBuildExecution/Blobs/FindMissing.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,14 +36,14 @@
 --
 -- /See:/ <https://cloud.google.com/remote-build-execution/docs/ Remote Build Execution API Reference> for @remotebuildexecution.blobs.findMissing@.
 module Gogol.RemoteBuildExecution.Blobs.FindMissing
-  ( -- * Resource
-    RemoteBuildExecutionBlobsFindMissingResource,
+    (
+    -- * Resource
+      RemoteBuildExecutionBlobsFindMissingResource
 
     -- ** Constructing a Request
-    newRemoteBuildExecutionBlobsFindMissing,
-    RemoteBuildExecutionBlobsFindMissing,
-  )
-where
+    , newRemoteBuildExecutionBlobsFindMissing
+    , RemoteBuildExecutionBlobsFindMissing
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.RemoteBuildExecution.Types
@@ -45,88 +51,80 @@ import Gogol.RemoteBuildExecution.Types
 -- | A resource alias for @remotebuildexecution.blobs.findMissing@ method which the
 -- 'RemoteBuildExecutionBlobsFindMissing' request conforms to.
 type RemoteBuildExecutionBlobsFindMissingResource =
-  "v2"
-    Core.:> Core.Capture "instanceName" Core.Text
-    Core.:> "blobs:findMissing"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody
-              '[Core.JSON]
-              BuildBazelRemoteExecutionV2FindMissingBlobsRequest
-    Core.:> Core.Post
-              '[Core.JSON]
-              BuildBazelRemoteExecutionV2FindMissingBlobsResponse
+     "v2" Core.:>
+       Core.Capture "instanceName" Core.Text Core.:>
+         "blobs:findMissing" Core.:>
+           Core.QueryParam "$.xgafv" Xgafv Core.:>
+             Core.QueryParam "access_token" Core.Text Core.:>
+               Core.QueryParam "callback" Core.Text Core.:>
+                 Core.QueryParam "uploadType" Core.Text Core.:>
+                   Core.QueryParam "upload_protocol" Core.Text Core.:>
+                     Core.QueryParam "alt" Core.AltJSON Core.:>
+                       Core.ReqBody '[Core.JSON]
+                         BuildBazelRemoteExecutionV2FindMissingBlobsRequest
+                         Core.:>
+                         Core.Post '[Core.JSON]
+                           BuildBazelRemoteExecutionV2FindMissingBlobsResponse
 
 -- | Determine if blobs are present in the CAS. Clients can use this API before uploading blobs to determine which ones are already present in the CAS and do not need to be uploaded again. Servers SHOULD increase the lifetimes of the referenced blobs if necessary and applicable. There are no method-specific errors.
 --
 -- /See:/ 'newRemoteBuildExecutionBlobsFindMissing' smart constructor.
 data RemoteBuildExecutionBlobsFindMissing = RemoteBuildExecutionBlobsFindMissing
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | The instance of the execution system to operate against. A server may support multiple instances of the execution system (with their own workers, storage, caches, etc.). The server MAY require use of this field to select between them in an implementation-defined fashion, otherwise it can be omitted.
-    instanceName :: Core.Text,
-    -- | Multipart request metadata.
-    payload :: BuildBazelRemoteExecutionV2FindMissingBlobsRequest,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | The instance of the execution system to operate against. A server may support multiple instances of the execution system (with their own workers, storage, caches, etc.). The server MAY require use of this field to select between them in an implementation-defined fashion, otherwise it can be omitted.
+    , instanceName :: Core.Text
+      -- | Multipart request metadata.
+    , payload :: BuildBazelRemoteExecutionV2FindMissingBlobsRequest
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'RemoteBuildExecutionBlobsFindMissing' with the minimum fields required to make a request.
-newRemoteBuildExecutionBlobsFindMissing ::
-  -- |  The instance of the execution system to operate against. A server may support multiple instances of the execution system (with their own workers, storage, caches, etc.). The server MAY require use of this field to select between them in an implementation-defined fashion, otherwise it can be omitted. See 'instanceName'.
-  Core.Text ->
-  -- |  Multipart request metadata. See 'payload'.
-  BuildBazelRemoteExecutionV2FindMissingBlobsRequest ->
-  RemoteBuildExecutionBlobsFindMissing
+newRemoteBuildExecutionBlobsFindMissing 
+    ::  Core.Text
+       -- ^  The instance of the execution system to operate against. A server may support multiple instances of the execution system (with their own workers, storage, caches, etc.). The server MAY require use of this field to select between them in an implementation-defined fashion, otherwise it can be omitted. See 'instanceName'.
+    -> BuildBazelRemoteExecutionV2FindMissingBlobsRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> RemoteBuildExecutionBlobsFindMissing
 newRemoteBuildExecutionBlobsFindMissing instanceName payload =
   RemoteBuildExecutionBlobsFindMissing
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      instanceName = instanceName,
-      payload = payload,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , instanceName = instanceName
+    , payload = payload
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    RemoteBuildExecutionBlobsFindMissing
-  where
-  type
-    Rs RemoteBuildExecutionBlobsFindMissing =
-      BuildBazelRemoteExecutionV2FindMissingBlobsResponse
-  type
-    Scopes RemoteBuildExecutionBlobsFindMissing =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient
-    RemoteBuildExecutionBlobsFindMissing {..} =
-      go
-        instanceName
-        xgafv
-        accessToken
-        callback
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        payload
-        remoteBuildExecutionService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  RemoteBuildExecutionBlobsFindMissingResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           RemoteBuildExecutionBlobsFindMissing
+         where
+        type Rs RemoteBuildExecutionBlobsFindMissing =
+             BuildBazelRemoteExecutionV2FindMissingBlobsResponse
+        type Scopes RemoteBuildExecutionBlobsFindMissing =
+             '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient
+          RemoteBuildExecutionBlobsFindMissing{..}
+          = go instanceName xgafv accessToken callback
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              remoteBuildExecutionService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           RemoteBuildExecutionBlobsFindMissingResource)
+                      Core.mempty
+
