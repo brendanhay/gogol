c1ea9051304f1afee2369176412bd5e60141115b
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 12:30:17 +0100

    gogol-cloudprivatecatalogproducer: regenerating service with updated namespace

---
+++ gogol-cloudprivatecatalogproducer/gen/Gogol/CloudPrivateCatalogProducer/Catalogs/Associations/Get.hs
@@ -0,0 +1,124 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.CloudPrivateCatalogProducer.Catalogs.Associations.Get
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Returns the requested Association resource.
+--
+-- /See:/ <https://cloud.google.com/private-catalog/ Cloud Private Catalog Producer API Reference> for @cloudprivatecatalogproducer.catalogs.associations.get@.
+module Gogol.CloudPrivateCatalogProducer.Catalogs.Associations.Get
+  ( -- * Resource
+    CloudPrivateCatalogProducerCatalogsAssociationsGetResource,
+
+    -- ** Constructing a Request
+    newCloudPrivateCatalogProducerCatalogsAssociationsGet,
+    CloudPrivateCatalogProducerCatalogsAssociationsGet,
+  )
+where
+
+import Gogol.CloudPrivateCatalogProducer.Types
+import qualified Gogol.Prelude as Core
+
+-- | A resource alias for @cloudprivatecatalogproducer.catalogs.associations.get@ method which the
+-- 'CloudPrivateCatalogProducerCatalogsAssociationsGet' request conforms to.
+type CloudPrivateCatalogProducerCatalogsAssociationsGetResource =
+  "v1beta1"
+    Core.:> Core.Capture "name" Core.Text
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Get
+              '[Core.JSON]
+              GoogleCloudPrivatecatalogproducerV1beta1Association
+
+-- | Returns the requested Association resource.
+--
+-- /See:/ 'newCloudPrivateCatalogProducerCatalogsAssociationsGet' smart constructor.
+data CloudPrivateCatalogProducerCatalogsAssociationsGet = CloudPrivateCatalogProducerCatalogsAssociationsGet
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | The resource name of the @Association@ to retrieve.
+    name :: Core.Text,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'CloudPrivateCatalogProducerCatalogsAssociationsGet' with the minimum fields required to make a request.
+newCloudPrivateCatalogProducerCatalogsAssociationsGet ::
+  -- |  The resource name of the @Association@ to retrieve. See 'name'.
+  Core.Text ->
+  CloudPrivateCatalogProducerCatalogsAssociationsGet
+newCloudPrivateCatalogProducerCatalogsAssociationsGet name =
+  CloudPrivateCatalogProducerCatalogsAssociationsGet
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      name = name,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    CloudPrivateCatalogProducerCatalogsAssociationsGet
+  where
+  type
+    Rs
+      CloudPrivateCatalogProducerCatalogsAssociationsGet =
+      GoogleCloudPrivatecatalogproducerV1beta1Association
+  type
+    Scopes
+      CloudPrivateCatalogProducerCatalogsAssociationsGet =
+      '["https://www.googleapis.com/auth/cloud-platform"]
+  requestClient
+    CloudPrivateCatalogProducerCatalogsAssociationsGet {..} =
+      go
+        name
+        xgafv
+        accessToken
+        callback
+        uploadType
+        uploadProtocol
+        (Core.Just Core.AltJSON)
+        cloudPrivateCatalogProducerService
+      where
+        go =
+          Core.buildClient
+            ( Core.Proxy ::
+                Core.Proxy
+                  CloudPrivateCatalogProducerCatalogsAssociationsGetResource
+            )
+            Core.mempty
---
+++ gogol-cloudprivatecatalogproducer/gen/Gogol/CloudPrivateCatalogProducer/Catalogs/Products/Create.hs
@@ -0,0 +1,134 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.CloudPrivateCatalogProducer.Catalogs.Products.Create
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Creates a Product instance under a given Catalog.
+--
+-- /See:/ <https://cloud.google.com/private-catalog/ Cloud Private Catalog Producer API Reference> for @cloudprivatecatalogproducer.catalogs.products.create@.
+module Gogol.CloudPrivateCatalogProducer.Catalogs.Products.Create
+  ( -- * Resource
+    CloudPrivateCatalogProducerCatalogsProductsCreateResource,
+
+    -- ** Constructing a Request
+    newCloudPrivateCatalogProducerCatalogsProductsCreate,
+    CloudPrivateCatalogProducerCatalogsProductsCreate,
+  )
+where
+
+import Gogol.CloudPrivateCatalogProducer.Types
+import qualified Gogol.Prelude as Core
+
+-- | A resource alias for @cloudprivatecatalogproducer.catalogs.products.create@ method which the
+-- 'CloudPrivateCatalogProducerCatalogsProductsCreate' request conforms to.
+type CloudPrivateCatalogProducerCatalogsProductsCreateResource =
+  "v1beta1"
+    Core.:> Core.Capture "parent" Core.Text
+    Core.:> "products"
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.ReqBody
+              '[Core.JSON]
+              GoogleCloudPrivatecatalogproducerV1beta1Product
+    Core.:> Core.Post
+              '[Core.JSON]
+              GoogleCloudPrivatecatalogproducerV1beta1Product
+
+-- | Creates a Product instance under a given Catalog.
+--
+-- /See:/ 'newCloudPrivateCatalogProducerCatalogsProductsCreate' smart constructor.
+data CloudPrivateCatalogProducerCatalogsProductsCreate = CloudPrivateCatalogProducerCatalogsProductsCreate
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | The catalog name of the new product\'s parent.
+    parent :: Core.Text,
+    -- | Multipart request metadata.
+    payload :: GoogleCloudPrivatecatalogproducerV1beta1Product,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'CloudPrivateCatalogProducerCatalogsProductsCreate' with the minimum fields required to make a request.
+newCloudPrivateCatalogProducerCatalogsProductsCreate ::
+  -- |  The catalog name of the new product\'s parent. See 'parent'.
+  Core.Text ->
+  -- |  Multipart request metadata. See 'payload'.
+  GoogleCloudPrivatecatalogproducerV1beta1Product ->
+  CloudPrivateCatalogProducerCatalogsProductsCreate
+newCloudPrivateCatalogProducerCatalogsProductsCreate parent payload =
+  CloudPrivateCatalogProducerCatalogsProductsCreate
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      parent = parent,
+      payload = payload,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    CloudPrivateCatalogProducerCatalogsProductsCreate
+  where
+  type
+    Rs
+      CloudPrivateCatalogProducerCatalogsProductsCreate =
+      GoogleCloudPrivatecatalogproducerV1beta1Product
+  type
+    Scopes
+      CloudPrivateCatalogProducerCatalogsProductsCreate =
+      '["https://www.googleapis.com/auth/cloud-platform"]
+  requestClient
+    CloudPrivateCatalogProducerCatalogsProductsCreate {..} =
+      go
+        parent
+        xgafv
+        accessToken
+        callback
+        uploadType
+        uploadProtocol
+        (Core.Just Core.AltJSON)
+        payload
+        cloudPrivateCatalogProducerService
+      where
+        go =
+          Core.buildClient
+            ( Core.Proxy ::
+                Core.Proxy
+                  CloudPrivateCatalogProducerCatalogsProductsCreateResource
+            )
+            Core.mempty
