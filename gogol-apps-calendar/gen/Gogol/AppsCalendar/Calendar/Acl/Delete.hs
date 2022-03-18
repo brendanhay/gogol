a6a5cffd187a8353624409e82ebe29b59a63883a
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 12:30:14 +0100

    gogol-apps-calendar: regenerating service with updated namespace

---
+++ gogol-apps-calendar/gen/Gogol/AppsCalendar/Calendar/Acl/Delete.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.AppsCalendar.Calendar.Acl.Delete
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Deletes an access control rule.
+--
+-- /See:/ <https://developers.google.com/google-apps/calendar/firstapp Calendar API Reference> for @calendar.acl.delete@.
+module Gogol.AppsCalendar.Calendar.Acl.Delete
+  ( -- * Resource
+    CalendarAclDeleteResource,
+
+    -- ** Constructing a Request
+    newCalendarAclDelete,
+    CalendarAclDelete,
+  )
+where
+
+import Gogol.AppsCalendar.Types
+import qualified Gogol.Prelude as Core
+
+-- | A resource alias for @calendar.acl.delete@ method which the
+-- 'CalendarAclDelete' request conforms to.
+type CalendarAclDeleteResource =
+  "calendar"
+    Core.:> "v3"
+    Core.:> "calendars"
+    Core.:> Core.Capture "calendarId" Core.Text
+    Core.:> "acl"
+    Core.:> Core.Capture "ruleId" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Delete '[Core.JSON] ()
+
+-- | Deletes an access control rule.
+--
+-- /See:/ 'newCalendarAclDelete' smart constructor.
+data CalendarAclDelete = CalendarAclDelete
+  { -- | Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the \"primary\" keyword.
+    calendarId :: Core.Text,
+    -- | ACL rule identifier.
+    ruleId :: Core.Text
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'CalendarAclDelete' with the minimum fields required to make a request.
+newCalendarAclDelete ::
+  -- |  Calendar identifier. To retrieve calendar IDs call the calendarList.list method. If you want to access the primary calendar of the currently logged in user, use the \"primary\" keyword. See 'calendarId'.
+  Core.Text ->
+  -- |  ACL rule identifier. See 'ruleId'.
+  Core.Text ->
+  CalendarAclDelete
+newCalendarAclDelete calendarId ruleId =
+  CalendarAclDelete {calendarId = calendarId, ruleId = ruleId}
+
+instance Core.GoogleRequest CalendarAclDelete where
+  type Rs CalendarAclDelete = ()
+  type
+    Scopes CalendarAclDelete =
+      '["https://www.googleapis.com/auth/calendar"]
+  requestClient CalendarAclDelete {..} =
+    go
+      calendarId
+      ruleId
+      (Core.Just Core.AltJSON)
+      appsCalendarService
+    where
+      go =
+        Core.buildClient
+          (Core.Proxy :: Core.Proxy CalendarAclDeleteResource)
+          Core.mempty
