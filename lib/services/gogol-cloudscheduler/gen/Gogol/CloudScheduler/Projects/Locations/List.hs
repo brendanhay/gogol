4e97476de7a4defbad839fc75801e37042440e2e
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:00 +0100

    gogol-cloudscheduler: regenerating service with updated namespace

--- lib/services/gogol-cloudscheduler/gen/Gogol/CloudScheduler/Projects/Locations/List.hs
+++ lib/services/gogol-cloudscheduler/gen/Gogol/CloudScheduler/Projects/Locations/List.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,106 +36,97 @@
 --
 -- /See:/ <https://cloud.google.com/scheduler/ Cloud Scheduler API Reference> for @cloudscheduler.projects.locations.list@.
 module Gogol.CloudScheduler.Projects.Locations.List
-  ( -- * Resource
-    CloudSchedulerProjectsLocationsListResource,
+    (
+    -- * Resource
+      CloudSchedulerProjectsLocationsListResource
 
     -- ** Constructing a Request
-    newCloudSchedulerProjectsLocationsList,
-    CloudSchedulerProjectsLocationsList,
-  )
-where
+    , newCloudSchedulerProjectsLocationsList
+    , CloudSchedulerProjectsLocationsList
+    ) where
 
-import Gogol.CloudScheduler.Types
 import qualified Gogol.Prelude as Core
+import Gogol.CloudScheduler.Types
 
 -- | A resource alias for @cloudscheduler.projects.locations.list@ method which the
 -- 'CloudSchedulerProjectsLocationsList' request conforms to.
 type CloudSchedulerProjectsLocationsListResource =
-  "v1"
-    Core.:> Core.Capture "name" Core.Text
-    Core.:> "locations"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "filter" Core.Text
-    Core.:> Core.QueryParam "pageSize" Core.Int32
-    Core.:> Core.QueryParam "pageToken" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Get '[Core.JSON] ListLocationsResponse
+     "v1" Core.:>
+       Core.Capture "name" Core.Text Core.:>
+         "locations" Core.:>
+           Core.QueryParam "$.xgafv" Xgafv Core.:>
+             Core.QueryParam "access_token" Core.Text Core.:>
+               Core.QueryParam "callback" Core.Text Core.:>
+                 Core.QueryParam "filter" Core.Text Core.:>
+                   Core.QueryParam "pageSize" Core.Int32 Core.:>
+                     Core.QueryParam "pageToken" Core.Text Core.:>
+                       Core.QueryParam "uploadType" Core.Text Core.:>
+                         Core.QueryParam "upload_protocol" Core.Text Core.:>
+                           Core.QueryParam "alt" Core.AltJSON Core.:>
+                             Core.Get '[Core.JSON] ListLocationsResponse
 
 -- | Lists information about the supported locations for this service.
 --
 -- /See:/ 'newCloudSchedulerProjectsLocationsList' smart constructor.
 data CloudSchedulerProjectsLocationsList = CloudSchedulerProjectsLocationsList
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | A filter to narrow down results to a preferred subset. The filtering language accepts strings like \"displayName=tokyo\", and is documented in more detail in <https://google.aip.dev/160 AIP-160>.
-    filter :: (Core.Maybe Core.Text),
-    -- | The resource that owns the locations collection, if applicable.
-    name :: Core.Text,
-    -- | The maximum number of results to return. If not set, the service selects a default.
-    pageSize :: (Core.Maybe Core.Int32),
-    -- | A page token received from the @next_page_token@ field in the response. Send that page token to receive the subsequent page.
-    pageToken :: (Core.Maybe Core.Text),
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | A filter to narrow down results to a preferred subset. The filtering language accepts strings like \"displayName=tokyo\", and is documented in more detail in <https://google.aip.dev/160 AIP-160>.
+    , filter :: (Core.Maybe Core.Text)
+      -- | The resource that owns the locations collection, if applicable.
+    , name :: Core.Text
+      -- | The maximum number of results to return. If not set, the service selects a default.
+    , pageSize :: (Core.Maybe Core.Int32)
+      -- | A page token received from the @next_page_token@ field in the response. Send that page token to receive the subsequent page.
+    , pageToken :: (Core.Maybe Core.Text)
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'CloudSchedulerProjectsLocationsList' with the minimum fields required to make a request.
-newCloudSchedulerProjectsLocationsList ::
-  -- |  The resource that owns the locations collection, if applicable. See 'name'.
-  Core.Text ->
-  CloudSchedulerProjectsLocationsList
+newCloudSchedulerProjectsLocationsList 
+    ::  Core.Text
+       -- ^  The resource that owns the locations collection, if applicable. See 'name'.
+    -> CloudSchedulerProjectsLocationsList
 newCloudSchedulerProjectsLocationsList name =
   CloudSchedulerProjectsLocationsList
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      filter = Core.Nothing,
-      name = name,
-      pageSize = Core.Nothing,
-      pageToken = Core.Nothing,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , filter = Core.Nothing
+    , name = name
+    , pageSize = Core.Nothing
+    , pageToken = Core.Nothing
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    CloudSchedulerProjectsLocationsList
-  where
-  type
-    Rs CloudSchedulerProjectsLocationsList =
-      ListLocationsResponse
-  type
-    Scopes CloudSchedulerProjectsLocationsList =
-      '["https://www.googleapis.com/auth/cloud-platform"]
-  requestClient CloudSchedulerProjectsLocationsList {..} =
-    go
-      name
-      xgafv
-      accessToken
-      callback
-      filter
-      pageSize
-      pageToken
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      cloudSchedulerService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy
-                CloudSchedulerProjectsLocationsListResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           CloudSchedulerProjectsLocationsList
+         where
+        type Rs CloudSchedulerProjectsLocationsList =
+             ListLocationsResponse
+        type Scopes CloudSchedulerProjectsLocationsList =
+             '["https://www.googleapis.com/auth/cloud-platform"]
+        requestClient CloudSchedulerProjectsLocationsList{..}
+          = go name xgafv accessToken callback filter pageSize
+              pageToken
+              uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              cloudSchedulerService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           CloudSchedulerProjectsLocationsListResource)
+                      Core.mempty
+
--- lib/services/gogol-cloudscheduler/gen/Gogol/CloudScheduler/Types.hs
+++ lib/services/gogol-cloudscheduler/gen/Gogol/CloudScheduler/Types.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -25,135 +31,136 @@
 -- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
+--
 module Gogol.CloudScheduler.Types
-  ( -- * Configuration
-    cloudSchedulerService,
+    (
+    -- * Configuration
+      cloudSchedulerService
 
     -- * OAuth Scopes
-    cloudPlatformScope,
+    , cloudPlatformScope
 
     -- * Types
 
     -- ** Xgafv
-    Xgafv (..),
+    , Xgafv (..)
 
     -- ** AppEngineHttpTarget
-    AppEngineHttpTarget (..),
-    newAppEngineHttpTarget,
+    , AppEngineHttpTarget (..)
+    , newAppEngineHttpTarget
 
     -- ** AppEngineHttpTarget_Headers
-    AppEngineHttpTarget_Headers (..),
-    newAppEngineHttpTarget_Headers,
+    , AppEngineHttpTarget_Headers (..)
+    , newAppEngineHttpTarget_Headers
 
     -- ** AppEngineHttpTarget_HttpMethod
-    AppEngineHttpTarget_HttpMethod (..),
+    , AppEngineHttpTarget_HttpMethod (..)
 
     -- ** AppEngineRouting
-    AppEngineRouting (..),
-    newAppEngineRouting,
+    , AppEngineRouting (..)
+    , newAppEngineRouting
 
     -- ** Empty
-    Empty (..),
-    newEmpty,
+    , Empty (..)
+    , newEmpty
 
     -- ** HttpTarget
-    HttpTarget (..),
-    newHttpTarget,
+    , HttpTarget (..)
+    , newHttpTarget
 
     -- ** HttpTarget_Headers
-    HttpTarget_Headers (..),
-    newHttpTarget_Headers,
+    , HttpTarget_Headers (..)
+    , newHttpTarget_Headers
 
     -- ** HttpTarget_HttpMethod
-    HttpTarget_HttpMethod (..),
+    , HttpTarget_HttpMethod (..)
 
     -- ** Job
-    Job (..),
-    newJob,
+    , Job (..)
+    , newJob
 
     -- ** Job_State
-    Job_State (..),
+    , Job_State (..)
 
     -- ** ListJobsResponse
-    ListJobsResponse (..),
-    newListJobsResponse,
+    , ListJobsResponse (..)
+    , newListJobsResponse
 
     -- ** ListLocationsResponse
-    ListLocationsResponse (..),
-    newListLocationsResponse,
+    , ListLocationsResponse (..)
+    , newListLocationsResponse
 
     -- ** Location
-    Location (..),
-    newLocation,
+    , Location (..)
+    , newLocation
 
     -- ** Location_Labels
-    Location_Labels (..),
-    newLocation_Labels,
+    , Location_Labels (..)
+    , newLocation_Labels
 
     -- ** Location_Metadata
-    Location_Metadata (..),
-    newLocation_Metadata,
+    , Location_Metadata (..)
+    , newLocation_Metadata
 
     -- ** OAuthToken
-    OAuthToken (..),
-    newOAuthToken,
+    , OAuthToken (..)
+    , newOAuthToken
 
     -- ** OidcToken
-    OidcToken (..),
-    newOidcToken,
+    , OidcToken (..)
+    , newOidcToken
 
     -- ** PauseJobRequest
-    PauseJobRequest (..),
-    newPauseJobRequest,
+    , PauseJobRequest (..)
+    , newPauseJobRequest
 
     -- ** PubsubMessage
-    PubsubMessage (..),
-    newPubsubMessage,
+    , PubsubMessage (..)
+    , newPubsubMessage
 
     -- ** PubsubMessage_Attributes
-    PubsubMessage_Attributes (..),
-    newPubsubMessage_Attributes,
+    , PubsubMessage_Attributes (..)
+    , newPubsubMessage_Attributes
 
     -- ** PubsubTarget
-    PubsubTarget (..),
-    newPubsubTarget,
+    , PubsubTarget (..)
+    , newPubsubTarget
 
     -- ** PubsubTarget_Attributes
-    PubsubTarget_Attributes (..),
-    newPubsubTarget_Attributes,
+    , PubsubTarget_Attributes (..)
+    , newPubsubTarget_Attributes
 
     -- ** ResumeJobRequest
-    ResumeJobRequest (..),
-    newResumeJobRequest,
+    , ResumeJobRequest (..)
+    , newResumeJobRequest
 
     -- ** RetryConfig
-    RetryConfig (..),
-    newRetryConfig,
+    , RetryConfig (..)
+    , newRetryConfig
 
     -- ** RunJobRequest
-    RunJobRequest (..),
-    newRunJobRequest,
+    , RunJobRequest (..)
+    , newRunJobRequest
 
     -- ** Status
-    Status (..),
-    newStatus,
+    , Status (..)
+    , newStatus
 
     -- ** Status_DetailsItem
-    Status_DetailsItem (..),
-    newStatus_DetailsItem,
-  )
-where
+    , Status_DetailsItem (..)
+    , newStatus_DetailsItem
+    ) where
 
+import qualified Gogol.Prelude as Core
 import Gogol.CloudScheduler.Internal.Product
 import Gogol.CloudScheduler.Internal.Sum
-import qualified Gogol.Prelude as Core
 
 -- | Default request referring to version @v1@ of the Cloud Scheduler API. This contains the host and root path used as a starting point for constructing service requests.
 cloudSchedulerService :: Core.ServiceConfig
-cloudSchedulerService =
-  Core.defaultService
-    (Core.ServiceId "cloudscheduler:v1")
-    "cloudscheduler.googleapis.com"
+cloudSchedulerService
+  = Core.defaultService
+      (Core.ServiceId "cloudscheduler:v1")
+      "cloudscheduler.googleapis.com"
 
 -- | See, edit, configure, and delete your Google Cloud data and see the email address for your Google Account.
 cloudPlatformScope :: Core.Proxy '["https://www.googleapis.com/auth/cloud-platform"]
