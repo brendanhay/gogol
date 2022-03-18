3e4e73de75868da7b8bb93f9458b034c6dba370b
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 12:30:22 +0100

    gogol-games: regenerating service with updated namespace

---
+++ gogol-games/gen/Gogol/Games.hs
@@ -0,0 +1,560 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.Games
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- The Google Play games service allows developers to enhance games with social leaderboards, achievements, game state, sign-in with Google, and more.
+--
+-- /See:/ <https://developers.google.com/games/ Google Play Game Services Reference>
+module Gogol.Games
+  ( -- * Configuration
+    gamesService,
+
+    -- * OAuth Scopes
+    driveAppdataScope,
+    gamesScope,
+
+    -- * Resources
+
+    -- ** games.achievementDefinitions.list
+    GamesAchievementDefinitionsListResource,
+    newGamesAchievementDefinitionsList,
+    GamesAchievementDefinitionsList,
+
+    -- ** games.achievements.increment
+    GamesAchievementsIncrementResource,
+    newGamesAchievementsIncrement,
+    GamesAchievementsIncrement,
+
+    -- ** games.achievements.list
+    GamesAchievementsListResource,
+    newGamesAchievementsList,
+    GamesAchievementsList,
+
+    -- ** games.achievements.reveal
+    GamesAchievementsRevealResource,
+    newGamesAchievementsReveal,
+    GamesAchievementsReveal,
+
+    -- ** games.achievements.setStepsAtLeast
+    GamesAchievementsSetStepsAtLeastResource,
+    newGamesAchievementsSetStepsAtLeast,
+    GamesAchievementsSetStepsAtLeast,
+
+    -- ** games.achievements.unlock
+    GamesAchievementsUnlockResource,
+    newGamesAchievementsUnlock,
+    GamesAchievementsUnlock,
+
+    -- ** games.achievements.updateMultiple
+    GamesAchievementsUpdateMultipleResource,
+    newGamesAchievementsUpdateMultiple,
+    GamesAchievementsUpdateMultiple,
+
+    -- ** games.applications.get
+    GamesApplicationsGetResource,
+    newGamesApplicationsGet,
+    GamesApplicationsGet,
+
+    -- ** games.applications.getEndPoint
+    GamesApplicationsGetEndPointResource,
+    newGamesApplicationsGetEndPoint,
+    GamesApplicationsGetEndPoint,
+
+    -- ** games.applications.played
+    GamesApplicationsPlayedResource,
+    newGamesApplicationsPlayed,
+    GamesApplicationsPlayed,
+
+    -- ** games.applications.verify
+    GamesApplicationsVerifyResource,
+    newGamesApplicationsVerify,
+    GamesApplicationsVerify,
+
+    -- ** games.events.listByPlayer
+    GamesEventsListByPlayerResource,
+    newGamesEventsListByPlayer,
+    GamesEventsListByPlayer,
+
+    -- ** games.events.listDefinitions
+    GamesEventsListDefinitionsResource,
+    newGamesEventsListDefinitions,
+    GamesEventsListDefinitions,
+
+    -- ** games.events.record
+    GamesEventsRecordResource,
+    newGamesEventsRecord,
+    GamesEventsRecord,
+
+    -- ** games.leaderboards.get
+    GamesLeaderboardsGetResource,
+    newGamesLeaderboardsGet,
+    GamesLeaderboardsGet,
+
+    -- ** games.leaderboards.list
+    GamesLeaderboardsListResource,
+    newGamesLeaderboardsList,
+    GamesLeaderboardsList,
+
+    -- ** games.metagame.getMetagameConfig
+    GamesMetagameGetMetagameConfigResource,
+    newGamesMetagameGetMetagameConfig,
+    GamesMetagameGetMetagameConfig,
+
+    -- ** games.metagame.listCategoriesByPlayer
+    GamesMetagameListCategoriesByPlayerResource,
+    newGamesMetagameListCategoriesByPlayer,
+    GamesMetagameListCategoriesByPlayer,
+
+    -- ** games.players.get
+    GamesPlayersGetResource,
+    newGamesPlayersGet,
+    GamesPlayersGet,
+
+    -- ** games.players.list
+    GamesPlayersListResource,
+    newGamesPlayersList,
+    GamesPlayersList,
+
+    -- ** games.revisions.check
+    GamesRevisionsCheckResource,
+    newGamesRevisionsCheck,
+    GamesRevisionsCheck,
+
+    -- ** games.scores.get
+    GamesScoresGetResource,
+    newGamesScoresGet,
+    GamesScoresGet,
+
+    -- ** games.scores.list
+    GamesScoresListResource,
+    newGamesScoresList,
+    GamesScoresList,
+
+    -- ** games.scores.listWindow
+    GamesScoresListWindowResource,
+    newGamesScoresListWindow,
+    GamesScoresListWindow,
+
+    -- ** games.scores.submit
+    GamesScoresSubmitResource,
+    newGamesScoresSubmit,
+    GamesScoresSubmit,
+
+    -- ** games.scores.submitMultiple
+    GamesScoresSubmitMultipleResource,
+    newGamesScoresSubmitMultiple,
+    GamesScoresSubmitMultiple,
+
+    -- ** games.snapshots.get
+    GamesSnapshotsGetResource,
+    newGamesSnapshotsGet,
+    GamesSnapshotsGet,
+
+    -- ** games.snapshots.list
+    GamesSnapshotsListResource,
+    newGamesSnapshotsList,
+    GamesSnapshotsList,
+
+    -- ** games.stats.get
+    GamesStatsGetResource,
+    newGamesStatsGet,
+    GamesStatsGet,
+
+    -- * Types
+
+    -- ** Xgafv
+    Xgafv (..),
+
+    -- ** AchievementDefinition
+    AchievementDefinition (..),
+    newAchievementDefinition,
+
+    -- ** AchievementDefinition_AchievementType
+    AchievementDefinition_AchievementType (..),
+
+    -- ** AchievementDefinition_InitialState
+    AchievementDefinition_InitialState (..),
+
+    -- ** AchievementDefinitionsListResponse
+    AchievementDefinitionsListResponse (..),
+    newAchievementDefinitionsListResponse,
+
+    -- ** AchievementIncrementResponse
+    AchievementIncrementResponse (..),
+    newAchievementIncrementResponse,
+
+    -- ** AchievementRevealResponse
+    AchievementRevealResponse (..),
+    newAchievementRevealResponse,
+
+    -- ** AchievementRevealResponse_CurrentState
+    AchievementRevealResponse_CurrentState (..),
+
+    -- ** AchievementSetStepsAtLeastResponse
+    AchievementSetStepsAtLeastResponse (..),
+    newAchievementSetStepsAtLeastResponse,
+
+    -- ** AchievementUnlockResponse
+    AchievementUnlockResponse (..),
+    newAchievementUnlockResponse,
+
+    -- ** AchievementUpdateMultipleRequest
+    AchievementUpdateMultipleRequest (..),
+    newAchievementUpdateMultipleRequest,
+
+    -- ** AchievementUpdateMultipleResponse
+    AchievementUpdateMultipleResponse (..),
+    newAchievementUpdateMultipleResponse,
+
+    -- ** AchievementUpdateRequest
+    AchievementUpdateRequest (..),
+    newAchievementUpdateRequest,
+
+    -- ** AchievementUpdateRequest_UpdateType
+    AchievementUpdateRequest_UpdateType (..),
+
+    -- ** AchievementUpdateResponse
+    AchievementUpdateResponse (..),
+    newAchievementUpdateResponse,
+
+    -- ** AchievementUpdateResponse_CurrentState
+    AchievementUpdateResponse_CurrentState (..),
+
+    -- ** Application
+    Application (..),
+    newApplication,
+
+    -- ** Application_EnabledFeaturesItem
+    Application_EnabledFeaturesItem (..),
+
+    -- ** ApplicationCategory
+    ApplicationCategory (..),
+    newApplicationCategory,
+
+    -- ** ApplicationVerifyResponse
+    ApplicationVerifyResponse (..),
+    newApplicationVerifyResponse,
+
+    -- ** Category
+    Category (..),
+    newCategory,
+
+    -- ** CategoryListResponse
+    CategoryListResponse (..),
+    newCategoryListResponse,
+
+    -- ** EndPoint
+    EndPoint (..),
+    newEndPoint,
+
+    -- ** EventBatchRecordFailure
+    EventBatchRecordFailure (..),
+    newEventBatchRecordFailure,
+
+    -- ** EventBatchRecordFailure_FailureCause
+    EventBatchRecordFailure_FailureCause (..),
+
+    -- ** EventChild
+    EventChild (..),
+    newEventChild,
+
+    -- ** EventDefinition
+    EventDefinition (..),
+    newEventDefinition,
+
+    -- ** EventDefinition_Visibility
+    EventDefinition_Visibility (..),
+
+    -- ** EventDefinitionListResponse
+    EventDefinitionListResponse (..),
+    newEventDefinitionListResponse,
+
+    -- ** EventPeriodRange
+    EventPeriodRange (..),
+    newEventPeriodRange,
+
+    -- ** EventPeriodUpdate
+    EventPeriodUpdate (..),
+    newEventPeriodUpdate,
+
+    -- ** EventRecordFailure
+    EventRecordFailure (..),
+    newEventRecordFailure,
+
+    -- ** EventRecordFailure_FailureCause
+    EventRecordFailure_FailureCause (..),
+
+    -- ** EventRecordRequest
+    EventRecordRequest (..),
+    newEventRecordRequest,
+
+    -- ** EventUpdateRequest
+    EventUpdateRequest (..),
+    newEventUpdateRequest,
+
+    -- ** EventUpdateResponse
+    EventUpdateResponse (..),
+    newEventUpdateResponse,
+
+    -- ** GamesAchievementIncrement
+    GamesAchievementIncrement (..),
+    newGamesAchievementIncrement,
+
+    -- ** GamesAchievementSetStepsAtLeast
+    GamesAchievementSetStepsAtLeast (..),
+    newGamesAchievementSetStepsAtLeast,
+
+    -- ** ImageAsset
+    ImageAsset (..),
+    newImageAsset,
+
+    -- ** Instance
+    Instance (..),
+    newInstance,
+
+    -- ** Instance_PlatformType
+    Instance_PlatformType (..),
+
+    -- ** InstanceAndroidDetails
+    InstanceAndroidDetails (..),
+    newInstanceAndroidDetails,
+
+    -- ** InstanceIosDetails
+    InstanceIosDetails (..),
+    newInstanceIosDetails,
+
+    -- ** InstanceWebDetails
+    InstanceWebDetails (..),
+    newInstanceWebDetails,
+
+    -- ** Leaderboard
+    Leaderboard (..),
+    newLeaderboard,
+
+    -- ** Leaderboard_Order
+    Leaderboard_Order (..),
+
+    -- ** LeaderboardEntry
+    LeaderboardEntry (..),
+    newLeaderboardEntry,
+
+    -- ** LeaderboardEntry_TimeSpan
+    LeaderboardEntry_TimeSpan (..),
+
+    -- ** LeaderboardListResponse
+    LeaderboardListResponse (..),
+    newLeaderboardListResponse,
+
+    -- ** LeaderboardScoreRank
+    LeaderboardScoreRank (..),
+    newLeaderboardScoreRank,
+
+    -- ** LeaderboardScores
+    LeaderboardScores (..),
+    newLeaderboardScores,
+
+    -- ** MetagameConfig
+    MetagameConfig (..),
+    newMetagameConfig,
+
+    -- ** Player
+    Player (..),
+    newPlayer,
+
+    -- ** Player_FriendStatus
+    Player_FriendStatus (..),
+
+    -- ** Player_Name
+    Player_Name (..),
+    newPlayer_Name,
+
+    -- ** PlayerAchievement
+    PlayerAchievement (..),
+    newPlayerAchievement,
+
+    -- ** PlayerAchievement_AchievementState
+    PlayerAchievement_AchievementState (..),
+
+    -- ** PlayerAchievementListResponse
+    PlayerAchievementListResponse (..),
+    newPlayerAchievementListResponse,
+
+    -- ** PlayerEvent
+    PlayerEvent (..),
+    newPlayerEvent,
+
+    -- ** PlayerEventListResponse
+    PlayerEventListResponse (..),
+    newPlayerEventListResponse,
+
+    -- ** PlayerExperienceInfo
+    PlayerExperienceInfo (..),
+    newPlayerExperienceInfo,
+
+    -- ** PlayerLeaderboardScore
+    PlayerLeaderboardScore (..),
+    newPlayerLeaderboardScore,
+
+    -- ** PlayerLeaderboardScore_TimeSpan
+    PlayerLeaderboardScore_TimeSpan (..),
+
+    -- ** PlayerLeaderboardScoreListResponse
+    PlayerLeaderboardScoreListResponse (..),
+    newPlayerLeaderboardScoreListResponse,
+
+    -- ** PlayerLevel
+    PlayerLevel (..),
+    newPlayerLevel,
+
+    -- ** PlayerListResponse
+    PlayerListResponse (..),
+    newPlayerListResponse,
+
+    -- ** PlayerScore
+    PlayerScore (..),
+    newPlayerScore,
+
+    -- ** PlayerScore_TimeSpan
+    PlayerScore_TimeSpan (..),
+
+    -- ** PlayerScoreListResponse
+    PlayerScoreListResponse (..),
+    newPlayerScoreListResponse,
+
+    -- ** PlayerScoreResponse
+    PlayerScoreResponse (..),
+    newPlayerScoreResponse,
+
+    -- ** PlayerScoreResponse_BeatenScoreTimeSpansItem
+    PlayerScoreResponse_BeatenScoreTimeSpansItem (..),
+
+    -- ** PlayerScoreSubmissionList
+    PlayerScoreSubmissionList (..),
+    newPlayerScoreSubmissionList,
+
+    -- ** ProfileSettings
+    ProfileSettings (..),
+    newProfileSettings,
+
+    -- ** ProfileSettings_FriendsListVisibility
+    ProfileSettings_FriendsListVisibility (..),
+
+    -- ** RevisionCheckResponse
+    RevisionCheckResponse (..),
+    newRevisionCheckResponse,
+
+    -- ** RevisionCheckResponse_RevisionStatus
+    RevisionCheckResponse_RevisionStatus (..),
+
+    -- ** ScoreSubmission
+    ScoreSubmission (..),
+    newScoreSubmission,
+
+    -- ** Snapshot
+    Snapshot (..),
+    newSnapshot,
+
+    -- ** Snapshot_Type
+    Snapshot_Type (..),
+
+    -- ** SnapshotImage
+    SnapshotImage (..),
+    newSnapshotImage,
+
+    -- ** SnapshotListResponse
+    SnapshotListResponse (..),
+    newSnapshotListResponse,
+
+    -- ** StatsResponse
+    StatsResponse (..),
+    newStatsResponse,
+
+    -- ** AchievementsListState
+    AchievementsListState (..),
+
+    -- ** ApplicationsGetPlatformType
+    ApplicationsGetPlatformType (..),
+
+    -- ** ApplicationsGetEndPointEndPointType
+    ApplicationsGetEndPointEndPointType (..),
+
+    -- ** MetagameListCategoriesByPlayerCollection
+    MetagameListCategoriesByPlayerCollection (..),
+
+    -- ** PlayersListCollection
+    PlayersListCollection (..),
+
+    -- ** ScoresGetIncludeRankType
+    ScoresGetIncludeRankType (..),
+
+    -- ** ScoresGetTimeSpan
+    ScoresGetTimeSpan (..),
+
+    -- ** ScoresListCollection
+    ScoresListCollection (..),
+
+    -- ** ScoresListTimeSpan
+    ScoresListTimeSpan (..),
+
+    -- ** ScoresListWindowCollection
+    ScoresListWindowCollection (..),
+
+    -- ** ScoresListWindowTimeSpan
+    ScoresListWindowTimeSpan (..),
+  )
+where
+
+import Gogol.Games.AchievementDefinitions.List
+import Gogol.Games.Achievements.Increment
+import Gogol.Games.Achievements.List
+import Gogol.Games.Achievements.Reveal
+import Gogol.Games.Achievements.SetStepsAtLeast
+import Gogol.Games.Achievements.Unlock
+import Gogol.Games.Achievements.UpdateMultiple
+import Gogol.Games.Applications.Get
+import Gogol.Games.Applications.GetEndPoint
+import Gogol.Games.Applications.Played
+import Gogol.Games.Applications.Verify
+import Gogol.Games.Events.ListByPlayer
+import Gogol.Games.Events.ListDefinitions
+import Gogol.Games.Events.Record
+import Gogol.Games.Leaderboards.Get
+import Gogol.Games.Leaderboards.List
+import Gogol.Games.Metagame.GetMetagameConfig
+import Gogol.Games.Metagame.ListCategoriesByPlayer
+import Gogol.Games.Players.Get
+import Gogol.Games.Players.List
+import Gogol.Games.Revisions.Check
+import Gogol.Games.Scores.Get
+import Gogol.Games.Scores.List
+import Gogol.Games.Scores.ListWindow
+import Gogol.Games.Scores.Submit
+import Gogol.Games.Scores.SubmitMultiple
+import Gogol.Games.Snapshots.Get
+import Gogol.Games.Snapshots.List
+import Gogol.Games.Stats.Get
+import Gogol.Games.Types
---
+++ gogol-games/gen/Gogol/Games/Achievements/SetStepsAtLeast.hs
@@ -0,0 +1,130 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Gogol.Games.Achievements.SetStepsAtLeast
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Sets the steps for the currently authenticated player towards unlocking an achievement. If the steps parameter is less than the current number of steps that the player already gained for the achievement, the achievement is not modified.
+--
+-- /See:/ <https://developers.google.com/games/ Google Play Game Services Reference> for @games.achievements.setStepsAtLeast@.
+module Gogol.Games.Achievements.SetStepsAtLeast
+  ( -- * Resource
+    GamesAchievementsSetStepsAtLeastResource,
+
+    -- ** Constructing a Request
+    newGamesAchievementsSetStepsAtLeast,
+    GamesAchievementsSetStepsAtLeast,
+  )
+where
+
+import Gogol.Games.Types
+import qualified Gogol.Prelude as Core
+
+-- | A resource alias for @games.achievements.setStepsAtLeast@ method which the
+-- 'GamesAchievementsSetStepsAtLeast' request conforms to.
+type GamesAchievementsSetStepsAtLeastResource =
+  "games"
+    Core.:> "v1"
+    Core.:> "achievements"
+    Core.:> Core.Capture "achievementId" Core.Text
+    Core.:> "setStepsAtLeast"
+    Core.:> Core.QueryParam "steps" Core.Int32
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Post
+              '[Core.JSON]
+              AchievementSetStepsAtLeastResponse
+
+-- | Sets the steps for the currently authenticated player towards unlocking an achievement. If the steps parameter is less than the current number of steps that the player already gained for the achievement, the achievement is not modified.
+--
+-- /See:/ 'newGamesAchievementsSetStepsAtLeast' smart constructor.
+data GamesAchievementsSetStepsAtLeast = GamesAchievementsSetStepsAtLeast
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | The ID of the achievement used by this method.
+    achievementId :: Core.Text,
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | The minimum value to set the steps to.
+    steps :: Core.Int32,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'GamesAchievementsSetStepsAtLeast' with the minimum fields required to make a request.
+newGamesAchievementsSetStepsAtLeast ::
+  -- |  The ID of the achievement used by this method. See 'achievementId'.
+  Core.Text ->
+  -- |  The minimum value to set the steps to. See 'steps'.
+  Core.Int32 ->
+  GamesAchievementsSetStepsAtLeast
+newGamesAchievementsSetStepsAtLeast achievementId steps =
+  GamesAchievementsSetStepsAtLeast
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      achievementId = achievementId,
+      callback = Core.Nothing,
+      steps = steps,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance
+  Core.GoogleRequest
+    GamesAchievementsSetStepsAtLeast
+  where
+  type
+    Rs GamesAchievementsSetStepsAtLeast =
+      AchievementSetStepsAtLeastResponse
+  type
+    Scopes GamesAchievementsSetStepsAtLeast =
+      '["https://www.googleapis.com/auth/games"]
+  requestClient GamesAchievementsSetStepsAtLeast {..} =
+    go
+      achievementId
+      (Core.Just steps)
+      xgafv
+      accessToken
+      callback
+      uploadType
+      uploadProtocol
+      (Core.Just Core.AltJSON)
+      gamesService
+    where
+      go =
+        Core.buildClient
+          ( Core.Proxy ::
+              Core.Proxy GamesAchievementsSetStepsAtLeastResource
+          )
+          Core.mempty
