38a597941a20b923b1de8504a75d6200f665f52c
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 11:23:08 +0100

    gogol-fonts: regenerating service

--- gogol-fonts/gen/Network/Google/Fonts/Types.hs
+++ gogol-fonts/gen/Network/Google/Fonts/Types.hs
@@ -1,60 +1,63 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DataKinds          #-}
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE NoImplicitPrelude  #-}
-{-# LANGUAGE OverloadedStrings  #-}
-
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 -- |
 -- Module      : Network.Google.Fonts.Types
--- Copyright   : (c) 2015-2016 Brendan Hay
+-- Copyright   : (c) 2015-2022 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
---
 module Network.Google.Fonts.Types
-    (
-    -- * Service Configuration
-      fontsService
+  ( -- * Configuration
+    fontsService,
 
-    -- * Webfont
-    , Webfont
-    , webfont
-    , wVariants
-    , wKind
-    , wCategory
-    , wFamily
-    , wVersion
-    , wFiles
-    , wSubSets
-    , wLastModified
+    -- * Types
 
-    -- * WebfontList
-    , WebfontList
-    , webfontList
-    , wlKind
-    , wlItems
+    -- ** Xgafv
+    Xgafv (..),
 
-    -- * Xgafv
-    , Xgafv (..)
+    -- ** Webfont
+    Webfont (..),
+    newWebfont,
 
-    -- * WebfontsListSort
-    , WebfontsListSort (..)
+    -- ** Webfont_Files
+    Webfont_Files (..),
+    newWebfont_Files,
 
-    -- * WebfontFiles
-    , WebfontFiles
-    , webfontFiles
-    , wfAddtional
-    ) where
+    -- ** WebfontList
+    WebfontList (..),
+    newWebfontList,
 
-import Network.Google.Fonts.Types.Product
-import Network.Google.Fonts.Types.Sum
-import Network.Google.Prelude
+    -- ** WebfontsListSort
+    WebfontsListSort (..),
+  )
+where
 
--- | Default request referring to version 'v1' of the Web Fonts Developer API. This contains the host and root path used as a starting point for constructing service requests.
-fontsService :: ServiceConfig
-fontsService
-  = defaultService (ServiceId "webfonts:v1")
-      "webfonts.googleapis.com"
+import Network.Google.Fonts.Internal.Product
+import Network.Google.Fonts.Internal.Sum
+import qualified Network.Google.Prelude as Core
+
+-- | Default request referring to version @v1@ of the Web Fonts Developer API. This contains the host and root path used as a starting point for constructing service requests.
+fontsService :: Core.ServiceConfig
+fontsService =
+  Core.defaultService
+    (Core.ServiceId "webfonts:v1")
+    "webfonts.googleapis.com"
