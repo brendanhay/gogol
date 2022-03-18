96ad2c09df625e7196f9d4959c9186cd326edeea
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 15:12:31 +0100

    gogol-admin-reports: regenerating service with correct additional spelling

--- lib/services/gogol-admin-reports/gen/Gogol/Admin/Reports.hs
+++ lib/services/gogol-admin-reports/gen/Gogol/Admin/Reports.hs
@@ -1,23 +1,17 @@
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE StrictData #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
-
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -36,132 +30,132 @@
 --
 -- /See:/ <https://developers.google.com/admin-sdk/ Admin SDK API Reference>
 module Gogol.Admin.Reports
-    (
-    -- * Configuration
-      adminReportsService
+  ( -- * Configuration
+    adminReportsService,
 
     -- * OAuth Scopes
-    , adminReportsAuditReadOnlyScope
-    , adminReportsUsageReadOnlyScope
+    adminReportsAuditReadOnlyScope,
+    adminReportsUsageReadOnlyScope,
 
     -- * Resources
 
     -- ** admin.channels.stop
-    , AdminChannelsStopResource
-    , newAdminChannelsStop
-    , AdminChannelsStop
+    AdminChannelsStopResource,
+    newAdminChannelsStop,
+    AdminChannelsStop,
 
     -- ** reports.activities.list
-    , ReportsActivitiesListResource
-    , newReportsActivitiesList
-    , ReportsActivitiesList
+    ReportsActivitiesListResource,
+    newReportsActivitiesList,
+    ReportsActivitiesList,
 
     -- ** reports.activities.watch
-    , ReportsActivitiesWatchResource
-    , newReportsActivitiesWatch
-    , ReportsActivitiesWatch
+    ReportsActivitiesWatchResource,
+    newReportsActivitiesWatch,
+    ReportsActivitiesWatch,
 
     -- ** reports.customerUsageReports.get
-    , ReportsCustomerUsageReportsGetResource
-    , newReportsCustomerUsageReportsGet
-    , ReportsCustomerUsageReportsGet
+    ReportsCustomerUsageReportsGetResource,
+    newReportsCustomerUsageReportsGet,
+    ReportsCustomerUsageReportsGet,
 
     -- ** reports.entityUsageReports.get
-    , ReportsEntityUsageReportsGetResource
-    , newReportsEntityUsageReportsGet
-    , ReportsEntityUsageReportsGet
+    ReportsEntityUsageReportsGetResource,
+    newReportsEntityUsageReportsGet,
+    ReportsEntityUsageReportsGet,
 
     -- ** reports.userUsageReport.get
-    , ReportsUserUsageReportGetResource
-    , newReportsUserUsageReportGet
-    , ReportsUserUsageReportGet
+    ReportsUserUsageReportGetResource,
+    newReportsUserUsageReportGet,
+    ReportsUserUsageReportGet,
 
     -- * Types
 
     -- ** Xgafv
-    , Xgafv (..)
+    Xgafv (..),
 
     -- ** Activities
-    , Activities (..)
-    , newActivities
+    Activities (..),
+    newActivities,
 
     -- ** Activity
-    , Activity (..)
-    , newActivity
+    Activity (..),
+    newActivity,
 
     -- ** Activity_Actor
-    , Activity_Actor (..)
-    , newActivity_Actor
+    Activity_Actor (..),
+    newActivity_Actor,
 
     -- ** Activity_EventsItem
-    , Activity_EventsItem (..)
-    , newActivity_EventsItem
+    Activity_EventsItem (..),
+    newActivity_EventsItem,
 
     -- ** Activity_EventsItem_ParametersItem
-    , Activity_EventsItem_ParametersItem (..)
-    , newActivity_EventsItem_ParametersItem
+    Activity_EventsItem_ParametersItem (..),
+    newActivity_EventsItem_ParametersItem,
 
     -- ** Activity_EventsItem_ParametersItem_MessageValue
-    , Activity_EventsItem_ParametersItem_MessageValue (..)
-    , newActivity_EventsItem_ParametersItem_MessageValue
+    Activity_EventsItem_ParametersItem_MessageValue (..),
+    newActivity_EventsItem_ParametersItem_MessageValue,
 
     -- ** Activity_EventsItem_ParametersItem_MultiMessageValueItem
-    , Activity_EventsItem_ParametersItem_MultiMessageValueItem (..)
-    , newActivity_EventsItem_ParametersItem_MultiMessageValueItem
+    Activity_EventsItem_ParametersItem_MultiMessageValueItem (..),
+    newActivity_EventsItem_ParametersItem_MultiMessageValueItem,
 
     -- ** Activity_Id
-    , Activity_Id (..)
-    , newActivity_Id
+    Activity_Id (..),
+    newActivity_Id,
 
     -- ** Channel
-    , Channel (..)
-    , newChannel
+    Channel (..),
+    newChannel,
 
     -- ** Channel_Params
-    , Channel_Params (..)
-    , newChannel_Params
+    Channel_Params (..),
+    newChannel_Params,
 
     -- ** NestedParameter
-    , NestedParameter (..)
-    , newNestedParameter
+    NestedParameter (..),
+    newNestedParameter,
 
     -- ** UsageReport
-    , UsageReport (..)
-    , newUsageReport
+    UsageReport (..),
+    newUsageReport,
 
     -- ** UsageReport_Entity
-    , UsageReport_Entity (..)
-    , newUsageReport_Entity
+    UsageReport_Entity (..),
+    newUsageReport_Entity,
 
     -- ** UsageReport_ParametersItem
-    , UsageReport_ParametersItem (..)
-    , newUsageReport_ParametersItem
+    UsageReport_ParametersItem (..),
+    newUsageReport_ParametersItem,
 
     -- ** UsageReport_ParametersItem_MsgValueItem
-    , UsageReport_ParametersItem_MsgValueItem (..)
-    , newUsageReport_ParametersItem_MsgValueItem
+    UsageReport_ParametersItem_MsgValueItem (..),
+    newUsageReport_ParametersItem_MsgValueItem,
 
     -- ** UsageReports
-    , UsageReports (..)
-    , newUsageReports
+    UsageReports (..),
+    newUsageReports,
 
     -- ** UsageReports_WarningsItem
-    , UsageReports_WarningsItem (..)
-    , newUsageReports_WarningsItem
+    UsageReports_WarningsItem (..),
+    newUsageReports_WarningsItem,
 
     -- ** UsageReports_WarningsItem_DataItem
-    , UsageReports_WarningsItem_DataItem (..)
-    , newUsageReports_WarningsItem_DataItem
+    UsageReports_WarningsItem_DataItem (..),
+    newUsageReports_WarningsItem_DataItem,
 
     -- ** ActivitiesListApplicationName
-    , ActivitiesListApplicationName (..)
+    ActivitiesListApplicationName (..),
 
     -- ** ActivitiesWatchApplicationName
-    , ActivitiesWatchApplicationName (..)
+    ActivitiesWatchApplicationName (..),
 
     -- ** EntityUsageReportsGetEntityType
-    , EntityUsageReportsGetEntityType (..)
-    ) where
+    EntityUsageReportsGetEntityType (..),
+  )
+where
 
 import Gogol.Admin.Reports.Activities.List
 import Gogol.Admin.Reports.Activities.Watch
--- lib/services/gogol-admin-reports/gen/Gogol/Admin/Reports/Types.hs
+++ lib/services/gogol-admin-reports/gen/Gogol/Admin/Reports/Types.hs
@@ -1,23 +1,17 @@
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE StrictData #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
-
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -31,113 +25,112 @@
 -- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
---
 module Gogol.Admin.Reports.Types
-    (
-    -- * Configuration
-      adminReportsService
+  ( -- * Configuration
+    adminReportsService,
 
     -- * OAuth Scopes
-    , adminReportsAuditReadOnlyScope
-    , adminReportsUsageReadOnlyScope
+    adminReportsAuditReadOnlyScope,
+    adminReportsUsageReadOnlyScope,
 
     -- * Types
 
     -- ** Xgafv
-    , Xgafv (..)
+    Xgafv (..),
 
     -- ** Activities
-    , Activities (..)
-    , newActivities
+    Activities (..),
+    newActivities,
 
     -- ** Activity
-    , Activity (..)
-    , newActivity
+    Activity (..),
+    newActivity,
 
     -- ** Activity_Actor
-    , Activity_Actor (..)
-    , newActivity_Actor
+    Activity_Actor (..),
+    newActivity_Actor,
 
     -- ** Activity_EventsItem
-    , Activity_EventsItem (..)
-    , newActivity_EventsItem
+    Activity_EventsItem (..),
+    newActivity_EventsItem,
 
     -- ** Activity_EventsItem_ParametersItem
-    , Activity_EventsItem_ParametersItem (..)
-    , newActivity_EventsItem_ParametersItem
+    Activity_EventsItem_ParametersItem (..),
+    newActivity_EventsItem_ParametersItem,
 
     -- ** Activity_EventsItem_ParametersItem_MessageValue
-    , Activity_EventsItem_ParametersItem_MessageValue (..)
-    , newActivity_EventsItem_ParametersItem_MessageValue
+    Activity_EventsItem_ParametersItem_MessageValue (..),
+    newActivity_EventsItem_ParametersItem_MessageValue,
 
     -- ** Activity_EventsItem_ParametersItem_MultiMessageValueItem
-    , Activity_EventsItem_ParametersItem_MultiMessageValueItem (..)
-    , newActivity_EventsItem_ParametersItem_MultiMessageValueItem
+    Activity_EventsItem_ParametersItem_MultiMessageValueItem (..),
+    newActivity_EventsItem_ParametersItem_MultiMessageValueItem,
 
     -- ** Activity_Id
-    , Activity_Id (..)
-    , newActivity_Id
+    Activity_Id (..),
+    newActivity_Id,
 
     -- ** Channel
-    , Channel (..)
-    , newChannel
+    Channel (..),
+    newChannel,
 
     -- ** Channel_Params
-    , Channel_Params (..)
-    , newChannel_Params
+    Channel_Params (..),
+    newChannel_Params,
 
     -- ** NestedParameter
-    , NestedParameter (..)
-    , newNestedParameter
+    NestedParameter (..),
+    newNestedParameter,
 
     -- ** UsageReport
-    , UsageReport (..)
-    , newUsageReport
+    UsageReport (..),
+    newUsageReport,
 
     -- ** UsageReport_Entity
-    , UsageReport_Entity (..)
-    , newUsageReport_Entity
+    UsageReport_Entity (..),
+    newUsageReport_Entity,
 
     -- ** UsageReport_ParametersItem
-    , UsageReport_ParametersItem (..)
-    , newUsageReport_ParametersItem
+    UsageReport_ParametersItem (..),
+    newUsageReport_ParametersItem,
 
     -- ** UsageReport_ParametersItem_MsgValueItem
-    , UsageReport_ParametersItem_MsgValueItem (..)
-    , newUsageReport_ParametersItem_MsgValueItem
+    UsageReport_ParametersItem_MsgValueItem (..),
+    newUsageReport_ParametersItem_MsgValueItem,
 
     -- ** UsageReports
-    , UsageReports (..)
-    , newUsageReports
+    UsageReports (..),
+    newUsageReports,
 
     -- ** UsageReports_WarningsItem
-    , UsageReports_WarningsItem (..)
-    , newUsageReports_WarningsItem
+    UsageReports_WarningsItem (..),
+    newUsageReports_WarningsItem,
 
     -- ** UsageReports_WarningsItem_DataItem
-    , UsageReports_WarningsItem_DataItem (..)
-    , newUsageReports_WarningsItem_DataItem
+    UsageReports_WarningsItem_DataItem (..),
+    newUsageReports_WarningsItem_DataItem,
 
     -- ** ActivitiesListApplicationName
-    , ActivitiesListApplicationName (..)
+    ActivitiesListApplicationName (..),
 
     -- ** ActivitiesWatchApplicationName
-    , ActivitiesWatchApplicationName (..)
+    ActivitiesWatchApplicationName (..),
 
     -- ** EntityUsageReportsGetEntityType
-    , EntityUsageReportsGetEntityType (..)
-    ) where
+    EntityUsageReportsGetEntityType (..),
+  )
+where
 
-import qualified Gogol.Prelude as Core
 import Gogol.Admin.Reports.Internal.Product
 import Gogol.Admin.Reports.Internal.Sum
+import qualified Gogol.Prelude as Core
 
 -- | Default request referring to version @reports_v1@ of the Admin SDK API. This contains the host and root path used as a starting point for constructing service requests.
 adminReportsService :: Core.ServiceConfig
-adminReportsService
-  = Core.defaultService
-      (Core.ServiceId "admin:reports_v1")
-      "admin.googleapis.com"
+adminReportsService =
+  Core.defaultService
+    (Core.ServiceId "admin:reports_v1")
+    "admin.googleapis.com"
 
 -- | View audit reports for your G Suite domain
 adminReportsAuditReadOnlyScope :: Core.Proxy '["https://www.googleapis.com/auth/admin.reports.audit.readonly"]
