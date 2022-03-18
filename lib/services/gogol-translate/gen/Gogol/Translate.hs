15f08061ec88574415cf05c3378adb5313c89cde
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:13 +0100

    gogol-translate: regenerating service with updated namespace

--- lib/services/gogol-translate/gen/Gogol/Translate.hs
+++ lib/services/gogol-translate/gen/Gogol/Translate.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,317 +36,317 @@
 --
 -- /See:/ <https://cloud.google.com/translate/docs/quickstarts Cloud Translation API Reference>
 module Gogol.Translate
-  ( -- * Configuration
-    translateService,
+    (
+    -- * Configuration
+      translateService
 
     -- * OAuth Scopes
-    cloudPlatformScope,
-    cloudTranslationScope,
+    , cloudPlatformScope
+    , cloudTranslationScope
 
     -- * Resources
 
     -- ** translate.projects.detectLanguage
-    TranslateProjectsDetectLanguageResource,
-    newTranslateProjectsDetectLanguage,
-    TranslateProjectsDetectLanguage,
+    , TranslateProjectsDetectLanguageResource
+    , newTranslateProjectsDetectLanguage
+    , TranslateProjectsDetectLanguage
 
     -- ** translate.projects.getSupportedLanguages
-    TranslateProjectsGetSupportedLanguagesResource,
-    newTranslateProjectsGetSupportedLanguages,
-    TranslateProjectsGetSupportedLanguages,
+    , TranslateProjectsGetSupportedLanguagesResource
+    , newTranslateProjectsGetSupportedLanguages
+    , TranslateProjectsGetSupportedLanguages
 
     -- ** translate.projects.locations.batchTranslateDocument
-    TranslateProjectsLocationsBatchTranslateDocumentResource,
-    newTranslateProjectsLocationsBatchTranslateDocument,
-    TranslateProjectsLocationsBatchTranslateDocument,
+    , TranslateProjectsLocationsBatchTranslateDocumentResource
+    , newTranslateProjectsLocationsBatchTranslateDocument
+    , TranslateProjectsLocationsBatchTranslateDocument
 
     -- ** translate.projects.locations.batchTranslateText
-    TranslateProjectsLocationsBatchTranslateTextResource,
-    newTranslateProjectsLocationsBatchTranslateText,
-    TranslateProjectsLocationsBatchTranslateText,
+    , TranslateProjectsLocationsBatchTranslateTextResource
+    , newTranslateProjectsLocationsBatchTranslateText
+    , TranslateProjectsLocationsBatchTranslateText
 
     -- ** translate.projects.locations.detectLanguage
-    TranslateProjectsLocationsDetectLanguageResource,
-    newTranslateProjectsLocationsDetectLanguage,
-    TranslateProjectsLocationsDetectLanguage,
+    , TranslateProjectsLocationsDetectLanguageResource
+    , newTranslateProjectsLocationsDetectLanguage
+    , TranslateProjectsLocationsDetectLanguage
 
     -- ** translate.projects.locations.get
-    TranslateProjectsLocationsGetResource,
-    newTranslateProjectsLocationsGet,
-    TranslateProjectsLocationsGet,
+    , TranslateProjectsLocationsGetResource
+    , newTranslateProjectsLocationsGet
+    , TranslateProjectsLocationsGet
 
     -- ** translate.projects.locations.getSupportedLanguages
-    TranslateProjectsLocationsGetSupportedLanguagesResource,
-    newTranslateProjectsLocationsGetSupportedLanguages,
-    TranslateProjectsLocationsGetSupportedLanguages,
+    , TranslateProjectsLocationsGetSupportedLanguagesResource
+    , newTranslateProjectsLocationsGetSupportedLanguages
+    , TranslateProjectsLocationsGetSupportedLanguages
 
     -- ** translate.projects.locations.glossaries.create
-    TranslateProjectsLocationsGlossariesCreateResource,
-    newTranslateProjectsLocationsGlossariesCreate,
-    TranslateProjectsLocationsGlossariesCreate,
+    , TranslateProjectsLocationsGlossariesCreateResource
+    , newTranslateProjectsLocationsGlossariesCreate
+    , TranslateProjectsLocationsGlossariesCreate
 
     -- ** translate.projects.locations.glossaries.delete
-    TranslateProjectsLocationsGlossariesDeleteResource,
-    newTranslateProjectsLocationsGlossariesDelete,
-    TranslateProjectsLocationsGlossariesDelete,
+    , TranslateProjectsLocationsGlossariesDeleteResource
+    , newTranslateProjectsLocationsGlossariesDelete
+    , TranslateProjectsLocationsGlossariesDelete
 
     -- ** translate.projects.locations.glossaries.get
-    TranslateProjectsLocationsGlossariesGetResource,
-    newTranslateProjectsLocationsGlossariesGet,
-    TranslateProjectsLocationsGlossariesGet,
+    , TranslateProjectsLocationsGlossariesGetResource
+    , newTranslateProjectsLocationsGlossariesGet
+    , TranslateProjectsLocationsGlossariesGet
 
     -- ** translate.projects.locations.glossaries.list
-    TranslateProjectsLocationsGlossariesListResource,
-    newTranslateProjectsLocationsGlossariesList,
-    TranslateProjectsLocationsGlossariesList,
+    , TranslateProjectsLocationsGlossariesListResource
+    , newTranslateProjectsLocationsGlossariesList
+    , TranslateProjectsLocationsGlossariesList
 
     -- ** translate.projects.locations.list
-    TranslateProjectsLocationsListResource,
-    newTranslateProjectsLocationsList,
-    TranslateProjectsLocationsList,
+    , TranslateProjectsLocationsListResource
+    , newTranslateProjectsLocationsList
+    , TranslateProjectsLocationsList
 
     -- ** translate.projects.locations.operations.cancel
-    TranslateProjectsLocationsOperationsCancelResource,
-    newTranslateProjectsLocationsOperationsCancel,
-    TranslateProjectsLocationsOperationsCancel,
+    , TranslateProjectsLocationsOperationsCancelResource
+    , newTranslateProjectsLocationsOperationsCancel
+    , TranslateProjectsLocationsOperationsCancel
 
     -- ** translate.projects.locations.operations.delete
-    TranslateProjectsLocationsOperationsDeleteResource,
-    newTranslateProjectsLocationsOperationsDelete,
-    TranslateProjectsLocationsOperationsDelete,
+    , TranslateProjectsLocationsOperationsDeleteResource
+    , newTranslateProjectsLocationsOperationsDelete
+    , TranslateProjectsLocationsOperationsDelete
 
     -- ** translate.projects.locations.operations.get
-    TranslateProjectsLocationsOperationsGetResource,
-    newTranslateProjectsLocationsOperationsGet,
-    TranslateProjectsLocationsOperationsGet,
+    , TranslateProjectsLocationsOperationsGetResource
+    , newTranslateProjectsLocationsOperationsGet
+    , TranslateProjectsLocationsOperationsGet
 
     -- ** translate.projects.locations.operations.list
-    TranslateProjectsLocationsOperationsListResource,
-    newTranslateProjectsLocationsOperationsList,
-    TranslateProjectsLocationsOperationsList,
+    , TranslateProjectsLocationsOperationsListResource
+    , newTranslateProjectsLocationsOperationsList
+    , TranslateProjectsLocationsOperationsList
 
     -- ** translate.projects.locations.operations.wait
-    TranslateProjectsLocationsOperationsWaitResource,
-    newTranslateProjectsLocationsOperationsWait,
-    TranslateProjectsLocationsOperationsWait,
+    , TranslateProjectsLocationsOperationsWaitResource
+    , newTranslateProjectsLocationsOperationsWait
+    , TranslateProjectsLocationsOperationsWait
 
     -- ** translate.projects.locations.translateDocument
-    TranslateProjectsLocationsTranslateDocumentResource,
-    newTranslateProjectsLocationsTranslateDocument,
-    TranslateProjectsLocationsTranslateDocument,
+    , TranslateProjectsLocationsTranslateDocumentResource
+    , newTranslateProjectsLocationsTranslateDocument
+    , TranslateProjectsLocationsTranslateDocument
 
     -- ** translate.projects.locations.translateText
-    TranslateProjectsLocationsTranslateTextResource,
-    newTranslateProjectsLocationsTranslateText,
-    TranslateProjectsLocationsTranslateText,
+    , TranslateProjectsLocationsTranslateTextResource
+    , newTranslateProjectsLocationsTranslateText
+    , TranslateProjectsLocationsTranslateText
 
     -- ** translate.projects.translateText
-    TranslateProjectsTranslateTextResource,
-    newTranslateProjectsTranslateText,
-    TranslateProjectsTranslateText,
+    , TranslateProjectsTranslateTextResource
+    , newTranslateProjectsTranslateText
+    , TranslateProjectsTranslateText
 
     -- * Types
 
     -- ** Xgafv
-    Xgafv (..),
+    , Xgafv (..)
 
     -- ** BatchDocumentInputConfig
-    BatchDocumentInputConfig (..),
-    newBatchDocumentInputConfig,
+    , BatchDocumentInputConfig (..)
+    , newBatchDocumentInputConfig
 
     -- ** BatchDocumentOutputConfig
-    BatchDocumentOutputConfig (..),
-    newBatchDocumentOutputConfig,
+    , BatchDocumentOutputConfig (..)
+    , newBatchDocumentOutputConfig
 
     -- ** BatchTranslateDocumentRequest
-    BatchTranslateDocumentRequest (..),
-    newBatchTranslateDocumentRequest,
+    , BatchTranslateDocumentRequest (..)
+    , newBatchTranslateDocumentRequest
 
     -- ** BatchTranslateDocumentRequest_FormatConversions
-    BatchTranslateDocumentRequest_FormatConversions (..),
-    newBatchTranslateDocumentRequest_FormatConversions,
+    , BatchTranslateDocumentRequest_FormatConversions (..)
+    , newBatchTranslateDocumentRequest_FormatConversions
 
     -- ** BatchTranslateDocumentRequest_Glossaries
-    BatchTranslateDocumentRequest_Glossaries (..),
-    newBatchTranslateDocumentRequest_Glossaries,
+    , BatchTranslateDocumentRequest_Glossaries (..)
+    , newBatchTranslateDocumentRequest_Glossaries
 
     -- ** BatchTranslateDocumentRequest_Models
-    BatchTranslateDocumentRequest_Models (..),
-    newBatchTranslateDocumentRequest_Models,
+    , BatchTranslateDocumentRequest_Models (..)
+    , newBatchTranslateDocumentRequest_Models
 
     -- ** BatchTranslateTextRequest
-    BatchTranslateTextRequest (..),
-    newBatchTranslateTextRequest,
+    , BatchTranslateTextRequest (..)
+    , newBatchTranslateTextRequest
 
     -- ** BatchTranslateTextRequest_Glossaries
-    BatchTranslateTextRequest_Glossaries (..),
-    newBatchTranslateTextRequest_Glossaries,
+    , BatchTranslateTextRequest_Glossaries (..)
+    , newBatchTranslateTextRequest_Glossaries
 
     -- ** BatchTranslateTextRequest_Labels
-    BatchTranslateTextRequest_Labels (..),
-    newBatchTranslateTextRequest_Labels,
+    , BatchTranslateTextRequest_Labels (..)
+    , newBatchTranslateTextRequest_Labels
 
     -- ** BatchTranslateTextRequest_Models
-    BatchTranslateTextRequest_Models (..),
-    newBatchTranslateTextRequest_Models,
+    , BatchTranslateTextRequest_Models (..)
+    , newBatchTranslateTextRequest_Models
 
     -- ** CancelOperationRequest
-    CancelOperationRequest (..),
-    newCancelOperationRequest,
+    , CancelOperationRequest (..)
+    , newCancelOperationRequest
 
     -- ** DetectLanguageRequest
-    DetectLanguageRequest (..),
-    newDetectLanguageRequest,
+    , DetectLanguageRequest (..)
+    , newDetectLanguageRequest
 
     -- ** DetectLanguageRequest_Labels
-    DetectLanguageRequest_Labels (..),
-    newDetectLanguageRequest_Labels,
+    , DetectLanguageRequest_Labels (..)
+    , newDetectLanguageRequest_Labels
 
     -- ** DetectLanguageResponse
-    DetectLanguageResponse (..),
-    newDetectLanguageResponse,
+    , DetectLanguageResponse (..)
+    , newDetectLanguageResponse
 
     -- ** DetectedLanguage
-    DetectedLanguage (..),
-    newDetectedLanguage,
+    , DetectedLanguage (..)
+    , newDetectedLanguage
 
     -- ** DocumentInputConfig
-    DocumentInputConfig (..),
-    newDocumentInputConfig,
+    , DocumentInputConfig (..)
+    , newDocumentInputConfig
 
     -- ** DocumentOutputConfig
-    DocumentOutputConfig (..),
-    newDocumentOutputConfig,
+    , DocumentOutputConfig (..)
+    , newDocumentOutputConfig
 
     -- ** DocumentTranslation
-    DocumentTranslation (..),
-    newDocumentTranslation,
+    , DocumentTranslation (..)
+    , newDocumentTranslation
 
     -- ** Empty
-    Empty (..),
-    newEmpty,
+    , Empty (..)
+    , newEmpty
 
     -- ** GcsDestination
-    GcsDestination (..),
-    newGcsDestination,
+    , GcsDestination (..)
+    , newGcsDestination
 
     -- ** GcsSource
-    GcsSource (..),
-    newGcsSource,
+    , GcsSource (..)
+    , newGcsSource
 
     -- ** Glossary
-    Glossary (..),
-    newGlossary,
+    , Glossary (..)
+    , newGlossary
 
     -- ** GlossaryInputConfig
-    GlossaryInputConfig (..),
-    newGlossaryInputConfig,
+    , GlossaryInputConfig (..)
+    , newGlossaryInputConfig
 
     -- ** InputConfig
-    InputConfig (..),
-    newInputConfig,
+    , InputConfig (..)
+    , newInputConfig
 
     -- ** LanguageCodePair
-    LanguageCodePair (..),
-    newLanguageCodePair,
+    , LanguageCodePair (..)
+    , newLanguageCodePair
 
     -- ** LanguageCodesSet
-    LanguageCodesSet (..),
-    newLanguageCodesSet,
+    , LanguageCodesSet (..)
+    , newLanguageCodesSet
 
     -- ** ListGlossariesResponse
-    ListGlossariesResponse (..),
-    newListGlossariesResponse,
+    , ListGlossariesResponse (..)
+    , newListGlossariesResponse
 
     -- ** ListLocationsResponse
-    ListLocationsResponse (..),
-    newListLocationsResponse,
+    , ListLocationsResponse (..)
+    , newListLocationsResponse
 
     -- ** ListOperationsResponse
-    ListOperationsResponse (..),
-    newListOperationsResponse,
+    , ListOperationsResponse (..)
+    , newListOperationsResponse
 
     -- ** Location
-    Location (..),
-    newLocation,
+    , Location (..)
+    , newLocation
 
     -- ** Location_Labels
-    Location_Labels (..),
-    newLocation_Labels,
+    , Location_Labels (..)
+    , newLocation_Labels
 
     -- ** Location_Metadata
-    Location_Metadata (..),
-    newLocation_Metadata,
+    , Location_Metadata (..)
+    , newLocation_Metadata
 
     -- ** Operation
-    Operation (..),
-    newOperation,
+    , Operation (..)
+    , newOperation
 
     -- ** Operation_Metadata
-    Operation_Metadata (..),
-    newOperation_Metadata,
+    , Operation_Metadata (..)
+    , newOperation_Metadata
 
     -- ** Operation_Response
-    Operation_Response (..),
-    newOperation_Response,
+    , Operation_Response (..)
+    , newOperation_Response
 
     -- ** OutputConfig
-    OutputConfig (..),
-    newOutputConfig,
+    , OutputConfig (..)
+    , newOutputConfig
 
     -- ** Status
-    Status (..),
-    newStatus,
+    , Status (..)
+    , newStatus
 
     -- ** Status_DetailsItem
-    Status_DetailsItem (..),
-    newStatus_DetailsItem,
+    , Status_DetailsItem (..)
+    , newStatus_DetailsItem
 
     -- ** SupportedLanguage
-    SupportedLanguage (..),
-    newSupportedLanguage,
+    , SupportedLanguage (..)
+    , newSupportedLanguage
 
     -- ** SupportedLanguages
-    SupportedLanguages (..),
-    newSupportedLanguages,
+    , SupportedLanguages (..)
+    , newSupportedLanguages
 
     -- ** TranslateDocumentRequest
-    TranslateDocumentRequest (..),
-    newTranslateDocumentRequest,
+    , TranslateDocumentRequest (..)
+    , newTranslateDocumentRequest
 
     -- ** TranslateDocumentRequest_Labels
-    TranslateDocumentRequest_Labels (..),
-    newTranslateDocumentRequest_Labels,
+    , TranslateDocumentRequest_Labels (..)
+    , newTranslateDocumentRequest_Labels
 
     -- ** TranslateDocumentResponse
-    TranslateDocumentResponse (..),
-    newTranslateDocumentResponse,
+    , TranslateDocumentResponse (..)
+    , newTranslateDocumentResponse
 
     -- ** TranslateTextGlossaryConfig
-    TranslateTextGlossaryConfig (..),
-    newTranslateTextGlossaryConfig,
+    , TranslateTextGlossaryConfig (..)
+    , newTranslateTextGlossaryConfig
 
     -- ** TranslateTextRequest
-    TranslateTextRequest (..),
-    newTranslateTextRequest,
+    , TranslateTextRequest (..)
+    , newTranslateTextRequest
 
     -- ** TranslateTextRequest_Labels
-    TranslateTextRequest_Labels (..),
-    newTranslateTextRequest_Labels,
+    , TranslateTextRequest_Labels (..)
+    , newTranslateTextRequest_Labels
 
     -- ** TranslateTextResponse
-    TranslateTextResponse (..),
-    newTranslateTextResponse,
+    , TranslateTextResponse (..)
+    , newTranslateTextResponse
 
     -- ** Translation
-    Translation (..),
-    newTranslation,
+    , Translation (..)
+    , newTranslation
 
     -- ** WaitOperationRequest
-    WaitOperationRequest (..),
-    newWaitOperationRequest,
-  )
-where
+    , WaitOperationRequest (..)
+    , newWaitOperationRequest
+    ) where
 
 import Gogol.Translate.Projects.DetectLanguage
 import Gogol.Translate.Projects.GetSupportedLanguages
--- lib/services/gogol-translate/gen/Gogol/Translate/Projects/Locations/Operations/Delete.hs
+++ lib/services/gogol-translate/gen/Gogol/Translate/Projects/Locations/Operations/Delete.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,94 +36,87 @@
 --
 -- /See:/ <https://cloud.google.com/translate/docs/quickstarts Cloud Translation API Reference> for @translate.projects.locations.operations.delete@.
 module Gogol.Translate.Projects.Locations.Operations.Delete
-  ( -- * Resource
-    TranslateProjectsLocationsOperationsDeleteResource,
+    (
+    -- * Resource
+      TranslateProjectsLocationsOperationsDeleteResource
 
     -- ** Constructing a Request
-    newTranslateProjectsLocationsOperationsDelete,
-    TranslateProjectsLocationsOperationsDelete,
-  )
-where
+    , newTranslateProjectsLocationsOperationsDelete
+    , TranslateProjectsLocationsOperationsDelete
+    ) where
 
 import qualified Gogol.Prelude as Core
 import Gogol.Translate.Types
 
 -- | A resource alias for @translate.projects.locations.operations.delete@ method which the
 -- 'TranslateProjectsLocationsOperationsDelete' request conforms to.
-type TranslateProjectsLocationsOperationsDeleteResource =
-  "v3"
-    Core.:> Core.Capture "name" Core.Text
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.Delete '[Core.JSON] Empty
+type TranslateProjectsLocationsOperationsDeleteResource
+     =
+     "v3" Core.:>
+       Core.Capture "name" Core.Text Core.:>
+         Core.QueryParam "$.xgafv" Xgafv Core.:>
+           Core.QueryParam "access_token" Core.Text Core.:>
+             Core.QueryParam "callback" Core.Text Core.:>
+               Core.QueryParam "uploadType" Core.Text Core.:>
+                 Core.QueryParam "upload_protocol" Core.Text Core.:>
+                   Core.QueryParam "alt" Core.AltJSON Core.:>
+                     Core.Delete '[Core.JSON] Empty
 
 -- | Deletes a long-running operation. This method indicates that the client is no longer interested in the operation result. It does not cancel the operation. If the server doesn\'t support this method, it returns @google.rpc.Code.UNIMPLEMENTED@.
 --
 -- /See:/ 'newTranslateProjectsLocationsOperationsDelete' smart constructor.
 data TranslateProjectsLocationsOperationsDelete = TranslateProjectsLocationsOperationsDelete
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | The name of the operation resource to be deleted.
-    name :: Core.Text,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | The name of the operation resource to be deleted.
+    , name :: Core.Text
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'TranslateProjectsLocationsOperationsDelete' with the minimum fields required to make a request.
-newTranslateProjectsLocationsOperationsDelete ::
-  -- |  The name of the operation resource to be deleted. See 'name'.
-  Core.Text ->
-  TranslateProjectsLocationsOperationsDelete
+newTranslateProjectsLocationsOperationsDelete 
+    ::  Core.Text
+       -- ^  The name of the operation resource to be deleted. See 'name'.
+    -> TranslateProjectsLocationsOperationsDelete
 newTranslateProjectsLocationsOperationsDelete name =
   TranslateProjectsLocationsOperationsDelete
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      name = name,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , name = name
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    TranslateProjectsLocationsOperationsDelete
-  where
-  type
-    Rs TranslateProjectsLocationsOperationsDelete =
-      Empty
-  type
-    Scopes
-      TranslateProjectsLocationsOperationsDelete =
-      '[ "https://www.googleapis.com/auth/cloud-platform",
-         "https://www.googleapis.com/auth/cloud-translation"
-       ]
-  requestClient
-    TranslateProjectsLocationsOperationsDelete {..} =
-      go
-        name
-        xgafv
-        accessToken
-        callback
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        translateService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  TranslateProjectsLocationsOperationsDeleteResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           TranslateProjectsLocationsOperationsDelete
+         where
+        type Rs TranslateProjectsLocationsOperationsDelete =
+             Empty
+        type Scopes
+               TranslateProjectsLocationsOperationsDelete
+             =
+             '["https://www.googleapis.com/auth/cloud-platform",
+               "https://www.googleapis.com/auth/cloud-translation"]
+        requestClient
+          TranslateProjectsLocationsOperationsDelete{..}
+          = go name xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              translateService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           TranslateProjectsLocationsOperationsDeleteResource)
+                      Core.mempty
+
