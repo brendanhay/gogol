aad1748875e762e6aa8be7154380676d859d2915
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:03 +0100

    gogol-doubleclick-search: regenerating service with updated namespace

--- lib/services/gogol-doubleclick-search/gen/Gogol/DoubleClickSearch/Conversion/UpdateAvailability.hs
+++ lib/services/gogol-doubleclick-search/gen/Gogol/DoubleClickSearch/Conversion/UpdateAvailability.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,95 +36,91 @@
 --
 -- /See:/ <https://developers.google.com/search-ads Search Ads 360 API Reference> for @doubleclicksearch.conversion.updateAvailability@.
 module Gogol.DoubleClickSearch.Conversion.UpdateAvailability
-  ( -- * Resource
-    DoubleClickSearchConversionUpdateAvailabilityResource,
+    (
+    -- * Resource
+      DoubleClickSearchConversionUpdateAvailabilityResource
 
     -- ** Constructing a Request
-    newDoubleClickSearchConversionUpdateAvailability,
-    DoubleClickSearchConversionUpdateAvailability,
-  )
-where
+    , newDoubleClickSearchConversionUpdateAvailability
+    , DoubleClickSearchConversionUpdateAvailability
+    ) where
 
-import Gogol.DoubleClickSearch.Types
 import qualified Gogol.Prelude as Core
+import Gogol.DoubleClickSearch.Types
 
 -- | A resource alias for @doubleclicksearch.conversion.updateAvailability@ method which the
 -- 'DoubleClickSearchConversionUpdateAvailability' request conforms to.
-type DoubleClickSearchConversionUpdateAvailabilityResource =
-  "doubleclicksearch"
-    Core.:> "v2"
-    Core.:> "conversion"
-    Core.:> "updateAvailability"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] UpdateAvailabilityRequest
-    Core.:> Core.Post '[Core.JSON] UpdateAvailabilityResponse
+type DoubleClickSearchConversionUpdateAvailabilityResource
+     =
+     "doubleclicksearch" Core.:>
+       "v2" Core.:>
+         "conversion" Core.:>
+           "updateAvailability" Core.:>
+             Core.QueryParam "$.xgafv" Xgafv Core.:>
+               Core.QueryParam "access_token" Core.Text Core.:>
+                 Core.QueryParam "callback" Core.Text Core.:>
+                   Core.QueryParam "uploadType" Core.Text Core.:>
+                     Core.QueryParam "upload_protocol" Core.Text Core.:>
+                       Core.QueryParam "alt" Core.AltJSON Core.:>
+                         Core.ReqBody '[Core.JSON] UpdateAvailabilityRequest
+                           Core.:>
+                           Core.Post '[Core.JSON] UpdateAvailabilityResponse
 
 -- | Updates the availabilities of a batch of floodlight activities in DoubleClick Search.
 --
 -- /See:/ 'newDoubleClickSearchConversionUpdateAvailability' smart constructor.
 data DoubleClickSearchConversionUpdateAvailability = DoubleClickSearchConversionUpdateAvailability
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Multipart request metadata.
-    payload :: UpdateAvailabilityRequest,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Multipart request metadata.
+    , payload :: UpdateAvailabilityRequest
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'DoubleClickSearchConversionUpdateAvailability' with the minimum fields required to make a request.
-newDoubleClickSearchConversionUpdateAvailability ::
-  -- |  Multipart request metadata. See 'payload'.
-  UpdateAvailabilityRequest ->
-  DoubleClickSearchConversionUpdateAvailability
+newDoubleClickSearchConversionUpdateAvailability 
+    ::  UpdateAvailabilityRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> DoubleClickSearchConversionUpdateAvailability
 newDoubleClickSearchConversionUpdateAvailability payload =
   DoubleClickSearchConversionUpdateAvailability
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      payload = payload,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , payload = payload
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    DoubleClickSearchConversionUpdateAvailability
-  where
-  type
-    Rs DoubleClickSearchConversionUpdateAvailability =
-      UpdateAvailabilityResponse
-  type
-    Scopes
-      DoubleClickSearchConversionUpdateAvailability =
-      '["https://www.googleapis.com/auth/doubleclicksearch"]
-  requestClient
-    DoubleClickSearchConversionUpdateAvailability {..} =
-      go
-        xgafv
-        accessToken
-        callback
-        uploadType
-        uploadProtocol
-        (Core.Just Core.AltJSON)
-        payload
-        doubleClickSearchService
-      where
-        go =
-          Core.buildClient
-            ( Core.Proxy ::
-                Core.Proxy
-                  DoubleClickSearchConversionUpdateAvailabilityResource
-            )
-            Core.mempty
+instance Core.GoogleRequest
+           DoubleClickSearchConversionUpdateAvailability
+         where
+        type Rs DoubleClickSearchConversionUpdateAvailability
+             = UpdateAvailabilityResponse
+        type Scopes
+               DoubleClickSearchConversionUpdateAvailability
+             =
+             '["https://www.googleapis.com/auth/doubleclicksearch"]
+        requestClient
+          DoubleClickSearchConversionUpdateAvailability{..}
+          = go xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              doubleClickSearchService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy
+                           DoubleClickSearchConversionUpdateAvailabilityResource)
+                      Core.mempty
+
--- lib/services/gogol-doubleclick-search/gen/Gogol/DoubleClickSearch/Reports/Request.hs
+++ lib/services/gogol-doubleclick-search/gen/Gogol/DoubleClickSearch/Reports/Request.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,89 +36,83 @@
 --
 -- /See:/ <https://developers.google.com/search-ads Search Ads 360 API Reference> for @doubleclicksearch.reports.request@.
 module Gogol.DoubleClickSearch.Reports.Request
-  ( -- * Resource
-    DoubleClickSearchReportsRequestResource,
+    (
+    -- * Resource
+      DoubleClickSearchReportsRequestResource
 
     -- ** Constructing a Request
-    newDoubleClickSearchReportsRequest,
-    DoubleClickSearchReportsRequest,
-  )
-where
+    , newDoubleClickSearchReportsRequest
+    , DoubleClickSearchReportsRequest
+    ) where
 
-import Gogol.DoubleClickSearch.Types
 import qualified Gogol.Prelude as Core
+import Gogol.DoubleClickSearch.Types
 
 -- | A resource alias for @doubleclicksearch.reports.request@ method which the
 -- 'DoubleClickSearchReportsRequest' request conforms to.
 type DoubleClickSearchReportsRequestResource =
-  "doubleclicksearch"
-    Core.:> "v2"
-    Core.:> "reports"
-    Core.:> Core.QueryParam "$.xgafv" Xgafv
-    Core.:> Core.QueryParam "access_token" Core.Text
-    Core.:> Core.QueryParam "callback" Core.Text
-    Core.:> Core.QueryParam "uploadType" Core.Text
-    Core.:> Core.QueryParam "upload_protocol" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] ReportRequest
-    Core.:> Core.Post '[Core.JSON] Report
+     "doubleclicksearch" Core.:>
+       "v2" Core.:>
+         "reports" Core.:>
+           Core.QueryParam "$.xgafv" Xgafv Core.:>
+             Core.QueryParam "access_token" Core.Text Core.:>
+               Core.QueryParam "callback" Core.Text Core.:>
+                 Core.QueryParam "uploadType" Core.Text Core.:>
+                   Core.QueryParam "upload_protocol" Core.Text Core.:>
+                     Core.QueryParam "alt" Core.AltJSON Core.:>
+                       Core.ReqBody '[Core.JSON] ReportRequest Core.:>
+                         Core.Post '[Core.JSON] Report
 
 -- | Inserts a report request into the reporting system.
 --
 -- /See:/ 'newDoubleClickSearchReportsRequest' smart constructor.
 data DoubleClickSearchReportsRequest = DoubleClickSearchReportsRequest
-  { -- | V1 error format.
-    xgafv :: (Core.Maybe Xgafv),
-    -- | OAuth access token.
-    accessToken :: (Core.Maybe Core.Text),
-    -- | JSONP
-    callback :: (Core.Maybe Core.Text),
-    -- | Multipart request metadata.
-    payload :: ReportRequest,
-    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
-    uploadType :: (Core.Maybe Core.Text),
-    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
-    uploadProtocol :: (Core.Maybe Core.Text)
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | V1 error format.
+      xgafv :: (Core.Maybe Xgafv)
+      -- | OAuth access token.
+    , accessToken :: (Core.Maybe Core.Text)
+      -- | JSONP
+    , callback :: (Core.Maybe Core.Text)
+      -- | Multipart request metadata.
+    , payload :: ReportRequest
+      -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    , uploadType :: (Core.Maybe Core.Text)
+      -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    , uploadProtocol :: (Core.Maybe Core.Text)
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'DoubleClickSearchReportsRequest' with the minimum fields required to make a request.
-newDoubleClickSearchReportsRequest ::
-  -- |  Multipart request metadata. See 'payload'.
-  ReportRequest ->
-  DoubleClickSearchReportsRequest
+newDoubleClickSearchReportsRequest 
+    ::  ReportRequest
+       -- ^  Multipart request metadata. See 'payload'.
+    -> DoubleClickSearchReportsRequest
 newDoubleClickSearchReportsRequest payload =
   DoubleClickSearchReportsRequest
-    { xgafv = Core.Nothing,
-      accessToken = Core.Nothing,
-      callback = Core.Nothing,
-      payload = payload,
-      uploadType = Core.Nothing,
-      uploadProtocol = Core.Nothing
+    { xgafv = Core.Nothing
+    , accessToken = Core.Nothing
+    , callback = Core.Nothing
+    , payload = payload
+    , uploadType = Core.Nothing
+    , uploadProtocol = Core.Nothing
     }
 
-instance
-  Core.GoogleRequest
-    DoubleClickSearchReportsRequest
-  where
-  type Rs DoubleClickSearchReportsRequest = Report
-  type
-    Scopes DoubleClickSearchReportsRequest =
-      '["https://www.googleapis.com/auth/doubleclicksearch"]
-  requestClient DoubleClickSearchReportsRequest {..} =
-    go
-      xgafv
-      accessToken
-      callback
-      uploadType
-      uploadProtocol
-      (Core.Just Core.AltJSON)
-      payload
-      doubleClickSearchService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy DoubleClickSearchReportsRequestResource
-          )
-          Core.mempty
+instance Core.GoogleRequest
+           DoubleClickSearchReportsRequest
+         where
+        type Rs DoubleClickSearchReportsRequest = Report
+        type Scopes DoubleClickSearchReportsRequest =
+             '["https://www.googleapis.com/auth/doubleclicksearch"]
+        requestClient DoubleClickSearchReportsRequest{..}
+          = go xgafv accessToken callback uploadType
+              uploadProtocol
+              (Core.Just Core.AltJSON)
+              payload
+              doubleClickSearchService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy DoubleClickSearchReportsRequestResource)
+                      Core.mempty
+
