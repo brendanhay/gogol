29d9633baf549056b3f7b7a51b2e2a889efd2bce
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 11:23:13 +0100

    gogol-servicebroker: regenerating service

---
+++ gogol-servicebroker/gen/Network/Google/ServiceBroker/GetIamPolicy.hs
@@ -0,0 +1,126 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Network.Google.ServiceBroker.GetIamPolicy
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Gets the access control policy for a resource. Returns an empty policy if the resource exists and does not have a policy set.
+--
+-- /See:/ <https://cloud.google.com/kubernetes-engine/docs/concepts/add-on/service-broker Service Broker API Reference> for @servicebroker.getIamPolicy@.
+module Network.Google.ServiceBroker.GetIamPolicy
+  ( -- * Resource
+    ServiceBrokerGetIamPolicyResource,
+
+    -- ** Constructing a Request
+    newServiceBrokerGetIamPolicy,
+    ServiceBrokerGetIamPolicy,
+  )
+where
+
+import qualified Network.Google.Prelude as Core
+import Network.Google.ServiceBroker.Types
+
+-- | A resource alias for @servicebroker.getIamPolicy@ method which the
+-- 'ServiceBrokerGetIamPolicy' request conforms to.
+type ServiceBrokerGetIamPolicyResource =
+  "v1"
+    Core.:> Core.CaptureMode "resource" "getIamPolicy" Core.Text
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam
+              "options.requestedPolicyVersion"
+              Core.Int32
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.Get '[Core.JSON] GoogleIamV1__Policy
+
+-- | Gets the access control policy for a resource. Returns an empty policy if the resource exists and does not have a policy set.
+--
+-- /See:/ 'newServiceBrokerGetIamPolicy' smart constructor.
+data ServiceBrokerGetIamPolicy = ServiceBrokerGetIamPolicy
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | Optional. The policy format version to be returned.
+    --
+    -- Valid values are 0, 1, and 3. Requests specifying an invalid value will be rejected.
+    --
+    -- Requests for policies with any conditional bindings must specify version 3. Policies without any conditional bindings may specify any valid value or leave the field unset.
+    optionsRequestedPolicyVersion :: (Core.Maybe Core.Int32),
+    -- | REQUIRED: The resource for which the policy is being requested. See the operation documentation for the appropriate value for this field.
+    resource :: Core.Text,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'ServiceBrokerGetIamPolicy' with the minimum fields required to make a request.
+newServiceBrokerGetIamPolicy ::
+  -- |  REQUIRED: The resource for which the policy is being requested. See the operation documentation for the appropriate value for this field. See 'resource'.
+  Core.Text ->
+  ServiceBrokerGetIamPolicy
+newServiceBrokerGetIamPolicy resource =
+  ServiceBrokerGetIamPolicy
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      optionsRequestedPolicyVersion = Core.Nothing,
+      resource = resource,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance Core.GoogleRequest ServiceBrokerGetIamPolicy where
+  type
+    Rs ServiceBrokerGetIamPolicy =
+      GoogleIamV1__Policy
+  type
+    Scopes ServiceBrokerGetIamPolicy =
+      '["https://www.googleapis.com/auth/cloud-platform"]
+  requestClient ServiceBrokerGetIamPolicy {..} =
+    go
+      resource
+      xgafv
+      accessToken
+      callback
+      optionsRequestedPolicyVersion
+      uploadType
+      uploadProtocol
+      (Core.Just Core.AltJSON)
+      serviceBrokerService
+    where
+      go =
+        Core.buildClient
+          ( Core.Proxy ::
+              Core.Proxy ServiceBrokerGetIamPolicyResource
+          )
+          Core.mempty
---
+++ gogol-servicebroker/gen/Network/Google/ServiceBroker/SetIamPolicy.hs
@@ -0,0 +1,128 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- |
+-- Module      : Network.Google.ServiceBroker.SetIamPolicy
+-- Copyright   : (c) 2015-2022 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+gogol@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Sets the access control policy on the specified resource. Replaces any existing policy.
+--
+-- Can return Public Errors: NOT/FOUND, INVALID/ARGUMENT and PERMISSION_DENIED
+--
+-- /See:/ <https://cloud.google.com/kubernetes-engine/docs/concepts/add-on/service-broker Service Broker API Reference> for @servicebroker.setIamPolicy@.
+module Network.Google.ServiceBroker.SetIamPolicy
+  ( -- * Resource
+    ServiceBrokerSetIamPolicyResource,
+
+    -- ** Constructing a Request
+    newServiceBrokerSetIamPolicy,
+    ServiceBrokerSetIamPolicy,
+  )
+where
+
+import qualified Network.Google.Prelude as Core
+import Network.Google.ServiceBroker.Types
+
+-- | A resource alias for @servicebroker.setIamPolicy@ method which the
+-- 'ServiceBrokerSetIamPolicy' request conforms to.
+type ServiceBrokerSetIamPolicyResource =
+  "v1"
+    Core.:> Core.CaptureMode "resource" "setIamPolicy" Core.Text
+    Core.:> Core.QueryParam "$.xgafv" Xgafv
+    Core.:> Core.QueryParam "access_token" Core.Text
+    Core.:> Core.QueryParam "callback" Core.Text
+    Core.:> Core.QueryParam "uploadType" Core.Text
+    Core.:> Core.QueryParam "upload_protocol" Core.Text
+    Core.:> Core.QueryParam "alt" Core.AltJSON
+    Core.:> Core.ReqBody
+              '[Core.JSON]
+              GoogleIamV1__SetIamPolicyRequest
+    Core.:> Core.Post '[Core.JSON] GoogleIamV1__Policy
+
+-- | Sets the access control policy on the specified resource. Replaces any existing policy.
+--
+-- Can return Public Errors: NOT/FOUND, INVALID/ARGUMENT and PERMISSION_DENIED
+--
+-- /See:/ 'newServiceBrokerSetIamPolicy' smart constructor.
+data ServiceBrokerSetIamPolicy = ServiceBrokerSetIamPolicy
+  { -- | V1 error format.
+    xgafv :: (Core.Maybe Xgafv),
+    -- | OAuth access token.
+    accessToken :: (Core.Maybe Core.Text),
+    -- | JSONP
+    callback :: (Core.Maybe Core.Text),
+    -- | Multipart request metadata.
+    payload :: GoogleIamV1__SetIamPolicyRequest,
+    -- | REQUIRED: The resource for which the policy is being specified. See the operation documentation for the appropriate value for this field.
+    resource :: Core.Text,
+    -- | Legacy upload protocol for media (e.g. \"media\", \"multipart\").
+    uploadType :: (Core.Maybe Core.Text),
+    -- | Upload protocol for media (e.g. \"raw\", \"multipart\").
+    uploadProtocol :: (Core.Maybe Core.Text)
+  }
+  deriving (Core.Eq, Core.Show, Core.Generic)
+
+-- | Creates a value of 'ServiceBrokerSetIamPolicy' with the minimum fields required to make a request.
+newServiceBrokerSetIamPolicy ::
+  -- |  Multipart request metadata. See 'payload'.
+  GoogleIamV1__SetIamPolicyRequest ->
+  -- |  REQUIRED: The resource for which the policy is being specified. See the operation documentation for the appropriate value for this field. See 'resource'.
+  Core.Text ->
+  ServiceBrokerSetIamPolicy
+newServiceBrokerSetIamPolicy payload resource =
+  ServiceBrokerSetIamPolicy
+    { xgafv = Core.Nothing,
+      accessToken = Core.Nothing,
+      callback = Core.Nothing,
+      payload = payload,
+      resource = resource,
+      uploadType = Core.Nothing,
+      uploadProtocol = Core.Nothing
+    }
+
+instance Core.GoogleRequest ServiceBrokerSetIamPolicy where
+  type
+    Rs ServiceBrokerSetIamPolicy =
+      GoogleIamV1__Policy
+  type
+    Scopes ServiceBrokerSetIamPolicy =
+      '["https://www.googleapis.com/auth/cloud-platform"]
+  requestClient ServiceBrokerSetIamPolicy {..} =
+    go
+      resource
+      xgafv
+      accessToken
+      callback
+      uploadType
+      uploadProtocol
+      (Core.Just Core.AltJSON)
+      payload
+      serviceBrokerService
+    where
+      go =
+        Core.buildClient
+          ( Core.Proxy ::
+              Core.Proxy ServiceBrokerSetIamPolicyResource
+          )
+          Core.mempty
