39f31162e5260fc5a9972f7a42220c71946bd418
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-18 13:42:05 +0100

    gogol-groups-settings: regenerating service with updated namespace

--- lib/services/gogol-groups-settings/gen/Gogol/GroupsSettings/Groups/Patch.hs
+++ lib/services/gogol-groups-settings/gen/Gogol/GroupsSettings/Groups/Patch.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 {-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
@@ -30,65 +36,62 @@
 --
 -- /See:/ <https://developers.google.com/google-apps/groups-settings/get_started Groups Settings API Reference> for @groupsSettings.groups.patch@.
 module Gogol.GroupsSettings.Groups.Patch
-  ( -- * Resource
-    GroupsSettingsGroupsPatchResource,
+    (
+    -- * Resource
+      GroupsSettingsGroupsPatchResource
 
     -- ** Constructing a Request
-    newGroupsSettingsGroupsPatch,
-    GroupsSettingsGroupsPatch,
-  )
-where
+    , newGroupsSettingsGroupsPatch
+    , GroupsSettingsGroupsPatch
+    ) where
 
-import Gogol.GroupsSettings.Types
 import qualified Gogol.Prelude as Core
+import Gogol.GroupsSettings.Types
 
 -- | A resource alias for @groupsSettings.groups.patch@ method which the
 -- 'GroupsSettingsGroupsPatch' request conforms to.
 type GroupsSettingsGroupsPatchResource =
-  "groups"
-    Core.:> "v1"
-    Core.:> "groups"
-    Core.:> Core.Capture "groupUniqueId" Core.Text
-    Core.:> Core.QueryParam "alt" Core.AltJSON
-    Core.:> Core.ReqBody '[Core.JSON] Groups
-    Core.:> Core.Patch '[Core.JSON] Groups
+     "groups" Core.:>
+       "v1" Core.:>
+         "groups" Core.:>
+           Core.Capture "groupUniqueId" Core.Text Core.:>
+             Core.QueryParam "alt" Core.AltJSON Core.:>
+               Core.ReqBody '[Core.JSON] Groups Core.:>
+                 Core.Patch '[Core.JSON] Groups
 
 -- | Updates an existing resource. This method supports patch semantics.
 --
 -- /See:/ 'newGroupsSettingsGroupsPatch' smart constructor.
 data GroupsSettingsGroupsPatch = GroupsSettingsGroupsPatch
-  { -- | The group\'s email address.
-    groupUniqueId :: Core.Text,
-    -- | Multipart request metadata.
-    payload :: Groups
-  }
-  deriving (Core.Eq, Core.Show, Core.Generic)
+    {
+      -- | The group\'s email address.
+      groupUniqueId :: Core.Text
+      -- | Multipart request metadata.
+    , payload :: Groups
+    }
+    deriving (Core.Eq, Core.Show, Core.Generic)
 
 -- | Creates a value of 'GroupsSettingsGroupsPatch' with the minimum fields required to make a request.
-newGroupsSettingsGroupsPatch ::
-  -- |  The group\'s email address. See 'groupUniqueId'.
-  Core.Text ->
-  -- |  Multipart request metadata. See 'payload'.
-  Groups ->
-  GroupsSettingsGroupsPatch
+newGroupsSettingsGroupsPatch 
+    ::  Core.Text
+       -- ^  The group\'s email address. See 'groupUniqueId'.
+    -> Groups
+       -- ^  Multipart request metadata. See 'payload'.
+    -> GroupsSettingsGroupsPatch
 newGroupsSettingsGroupsPatch groupUniqueId payload =
   GroupsSettingsGroupsPatch {groupUniqueId = groupUniqueId, payload = payload}
 
-instance Core.GoogleRequest GroupsSettingsGroupsPatch where
-  type Rs GroupsSettingsGroupsPatch = Groups
-  type
-    Scopes GroupsSettingsGroupsPatch =
-      '["https://www.googleapis.com/auth/apps.groups.settings"]
-  requestClient GroupsSettingsGroupsPatch {..} =
-    go
-      groupUniqueId
-      (Core.Just Core.AltJSON)
-      payload
-      groupsSettingsService
-    where
-      go =
-        Core.buildClient
-          ( Core.Proxy ::
-              Core.Proxy GroupsSettingsGroupsPatchResource
-          )
-          Core.mempty
+instance Core.GoogleRequest GroupsSettingsGroupsPatch
+         where
+        type Rs GroupsSettingsGroupsPatch = Groups
+        type Scopes GroupsSettingsGroupsPatch =
+             '["https://www.googleapis.com/auth/apps.groups.settings"]
+        requestClient GroupsSettingsGroupsPatch{..}
+          = go groupUniqueId (Core.Just Core.AltJSON) payload
+              groupsSettingsService
+          where go
+                  = Core.buildClient
+                      (Core.Proxy ::
+                         Core.Proxy GroupsSettingsGroupsPatchResource)
+                      Core.mempty
+
