2371fabd82b191ef0a87a7ead8c3944e237b9d19
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-31 09:15:32 +0200

    gogol-speech: regenerating service with scope type aliases

--- lib/services/gogol-speech/gen/Gogol/Speech.hs
+++ lib/services/gogol-speech/gen/Gogol/Speech.hs
@@ -34,7 +34,7 @@ module Gogol.Speech
     speechService,
 
     -- * OAuth Scopes
-    cloudPlatformScope,
+    CloudPlatform'FullControl,
 
     -- * Resources
 
--- lib/services/gogol-speech/gen/Gogol/Speech/Types.hs
+++ lib/services/gogol-speech/gen/Gogol/Speech/Types.hs
@@ -30,7 +30,7 @@ module Gogol.Speech.Types
     speechService,
 
     -- * OAuth Scopes
-    cloudPlatformScope,
+    CloudPlatform'FullControl,
 
     -- * Types
 
@@ -95,5 +95,4 @@ speechService =
     "speech.googleapis.com"
 
 -- | See, edit, configure, and delete your Google Cloud data and see the email address for your Google Account.
-cloudPlatformScope :: Core.Proxy '["https://www.googleapis.com/auth/cloud-platform"]
-cloudPlatformScope = Core.Proxy
+type CloudPlatform'FullControl = "https://www.googleapis.com/auth/cloud-platform"
