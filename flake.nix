1904baa6f010882e1d7a19100b8d5d8109e5fa78
Ivan Malison <IvanMalison@gmail.com>
2023-06-29 15:32:40 -0600

    Support all services packages in flake

--- flake.nix
+++ flake.nix
@@ -8,7 +8,14 @@
     outputs = { self, flake-utils, nixpkgs, git-ignore-nix, xmonad }:
   with xmonad.lib;
   let
-    hoverlay = final: prev: hself: hsuper: {
+    servicesFilepath = ./lib/services;
+    serviceNames = builtins.attrNames (builtins.readDir servicesFilepath);
+    mkOverride = hself: serviceName: {
+      name = serviceName;
+      value = hself.callCabal2nix serviceName
+         (git-ignore-nix.lib.gitignoreSource servicesFilepath + ("/" + serviceName)) { };
+    };
+    hoverlay = final: prev: hself: hsuper: (builtins.listToAttrs (map (mkOverride hself) serviceNames)) // {
       gogol = hself.callCabal2nix "gogol"
          (git-ignore-nix.lib.gitignoreSource ./lib/gogol) { };
       gogol-core = hself.callCabal2nix "gogol-core"
@@ -27,6 +34,11 @@
       if builtins.pathExists ./develop.nix
       then import ./develop.nix
       else import ./default-develop.nix;
+    mkPackage = name: {
+      name = name;
+      value = hpkg.${name};
+    };
+    allPackages = serviceNames ++ ["gogol" "gogol-core"];
   in
   rec {
     devShell = hpkg.shellFor (modifyDevShell pkgs {
@@ -35,6 +47,10 @@
         cabal-install
       ];
     });
-    defaultPackage = hpkg.gogol;
-  }) // { inherit hoverlay overlay overlays nixosModule nixosModules; } ;
+    packages = builtins.listToAttrs (map mkPackage allPackages);
+    defaultPackage = self.packages.${system}.gogol;
+  }) // {
+    inherit hoverlay overlay;
+    overlays = { gogol = overlay; };
+  };
 }
